24f32f3f0337cf440051278d90597c0d7292d1e8
Craig Allen <callen@gemini.edu>
2009-09-30 00:30:47 +0000

    little one liner can find and replace for all files in a directory
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@90 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ astrodata/renameClass.sh
@@ -0,0 +1,7 @@
+# this find based perl script will replace all occurrances of a string with
+# another string.  It could be modified to take arguments, but I put it here
+# to remember it, it works well enough as is to do a class name change (would
+# require a better re to change variable names 
+
+
+find /path/to/start/from/ -type f | xargs perl -pi -e 's/originalclass/newclass/g'
