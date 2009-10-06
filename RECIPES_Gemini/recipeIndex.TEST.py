362f0805c2b168e4bb146e2d209f689cea77a0a6
Craig Allen <callen@gemini.edu>
2009-10-06 10:53:27 +0000

    changes for calibration requests
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@92 e04282df-fb77-4b70-bf7a-c1223586725c

--- RECIPES_Gemini/recipeIndex.TEST.py
+++ RECIPES_Gemini/recipeIndex.TEST.py
@@ -1,6 +1,7 @@
 # adds recipes to the global list for a type
 
 localAstroTypeRecipeIndex = {  #"GEMINI": ["test_two"],
-                               "GMOS_OBJECT_RAW": ["sciReduce"]
+                               "GMOS_OBJECT_RAW": ["getProcessedBias"]
+                               # ["sciReduce"]
                                #"GMOS_OBJECT_RAW": ["test_one"]
                             }
