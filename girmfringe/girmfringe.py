7df274c02f9b822fd73f9847e2d2fb764f93b30c
Kaniela Dement <kdement@gemini.edu>
2009-10-09 00:53:42 +0000

    girmfringe data and python version added
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@119 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ girmfringe/girmfringe.py
@@ -0,0 +1,140 @@
+# Copyright(c) 2003-2009 Association of Universities for Research in Astronomy, Inc.
+#
+# Scale and subtract a fringe frame from a GMOS gireduced image
+# 
+# Version Python
+#         Sep 17, 2009  KD  First translation from CL to Python
+# 
+# Version CL
+#         Jul 30, 2003  BM  created
+#         Aug 27, 2003  KL  IRAF2.12 - new parameters
+#                             imstat: nclip, lsigma, hsigma, cache
+#         Dec 08, 2003 PLG  Changed scale = 1.0 as default
+#         Feb 22, 2004 BM   Changed default scale back to 0.0, needed or OLDP 
+#                           and exposure time scaling is needed in general
+
+import pyfits as pf
+import numpy as np
+import os 
+import geminiLogger
+
+import gemutil
+reload(gemutil)
+import mefutil
+reload(mefutil)
+import paramutil
+reload(paramutil)
+
+def girmfringe(inimages,fringe, outimages="", outpref="", fl_statscale=False,\
+             statsec="[SCI,2][*,*]", scale=1.0, logfile="girmfringe.log", verbose=False):                
+                
+    '''Scale and subtract a fringe frame from GMOS gireduced image.
+    
+    @param inimages: Input MEF image(s)
+    @type inimages: string
+    
+    @param fringe: Fringe Frame
+    @type fringe: string 
+    
+    @param outimages: Output MEF image(s) 
+    @type outimages: string
+    
+    @param outpref: Prefix for output images
+    @type outpref: string
+    
+    @param fl_statscle: Scale by statistics rather than exposure time
+    @type fl_statscle: Booleane ='+ str(scale))
+    if verbose:
+    
+    @param scale: Override auto-scaling if not 0.0
+    @type scale: real
+    
+    @param logfile: Override default log name (rmFringeLog)
+    @type logfile: string
+    
+    rgN20031029S0132.fits
+    @param verbose: Verbose to screen rather than critical only
+    @type verbose: Boolean    
+    '''    
+    ut = gemutil.gemdate(zone="UT") 
+    
+    # mimics the cl log using built in python logger    
+    gemLog = geminiLogger.getLogger( name="girmfringe", logfile=logfile, verbose=verbose)    
+    gemLog.info('-'*40)
+    gemLog.info('GIRMFRINGE -- UT:  ' + ut)  
+    gemLog.info('')  
+    gemLog.info('inimages = ' + inimages)
+    gemLog.info('outimages = ' + outimages)
+    gemLog.info('outpref = '+ outpref) 
+    gemLog.info('fringe = '+ fringe)   
+    gemLog.info('fl_statscale = '+ str(fl_statscale))
+    gemLog.info('statsec = '+ str(statsec))
+    gemLog.info('scale = '+ str(scale))        
+    gemLog.info('')
+    
+    # checkImageParam() will check types, remove newlines from end(s),e ='+ str(scale))    
+    # add .fits where none, checks if path to files exist,
+    # and if files are readable
+    inlist = paramutil.checkImageParam(inimages)
+    
+    # checkOutputParam() will check types, add .fits where none,
+    # and removes newlines from end
+    outlist = paramutil.checkOutputParam(outimages)        
+    
+    # will append name(s) to outlist if not enough (even if none exist)
+    outlist = paramutil.verifyOutlist(inlist, outlist)
+    
+    #check if fringe exists    
+    fringeHdulist = pf.open(fringe) 
+    fringeHdr = fringeHdulist[0].header
+    
+    # mefNumSciext() will fail if not mef 
+    numFringe_sciext = mefutil.mefNumSciext(fringeHdr)
+    
+    #image loop (uses the same fringe)
+    for i in range(len(inlist)):        
+        inimages = inlist[i]        
+        outimages = outlist[i]               
+        iniHdulist = pf.open(inimages)            
+        iniHdr = iniHdulist[0].header     
+        
+        #check for science extension match between fringe and inimage        
+        numInimage_sciext = mefutil.mefNumSciext(iniHdr)
+        if numFringe_sciext != numInimage_sciext or numInimage_sciext != 3:
+            gemLog.critical('Num of SCI EXT is NOT the same between '+ inimages + ' and ' + fringe)            
+            raise 'CRITICAL, science extension match failure between' + inimages + fringe        
+        newHdulist = iniHdulist
+        
+        #sci ext.loop         
+        for i in range(1,numInimage_sciext+1): 
+            iniData = iniHdulist[i].data
+            fringeData = fringeHdulist[i].data
+            iniHeader = iniHdulist[i].header
+            if scale == 0.0 :
+                if fl_statscale:  
+                                      
+                    #must flatten because uses older version of numpy
+                    iniMed = np.median(iniData.flatten())                
+                    iniStd = iniData.std()
+                    temp1 = iniData[np.where(iniData < iniMed + (2.5*iniStd))]
+                    temp2 = temp1[np.where(temp1 > iniMed - (3.*iniStd))]
+                    
+                    #note Kathleen believes the median should be used below instead of std
+                    scale = temp2.std() / fringeData.std()  
+                else:
+                    scale = iniHdr['EXPTIME'] / fringeHdr['EXPTIME']                    
+            newHdulist[i].data = iniData - scale * fringeData                  
+                 
+        #update header
+        newHeader = newHdulist[0].header
+        newHeader['GEM-TLM'] = ut
+        newHeader.update('GIRMFRIN',ut,comment='/ UT Time stamp form GIRMFRINGE')       
+        newHdulist.writeto(outpref + outimages)
+        gemLog.info(inimages +'  '+ outpref + outimages +'  '+ 'ratio=' + str(scale))
+        gemLog.info('')          
+    gemLog.info('GIRMFRINGE done')
+    gemLog.info('-'*40 + '\n\n')
+   
+    
+    
+    
---
+++ girmfringe/paramutil.py
@@ -0,0 +1,245 @@
+import os
+import gemutil
+reload(gemutil)
+import geminiLogger
+reload(geminiLogger)
+
+"""This file contains the following utilities:
+
+    checkImageParam( image )
+    checkOutputParam( outfile, defaultValue="out.fits" ):
+    def verifyOutlist( inlist, outlist )
+    checkParam( parameter, paramType, defaultValue, compareValue=0.0 ):
+    checkFileFitExtension( filename ):
+"""
+def checkImageParam( image ):
+    """
+    Tries to accomplish the same thing as most IRAF tasks in regards to how they handle the input file parameter.
+    
+    @param image: 
+    What is supported:
+    
+    Strings:
+    1) If the string starts with '@' then it is expected that after the '@' is a filename with the input image filenames
+    in it.
+    2) Input image filename.checkParam( parameter, paramType, defaultValue, compareValue=0.0 ):
+    
+    List:
+    1) Must be a list of strings. It is assumed the each string refers an input image filename.
+    @type image: String or List of Strings
+    
+    @return: The list of filenames of images to be run. If an error occurs, None is returned.
+    @rtype: List of Strings or None
+    checkParam( parameter, paramType, defaultValue, compareValue=0.0 ):
+    """
+    #logger set up
+    glog = geminiLogger.getLogger( name="checkImageParam") 
+    inList = []    
+    if type(image) == type(""):
+        if image[0] == "@":            
+            image = image[1:]            
+            try:                 
+                imageFile = open(image, 'r')                
+                inList = imageFile.readlines() 
+                #removes any newline with gemutil method 'pyChomp'                 
+                for i in range(len(inList)):
+                    inList[i] = gemutil.pyChomp(inList[i])   
+                #adds .fits if there is none
+                inList = gemutil.appendFits(inList)                                                                                                
+            except:
+                glog.exception("An error occurred when opening and reading from the image.")
+                return None
+            finally:
+                imageFile.close()
+        else:
+            inList.append( image )            
+            inList[0] = gemutil.appendFits(inList[0])
+    #exception for an image of type 'List'       
+    elif type(image) == type([]):
+        for img in image:
+            if type(img) == type(""):
+                inList.append( img )
+            else:
+                glog.warning('Type'+ str(type(image))+ \
+                    'is not supported. The only supported types are String and List of Strings.')
+                return None
+    else:
+        glog.warning('Type'+ str(type(image))+ \
+                    'is not supported. The only supported types are String and List of Strings.')
+        return None
+    for img in inList:        
+        if not os.access(img,os.R_OK):
+            glog.error('cannot read file: ' + str(img))
+            raise 'Cannot read file', img    
+    return inList
+ #------------------------------------------------------------------------------------------      
+        
+def checkOutputParam( outfile, defaultValue="out.fits" ):
+    """
+    Tries to accomplish the same thing as most IRAF tasks in regards to how they handle the output file parameter.
+        
+    @param outfile: 
+    What is supported:
+    
+    Strings:
+    1) If the string starts with '@' then it is expected that after the '@' is a filename with the output filenames in
+    it.
+    2) Output file name.
+    
+    List:
+    1) Must be a list of strings. It is assumed the each string refers to a desired output file name. 
+    @type outfile: String or List of Strings 
+    
+    @param defaultValue: If the outfile is "", then the defaultValue will returned.
+    @type defaultValue: String
+    
+    @return: A list with all the desired output names. If an error occurs, None is returned.
+    @rtype: List of Strings or None
+    """
+    #logger set up
+    glog = geminiLogger.getLogger( name="checkOutputParam") 
+    outList = []
+    if type(outfile) == type(""):
+        outfile = checkParam( outfile, type(""), defaultValue )
+        if outfile[0] == "@": 
+            outfile = outfile[1:]            
+            try:
+                outListFile = open(outfile,'r')                
+                outList = outListFile.readlines() 
+                if outList != []:              
+                    if outList[0] == '\n':                                       
+                        for i in range(len(outList)):                        
+                            print i
+                            line=outList[i]
+                            outList[i] = line[0:len(line)-1]                                      
+                    else:              
+                        for i in range(len(outList)):  
+                            #removes newlines from end of list                      
+                            outList[i] = gemutil.pyChomp(outList[i])
+                            #adds .fits if there is none                            
+                            outList = gemutil.appendFits(outList)                
+            except:
+                glog.exception("An error occurred when opening and reading from the outlist file.")
+                return None
+            finally:
+                outListFile.close()            
+        else:
+            outList.append( outfile )
+            outList[0] = gemutil.appendFits( outList[0] )
+    elif type(outfile) == type([]):
+        for file in outfile:
+            if type( file ) == type( "" ):
+                inList.append( file )
+            else:
+                glog.warning('Type'+ str(type(image))+ \
+                    'is not supported. The only supported types are String and List of Strings.')
+                return None
+    else:
+         glog.warning('Type'+ str(type(image))+ \
+                    'is not supported. The only supported types are String and List of Strings.')
+         return None
+    return outList
+
+def verifyOutlist( inlist, outlist ):
+    """
+    Verifies that for every file in the inList, there is a corresponding output file.
+    
+    @param inList: A list of input files
+    @type inList: List of Strings
+    
+    @param outlist: A list of output files
+    @type outlist: List of Strings    
+    
+    """
+    #logger set up
+    glog = geminiLogger.getLogger( name="verifyOutlist") 
+    try:    
+        if outlist == []:
+            #will append unique filenames if none exist in outlist             
+            for i in range(len(inlist)):
+                line = 'output' + str(i+1)+ '.fits'
+                outlist.append(line)             
+            return outlist
+        elif len(outlist) < len(inlist):
+            #will append unique filenames if not enough in outlist
+            l = len(inlist) - len(outlist)           
+            for i in range(l):
+                line = 'output' + str(l+i)+ '.fits'
+                outlist.append(line)
+            return outlist
+        else:
+            return outlist
+    except:
+        glog.exception('verifyOutlist in paramUtil failure')
+        return None        
+            
+                   
+      
+#------------------------------------------------------------------------------------------
+
+def checkParam( parameter, paramType, defaultValue, compareValue=0.0 ):
+    """
+    This is a very basic and general parameter checking function. Basically, pass a parameter, expected type 
+    and a default value. If it is the same type then:
+    1) If it is a list, return it.
+    2) If it is a string, check if it is  "", if it is then return default value.
+    3) If it is a number, check that it is greater than 'compareValue', if it is not then return defaultValue.
+    
+    Example usage:
+    
+    -checkParam( mode, type(""), "constant" )
+    -checkParam( fwhm, type(0.0), 5.5, compareValue=1.5 ) 
+    
+    @param parameter: Parameter for testing.
+    @type parameter: Any
+    
+    @param param_type: What the expected type of the parameter should be.
+    @type param_type: type( Any )
+    
+    @param defaultValue: What the default value will be if failures occur. Hopefully, the type matches paramType.
+    @type defaultValue: type( Any )
+    
+    @param compareValue: Value to compare against in the case the parameter is a float or int. This will check whether
+    the parameter is greater than the compareValue.
+    @type compareValue: float or int
+    
+    @return: Parameter if it is the correct type and compare, default if it fails comparison, or None if errors.
+    @rtype: paramType or None
+    """
+    if type( parameter ) == paramType:
+        if paramType == type( 0 ) or paramType == type( 0.0 ):
+            if parameter > compareValue:
+                return parameter
+        elif paramType == type(""):
+            if parameter != "": 
+                return parameter
+        else:
+            return parameter
+    else:
+        print "Parameter: '", parameter, "' is not the correct type:", paramType
+        raise "Incorrect Parameter Type", type(parameter)
+    return defaultValue
+            
+#------------------------------------------------------------------------------------------
+
+def checkFileFitExtension( filename ):
+    """
+    Determines if the fits file has a [X], to see if a particular extension is to be opened.
+    
+    @param filename: Name of a fits file
+    @type filename: String
+    
+    @return: Tuple with the filename and extension specified. 0 means all extension > 0. In the case of any error, 0 
+    is returned.
+    @rtype: Tuple 
+    """
+    file_and_extensions = filename.rsplit( "[" )
+    if( len(file_and_extensions) > 1 ):
+        try:
+            extension_number = file_and_extensions[-1][0]
+            extension_number = int( extension_number )
+        except:
+            extension_number = 0
+        return (file_and_extensions[0], extension_number)
+    else:
+        return (filename, 0)
