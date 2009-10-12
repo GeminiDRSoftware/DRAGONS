7a08d42c9ea7d29a3af47817d639a6d43c6636d7
Craig Allen <callen@gemini.edu>
2009-10-12 04:13:02 +0000

    using this for a test
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@146 e04282df-fb77-4b70-bf7a-c1223586725c

--- ADCONFIG_Gemini/classifications/types/gemdtype.GEMINI_NORTH.py
+++ ADCONFIG_Gemini/classifications/types/gemdtype.GEMINI_NORTH.py
@@ -2,7 +2,6 @@
 class GEMINI_NORTH(DataClassification):
     name="GEMINI_NORTH"
     usage = "Data taken at Gemini North upon Mauna Kea"
-    typeReqs= []
     phuReqs= {'TELESCOP': 'Gemini-North', 'OBSERVAT': 'Gemini-North'}
 
 newtypes.append(GEMINI_NORTH())
