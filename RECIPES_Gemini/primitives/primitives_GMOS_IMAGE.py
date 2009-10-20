dbebeb48ae484dad17b51f1ff8c69bb3e1b70281
Kaniela Dement <kdement@gemini.edu>
2009-10-20 00:57:24 +0000

    changed fl_trim to yes in subtractBias and updated findshiftsAndCombine to include stack_inputsAsStr() method
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@183 e04282df-fb77-4b70-bf7a-c1223586725c

--- RECIPES_Gemini/primitives/primitives_GMOS_IMAGE.py
+++ RECIPES_Gemini/primitives/primitives_GMOS_IMAGE.py
@@ -116,7 +116,7 @@ class GMOS_IMAGEPrimitives(GEMINIPrimitives):
         try:
             print "Subtracting off bias "+co.calFilename("bias")+" from "+co.inputsAsStr()
             gemini.gmos.gireduce(co.inputsAsStr(), fl_over=no,
-                fl_trim=yes, fl_bias=yes,bias=co.calFilename("bias"),
+                fl_trim=no, fl_bias=yes,bias=co.calFilename("bias"),
                 fl_flat=no, outpref="biassub_") # this flag was removed?,fl_mult=no)
             co.reportOutput(co.prependNames("biassub_"))
         except:
@@ -165,7 +165,7 @@ class GMOS_IMAGEPrimitives(GEMINIPrimitives):
     def findshiftsAndCombine(self, co):
        try:
           print "shifting and combining images"
-          gemini.imcoadd(co.inputsAsStr())
+          gemini.imcoadd(co.stack_inputsAsStr(),fwhm=5, threshold=100, fl_over=yes, fl_avg=yes)
        except:
           print "Problem shifting and combining images "+co.inputsAsStr()
           print "Problem in IMCOADD"
