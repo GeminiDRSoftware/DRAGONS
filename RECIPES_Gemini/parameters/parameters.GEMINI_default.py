4327776a7fd05f285fbccef0c5a5393dc5c3de4b
River Allen <rallen@gemini.edu>
2009-10-23 19:33:41 +0000

    Some more parameter work.
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@205 e04282df-fb77-4b70-bf7a-c1223586725c

--- RECIPES_Gemini/parameters/parameters.GEMINI_default.py
+++ RECIPES_Gemini/parameters/parameters.GEMINI_default.py
@@ -1,3 +1,4 @@
 from astrodata.ParamObject import PrimitiveParameter
 
-localParameterIndex = {"adata":PrimitiveParameter("adata", "./recipedata/", True)}
+localParameterIndex = {"adata":PrimitiveParameter("adata", "./recipedata/", True, help="Path of where the fits data lives.")
+                      }
--- RECIPES_Gemini/parameters/parameters.GMOS_IMAGE_default.py
+++ RECIPES_Gemini/parameters/parameters.GMOS_IMAGE_default.py
@@ -1,6 +1,6 @@
 #
 from astrodata.ParamObject import PrimitiveParameter
 
-localParameterIndex = {'theta':PrimitiveParameter('theta', 4, True), 
-                      'sigma':PrimitiveParameter('sigma', 8, False)
+localParameterIndex = {'theta':PrimitiveParameter('theta', 4, True, help="For testing."), 
+                      'sigma':PrimitiveParameter('sigma', 8, False, help="For testing.")
                       }
