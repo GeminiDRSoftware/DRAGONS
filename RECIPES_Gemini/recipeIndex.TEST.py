284b91a1b97f42172b0b1a6c5cf4ad6f1ceb7280
Craig Allen <callen@gemini.edu>
2009-11-13 00:29:25 +0000

    a few fixes for keeping the tkmonitor up
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@239 e04282df-fb77-4b70-bf7a-c1223586725c

--- RECIPES_Gemini/recipeIndex.TEST.py
+++ RECIPES_Gemini/recipeIndex.TEST.py
@@ -1,7 +1,8 @@
 # adds recipes to the global list for a type
 
 localAstroTypeRecipeIndex = {  #"GEMINI": ["test_two"],
-                               "GMOS_IMAGE": ["sciReduce"]
+                               "GMOS_IMAGE": ["demo"]
+                               #"GMOS_IMAGE": ["sciReduce"]
                                # ["sciReduce"]
                                #"GMOS_OBJECT_RAW": ["test_one"]
                             }
