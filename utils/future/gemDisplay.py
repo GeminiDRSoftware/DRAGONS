e4bfdfe5cf3681f6a1402fddd77cd1147fc75081
River Allen <rallen@gemini.edu>
2009-12-09 19:10:01 +0000

    Some placeholder files/code for an observation service.
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@281 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ utils/future/gemDisplay.py
@@ -0,0 +1,12 @@
+from utils.future import pyDisplay
+import astrodata
+from astrodata.AstroData import AstroData
+
+
+class ObservationService(object):
+    
+    
+    def __init__(self):
+        pass
+    
+    
