f5d6529cecb35a5995e5dac60edc27869da1d8fc
Kaniela Dement <kdement@gemini.edu>
2009-12-24 19:58:03 +0000

    changed name back to nici
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@323 e04282df-fb77-4b70-bf7a-c1223586725c

--- nici_install/nici/__init__.py
+++
@@ -1,5 +0,0 @@
-from xytran import XYtran
-from ncqlook import ncqlook
-from ncmkflats import ncmkflats
-from ncprepare import ncprepare
-from ncscience import ncscience
--- nici_install/nici/gspline/setup.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python
-
-from __version__ import version as __version__
