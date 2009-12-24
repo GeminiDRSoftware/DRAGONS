4c2ac82d39723dc6b74db94b55fbc259bc1564b1
River Allen <rallen@gemini.edu>
2009-12-24 23:56:21 +0000

    Final Commit. Some changes and final code I worked on. Goodbye Everyone!
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@326 e04282df-fb77-4b70-bf7a-c1223586725c

--- utils/future/starFilter.py
+++ utils/future/starFilter.py
@@ -9,6 +9,7 @@ import pickle
 from numpy import *
 import numpy as np
 import os
+import hashlib
 
 class starFilter(object):
     '''
@@ -24,6 +25,7 @@ class starFilter(object):
         self.boxSize = boxSize
         self.halfBoxSize = boxSize / 2
         self.loadFilter()
+        self.hashFunc = hashlib.sha1()
         
     
     def flushFilter( self ):
@@ -51,48 +53,63 @@ class starFilter(object):
                             int(xyArray[index][0]-self.halfBoxSize):int(xyArray[index][0]+self.halfBoxSize)]
             
             hashsr = self.hashStar( tempData )
+            val = self.retrieveValueFromFilter( hashsr )
+            if val >= 0:
+                good_star_list.append( (xcoord, ycoord) )
+            '''
             sum = 0
             counter = 0 
             for point in hashsr:
                 key = (counter,point)
-                val = self.retrievePointFromFilter( key )
+                val = self.retrieveValueFromFilter( key )
                 sum += val
                 counter += 1
             
             if sum >= 0:
                 good_star_list.append( index )
+            '''
             index += 1
-        
+        ''' 
         zz = []
         
         for ind in good_star_list:
             zz.append( xyArray[ind] )
-
-        return zz
+        '''
+        return good_star_list
         
     def nonStar( self, data, xcoord, ycoord ):
         print 'NON STAR', xcoord, ycoord
         #print 'DATA', data
         flatHash = self.hashStar( data )
+        #print flatHash
+        '''
         counter = 0 
         for point in flatHash:
             key = (counter,point)
-            val = self.retrievePointFromFilter( key )
+            val = self.retrieveValueFromFilter( key )
             self.filter.update( {key:val-2} )
             counter += 1
+        '''
+        val = self.retrieveValueFromFilter( flatHash )
+        self.filter.update( {flatHash:val-5} )
         
     def goodStar( self, data, xcoord, ycoord ):
         print 'STAR', xcoord, ycoord
         #print 'DATA', data
         flatHash = self.hashStar( data )
+        #print flatHash
+        '''
         counter = 0 
         for point in flatHash:
             key = (counter,point)
-            val = self.retrievePointFromFilter( key )
+            val = self.retrieveValueFromFilter( key )
             self.filter.update( {key:val+1} )
             counter += 1
+        '''
+        val = self.retrieveValueFromFilter( flatHash )
+        self.filter.update( {flatHash:val+1} )
     
-    def retrievePointFromFilter(self, key):
+    def retrieveValueFromFilter(self, key):
         if self.filter.has_key( key ):
             return self.filter[key]
         else:
@@ -110,8 +127,18 @@ class starFilter(object):
         hashsr = hashsr.flatten()
         
         hashsr = hashsr.astype( int )
-        #print 'hashsr', hashsr
+
+        zz = np.where( hashsr >= 7 )
+        tmp = np.zeros( self.boxSize * self.boxSize )
+        tmp[zz] = 3
+        xx = np.where( (hashsr >=5) & (hashsr < 7) )
+        tmp[xx] = 2
+        yy = np.where( (hashsr >=4) & (hashsr < 5) )
+        tmp[yy] = 1
         
-        return hashsr
+        
+        #print 'hashsr', hashsr
+        #print tmp
+        return hash( str(tmp.tolist()) )
     
     
