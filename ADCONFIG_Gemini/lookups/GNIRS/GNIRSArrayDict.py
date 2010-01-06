8e3e47ab0045a2f22f6910050c90adeb5516ee87
Craig Allen <callen@gemini.edu>
2010-01-06 00:32:06 +0000

    adding gnirs lookups
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@339 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ ADCONFIG_Gemini/lookups/GNIRS/GNIRSArrayDict.py
@@ -0,0 +1,9 @@
+gnirsArrayDict = {
+        # Taken from gnirs$data/array.fits
+        # Dictionary key is the bias
+        # Dictionary values are in the following order:
+        # label, readnoise, gain, well, linearlimit, coeff1, coeff2, coeff3,
+        # nonlinearlimit
+        "0.3000" : ( "1" , "126.0000" , "13.5000" , "7950" , "0.9000" , "1.017475" , "0.244937" , "1.019483" , "0.500000" ),
+        "0.6000" : ( "2" , "150.0000" , "13.5000" , "16400" , "0.9000" , "1.021168" , "0.134277" , "0.417923" , "0.600000" )
+    }
