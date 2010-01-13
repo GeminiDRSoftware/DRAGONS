1600dc0ab8b37b061bcd165acbbd6b67d880e2b6
Nelson Zarate <nzarate@gemini.edu>
2010-01-13 19:26:46 +0000

    adding nici descriptor
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@358 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ ADCONFIG_Gemini/descriptors/nici/StandardNICIKeyDict.py
@@ -0,0 +1,16 @@
+#
+# FROM Emma Hogan
+#
+stdkeyDictNICI = {
+    "key_nici_airmass":"AIRMASS",
+    "key_nici_camera":"INSTRUME",
+    "key_nici_fpmask":"FPMW",
+    "key_nici_instrument":"INSTRUME",
+    "key_nici_object":"OBJECT",
+    "key_nici_pupilmask":"PMW",
+    "key_nici_utdate":"DATE-OBS",
+    "key_nici_uttime":"UT",
+    "key_nici_filter_r":"FILTER_R",
+    "key_nici_filter_b":"FILTER_B",
+    }
+
