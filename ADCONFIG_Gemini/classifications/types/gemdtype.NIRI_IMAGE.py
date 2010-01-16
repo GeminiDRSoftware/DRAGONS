900be9cfc8f79d519a397b17ca7c8dd64dc78feb
Paul Hirst <phirst@gemini.edu>
2010-01-16 03:23:23 +0000

    Pupil has a capital P
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@372 e04282df-fb77-4b70-bf7a-c1223586725c

--- ADCONFIG_Gemini/classifications/types/gemdtype.NIRI_IMAGE.py
+++ ADCONFIG_Gemini/classifications/types/gemdtype.NIRI_IMAGE.py
@@ -3,6 +3,6 @@ class NIRI_IMAGE(DataClassification):
     name="NIRI_IMAGE"
     usage = ""
     typeReqs= ['NIRI']
-    phuReqs= {'FILTER3': '(.*?)pupil(.*?)'}
+    phuReqs= {'FILTER3': '(.*?)Pupil(.*?)'}
 
 newtypes.append(NIRI_IMAGE())
