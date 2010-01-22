f3325524a1ae4d84a685ba95efecae058831d55c
Kaniela Dement <kdement@gemini.edu>
2010-01-22 00:29:10 +0000

    moving to lowercase which is how it is imported, can change it later if we decide
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@376 e04282df-fb77-4b70-bf7a-c1223586725c

--- IQTool/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py for pygem
-
-__version__ = '0.9.1beta (November 2008)'
-
-from iq import *
-import gemplotlib
-
--- IQTool/iq/__init__.py
+++
@@ -1,5 +0,0 @@
-# __init__.py for IQTool
-
-__version__ = '0.9.1beta (November 2008)'
-__all__ = ['detectSources', 'fit', 'gemiq_iraf', 'getiq', 'iqUtil']
-
