1977e5762e563418e7a57695f7efa5c03e0d5d61
River Allen <rallen@gemini.edu>
2010-03-05 20:24:08 +0000

    added GEMINI primitives
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@409 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ RECIPES_Gemini/primitives/primitivesIndex.GEMINI.py
@@ -0,0 +1,7 @@
+# this is added to the reduction object dictionary, but only one
+# reduction object per astro data type.
+# NOTE: primitives are the member functions of a Reduction Object.
+
+localPrimitiveIndex = {
+    "GEMINI": ("primitives_GEMINI.py", "GEMINIPrimitives")
+    }
