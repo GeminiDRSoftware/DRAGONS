2e99d7bbb84390a11984938b8ff312e84d3f9868
Craig Allen <callen@gemini.edu>
2010-03-06 00:05:20 +0000

    made GMOS_IMAGE child of GEMINI so there is no primitive conflict
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@412 e04282df-fb77-4b70-bf7a-c1223586725c

--- ADCONFIG_Gemini/classifications/types/gmos/gemdtype.GMOS_IMAGE.py
+++ ADCONFIG_Gemini/classifications/types/gmos/gemdtype.GMOS_IMAGE.py
@@ -2,7 +2,7 @@
 class GMOS_IMAGE(DataClassification):
     name="GMOS_IMAGE"
     usage = ""
-    typeReqs= ['GMOS']
+    typeReqs= ['GEMINI','GMOS']
     phuReqs= {  
                 # Imaging by definition uses the MIRROR as a grating.
                 'GRATING': 'MIRROR'
