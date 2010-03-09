2cff35d1bc5df0ba96cf69b397ee0e52f7a6e82c
River Allen <rallen@gemini.edu>
2010-03-09 21:30:19 +0000

    mad a few small changes
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@416 e04282df-fb77-4b70-bf7a-c1223586725c

--- RECIPES_Gemini/recipeIndex.TEST.py
+++ RECIPES_Gemini/recipeIndex.TEST.py
@@ -1,8 +1,5 @@
 # adds recipes to the global list for a type
 
-localAstroTypeRecipeIndex = {  #"GEMINI": ["test_two"],
-                               "GMOS_IMAGE": ["demo"]
-                               #"GMOS_IMAGE": ["sciReduce"]
-                               # ["sciReduce"]
-                               #"GMOS_OBJECT_RAW": ["test_one"]
+localAstroTypeRecipeIndex = {   "GMOS_IMAGE": ["demo"],
+                                "NIRI_IMAGE": ["reduce"],
                             }
