03ec03bc6475d41eb4c9dfe3a67a4d33098e571e
Craig Allen <callen@gemini.edu>
2010-03-11 23:55:23 +0000

    various changes, some improvements not done as a result of trying to refactor some da code... e.g. added a way to add outputs to the reduction context without loading AstroData instances for each one, so the user can direct when data is loaded into memory... some primitives may organize and sort lists of file and not need the header or data or types.
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@418 e04282df-fb77-4b70-bf7a-c1223586725c

--- RECIPES_Gemini/primitives/primitives_GEMINI.py
+++ RECIPES_Gemini/primitives/primitives_GEMINI.py
@@ -6,6 +6,8 @@ from astrodata import IDFactory
 import os,sys
 from astrodata import IDFactory
 from sets import Set
+from iqtool.iq import getiq
+
 
 if True:
 
@@ -119,7 +121,42 @@ class GEMINIPrimitives(ReductionObject):
             sleep(stepduration)
             yield rc
 
-
+#------------------------------------------------------------------------------ 
+    def measureIQ(self, rc):
+        try:
+            #@@FIXME: Detecting sources is done here as well. This should eventually be split up into
+            # separate primitives, i.e. detectSources and measureIQ.
+            print "measuring iq"
+            '''
+            image, outFile='default', function='both', verbose=True,\
+            residuals=False, display=True, \
+            interactive=False, rawpath='.', prefix='auto', \
+            observatory='gemini-north', clip=True, \
+            sigma=2.3, pymark=True, niters=4, boxSize=2., debug=False):
+            '''
+            for inp in rc.inputs:
+                if 'GEMINI_NORTH' in inp.ad.getTypes():
+                    observ = 'gemini-north'
+                elif 'GEMINI_SOUTH' in inp.ad.getTypes():
+                    observ = 'gemini-south'
+                else:
+                    observ = 'gemini-north'
+                st = time.time()
+                iqdata = getiq.gemiq( inp.filename, function='moffat', display=False, mosaic=True, qa=True)
+                et = time.time()
+                print 'MeasureIQ time:', (et - st)
+                # iqdata is list of tuples with image quality metrics
+                # (ellMean, ellSig, fwhmMean, fwhmSig)
+                if len(iqdata) == 0:
+                    print "WARNING: Problem Measuring IQ Statistics, none reported"
+                else:
+                    rc.rqIQ( inp.ad, *iqdata[0] )
+            
+        except:
+            print 'Problem measuring IQ'
+            raise 
+        
+        yield rc
 
 #------------------------------------------------------------------------------ 
     def printParameters(self, rc):
