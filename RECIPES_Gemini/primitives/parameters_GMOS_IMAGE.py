94eafce8101eca9445be281051ea4d60f88178e5
Craig Allen <callen@gemini.edu>
2010-03-26 01:36:46 +0000

    lots of cool changes to listprimitives and various tangents into the infrastructure to support or clarify
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@443 e04282df-fb77-4b70-bf7a-c1223586725c

--- RECIPES_Gemini/primitives/parameters_GMOS_IMAGE.py
+++ RECIPES_Gemini/primitives/parameters_GMOS_IMAGE.py
@@ -10,5 +10,14 @@
                  "otherTest":
                     {"default": "default",
                      "userOverride":True}
-                }
+                },
+  "pause": {"message":
+            { "default":"Pausing Reduction by Control System Request",
+              "type":"string",
+              "a":"default comes first, the rest alphabetically",
+              "note1":"these are just test parameters...",
+              "note2":"pause doesn't need a 'message' parameter"
+            }
+            }
+            
 }
