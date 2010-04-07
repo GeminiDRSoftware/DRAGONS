adfeead5d52ca7362c38e6b0780d326af508e6ab
Craig Allen <callen@gemini.edu>
2010-04-07 02:44:38 +0000

    changes
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@467 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/scripts/typelib.py
+++ astrodata/scripts/typelib.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
 from astrodata.AstroDataType import *
+from optparse import OptionParser
 
 cl = getClassificationLibrary()
-print repr(dir(cl))
+# print repr(cl.typesDict)
+#FROM COMMANDLINE WHEN READY
+parser = OptionParser()
+(options, args) = parser.parse_args()
+
+if len(args)>0:
+    astrotype = args[0]
+else:
+    astrotype = None
+a = cl.gvizDoc(astrotype= astrotype, writeout = True)
