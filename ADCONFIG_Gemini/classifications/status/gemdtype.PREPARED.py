ed0c9116d0717eaf52ef5d0532ea9f0d916b7556
Craig Allen <callen@gemini.edu>
2010-04-16 02:18:39 +0000

    fixed typo
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@498 e04282df-fb77-4b70-bf7a-c1223586725c

--- ADCONFIG_Gemini/classifications/status/gemdtype.PREPARED.py
+++ ADCONFIG_Gemini/classifications/status/gemdtype.PREPARED.py
@@ -1,8 +1,8 @@
 class PREPARED(DataClassification):
-    editprotect=True
+    
     name="PREPARED"
     usage = 'Applies to all "prepared" data.'
     parent = "UNPREPARED"
-    requiement = PHU( {'{re}.*?PREPARE': ".*?" })
+    requirement = PHU( {'{re}.*?PREPARE': ".*?" })
     
 newtypes.append(PREPARED())
