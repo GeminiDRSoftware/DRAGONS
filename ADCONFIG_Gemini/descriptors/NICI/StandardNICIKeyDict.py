ab53b2f82e51d88228d04f56362d4ab9f82f69ce
Emma Hogan <ehogan@gemini.edu>
2010-04-21 22:33:30 +0000

    Renamed directory for consistency
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@554 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ ADCONFIG_Gemini/descriptors/NICI/StandardNICIKeyDict.py
@@ -0,0 +1,19 @@
+#
+# FROM Emma Hogan
+#
+stdkeyDictNICI = {
+    "key_nici_airmass":"AIRMASS",
+    "key_nici_camera":"INSTRUME",
+    "key_nici_fpmask":"FPMW",
+    "key_nici_instrument":"INSTRUME",
+    "key_nici_object":"OBJECT",
+    "key_nici_pupilmask":"PMW",
+    "key_nici_utdate":"DATE-OBS",
+    "key_nici_uttime":"UT",
+    "key_nici_filter_r":"FILTER_R",
+    "key_nici_filter_b":"FILTER_B",
+    "key_nici_exptime_r":"ITIME_R",
+    "key_nici_exptime_b":"ITIME_B",
+    "key_nici_coadds_r":"NCOADD_R",
+    "key_nici_coadds_b":"NCOADD_B",
+    }
