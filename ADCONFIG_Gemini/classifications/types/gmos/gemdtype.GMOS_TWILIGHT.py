e9bdccf155f296be9930a4d27c77fd2d2efc0195
Craig Allen <callen@gemini.edu>
2010-04-21 20:38:45 +0000

    parent fix
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@525 e04282df-fb77-4b70-bf7a-c1223586725c

--- ADCONFIG_Gemini/classifications/types/gmos/gemdtype.GMOS_TWILIGHT.py
+++ ADCONFIG_Gemini/classifications/types/gmos/gemdtype.GMOS_TWILIGHT.py
@@ -2,7 +2,7 @@
 class GMOS_TWILIGHT(DataClassification):
     name="GMOS_TWILIGHT"
     usage = ""
-    parent = "GMOS_CAL"
+    parent = "GMOS_FLAT"
     requirement = ISCLASS('GMOS') & PHU(OBJECT='Twilight')
 
 newtypes.append(GMOS_TWILIGHT())
