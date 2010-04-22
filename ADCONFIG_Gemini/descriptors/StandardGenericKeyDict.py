f3534d8d5235c60bf5b9ed15e2b136c59e9a9c47
Emma Hogan <ehogan@gemini.edu>
2010-04-22 00:35:44 +0000

    Dictionary file for new Generic_Descriptor file
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@586 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ ADCONFIG_Gemini/descriptors/StandardGenericKeyDict.py
@@ -0,0 +1,7 @@
+stdkeyDictGeneric = {
+    "key_generic_utdate":"DATE-OBS",
+    "key_generic_instrument":"INSTRUME",
+    "key_generic_object":"OBJECT",
+    "key_generic_observer":"OBSERVER",
+    "key_generic_telescope":"TELESCOP",
+    }
