02547b2acead12f71b67227550e88fd5b093dd7d
Craig Allen <callen@gemini.edu>
2010-05-04 21:03:40 +0000

    lots of changes... this was some code issues with a bug that didn't allow primitives to finish via rc->isFinished(True)... works now also this ci is getting a bunch of docscripts changes which are the ADMANUAL project
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@607 e04282df-fb77-4b70-bf7a-c1223586725c

--- ADCONFIG_Gemini/classifications/types/gemdtype.MICHELLE.py
+++ ADCONFIG_Gemini/classifications/types/gemdtype.MICHELLE.py
@@ -6,3 +6,21 @@ class MICHELLE(DataClassification):
     requirement = PHU(INSTRUME= 'michelle')
 
 newtypes.append(MICHELLE())
+
+class MICHELLE_IMAGE(DataClassification):
+    name="MICHELLE_IMAGE"
+    usage = "Applies to IMAGE datasets from the MICHELLE instrument"
+    parent = "MICHELLE"
+    requirement = ISCLASS("MICHELLE") & PHU(CAMERA= 'imaging')
+
+newtypes.append(MICHELLE_IMAGE())
+
+class MICHELLE_SPECT(DataClassification):
+    name="MICHELLE_SPECT"
+    usage = "Applies to IMAGE datasets from the MICHELLE instrument"
+    parent = "MICHELLE"
+    requirement = ISCLASS("MICHELLE") & PHU(CAMERA= 'spectroscopy')
+
+newtypes.append(MICHELLE_SPECT())
+
+
