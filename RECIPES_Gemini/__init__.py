f85976c5291647b704a6d5dc033d889da2918626
Kathleen Labrie <klabrie@gemini.edu>
2010-05-22 02:46:43 +0000

    Removed old parameter directory. Added a setup.py for RECIPE_Gemini.  Still needs work to include the packaging of ADCONFIG_Gemini.
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@656 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ RECIPES_Gemini/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py file for RECIPES_Gemini
+
+__version__ = '0.1.0 (May 2010)'
--- RECIPES_Gemini/parameters/parametersIndex.TEST.py
+++
@@ -1,5 +0,0 @@
-localparameterTypeIndex = {  "GEMINI": ["GEMINI"],
-                               "GMOS_IMAGE": ["GMOS_IMAGE"]
-                               # ["sciReduce"]
-                               #"GMOS_OBJECT_RAW": ["test_one"]
-                            }
