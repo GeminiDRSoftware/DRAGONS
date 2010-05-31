0bbc2d32a710646e637179a2a7d128d3b6339bb6
Emma Hogan <ehogan@gemini.edu>
2010-05-31 04:03:03 +0000

    Implemented new descriptor names
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@677 e04282df-fb77-4b70-bf7a-c1223586725c

--- ADCONFIG_Gemini/descriptors/Generic_Descriptor.py
+++ ADCONFIG_Gemini/descriptors/Generic_Descriptor.py
@@ -6,13 +6,14 @@ from astrodata.Calculator import Calculator
 import GemCalcUtil
 import re
 
+from StandardDescriptorKeyDict import globalStdkeyDict
 from StandardGenericKeyDict import stdkeyDictGeneric
 
 class Generic_DescriptorCalc(Calculator):
     
-    def utdate(self, dataset, **args):
+    def ut_date(self, dataset, **args):
         """
-        Return the UT date value for generic data
+        Return the ut_date value for generic data
         @param dataset: the data set
         @type dataset: AstroData
         @rtype: string
@@ -20,94 +21,24 @@ class Generic_DescriptorCalc(Calculator):
         """
         try:
             hdu = dataset.hdulist
-            utdatestring = hdu[0].header[stdkeyDictGeneric["key_generic_utdate"]]
-
+            ut_date = hdu[0].header[globalStdkeyDict['key_ut_date']]
+            
+            # Validate the result. The definition is taken from the FITS
+            # standard document v3.0. Must be YYYY-MM-DD or
+            # YYYY-MM-DDThh:mm:ss[.sss]. Here I also do some very basic checks
+            # like ensuring the first digit of the month is 0 or 1, but I
+            # don't do cleverer checks like 01<=M<=12. nb. seconds ss > 59 is
+            # valid when leap seconds occur.
+            
+            if (re.match('\d\d\d\d-[01]\d-[0123]\d', ut_date)):
+                ret_ut_date = ut_date
+            
+            m = re.match('(\d\d\d\d-[01]\d-[0123]\d)(T)([012]\d:[012345]\d:\d\d.*\d*)', ut_date)
+            
+            if (m):
+                ret_ut_date = m.group(1)
+        
         except KeyError:
             return None
         
-        # Validate the result. The definition is taken from the FITS standard document v3.0
-        # Must be YYYY-MM-DD or YYYY-MM-DDThh:mm:ss[.sss]
-
-        # Here I also do some very basic checks like ensuring the first digit of the
-        # month is 0 or 1, but I don't do cleverer checks like 01<=M<=12
-
-        # nb. seconds ss > 59 is valid when leap seconds occur.
-
-        retval = None
-
-        if(re.match('\d\d\d\d-[01]\d-[0123]\d', utdatestring)):
-            retval = utdatestring
-
-        m=re.match('(\d\d\d\d-[01]\d-[0123]\d)(T)([012]\d:[012345]\d:\d\d.*\d*)', utdatestring)
-        if(m):
-            retval = m.group(1)
-
-        return retval
-    
-    def instrument(self, dataset, **args):
-        """
-        Return the instrument value for generic data
-        @param dataset: the data set
-        @type dataset: AstroData
-        @rtype: string
-        @return: the instrument used to acquire the data
-        """
-        try:
-            hdu = dataset.hdulist
-            retinstrumentstring = hdu[0].header[stdkeyDictGeneric["key_generic_instrument"]]
-                    
-        except KeyError:
-            return None
-        
-        return str(retinstrumentstring)
-    
-    def object(self, dataset, **args):
-        """
-        Return the object value for generic data
-        @param dataset: the data set
-        @type dataset: AstroData
-        @rtype: string
-        @return: the name of the object acquired
-        """
-        try:
-            hdu = dataset.hdulist
-            retobjectstring = hdu[0].header[stdkeyDictGeneric["key_generic_object"]]
-
-        except KeyError:
-            return None
-        
-        return str(retobjectstring)
-    
-    def observer(self, dataset, **args):
-        """
-        Return the observer value for generic data
-        @param dataset: the data set
-        @type dataset: AstroData
-        @rtype: string
-        @return: the observer who acquired the observation
-        """
-        try:
-            hdu = dataset.hdulist
-            retobserverstring = hdu[0].header[stdkeyDictGeneric["key_generic_observer"]]
-
-        except KeyError:
-            return None
-        
-        return str(retobserverstring)
-    
-    def telescope(self, dataset, **args):
-        """
-        Return the telescope value for generic data
-        @param dataset: the data set
-        @type dataset: AstroData
-        @rtype: string
-        @return: the telescope where the observation was taken
-        """
-        try:
-            hdu = dataset.hdulist
-            rettelescopestring = hdu[0].header[stdkeyDictGeneric["key_generic_telescope"]]
-
-        except KeyError:
-            return None
-        
-        return str(rettelescopestring)
+        return str(ret_ut_date)
