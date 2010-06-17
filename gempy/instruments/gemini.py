ec6550f5df70e8cda036e93ef7cf29be68ab0e90
Kyle Mede <kmede@gemini.edu>
2010-06-17 23:11:42 +0000

    now uses updated --debug and all other format logging
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@773 e04282df-fb77-4b70-bf7a-c1223586725c

--- gempy/instruments/gemini.py
+++ gempy/instruments/gemini.py
@@ -8,8 +8,11 @@ import pyfits as pf
 import numpy as np
 import time
 from datetime import datetime
-from astrodata.adutils import mefutil, paramutil, geminiLogger
+from astrodata.adutils import mefutil, paramutil
+from astrodata.adutils.future import gemLog
 from astrodata.AstroData import AstroData
+
+log=gemLog.getGeminiLog() 
 
 def stdObsHdrs(ad):
    
@@ -19,7 +22,6 @@ def stdObsHdrs(ad):
     ad.phuSetKeyValue('NEXTEND', len(ad) , 'Number of extensions')
     ad.phuSetKeyValue('OBSMODE', ad.observation_mode() , 'Observing mode (IMAGE|IFU|MOS|LONGSLIT)')
     ad.phuSetKeyValue('COADDEXP', ad.phuValue("EXPTIME") , 'Exposure time for each coadd frame')
-    
     numcoadds = ad.coadds()
     if not numcoadds:  
         numcoadds = 1      #for if there are no coadds performed, set to 1
@@ -28,6 +30,20 @@ def stdObsHdrs(ad):
 
     ut = datetime.now().isoformat()  #$$$$$$$$$$$ just for saving the sintax, move this to its final destination when determined
     ad.phuSetKeyValue('GEM-TLM', ut , 'UT Last modification with GEMINI')  #$$$$$$$$$$$ just for saving the sintax, move this to its final destination when determined
+         
+    ##updating logger with updated/added keywords
+    log.fullinfo('****************************************************','header')
+    log.fullinfo('file = '+ad.filename,'header')
+    log.fullinfo('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~','header')
+    log.fullinfo('PHU keywords updated/added:\n', 'header')
+    log.fullinfo('NSCIEXT = '+str(ad.countExts("SCI")),'header' )
+    log.fullinfo('PIXSCALE = '+str(ad.pixel_scale()),'header' )
+    log.fullinfo('NEXTEND = '+str(len(ad)),'header' )
+    log.fullinfo('OBSMODE = '+str(ad.observation_mode()),'header' )
+    log.fullinfo('COADDEXP = '+str(ad.phuValue("EXPTIME")),'header' )
+    log.fullinfo('EXPTIME = '+str(effExpTime),'header' )
+    log.fullinfo('GEM-TLM = '+str(ut),'header' )
+    log.fullinfo('---------------------------------------------------','header')
          
     ##a loop to add the missing/needed keywords in the Gemini SCI extensions
     for ext in ad["SCI"]:
@@ -41,27 +57,36 @@ def stdObsHdrs(ad):
         ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'NONLINEA',nonlin , 'Non-linear regime level in ADU')
         ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'SATLEVEL',ext.saturation_level() , 'Saturationlevel in ADU')
         ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'EXPTIME', effExpTime , 'Saturationlevel in ADU')
+        log.fullinfo('SCI extension number '+str(ext.header['EXTVER'])+' keywords updated/added:\n', 'header')
+        log.fullinfo('GAIN = '+str(ext.gain()),'header' )
+        log.fullinfo('PIXSCALE = '+str(ext.pixel_scale()),'header' )
+        log.fullinfo('RDNOISE = '+str(ext.read_noise()),'header' )
+        log.fullinfo('BUNIT = '+'adu','header' )
+        log.fullinfo('NONLINEA = '+str(nonlin),'header' )
+        log.fullinfo('SATLEVEL = '+str(ext.saturation_level()),'header' )
+        log.fullinfo('EXPTIME = '+str(effExpTime),'header' )
+        log.fullinfo('---------------------------------------------------','header')
 
 def stdObsStruct(ad):
+    log.fullinfo('****************************************************','header') 
+    log.fullinfo('file = '+ad.filename, 'header')
+    log.fullinfo('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~','header')
     ##a loop to add the missing/needed keywords in the SCI extensions
     for ext in ad["SCI"]:
         ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'EXTNAME', 'SCI', "Extension name")        
-        ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'EXTVER', int(ext.header['EXTVER']), "Extension version")        
+        ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'EXTVER', int(ext.header['EXTVER']), "Extension version") 
+        log.fullinfo('SCI extension number '+str(ext.header['EXTVER'])+' keywords updated/added:\n', 'header')       
+        log.fullinfo('EXTNAME = '+'SCI','header' )
+        log.fullinfo('EXTVER = '+str(ext.header['EXTVER']),'header' )
+        log.fullinfo('---------------------------------------------------','header')
         
-def fileNameUpdater(ad, debugLevel, postpend , strip=False):
-    print 'gemini52: entered fileNameUpdater'
+def fileNameUpdater(ad, postpend , strip=False):
     infilename = os.path.basename(ad.filename)
     if strip:
         infilename = stripPostfix(infilename)
     (name,filetype) = os.path.splitext(infilename)
-    if debugLevel>=2:  
-        print 'gemini56: infilename = ', infilename
     outFileName = name+postpend+filetype
     ad.filename = outFileName
-    if debugLevel>=2: 
-        print 'gemini60: current output filename  = ',ad.filename
-    
- 
 
 def stripPostfix(filename):
     dirname = os.path.dirname(filename)
