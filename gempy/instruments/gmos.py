e0a4fac31d3deaa4ce22326bdbd2c347b2c6f01e
Kyle Mede <kmede@gemini.edu>
2010-06-17 23:11:50 +0000

    now uses updated --debug and all other format logging
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@774 e04282df-fb77-4b70-bf7a-c1223586725c

--- gempy/instruments/gmos.py
+++ gempy/instruments/gmos.py
@@ -8,27 +8,29 @@ import pyfits as pf
 import numpy as np
 import time
 from datetime import datetime
-from astrodata.adutils import mefutil, paramutil, geminiLogger
+from astrodata.adutils import mefutil, paramutil
+from astrodata.adutils.future import gemLog
 from astrodata.AstroData import AstroData
 
+log=gemLog.getGeminiLog() 
 
 def stdInstHdrs(ad):  
     # adding the missing/needed keywords into the headers 
-    
-        if not ad.isType('GMOS_IMAGE'):
-        # do the stuff to the headers that is for the MOS 
-            for ext in ad["SCI"]:
-                print 'gmostools22: still need to get the dispersion axis descriptor working you know!!'
-                #ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'DISPAXIS',  , 'Dispersion axis')
-                #ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'',, "")
-                pass
+    if not ad.isType('GMOS_IMAGE'):
+    # do the stuff to the headers that is for the MOS, those for IMAGE are taken care of with stdObsHdrs all ready 
+        for ext in ad["SCI"]:
+            log.critical('gmostools22: still need to get the dispersion axis descriptor working you know!!','critical')
+            #ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'DISPAXIS',  , 'Dispersion axis')
+            #ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'',, "")
+            pass
 
 def valInstData(ad):
     # to ensure structure is the normal PHU followed by 3 SCI extensions for GMOS
     if len(ad)==1 or len(ad)==3 or len(ad)==6 or len(ad)==12:
         pass
     else: 
-        raise "gmostools33: there are NOT 1, 3, 6 or 12 extensions in file = ", ad.filename
+        log.critical("gmostools33: there are NOT 1, 3, 6 or 12 extensions in file = "+ad.filename,'critical')
+        raise 
         
     
                  
