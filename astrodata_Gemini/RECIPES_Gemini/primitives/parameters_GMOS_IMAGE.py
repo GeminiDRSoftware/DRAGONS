5e3efbff0ed9168a3b9a5a0fca823652019d9ade
Kyle Mede <kmede@gemini.edu>
2010-06-26 02:50:52 +0000

    added new parameters for overscanSubtract
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@793 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_IMAGE.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_IMAGE.py
@@ -1,23 +1,52 @@
-{ "showParams": {"test": 
-                 {
+{   "showParams": {"test": 
+                   {
                     "default": "GMOS_SPECT_SETTING",
                     "recipeOverride": True,
                     "uiLevel": "debug",
                     "userOverride":True
                     
-                 },
+                    },
                  
                  "otherTest":
                     {"default": "default",
                      "userOverride":True}
                 },
-  "pause": {"message":
-            { "default":"Pausing Reduction by Control System Request",
-              "type":"string",
-              "a":"default comes first, the rest alphabetically",
-              "note1":"these are just test parameters...",
-              "note2":"pause doesn't need a 'message' parameter"
-            }
-            }
+    "pause": {"message":
+              { "default":"Pausing Reduction by Control System Request",
+               "type":"string",
+               "a":"default comes first, the rest alphabetically",
+               "note1":"these are just test parameters...",
+               "note2":"pause doesn't need a 'message' parameter"
+               }
+              },
+    "overscanSubtract":{"fl_over":
+                        {
+                          'default': True ,
+                          'recipeOverride': True,
+                          'type': 'bool',
+                          'userOverride':True                                      
+                         },
+                         "fl_trim":
+                         {
+                          'default': False ,
+                          'recipeOverride': True,
+                          'type': 'bool',
+                          'userOverride':True    
+                          },
+                          "fl_vardq":
+                          {
+                           'default': False ,
+                          'recipeOverride': True,
+                          'type': 'bool',
+                          'userOverride':True    
+                           },
+                          "outpref":
+                          {
+                           'default': 'oversub' ,
+                          'recipeOverride': True,
+                          'type': 'str',
+                          'userOverride':True    
+                           }
+                        }
             
 }
