7c56096ff9af406b0261fdebb07674aee9d2d846
Kyle Mede <kmede@gemini.edu>
2010-07-08 22:35:39 +0000

    loaded in currently needed parameters
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@810 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GEMINI.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GEMINI.py
@@ -19,6 +19,88 @@
                "note1":"these are just test parameters...",
                "note2":"pause doesn't need a 'message' parameter"
                }
-              }
+              },
+    "validateData":{'outsuffix':
+                   {
+                    'default': '_validated' , #value to be post pended to this primitives outputs
+                      'recipeOverride': True,
+                      'type': 'str',
+                      'userOverride':False 
+                    },
+                    "repair":
+                    {
+                      'default': True , 
+                      'recipeOverride': True,
+                      'type': 'bool',
+                      'userOverride':True                                      
+                     }     
+                          },
+    "standardizeStructure":{'outsuffix':
+                   {
+                    'default': '_struct' , #value to be post pended to this primitives outputs
+                      'recipeOverride': True,
+                      'type': 'str',
+                      'userOverride':False 
+                    },
+                    "addMDF":
+                    {
+                      'default': True , 
+                      'recipeOverride': True,
+                      'type': 'bool',
+                      'userOverride':True                                      
+                     }     
+                          },
+    "standardizeHeaders":{'outsuffix':
+                   {
+                    'default': '_prepared' , #value to be post pended to this primitives outputs
+                      'recipeOverride': True,
+                      'type': 'str',
+                      'userOverride':False 
+                    }     
+                          },
+    "calculateVAR":{'outsuffix':
+                   {
+                    'default': '_vardq' , #value to be post pended to this primitives outputs
+                      'recipeOverride': True,
+                      'type': 'str',
+                      'userOverride':False 
+                    },
+                    "fl_saturated":
+                    {
+                      'default': True , 
+                      'recipeOverride': True,
+                      'type': 'bool',
+                      'userOverride':True                                      
+                     },
+                     "fl_nonlinear":
+                    {
+                      'default': True , 
+                      'recipeOverride': True,
+                      'type': 'bool',
+                      'userOverride':True                                      
+                     }
+                    },
+    "calculateDQ":{'outsuffix':
+                   {
+                    'default': '_vardq' , #value to be post pended to this primitives outputs
+                      'recipeOverride': True,
+                      'type': 'str',
+                      'userOverride':False 
+                    },
+                    "fl_saturated":
+                    {
+                      'default': True , 
+                      'recipeOverride': True,
+                      'type': 'bool',
+                      'userOverride':True                                      
+                     },
+                     "fl_nonlinear":
+                    {
+                      'default': True , 
+                      'recipeOverride': True,
+                      'type': 'bool',
+                      'userOverride':True                                      
+                     }
+                    }
 }    
 
