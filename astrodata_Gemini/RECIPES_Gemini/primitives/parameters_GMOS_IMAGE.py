005100d1b28d46fe2ad0bd1e706b4736a1ab0132
Kyle Mede <kmede@gemini.edu>
2010-07-29 23:32:25 +0000

    moved prims from GMOS_IMAGE -> GMOS so the params moved too
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@878 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_IMAGE.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_IMAGE.py
@@ -18,42 +18,5 @@
                "note1":"these are just test parameters...",
                "note2":"pause doesn't need a 'message' parameter"
                }
-              },
-    "overscanSubtract":{"fl_over":
-                        {
-                          'default': True , #maybe hardcode this as this is the purpose of this prim, so setting it to False is pointless
-                          'recipeOverride': True,
-                          'type': 'bool',
-                          'userOverride':True                                      
-                         },
-                         "fl_trim":
-                         {
-                          'default': False , #trim the overscan region after it has been subracted? maybe hardcode this as there is a separate prim to do this
-                          'recipeOverride': False,
-                          'type': 'bool',
-                          'userOverride':False    
-                          },
-                          "fl_vardq":
-                          {
-                           'default': False ,
-                          'recipeOverride': True,
-                          'type': 'bool',
-                          'userOverride':True    
-                           },
-                          "outpref":
-                          {
-                           'default': '_oversubed' , #value to be post pended to this primitives outputs
-                          'recipeOverride': True,
-                          'type': 'str',
-                          'userOverride':False    
-                           }
-                        },
-        "overscanTrim":{"outsuffix":
-                          {
-                           'default': '_overtrimd' , #value to be post pended to this primitives outputs
-                          'recipeOverride': True,
-                          'type': 'str',
-                          'userOverride':False    
-                           }
-                        } 
+              }
 }
