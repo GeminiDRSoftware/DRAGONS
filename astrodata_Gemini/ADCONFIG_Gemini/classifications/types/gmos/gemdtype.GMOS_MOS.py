bbe584714f3fae1ba1862c32da17afd2b8c7696c
Craig Allen <callen@gemini.edu>
2010-07-30 21:29:56 +0000

    type fix with kaniela
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@888 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/gmos/gemdtype.GMOS_MOS.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/gmos/gemdtype.GMOS_MOS.py
@@ -3,7 +3,14 @@ class GMOS_MOS(DataClassification):
     name="GMOS_MOS"
     usage = ""
     parent = "GMOS_SPECT"
-    requirement = ISCLASS('GMOS') & PHU(OBSMODE='MOS',      
-                                        OBSTYPE='OBJECT')
+    requirement = OR(AND(ISCLASS('GMOS'),
+                         PHU(OBSMODE='MOS',OBSTYPE='OBJECT')),
+                     ISCLASS('GMOS_MOS_FLAT'),
+                     ISCLASS('GMOS_MOS_TWFLAT'),
+                     ISCLASS('GMOS_MOS_ARC'))
+
+                    
+                                        
+                                        
 
 newtypes.append(GMOS_MOS())
