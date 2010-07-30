ad7ab444e12d6a85564ce169b4b50dc565322467
Paul Hirst <phirst@gemini.edu>
2010-07-30 20:35:07 +0000

    there are narrowband imaging filters in FILTER3 too
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@884 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/niri/gemdtype.NIRI_IMAGE.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/niri/gemdtype.NIRI_IMAGE.py
@@ -3,6 +3,7 @@ class NIRI_IMAGE(DataClassification):
     name="NIRI_IMAGE"
     usage = "Applies to any IMAGE dataset from the NIRI instrument."
     parent = "NIRI"
-    requirement = ISCLASS('NIRI') & PHU(FILTER3='(.*?)[Pp]upil(.*?)')
+    requirement = ISCLASS('NIRI') & PHU({"{prohibit}FILTER3":"(.*?)grism(.*?)"})
+
 
 newtypes.append(NIRI_IMAGE())
