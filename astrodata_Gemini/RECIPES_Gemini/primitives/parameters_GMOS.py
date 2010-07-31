e32d5c37770ac3ed6c6fe056c7ebbb2968d71fd8
Kyle Mede <kmede@gemini.edu>
2010-07-31 04:40:38 +0000

    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@899 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS.py
@@ -34,5 +34,88 @@
                           'type': 'str',
                           'userOverride':False    
                            }
-                        } 
+                        },
+        'storeProcessedBias':{'clob':
+                              {
+                                'default': False,
+                                'recipeOverride': True,
+                                'type': 'bool',
+                                'userOverride':True  
+                               }
+                              
+                              } ,
+            'storeProcessedFlat':{'clob':
+                              {
+                                'default': False,
+                                'recipeOverride': True,
+                                'type': 'bool',
+                                'userOverride':True  
+                               }
+                              
+                              } ,
+             "biasCorrect":{"fl_over":
+                        {
+                          'default': False , 
+                          'recipeOverride': True,
+                          'type': 'bool',
+                          'userOverride':True                                      
+                         },
+                         "fl_trim":
+                         {
+                          'default': False , 
+                          'recipeOverride': False,
+                          'type': 'bool',
+                          'userOverride':False    
+                          },
+                          "fl_vardq":
+                          {
+                           'default': True ,
+                          'recipeOverride': True,
+                          'type': 'bool',
+                          'userOverride':True    
+                           },
+                          "outpref":
+                          {
+                           'default': '_biassub' , #value to be post pended to this primitives outputs
+                          'recipeOverride': True,
+                          'type': 'str',
+                          'userOverride':False    
+                           }
+                          },
+            "makeNormalizedFlat":{"fl_over":
+                        {
+                          'default': False , 
+                          'recipeOverride': False,
+                          'type': 'bool',
+                          'userOverride':False                                      
+                         },
+                         "fl_trim":
+                         {
+                          'default': False , 
+                          'recipeOverride': False,
+                          'type': 'bool',
+                          'userOverride':False    
+                          },
+                          "fl_vardq":
+                          {
+                           'default': True ,
+                          'recipeOverride': True,
+                          'type': 'bool',
+                          'userOverride':True    
+                           },
+                          "outpref":
+                          {
+                           'default': '_normalized' , #value to be post pended to this primitives outputs
+                          'recipeOverride': True,
+                          'type': 'str',
+                          'userOverride':False    
+                           },
+                          'fl_bias':
+                          {
+                           'default': False , 
+                          'recipeOverride': False,
+                          'type': 'bool',
+                          'userOverride':False 
+                           }
+                          }
 }
