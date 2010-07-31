bc9581280fbb9027012c4f153c4dd1f921d37037
Kyle Mede <kmede@gemini.edu>
2010-07-31 04:40:25 +0000

    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@897 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/primitives_GMOS_IMAGE.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/primitives_GMOS_IMAGE.py
@@ -65,7 +65,7 @@ class GMOS_IMAGEPrimitives(GMOSPrimitives):
                 if "adata" not in rc["iraf"]:
                     rc["iraf"].update({"adata":root}) 
         
-        GEMINIPrimitives.init(self, rc)
+        GMOSPrimitives.init(self, rc)
         return rc
        
     #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -119,7 +119,7 @@ class GMOS_IMAGEPrimitives(GMOSPrimitives):
         yield rc
     #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     
-    def biasCorrect(self, rc):
+    def dbiasCorrect(self, rc):
         # not really sure we need to use gireduce here. I think we could easily make a
         # more generic bias sub task
         try:
@@ -488,135 +488,4 @@ class GMOS_IMAGEPrimitives(GMOSPrimitives):
             raise 
         
         yield rc    
-        
-    #$$$$$$$$$$$$$$$$$$$$ NEW STUFF BY KYLE FOR: PREPARE $$$$$$$$$$$$$$$$$$$$$
-    '''
-    These primitives are now functioning and can be used, BUT are not set up to run with the current demo system.
-    commenting has been added to hopefully assist those reading the code.
-    Excluding validateWCS, all the primitives for 'prepare' are complete (as far as we know of at the moment that is)
-    and so I am moving onto working on the primitives following 'prepare'.
-    '''
-    
-    def standardizeInstrumentHeaders(self,rc):
-        '''
-        This primitive is called by standardizeHeaders to makes the changes and additions to
-        the headers of the input files that are instrument specific.
-        '''
-        try:            
-            writeInt = rc['writeInt']
-                               
-            for ad in rc.getInputs(style="AD"): 
-                log.debug('calling stdInstHdrs','status') 
-                stdInstHdrs(ad) 
-            
-            if writeInt:
-                log.debug('writing the outputs to disk')
-                rc.run('writeOutputs(postpend=_instHdrs)')  #$$$$$$$$$$$$$this needs to accept arguments to work right!!!!!!!!!!!! currently hardcoded
-                log.debug('writting complete')
-                    
-        except:
-            log.critical("Problem preparing the image.",'critical')
-            raise 
-        
-        yield rc
-
- #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Prepare primitives end here $$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
-#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ primitives following Prepare below $$$$$$$$$$$$$$$$$$$$  
-    def overscanSubtract(self,rc):
-        """
-        This primitive uses the CL script gireduce to subtract the overscan from the input images.
-        """
-        try:
-            log.status('*STARTING* to subtract the overscan from the input data','status')
-            ## writing input files to disk with prefixes onto their file names so they can be deleted later easily 
-            clm = CLManager(rc)
-            clm.LogCurParams()
-            ## params in the dictionaries: fl_over, fl_trim, fl_vardq, outpref
-            log.fullinfo('calling the gireduce CL script', 'status')
-            gemini.gmos.gireduce(clm.inputsAsStr(), gp_outpref=clm.uniquePrefix(),fl_over=pyrafBoolean(rc["fl_over"]), \
-                    fl_trim=pyrafBoolean(rc["fl_trim"]), fl_bias=no, \
-                    fl_flat=no, outpref=rc["outpref"], fl_vardq=pyrafBoolean(rc['fl_vardq']),\
-                    Stdout = IrafStdout(), Stderr = IrafStdout())
-            #"dev$null" #use this for Stdout for no outputs of the CL script to go to screen
-            #rc.getIrafStdout() #us this for Stdout for outputs of the CL script to go to screen
-            if gemini.gmos.gireduce.status:
-                log.critical('gireduce failed','critical') 
-                raise
-            else:
-                log.fullinfo('exited the gireduce CL script successfully', 'status')
-         
-            # renaming CL outputs and loading them back into memory and cleaning up the intermediate tmp files written to disk
-            clm.finishCL()
-            i=0
-            for ad in rc.getOutputs(style="AD"):
-                if ad.phuGetKeyValue('GIREDUCE'): # varifies gireduce was actually ran on the file
-                    log.fullinfo('file '+clm.preCLNames()[i]+' had its overscan subracted successfully', 'status')
-                    log.fullinfo('new file name is: '+ad.filename, 'status')
-                i=i+1
-                ut = ad.historyMark()  
-                #$$$$$ should we also have a OVERSUB UT time stame in the PHU???
-                log.fullinfo('****************************************************','header')
-                log.fullinfo('file = '+ad.filename,'header')
-                log.fullinfo('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~','header')
-                log.fullinfo('PHU keywords updated/added:\n', 'header')
-                log.fullinfo('GEM-TLM = '+str(ut)+'/n','header' )
-            
-            log.status('*FINISHED* subtracting the overscan from the input data','status')
-        except:
-            log.critical("Problem preparing the image.",'critical')
-            raise 
-        
-        yield rc    
-        
-    def overscanTrim(self,rc):
-        """
-        This primitive uses pyfits and AstroData to trim the overscan region from the input images
-        and update their headers.
-        """
-        try:
-            log.status('*STARTING* to trim the overscan region from the input data','status')
-            
-            for ad in rc.getInputs(style='AD'):
-                for sciExt in ad['SCI']:
-                    datasecStr=sciExt.data_section()
-                    datasecList=secStrToIntList(datasecStr) 
-                    dsl=datasecList
-                    log.stdinfo('\nfor '+ad.filename+' extension '+str(sciExt.extver())+\
-                                                            ', keeping the data from the section '+datasecStr,'science')
-                    sciExt.data=sciExt.data[dsl[2]-1:dsl[3],dsl[0]-1:dsl[1]]
-                    sciExt.header['NAXIS1']=dsl[1]-dsl[0]+1
-                    sciExt.header['NAXIS2']=dsl[3]-dsl[2]+1
-                    newDataSecStr='[1:'+str(dsl[1]-dsl[0]+1)+',1:'+str(dsl[3]-dsl[2]+1)+']' 
-                    sciExt.header['DATASEC']=newDataSecStr
-                    ## updating logger with updated/added keywords to each SCI frame
-                    log.fullinfo('****************************************************','header')
-                    log.fullinfo('file = '+ad.filename,'header')
-                    log.fullinfo('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~','header')
-                    log.fullinfo('SCI extension number '+str(sciExt.extver())+' keywords updated/added:\n', 'header')
-                    log.fullinfo('NAXIS1= '+str(sciExt.header['NAXIS1']),'header' )
-                    log.fullinfo('NAXIS2= '+str(sciExt.header['NAXIS2']),'header' )
-                    log.fullinfo('DATASEC= '+newDataSecStr,'header' )
-                    
-                # updating the GEM-TLM value and reporting the output to the RC    
-                ut = ad.historyMark()
-                #$$$$$ should we also have a OVERTRIM UT time stame in the PHU???
-                ad.filename=fileNameUpdater(ad.filename,postpend=rc["outsuffix"], strip=False)
-                rc.reportOutput(ad)
-                
-                # updating logger with updated/added keywords to the PHU
-                log.fullinfo('****************************************************','header')
-                log.fullinfo('file = '+ad.filename,'header')
-                log.fullinfo('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~','header')
-                log.fullinfo('PHU keywords updated/added:\n', 'header')
-                log.fullinfo('GEM-TLM = '+str(ut)+'\n','header' ) 
-                
-            log.status('*FINISHED* trimming the overscan region from the input data','status')
-        except:
-            log.critical("Problem preparing the image.",'critical')
-            raise 
-        
-        yield rc
-
-    #$$$$$$$$$$$$$$$$$$$$$$$ END OF KYLES NEW STUFF $$$$$$$$$$$$$$$$$$$$$$$$$$
         
