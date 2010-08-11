67fdc3089b9daa9e9482e4d30c224703390a7c52
Kyle Mede <kmede@gemini.edu>
2010-08-11 21:25:43 +0000

    fixed a overwriting the input data issue
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@941 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/adutils/arith.py
+++ astrodata/adutils/arith.py
@@ -23,47 +23,69 @@ def div(numerator, denominator):
     @param denominator: denominator to divide the numerator by
     @type denominator: a MEF of SCI, VAR and DQ frames in the form of an AstroData instance or a float int  
     '''
-   
+    print 'a26: in arith.div()'
     num=numerator
     den=denominator 
     from copy import deepcopy
     #out=AstroData.prepOutput(inputAry = num, clobber = False)
     out=deepcopy(num)
-    #print out.info()
+    print out.info()
     #print 'a30: type(den) ',type(den), den.filename,(type(den)==astrodata.AstroData)
     if type(den)==astrodata.AstroData:
-        #print 'a30: den is type AstroData'
+        print 'a30: den is type AstroData'
         for sci in num['SCI']:
             extver = sci.extver()
             try:
                 if num[('SCI',extver)].data.shape==den[('SCI',extver)].data.shape: #making sure arrays are same size/shape
                     #print 'a35: deviding SCI frames'
                     # dividing the SCI frames
-                    np.divide(num[('SCI',extver)].data,den[('SCI',extver)].data,out[('SCI',extver)].data)
-              
-                    #print 'a38: adding the VAR frames'
-                    # simply adding the VAR frames WARNING THIS IS ONLY TEMP, MUST CORRECT LATER!!!!!!!!!!!!!!
-                    np.add(num[('VAR',extver)].data,den[('VAR',extver)].data,out[('VAR',extver)].data)
-                   
-                    #print 'a41: bitwise_or on DQ frames'
+                    out[('SCI',extver)].data=np.divide(num[('SCI',extver)].data,den[('SCI',extver)].data)
+                    print num.filename
+                    print den.filename
+                    print 'a44: starting the VAR frame calculations'
+                    ## creating the output VAR frame following varOut=sciOut^2 * ( varA/(sciA^2) + varB/(sciB^2) )
+                    # making empty sciOutSqured array and squaring the sciOut frame to load it up
+                    #sciOutSquared=np.zeros(out[('SCI',extver)].data.shape,dtype=np.float32) 
+                    print 'a48:'
+                    sciOutSquared=np.multiply(out[('SCI',extver)].data,out[('SCI',extver)].data)
+                    print 'a49:'
+                    # ditto for sciA and sciB 
+                    #sciASquared=np.zeros(num[('SCI',extver)].data.shape,dtype=np.float32) # $$$ all zeros arrays are the same size, consider optimizing initializing these arrays
+                    #sciBSquared=np.zeros(den[('SCI',extver)].data.shape,dtype=np.float32)
+                    sciASquared=np.multiply(num[('SCI',extver)].data,num[('SCI',extver)].data)
+                    sciBSquared=np.multiply(den[('SCI',extver)].data,den[('SCI',extver)].data)
+                    print 'a55:'
+                    # now var_A/sciASquared and var_B/sciBSquared
+                    #varAoverSciASquared=np.zeros(num[('SCI',extver)].data.shape,dtype=np.float32)
+                    #varBoverSciBSquared=np.zeros(den[('SCI',extver)].data.shape,dtype=np.float32)
+                    varAoverSciASquared=np.divide(num[('VAR',extver)].data,sciASquared)
+                    varBoverSciBSquared=np.divide(den[('VAR',extver)].data,sciBSquared)
+                    print 'a61:'
+                    # now varAoverSciASquared + varBoverSciBSquared
+                    #varOverAplusB=np.zeros(num[('SCI',extver)].data.shape,dtype=np.float32)
+                    varOverAplusB=np.add(varAoverSciASquared,varBoverSciBSquared)
+                    # put it all together varOut=sciOut^2 * ( varA/(sciA^2) + varB/(sciB^2) )
+                    out[('VAR',extver)].data=np.multiply(sciOutSquared,varOverAplusB)
+                                           
+                    print 'a41: bitwise_or on DQ frames'
                     # bitwise-or 'adding' DQ frames 
-                    np.bitwise_or(num[('DQ',extver)].data,den[('DQ',extver)].data,out[('DQ',extver)].data)
+                    out[('DQ',extver)].data=np.bitwise_or(num[('DQ',extver)].data,den[('DQ',extver)].data)
                     
                 else:
-                    log.critical('arrays are different sizes for SCI extension '+i+' of the input '\
+                    log.critical('arrays are different sizes for SCI extension '+extver+' of the input '\
                                  +num.filename+' and '+den.filename,'critical')
-                    raise ArithError('An error occurred while performing an arith task')
+                    raise 'An error occurred while performing an arith task'
             except:
-                raise ArithError('An error occurred while performing an arith task')
+                raise 'An error occurred while performing an arith task'
     elif type(den)==float:
         for i in range(0,len(num['SCI'])-1):
             try:
                 print 'a53: simple division of SCI frames by the float '+den
                 # dividing the SCI frames
-                np.divide(num[('SCI',extver)].data,den,out[('SCI',extver)].data)
+                out[('SCI',extver)].data=np.divide(num[('SCI',extver)].data,den)
                 
             except:
-                raise ArithError('An error occurred while performing an arith task')
+                raise 'An error occurred while performing an arith task'
             
     return out       
                 
@@ -97,53 +119,53 @@ def mult(inputA, inputB):
             extver = sci.extver()
             try:
                 if inA[('SCI',extver)].data.shape==inB[('SCI',extver)].data.shape: #making sure arrays are same size/shape
-                    print 'a100: deviding SCI frames'
+                    print 'a100: multiplying SCI frames'
                     #  multipling the SCI frames
-                    np.multiply(inA[('SCI',extver)].data,inB[('SCI',extver)].data,out[('SCI',extver)].data)
+                    out[('SCI',extver)].data=np.multiply(inA[('SCI',extver)].data,inB[('SCI',extver)].data)
                     
                     print 'a104: starting the VAR frame calculations'
                     ## creating the output VAR frame following varOut=sciOut^2 * ( varA/(sciA^2) + varB/(sciB^2) )
                     # making empty sciOutSqured array and squaring the sciOut frame to load it up
-                    sciOutSquared=np.zeros(out[('SCI',extver)].data.shape,dtype=np.float32) 
-                    np.multiply(out[('SCI',extver)].data,out[('SCI',extver)].data,sciOutSquared)
+                    #sciOutSquared=np.zeros(out[('SCI',extver)].data.shape,dtype=np.float32) 
+                    sciOutSquared=np.multiply(out[('SCI',extver)].data,out[('SCI',extver)].data)
                     # ditto for sciA and sciB 
-                    sciASquared=np.zeros(inA[('SCI',extver)].data.shape,dtype=np.float32) # $$$ all zeros arrays are the same size, consider optimizing initializing these arrays
-                    sciBSquared=np.zeros(inB[('SCI',extver)].data.shape,dtype=np.float32)
-                    np.multiply(inA[('SCI',extver)].data,inA[('SCI',extver)].data,sciASquared)
-                    np.multiply(inB[('SCI',extver)].data,inB[('SCI',extver)].data,sciBSquared)
+                    #sciASquared=np.zeros(inA[('SCI',extver)].data.shape,dtype=np.float32) # $$$ all zeros arrays are the same size, consider optimizing initializing these arrays
+                    #sciBSquared=np.zeros(inB[('SCI',extver)].data.shape,dtype=np.float32)
+                    sciASquared=np.multiply(inA[('SCI',extver)].data,inA[('SCI',extver)].data)
+                    sciBSquared=np.multiply(inB[('SCI',extver)].data,inB[('SCI',extver)].data)
                     # now var_A/sciASquared and var_B/sciBSquared
-                    varAoverSciASquared=np.zeros(inA[('SCI',extver)].data.shape,dtype=np.float32)
-                    varBoverSciBSquared=np.zeros(inB[('SCI',extver)].data.shape,dtype=np.float32)
-                    np.divide(inA[('VAR',extver)].data,sciASquared,varAoverSciASquared)
-                    np.divide(inB[('VAR',extver)].data,sciBSquared,varBoverSciBSquared)
+                    #varAoverSciASquared=np.zeros(inA[('SCI',extver)].data.shape,dtype=np.float32)
+                    #varBoverSciBSquared=np.zeros(inB[('SCI',extver)].data.shape,dtype=np.float32)
+                    varAoverSciASquared=np.divide(inA[('VAR',extver)].data,sciASquared)
+                    varBoverSciBSquared=np.divide(inB[('VAR',extver)].data,sciBSquared)
                     # now varAoverSciASquared + varBoverSciBSquared
-                    varOverAplusB=np.zeros(inA[('SCI',extver)].data.shape,dtype=np.float32)
-                    np.add(varAoverSciASquared,varBoverSciBSquared,varOverAplusB)
+                    #varOverAplusB=np.zeros(inA[('SCI',extver)].data.shape,dtype=np.float32)
+                    varOverAplusB=np.add(varAoverSciASquared,varBoverSciBSquared)
                     # put it all together varOut=sciOut^2 * ( varA/(sciA^2) + varB/(sciB^2) )
-                    np.multiply(sciOutSquared,varOverAplusB,out[('VAR',extver)].data)
+                    out[('VAR',extver)].data=np.multiply(sciOutSquared,varOverAplusB)
                    
                     print 'a41: bitwise_or on DQ frames'
                     # bitwise-or 'adding' DQ frames 
-                    np.bitwise_or(inA[('DQ',extver)].data,inB[('DQ',extver)].data,out[('DQ',extver)].data) #$$$$$$$$$$$$$$$$$$$$$$$$$
+                    out[('DQ',extver)].data=np.bitwise_or(inA[('DQ',extver)].data,inB[('DQ',extver)].data) #$$$$$$$$$$$$$$$$$$$$$$$$$
                     
                 else:
                     log.critical('arrays are different sizes for SCI extension '+i+' of the input '\
                                  +inA.filename+' and '+inB.filename,'critical')
-                    raise ArithError('An error occurred while performing an arith task')
+                    raise 'An error occurred while performing an arith task'
             except:
-                raise ArithError('An error occurred while performing an arith task')
+                raise 'An error occurred while performing an arith task'
     elif type(inB)==float:
         for i in range(0,len(inA['SCI'])-1):
             try:
                 print 'a53: simple division of SCI frames by the float '+inB
                 # multipling the SCI frames by the constant
-                np.multiply(inA[('SCI',extver)].data,inB,out[('SCI',extver)].data)
+                out[('SCI',extver)].data=np.multiply(inA[('SCI',extver)].data,inB)
                 
                 #multipling the VAR frames by the constant^2
-                np.multiply(inA[('VAR',extver)].data,inB*inB,out[('VAR',extver)].data)
+                out[('VAR',extver)].data=np.multiply(inA[('VAR',extver)].data,inB*inB)
                 
             except:
-                raise ArithError('An error occurred while performing an arith task')
+                raise 'An error occurred while performing an arith task'
             
     return out       
                 
