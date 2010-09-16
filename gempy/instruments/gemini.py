7e3ac5ad104b54671d5a4a46372b57ba53a82f2d
Kyle Mede <kmede@gemini.edu>
2010-09-16 19:54:34 +0000

    clean up during code review prep
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1019 e04282df-fb77-4b70-bf7a-c1223586725c

--- gempy/instruments/gemini.py
+++ gempy/instruments/gemini.py
@@ -6,18 +6,15 @@
 import os
 import pyfits as pf
 import numpy as np
-import time
-from datetime import datetime
-from astrodata.adutils import mefutil, paramutil
+#from astrodata.adutils import mefutil, paramutil
 from astrodata.adutils import gemLog
 from astrodata.AstroData import AstroData
-import pyraf
 
 log=gemLog.getGeminiLog() 
 
 def stdObsHdrs(ad):
-   
-    ##keywords that are updated/added for all Gemini PHUs 
+    
+    ## keywords that are updated/added for all Gemini PHUs 
     ad.phuSetKeyValue('NSCIEXT', ad.countExts("SCI"), 'Number of science extensions')
     ad.phuSetKeyValue('PIXSCALE', ad.pixel_scale(), 'Pixel scale in Y in arcsec/pixel')
     ad.phuSetKeyValue('NEXTEND', len(ad) , 'Number of extensions')
@@ -25,14 +22,15 @@ def stdObsHdrs(ad):
     ad.phuSetKeyValue('COADDEXP', ad.phuValue("EXPTIME") , 'Exposure time for each coadd frame')
     numcoadds = ad.coadds()
     if not numcoadds:  
-        numcoadds = 1      #for if there are no coadds performed, set to 1
+        numcoadds = 1      # for if there are no coadds performed, set to 1
     effExpTime = ad.phuValue("EXPTIME")*numcoadds    
     ad.phuSetKeyValue('EXPTIME', effExpTime , 'Effective exposure time') 
     ad.phuSetKeyValue('NCOADD', str(numcoadds) , 'Number of coadds')
 
     ut = ad.historyMark()
-    ad.historyMark(key="GPREPARE",stomp=False)    
-    ##updating logger with updated/added keywords
+    ad.historyMark(key="GPREPARE",stomp=False) 
+       
+    ## updating logger with updated/added keywords
     log.fullinfo('****************************************************','header')
     log.fullinfo('file = '+ad.filename,'header')
     log.fullinfo('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~','header')
@@ -46,7 +44,7 @@ def stdObsHdrs(ad):
     log.fullinfo('GEM-TLM = '+str(ut),'header' )
     log.fullinfo('---------------------------------------------------','header')
          
-    ##a loop to add the missing/needed keywords in the Gemini SCI extensions
+    ## a loop to add the missing/needed keywords in the Gemini SCI extensions
     for ext in ad["SCI"]:
         ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'GAIN', ext.gain(), "Gain (e-/ADU)")
         ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'PIXSCALE', ext.pixel_scale(), 'Pixel scale in Y in arcsec/pixel')
@@ -56,10 +54,11 @@ def stdObsHdrs(ad):
         #print nonlin
         #print type(nonlin)
         if not nonlin:
-            nonlin = 'None'     #if no nonlinear section provided then set to string 'None'
+            nonlin = 'None'     # if no nonlinear section provided then set to string 'None'
         ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'NONLINEA',nonlin , 'Non-linear regime level in ADU')
         ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'SATLEVEL',ext.saturation_level() , 'Saturationlevel in ADU')
         ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'EXPTIME', effExpTime , 'Effective exposure time')
+        
         log.fullinfo('SCI extension number '+str(ext.header['EXTVER'])+' keywords updated/added:\n', 'header')
         log.fullinfo('GAIN = '+str(ext.gain()),'header' )
         log.fullinfo('PIXSCALE = '+str(ext.pixel_scale()),'header' )
@@ -71,13 +70,15 @@ def stdObsHdrs(ad):
         log.fullinfo('---------------------------------------------------','header')
 
 def stdObsStruct(ad):
+    
     log.fullinfo('****************************************************','header') 
     log.fullinfo('file = '+ad.filename, 'header')
     log.fullinfo('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~','header')
-    ##a loop to add the missing/needed keywords in the SCI extensions
+    ## a loop to add the missing/needed keywords in the SCI extensions
     for ext in ad["SCI"]:
         ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'EXTNAME', 'SCI', "Extension name")        
         ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'EXTVER', int(ext.header['EXTVER']), "Extension version") 
+        
         log.fullinfo('SCI extension number '+str(ext.header['EXTVER'])+' keywords updated/added:\n', 'header')       
         log.fullinfo('EXTNAME = '+'SCI','header' )
         log.fullinfo('EXTVER = '+str(ext.header['EXTVER']),'header' )
@@ -96,19 +97,20 @@ def fileNameUpdater(origfilename, postpend='',prepend='' , strip=False):
             infilename = stripPostfix(infilename)
         (name,filetype) = os.path.splitext(infilename)
         outFileName = prepend+name+filetype
-    #ad.filename=outFileName
     return outFileName
 
 def stripPostfix(filename):
+    
     dirname = os.path.dirname(filename)
     basename = os.path.basename(filename)
-    (name, filetype) = os.path.splitext(basename)
+    (name,filetype) = os.path.splitext(basename)
     a = name.split("_")
     name = a[0]
     retname = os.path.join(dirname,name+filetype)
     return retname    
 
 def secStrToIntList(string):
+    
     ## string must be of the format '[#1:#2,#3:#4]', returns list of ints [#1,#2,#3,#4]
     coords=string.strip('[').strip(']').split(',')
     Ys=coords[0].split(':')
@@ -118,12 +120,12 @@ def secStrToIntList(string):
     retl.append(int(Ys[1]))
     retl.append(int(Xs[0]))
     retl.append(int(Xs[1]))
-    
     return retl
     
 def biassecStrTonbiascontam(biassec,ad):
-    ''' this function works with nbiascontam() of the CLManager. 
-    It will find the largest horizontal difference between biassec and BIASSEC for each SCI extension i a single input.
+    ''' 
+    This function works with nbiascontam() of the CLManager. 
+    It will find the largest horizontal difference between biassec and BIASSEC for each SCI extension in a single input.
     
     @param biassec: biassec parameter of format '[#:#,#:#],[#:#,#:#],[#:#,#:#]'
     @type biassec: string  
@@ -141,23 +143,21 @@ def biassecStrTonbiascontam(biassec,ad):
         for i in range(0,len(ad['SCI'])):
             BIASSEClist=secStrToIntList(ad.extGetKeyValue(i,'BIASSEC'))
             biasseclist=ccdIntList[i]
-            #print 'g136: BIASSEClist= ',BIASSEClist
-            #print 'g137: biasseclist= ',biasseclist
             if biasseclist[2]==BIASSEClist[2] and biasseclist[3]==BIASSEClist[3]:
-                if biasseclist[0]<50: #ie overscan/bias section is on the left side of chip
+                if biasseclist[0]<50: # ie overscan/bias section is on the left side of chip
                     if biasseclist[0]==BIASSEClist[0]: 
                         nbiascontam=BIASSEClist[1]-biasseclist[1]
                     else:
-                        log.error('left horizontal components of biassec and BIASSEC did not match so using default nbiascontam=4','error')
+                        log.error('left horizontal components of biassec and BIASSEC did not match, so using default nbiascontam=4','error')
                         nbiascontam=4
-                else: #ie overscan/bias section is on the right side of chip
+                else: # ie overscan/bias section is on the right side of chip
                     if biasseclist[1]==BIASSEClist[1]: 
                         nbiascontam=BIASSEClist[0]-biasseclist[0]
                     else:
-                        log.error('right horizontal components of biassec and BIASSEC did not match so using default nbiascontam=4','error') 
+                        log.error('right horizontal components of biassec and BIASSEC did not match, so using default nbiascontam=4','error') 
                         nbiascontam=4
             else:
-                log.error('vertical components of biassec and BIASSEC parameters did not match so using default nbiascontam=4', 'error')
+                log.error('vertical components of biassec and BIASSEC parameters did not match, so using default nbiascontam=4', 'error')
                 nbiascontam=4
             if nbiascontam>retvalue: # ie returning the largest nbiascontam value throughout all chips 
                 retvalue=nbiascontam
@@ -165,25 +165,27 @@ def biassecStrTonbiascontam(biassec,ad):
         return retvalue
             
     except:
-        log.error('An error ocured while trying to calculate the nbiascontam so using default value = 4','error')
-        return 4
+        log.error('An error ocured while trying to calculate the nbiascontam, so using default value = 4','error')
+        return 4 # so exiting 'gracefully' and still returning a value
         
         
 def pyrafBoolean(pythonBool):
     '''
-    a very basic function to reduce code repetition that simply 'casts' any given 
-    Python boolean into a pyraf/iraf one for use in the CL scripts
+    A very basic function to reduce code repetition that simply 'casts' any given 
+    Python boolean into a pyraf/iraf one for use in the CL scripts.
     '''
+    import pyraf
+    
     if pythonBool:
         return pyraf.iraf.yes
     elif  not pythonBool:
         return pyraf.iraf.no
     else:
-        print "DANGER DANGER Will Robinson, pythonBool passed in not True or False"
+        print "DANGER DANGER Will Robinson, pythonBool passed in was not True or False, and thats just crazy talk :P"
 
 class CLManager(object):
-    _preCLcachestorenames = []
-    _preCLfilenames = []
+    _preCLcachestorenames = [] # the version of the names for input to the CL script
+    _preCLfilenames = [] # the original names of the files at the start of the primitive which called CLManager
     rc = None
     prefix = None
     outpref = None
@@ -199,19 +201,19 @@ class CLManager(object):
         self.prefix = self.uniquePrefix()
         self.preCLwrites()
     
-    #perform all the finalizing steps after CL script is ran, currently just an alias for postCLloads
+    # perform all the finalizing steps after CL script is ran, currently just an alias for postCLloads
     def finishCL(self,combine=False): 
         self.postCLloads(combine)    
     
     def preCLwrites(self):
         for ad in self.rc.getInputs(style="AD"):
             self._preCLfilenames.append(ad.filename)
-            name = fileNameUpdater(ad.filename,prepend=self.prefix, strip=True)
+            name = fileNameUpdater(ad.filename,prepend=self.prefix,strip=True)
             self._preCLcachestorenames.append(name)
             log.fullinfo('Temporary file on disk for input to CL: '+name,'CLprep')
             ad.write(name, rename = False) 
     
-    #just a function to return the 'private' member variable _preCLcachestorenames
+    # just a function to return the 'private' member variable _preCLcachestorenames
     def cacheStoreNames(self):
         return self._preCLcachestorenames
        
@@ -221,7 +223,7 @@ class CLManager(object):
             log.fullinfo('removing file '+file+' from disk', 'postCL')
             os.remove(file)
         
-    #just a function to return the 'private' member variable _preCLfilenames
+    # just a function to return the 'private' member variable _preCLfilenames
     def preCLNames(self):
         return self._preCLfilenames
     
@@ -233,13 +235,10 @@ class CLManager(object):
         return self.rc.makeInlistFile(self.listname,self._preCLcachestorenames)
         
     def uniquePrefix(self):
-        #print 'g163: using uniquePrefix = ',"tmp"+ str(os.getpid())+self.rc.ro.curPrimName
         return "tmp"+ str(os.getpid())+self.rc.ro.curPrimName
     
     def combineOutname(self):
         #@@ REFERENCE IMAGE: for output name
-        # print "g184:", repr(self.outpref)
-        # print "g185", repr( self._preCLcachestorenames)
         return self.outpref+self._preCLcachestorenames[0]
     
     def postCLloads(self,combine=False):
@@ -249,7 +248,7 @@ class CLManager(object):
             os.rename(cloutname, finalname )
             self.rc.reportOutput(finalname)
             os.remove(finalname)
-            print 'g209: self.listname = ',self.listname
+            #print 'g209: self.listname = ',self.listname
             os.remove(self.listname)
             log.fullinfo('CL outputs '+cloutname+' was renamed on disk to:\n'+finalname,'postCL')
             log.fullinfo(finalname+' was loaded into memory', 'postCL')
@@ -257,15 +256,15 @@ class CLManager(object):
             log.fullinfo(self.listname+' was deleted from disk','postCL')
             
             for i in range(0, len(self._preCLcachestorenames)):
-                storename = self._preCLcachestorenames[i]  #name of file written to disk for input to CL script
+                storename = self._preCLcachestorenames[i]  # name of file written to disk for input to CL script
                 os.remove(storename) # clearing renamed file ouput by CL
                 log.fullinfo(storename+' was deleted from disk', 'postCL')
                 
         elif combine==False:
             for i in range(0, len(self._preCLcachestorenames)):
-                storename = self._preCLcachestorenames[i]  #name of file written to disk for input to CL script
-                cloutname = self.outpref + storename  #name of file CL wrote to disk
-                finalname = fileNameUpdater(self._preCLfilenames[i], postpend= self.outpref, strip=False)  #name i want the file to be
+                storename = self._preCLcachestorenames[i]  # name of file written to disk for input to CL script
+                cloutname = self.outpref + storename  # name of file CL wrote to disk
+                finalname = fileNameUpdater(self._preCLfilenames[i], postpend= self.outpref, strip=False)  # name i want the file to be
                 
                 os.rename(cloutname, finalname )
                 
@@ -273,7 +272,7 @@ class CLManager(object):
                 self.rc.reportOutput(finalname)
                 
                 os.remove(finalname) # clearing file written for CL input
-                os.remove(storename) # clearing renamed file ouput by CL
+                os.remove(storename) # clearing renamed file output by CL
                 log.fullinfo('CL outputs '+cloutname+' was renamed on disk to:\n '+finalname,'postCL')
                 log.fullinfo(finalname+' was loaded into memory', 'postCL')
                 log.fullinfo(finalname+' was deleted from disk', 'postCL')
@@ -291,7 +290,7 @@ class CLManager(object):
             log.fullinfo(repr(key)+' = '+repr(val),'params')
             
     def nbiascontam(self):
-        '''this function will find the largest difference between the horizontal component 
+        '''This function will find the largest difference between the horizontal component 
            of every BIASSEC value and those of the biassec parameter and return that difference 
            as an integer to be the value for the nbiascontam parameter used in the gireduce call of the overscanSubtract prim'''
         
