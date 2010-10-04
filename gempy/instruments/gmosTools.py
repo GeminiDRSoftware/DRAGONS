c7332f14ee1e2c3bad229976325a70a162341d17
Kyle Mede <kmede@gemini.edu>
2010-10-04 22:14:12 +0000

    many changes to update to coding standards
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1056 e04282df-fb77-4b70-bf7a-c1223586725c

--- gempy/instruments/gmosTools.py
+++ gempy/instruments/gmosTools.py
@@ -1,5 +1,5 @@
 #Author: Kyle Mede, May 2010
-#this module is being used as the workhorse for the prepare primitives.
+#This module provides functions to be used by all GMOS primitives.
 
 from astrodata.adutils import gemLog
 from astrodata.AstroData import AstroData
@@ -7,28 +7,44 @@ from astrodata.AstroData import AstroData
 log=gemLog.getGeminiLog() 
 
 def stdInstHdrs(ad):  
-    # adding the missing/needed keywords into the headers 
+    """ A function used by StandardizeInstrumentHeaders in primitives_GMOS.
+        
+        It currently just adds the DISPAXIS header key to the SCI extensions."""
+    # Adding the missing/needed keywords into the headers
     if not ad.isType('GMOS_IMAGE'):
-    # do the stuff to the headers that is for the MOS, those for IMAGE are 
+    # Do the stuff to the headers that is for the MOS, those for IMAGE are 
     # taken care of with stdObsHdrs all ready 
-        for ext in ad["SCI"]:
-            ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'DISPAXIS', ext.dispersion_axis() , 'Dispersion axis')
-            ext.extSetKeyValue(('SCI',int(ext.header['EXTVER'])),'DISPAXIS',ext.dispersion_axis(), "Dispersion axis")
+    
+        # Formatting so logger looks organized for these messages
+        log.fullinfo('****************************************************', \
+                     'header') 
+        log.fullinfo('file = '+ad.filename, 'header')
+        log.fullinfo('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~', \
+                     'header')
+        for ext in ad['SCI']:
+            ext.SetKeyValue(('SCI',ext.extver()),'DISPAXIS', \
+                            ext.dispersion_axis() , 'Dispersion axis')
+            # Updating logger with new header key values
+            log.fullinfo('SCI extension number '+str(ext.header['EXTVER'])+\
+                         ' keywords updated/added:\n', 'header')       
+            log.fullinfo('DISPAXIS = '+str(ext.header['DISPAXIS']), 'header' )
+            log.fullinfo('---------------------------------------------------',\
+                         'header')
 
 def valInstData(ad):
-    # to ensure structure is the normal PHU followed by 3 SCI 
-    # extensions for GMOS
+    """A function used by validateInstrumentData in primitives_GMOS.
+    
+        It currently just checks if there are 1, 3, 6 or 12 SCI extensions 
+        in the input. """
     length=ad.countExts('SCI')
+    # If there are 1, 3, 6, or 12 extensions, all good, if not log a critical 
+    # message and raise an exception
     if length==1 or length==3 or length==6 or length==12:
         pass
     else: 
-        log.critical("There are NOT 1, 3, 6 or 12 extensions in file = "+ad.filename)
-        raise 
-            
-def addMDF(ad,mdf): 
-        # so far all of this is done in the primitive, will 
-        # figure this out later
-        pass
-        
+        log.critical('There are NOT 1, 3, 6 or 12 extensions in file = '+\
+                     ad.filename)
+        raise 'Error occurred in valInstData for input '+ad.filename
+  
          
        
