c3a1b428a173f6d5c219bd63894a4403f6cc6d8b
Craig Allen <callen@gemini.edu>
2010-10-26 20:49:30 +0000

    added
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1111 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/scripts/adcc.py
+++ astrodata/scripts/adcc.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python
-
-import xmlrpclib
-from urllib import urlopen
-from SimpleXMLRPCServer import SimpleXMLRPCServer
-import os, signal
-import re
 import exceptions
-import urllib
-from xml.dom import minidom
-from threading import Thread
-import prsproxyweb
-import socket
+from optparse import OptionParser
+import os
+import pickle
+import re
 import select
+import signal
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+import socket
+from threading import Thread
+import urllib
+from urllib import urlopen
+from xml.dom import minidom
+import xmlrpclib
+from copy import copy
+
+from CmdQueue import TSCmdQueue
+from astrodata.StackKeeper import StackKeeper
+
+# @@DEPEND: PIL
+import PIL
+from PIL import Image
+# @@ main dependencies
+import numpy
+import numpy as np
+from numpy import where
+from datetime import datetime
+import prsproxyweb
 from prsproxyutil import calibrationSearch, CALMGR, CALTYPEDICT,urljoin
 
-from optparse import OptionParser
+from astrodata import AstroData
+
 
 parser = OptionParser()
 
@@ -29,6 +45,10 @@ parser.add_option("--preload", dest = "preload", action = "store_true",
             help = """Useful in proxy mode, where some information otherwise produced
             during the first relevant request is prepared prior to starting the 
             HTTPServer.""")
+parser.add_option("--reload", dest = "reload", action = "store_true",
+            default = False,
+            help = """Just like --preload, but uses last, cached (pickled)
+            directory scan.""")
 parser.add_option("-r", "--reduce-port", dest = "reduceport", default=53531, type="int",
             help="""When invoked by reduce, this is used to inform the prsproxy of the 
             port on which reduce listens for xmlrpc commands.""")
@@ -42,13 +62,38 @@ parser.add_option("-w", "--http-port", dest = "httpport", default=8777, type="in
             http://localhost:<http-port>/""")
 options, args = parser.parse_args()
 # ----- UTILITY FUNCS
+def getPersistDir(dirtitle = "adcc"):
+    dirs = {"adcc":".adcc"}
+    
+    for dirt in dirs.keys():
+        if not os.path.exists(dirs[dirt]):
+            os.mkdir(dirs[dirt])
+    
+    return dirs[dirtitle]
+    
+    # move to other module or into a class!
 
+def numpy2im(ad):
+    if isinstance(ad, AstroData):
+        data = ad.hdulist[1].data
+    else:
+        data = ad        
+    #newdata = nump    .float(ad.data)
+    newdata = numpy.uint32(data)
+    im = Image.fromarray(newdata, mode="I")
+    return im
+    
+stackKeeper = StackKeeper(local=True)
 # -------------------
 class ReduceInstanceManager(object):
     numinsts = 0
     finished = False
     reducecmds = None
     reducedict = None
+    displayCmdHistory = None
+    cmdNum = 0
+    stackKeeper = None
+    
     def __init__(self):
         # get my client for the reduce commands
         print "starting xmlrpc client to port %d..." %options.reduceport,
@@ -59,6 +104,10 @@ class ReduceInstanceManager(object):
         except socket.error:
             print "prs50: no reduce instances running"
         self.reducedict = {}
+        # these members save command history so that tools have access, e.g.
+        #   a display tool
+        self.stackKeeper = stackKeeper
+        self.displayCmdHistory = TSCmdQueue()
         
     def register(self, pid, details):
         """This function is exposed to the xmlrpc interface, and is used
@@ -83,7 +132,98 @@ class ReduceInstanceManager(object):
             self.finished = True
             
             #quitServer()s
+    def stackPut(self, ID, filelist, cachefile = None):
+        print "ADCC136:", ID, repr(filelist), repr(cachefile)
+        self.stackKeeper.add(ID, filelist, cachefile)
+        print "ADCC138: element added to stacklist:" , ID
+        self.stackKeeper.persist(cachefile)
+    
+    def stackGet(self, ID, cachefile = None):
+        retval = self.stackKeeper.get(ID, cachefile)
+        return retval
+        
+    def displayRequest(self, rq):
+        
+        print "adcc99:", repr(rq)
+        if "display" in rq:
+            dispcmd = rq["display"]
+            dispcmd.update({"timestamp":datetime.now(),
+                            "cmdNum":self.cmdNum})
+            self.cmdNum += 1
+            rqcopy = copy(rq)
 
+            print "adcc108:", repr(rqcopy)
+            if "files" in dispcmd:
+                files = dispcmd["files"]
+                print "adcc110:", repr(files)
+                for basename in files:
+                    fileitem = files[basename]
+                    ad = AstroData(fileitem["filename"])
+                    print "adcc115: loaded ",ad.filename
+                    from copy import deepcopy
+
+                    numsci = ad.countExts("SCI")
+                    if numsci > 2:
+                        sci = ad[("SCI",2)]                    
+                    else:
+                        sci = ad[("SCI",1)]
+                    data = sci.data
+                    mean = data.mean()
+                    bottom = data[where(data<mean)].mean()*.80
+                    print "adcc140: bottom", bottom
+                    top = data[where(data>(1.25*mean))].mean()
+                    print "adcc142: top =",top
+                    for sci in ad["SCI"]:
+                        data = numpy.uint32(deepcopy(sci.data))
+                        #data = numpy.uint32(sci.data)
+                        if False:
+                            mean = data.mean()
+                            bottom = data[where(data<mean)].mean()
+                            extver = sci.extver()
+                            if extver == 1 or extver ==3:
+                                bottom = bottom*1.33
+                            print "adcc140: bottom", bottom
+                            top = data[where(data>(1.25*mean))].mean()
+                            print "adcc142: top =",top
+                        bottom = int(bottom)
+                        top = int(top)
+                        #data [where(data<bottom)] = bottom
+                        #data[where(data>top)] = top
+                        print "adcc164, extver -= %d top,bottom = %d,%d " %(sci.extver(),top, bottom)
+                        abstop = 65535
+                        factor = abstop/(top-bottom)
+                        data = data - bottom
+                        data = data*(factor)
+                        # data[where(data<0)] = 0
+                        # data[where(data>abstop)] = abstop
+
+                        im = numpy2im(data)
+                        im = im.transpose(Image.FLIP_TOP_BOTTOM)
+                        from CacheManager import get_cache_dir,put_cache_file
+                        tdir = get_cache_dir("adcc.display")
+                        dispname = "sci%d-%s_%d.png" % (sci.extver(), 
+                                                     sci.data_label(),
+                                                     dispcmd["cmdNum"])
+                        nam = os.path.join(tdir, dispname)
+                        put_cache_file(dispname, nam)
+
+                        url = "/displaycache/"+dispname
+                        baserq = rqcopy["display"]["files"][basename]
+                        
+                        if "extdict" not in baserq:
+                            baserq.update({"extdict":{}})
+                        baserq["extdict"].update(
+                                    {"SCI%d"%sci.extver(): url }
+                                )
+                        rqcopy["display"]["files"][basename].update({"url": None})
+                        
+                        if os.path.exists(nam):
+                            os.remove(nam)
+                        im.save(nam, "PNG")
+                        # print "adcc186:", repr(rqcopy)
+        print "adcc197: about to addcmd"
+        self.displayCmdHistory.addCmd(rqcopy)
+        
 def get_version():
     version = [("PRSProxy","0.1")]
     print "prsproxy version:", repr(version)
@@ -100,14 +240,27 @@ rim = ReduceInstanceManager()
 server.register_instance(rim)
 
 if options.preload == True:
-    print "prsproxy: scanning current directory tree"
+    print "adcc: scanning current directory tree"
     from astrodata.DataSpider import DataSpider
     ds = DataSpider(".")
     dirdict = ds.datasetwalk()
-    print "prsproxy: done scanning current directory tree"
+    persistpath = os.path.join(getPersistDir(), "dataspider_cache.pkl")
+    ddf = open(persistpath, "w")
+    pickle.dump(dirdict, ddf)
+    ddf.close()
+    print "adcc: done scanning current directory tree"
 else:
     ds = None
     dirdict = None
+    
+if options.reload == True:
+    from astrodata.DataSpider import DataSpider
+    print "prsproxy: reloading result of previous scan of directory tree"
+    ds = DataSpider(".")
+    persistpath = os.path.join(getPersistDir(), "dataspider_cache.pkl")
+    ddf = open(persistpath)
+    dirdict = pickle.load(ddf)
+    ddf.close()
 
 
 # server.serve_forever(
@@ -156,6 +309,9 @@ while True:
             # note: save reduce pide (pass in register) and 
             #       and check if pids are running!
             print "\nprsproxy: %d instances of reduce running" % rim.numinsts
+            #below allows exit anyway
+            outerloopdone = True
+            prsproxyweb.webserverdone = True
             break
         else:
             print "\nprsproxy: exiting due to Ctrl-C"
