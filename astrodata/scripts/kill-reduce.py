2ff7d004ad48134f3e304159653a67a3bd3ab962
Craig Allen <callen@gemini.edu>
2010-10-26 19:37:46 +0000

    will kill all reduce and adcc scripts... may work only in linux
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1107 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ astrodata/scripts/kill-reduce.py
@@ -0,0 +1,34 @@
+import commands, os, string
+import signal
+
+program = "python" #raw_input("Enter the name of the program to check: ")
+
+try:
+    #perform a ps command and assign results to a list
+    outputall = commands.getoutput("ps -ef|grep " + program)
+    lines = outputall.split("\n")
+    for output in lines:
+        proginfo = string.split(output)
+        #display results
+        if os.getpid() == int(proginfo[1]):
+            continue
+        if "reduce" in proginfo[8]:
+            print "\n\
+cmd:", proginfo[8], "\n\
+                Owner: ", proginfo[0], "\n\
+           Process ID: ", proginfo[1], "\n\
+    Parent process ID: ", proginfo[2], "\n\
+         Time started: ", proginfo[4]
+            os.kill(int(proginfo[1]), signal.SIGHUP)
+        if "adcc" in proginfo[8]:
+            print "\n\
+cmd", proginfo[8], "\n\
+                Owner: ", proginfo[0], "\n\
+           Process ID: ", proginfo[1], "\n\
+    Parent process ID: ", proginfo[2], "\n\
+         Time started: ", proginfo[4]
+            os.kill(int(proginfo[1]), signal.SIGHUP)
+        
+except:
+    print "There was a problem with the program."
+    raise
