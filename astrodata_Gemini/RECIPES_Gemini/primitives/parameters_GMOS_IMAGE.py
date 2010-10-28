649c756e9746d2ff8a6467fc175d89d7a7d98012
Kyle Mede <kmede@gemini.edu>
2010-10-28 14:32:18 +0000

    added parameter dicts for new/updated prims for fringing
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1121 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_IMAGE.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_IMAGE.py
@@ -18,5 +18,62 @@
                "note1":"these are just test parameters...",
                "note2":"pause doesn't need a 'message' parameter"
                }
-              }
+              },
+    "makeFringeFrame": {
+                'outpref':
+                   {
+                    'default': '_fringe' , #value to be post pended to this primitives outputs
+                      'recipeOverride': True,
+                      'type': 'str',
+                      'userOverride':False 
+                    },
+                      
+                'fl_vardq':
+                   {
+                   'default': True , 
+                      'recipeOverride': True,
+                      'type': 'bool',
+                      'userOverride':True 
+                    },
+                    
+                'method':
+                   {
+                   'default': 'median' , 
+                      'recipeOverride': True,
+                      'type': 'str',
+                      'userOverride':False  
+                     },  
+                    },   
+    "fringeCorrect": {
+                'outpref':
+                   {
+                    'default': '_fringecorrected' , #value to be post pended to this primitives outputs
+                      'recipeOverride': True,
+                      'type': 'str',
+                      'userOverride':False 
+                    },
+            'statsec':
+                   {
+                    'default': '' , 
+                      'recipeOverride': True,
+                      'type': 'str',
+                      'userOverride':False 
+                    },
+                      
+                'fl_statscale':
+                   {
+                   'default': True , 
+                      'recipeOverride': True,
+                      'type': 'bool',
+                      'userOverride':True 
+                    },
+                    
+                'scale':
+                   {
+                   'default': 0.0 , 
+                      'recipeOverride': True,
+                      'type': 'float',
+                      'userOverride':False  
+                     },  
+                    },             
 }
