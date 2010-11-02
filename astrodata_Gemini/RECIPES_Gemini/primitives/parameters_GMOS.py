9bb47e8d82bb5105e75a501edd3f829a5583b8a3
Kyle Mede <kmede@gemini.edu>
2010-11-02 18:00:18 +0000

    alphabetized parameters to match primitives
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1148 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS.py
@@ -1,83 +1,25 @@
-{    
-
-
-        "overscanSubtract":{"fl_trim":
-                         {
-                          'default': False , 
-                          'recipeOverride': False,
-                          'type': 'bool',
-                          'userOverride':False    
-                          },
-                          "fl_vardq":
-                          {
-                           'default': False ,
-                          'recipeOverride': True,
-                          'type': 'bool',
-                          'userOverride':True    
-                           },
-                          "postpend":
-                          {
-                           'default': '_oversubed' , #value to be post pended to this primitives outputs
-                          'recipeOverride': True,
-                          'type': 'str',
-                          'userOverride':False    
-                           },
-                           'biassec':
-                           {
-                            'default': '[1:25,1:2304],[1:32,1:2304],[1025:1056,1:2304]' ,
-                            'recipeOverride': True,
-                            'type': 'str',
-                            'userOverride':True
-                            }
-                        },
-        "overscanTrim":{"postpend":
-                          {
-                           'default': '_overtrimd' , #value to be post pended to this primitives outputs
-                          'recipeOverride': True,
-                          'type': 'str',
-                          'userOverride':False    
-                           }
-                        },
-        'storeProcessedBias':{'clob':
-                              {
-                                'default': False,
-                                'recipeOverride': True,
-                                'type': 'bool',
-                                'userOverride':True  
-                               }
-                              
-                              } ,
-        'storeProcessedFlat':{'clob':
-                              {
-                                'default': False,
-                                'recipeOverride': True,
-                                'type': 'bool',
-                                'userOverride':True  
-                               }
-                              
-                              } ,
-        "biasCorrect":{"fl_over":
+{        'biasCorrect':{'fl_over':
                         {
                           'default': False , 
                           'recipeOverride': True,
                           'type': 'bool',
                           'userOverride':True                                      
                          },
-                         "fl_trim":
+                         'fl_trim':
                          {
                           'default': False , 
                           'recipeOverride': False,
                           'type': 'bool',
                           'userOverride':False    
                           },
-                          "fl_vardq":
+                          'fl_vardq':
                           {
                            'default': True ,
                           'recipeOverride': True,
                           'type': 'bool',
                           'userOverride':True    
                            },
-                          "postpend":
+                          'postpend':
                           {
                            'default': '_biassub' , #value to be post pended to this primitives outputs
                           'recipeOverride': True,
@@ -85,60 +27,17 @@
                           'userOverride':False    
                            }
                           },
-            "normalizeFlat":{
-                        "fl_over":
-                        {
-                          'default': False , 
-                          'recipeOverride': False,
-                          'type': 'bool',
-                          'userOverride':False,
-                          'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-                         },
-                         "fl_trim":
-                         {
-                          'default': False , 
-                          'recipeOverride': False,
-                          'type': 'bool',
-                          'userOverride':False,    
-                          'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-                         
-                          },
-                          "fl_vardq":
-                          {
-                           'default': True ,
-                          'recipeOverride': True,
-                          'type': 'bool',
-                          'userOverride':True,  
-                          'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-                           },
-                          "postpend":
-                          {
-                           'default': '_normalized' , #value to be post pended to this primitives outputs (used by CLManager not CL directly for this prim)
-                          'recipeOverride': True,
-                          'type': 'str',
-                          'userOverride':False    
-                           },
-                          'fl_bias':
-                          {
-                           'default': False , 
-                          'recipeOverride': False,
-                          'type': 'bool',
-                          'userOverride':False, 
-                          'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-                           }
-                          },
         'flatCorrect':{
                        'postpend':
                        {
-                        'default': '_FlatCorrected' , 
+                        'default': '_flatcorrected' , 
                           'recipeOverride': True,
                           'type': 'str',
                           'userOverride':False  
-                        
                         }
                        },
-        'mosaic':{
-                  "fl_paste":
+        'mosaicDetectors':{
+                  'fl_paste':
                           {
                            'default': False ,
                           'recipeOverride': True,
@@ -154,7 +53,7 @@
                           'userOverride':False  ,
                         'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
                         },
-                        "outimages":
+                        'outimages':
                           {
                            'default': '' ,
                           'recipeOverride': True,
@@ -162,7 +61,7 @@
                           'userOverride':True,  
                           'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
                            },
-                        "interp_function":  #this is the geointer parameters new name for easier reading to the user
+                        'interp_function':  #this is the geointer parameters new name for easier reading to the user
                           {
                            'default': 'linear' ,
                           'recipeOverride': True,
@@ -171,5 +70,102 @@
                           'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
                            },
                            
-                  }
+                  },
+    'normalizeFlat':{
+                        'fl_over':
+                        {
+                          'default': False , 
+                          'recipeOverride': False,
+                          'type': 'bool',
+                          'userOverride':False,
+                          'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+                         },
+                         'fl_trim':
+                         {
+                          'default': False , 
+                          'recipeOverride': False,
+                          'type': 'bool',
+                          'userOverride':False,    
+                          'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+                         
+                          },
+                          'fl_vardq':
+                          {
+                           'default': True ,
+                          'recipeOverride': True,
+                          'type': 'bool',
+                          'userOverride':True,  
+                          'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+                           },
+                          'postpend':
+                          {
+                           'default': '_normalized' , #value to be post pended to this primitives outputs (used by CLManager not CL directly for this prim)
+                          'recipeOverride': True,
+                          'type': 'str',
+                          'userOverride':False    
+                           },
+                          'fl_bias':
+                          {
+                           'default': False , 
+                          'recipeOverride': False,
+                          'type': 'bool',
+                          'userOverride':False, 
+                          'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+                           }
+                          },
+                                                   
+        'overscanSubtract':{'fl_trim':
+                         {
+                          'default': False , 
+                          'recipeOverride': False,
+                          'type': 'bool',
+                          'userOverride':False    
+                          },
+                          'fl_vardq':
+                          {
+                           'default': False ,
+                          'recipeOverride': True,
+                          'type': 'bool',
+                          'userOverride':True    
+                           },
+                          'postpend':
+                          {
+                           'default': '_oversubed' , #value to be post pended to this primitives outputs
+                          'recipeOverride': True,
+                          'type': 'str',
+                          'userOverride':False    
+                           },
+                           'biassec':
+                           {
+                            'default': '[1:25,1:2304],[1:32,1:2304],[1025:1056,1:2304]' ,
+                            'recipeOverride': True,
+                            'type': 'str',
+                            'userOverride':True
+                            }
+                        },
+        'overscanTrim':{'postpend':
+                          {
+                           'default': '_overtrimd' , #value to be post pended to this primitives outputs
+                          'recipeOverride': True,
+                          'type': 'str',
+                          'userOverride':False    
+                           }
+                        },
+        'storeProcessedBias':{'clob':
+                              {
+                                'default': False,
+                                'recipeOverride': True,
+                                'type': 'bool',
+                                'userOverride':True  
+                               }
+                              } ,
+        'storeProcessedFlat':{'clob':
+                              {
+                                'default': False,
+                                'recipeOverride': True,
+                                'type': 'bool',
+                                'userOverride':True  
+                               }
+                              } ,
+        
 }
