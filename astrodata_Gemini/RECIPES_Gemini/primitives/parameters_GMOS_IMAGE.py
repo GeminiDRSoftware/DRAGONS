441b8ee026ce69a28ba0bd3dfe166c8ddabe527c
Kyle Mede <kmede@gemini.edu>
2010-11-03 22:15:18 +0000

    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1151 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_IMAGE.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_IMAGE.py
@@ -1,51 +1,5 @@
-{   "showParams": {"test": 
-                   {
-                    "default": "GMOS_SPECT_SETTING",
-                    "recipeOverride": True,
-                    "uiLevel": "debug",
-                    "userOverride":True
-                    
-                    },
-                 
-                 "otherTest":
-                    {"default": "default",
-                     "userOverride":True}
-                },
-    "pause": {"message":
-              { "default":"Pausing Reduction by Control System Request",
-               "type":"string",
-               "a":"default comes first, the rest alphabetically",
-               "note1":"these are just test parameters...",
-               "note2":"pause doesn't need a 'message' parameter"
-               }
-              },
-    "makeFringeFrame": {
-                'outpref':
-                   {
-                    'default': '_fringe' , #value to be post pended to this primitives outputs
-                      'recipeOverride': True,
-                      'type': 'str',
-                      'userOverride':False 
-                    },
-                      
-                'fl_vardq':
-                   {
-                   'default': True , 
-                      'recipeOverride': True,
-                      'type': 'bool',
-                      'userOverride':True 
-                    },
-                    
-                'method':
-                   {
-                   'default': 'median' , 
-                      'recipeOverride': True,
-                      'type': 'str',
-                      'userOverride':False  
-                     },  
-                    },   
-    "fringeCorrect": {
-                'outpref':
+{  'fringeCorrect': {
+                'postpend':
                    {
                     'default': '_fringecorrected' , #value to be post pended to this primitives outputs
                       'recipeOverride': True,
@@ -75,5 +29,30 @@
                       'type': 'float',
                       'userOverride':False  
                      },  
-                    },             
+                    },   
+    'makeFringeFrame': {
+                'postpend':
+                   {
+                    'default': '_fringe' , #value to be post pended to this primitives outputs
+                      'recipeOverride': True,
+                      'type': 'str',
+                      'userOverride':False 
+                    },
+                      
+                'fl_vardq':
+                   {
+                   'default': True , 
+                      'recipeOverride': True,
+                      'type': 'bool',
+                      'userOverride':True 
+                    },
+                    
+                'method':
+                   {
+                   'default': 'median' , 
+                      'recipeOverride': True,
+                      'type': 'str',
+                      'userOverride':False  
+                     },  
+                    }, 
 }
