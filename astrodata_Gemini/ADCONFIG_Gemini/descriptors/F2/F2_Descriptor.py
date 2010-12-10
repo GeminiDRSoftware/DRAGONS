f45efe187419ece4e8e1163467f96b8d31a46fb7
Paul Hirst <phirst@gemini.edu>
2010-12-10 00:13:39 +0000

    stub version to pull in gemini generics for F2
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1254 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ astrodata_Gemini/ADCONFIG_Gemini/descriptors/F2/F2_Descriptor.py
@@ -0,0 +1,16 @@
+from astrodata import Lookups
+from astrodata import Descriptors
+
+from astrodata.Calculator import Calculator
+
+import GemCalcUtil 
+
+from StandardDescriptorKeyDict import globalStdkeyDict
+from StandardF2KeyDict import stdkeyDictF2
+from GEMINI_Descriptor import GEMINI_DescriptorCalc
+
+class F2_DescriptorCalc(GEMINI_DescriptorCalc):
+    
+    def __init__(self):
+        pass
+
