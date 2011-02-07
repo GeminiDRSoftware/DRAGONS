83c88dbc3ee61d676be13989c00b96d00945c46c
Kyle Mede <kmede@gemini.edu>
2011-02-07 22:27:59 +0000

    updated overscanSubtract to use gmosScience.overscanSubtract function
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1421 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/primitives_GMOS.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/primitives_GMOS.py
@@ -6,6 +6,7 @@ from astrodata.data import AstroData
 from gempy.instruments import geminiTools as gemt
 from gempy.instruments import gmosTools as gmost
 from gempy.science import geminiScience
+from gempy.science import gmosScience
 from primitives_GEMINI import GEMINIPrimitives
 from astrodata.adutils.gemutil import pyrafLoader
 import shutil
@@ -424,94 +425,18 @@ class GMOSPrimitives(GEMINIPrimitives):
         
         try:
             log.status('*STARTING* to subtract the overscan from the inputs')
-            # Writing input files to disk with prefixes onto their file 
-            # names so they can be deleted later easily 
-            clm = gemt.CLManager(rc)
-            #clm.LogCurParams()
             
-            # Parameters set by the gemt.CLManager or the definition 
-            # of the primitive 
-            clPrimParams = {
-              'inimages'    :clm.inputsAsStr(),
-              'gp_outpref'  :clm.uniquePrefix(),
-              # This returns a unique/temp log file for IRAF
-              'logfile'     :clm.logfile(),      
-              'fl_over'     :yes, 
-              # This is actually in the default dict but wanted to show it again
-              'Stdout'      :gemt.IrafStdout(), 
-              # This is actually in the default dict but wanted to show it again
-              'Stderr'      :gemt.IrafStdout(), 
-              # This is actually in the default dict but wanted to show it again
-              'verbose'     :yes                
-                          }
-            # Parameters from the Parameter file that are adjustable by the user
-            clSoftcodedParams = {
-               # pyrafBoolean converts the python booleans to pyraf ones
-               'fl_trim'    :gemt.pyrafBoolean(rc['fl_trim']),
-               'outpref'    :rc['postpend'],
-               'fl_vardq'   :gemt.pyrafBoolean(rc['fl_vardq'])
-                               }
-            # Grabbing the default params dict and updating it with 
-            # the two above dicts
-            clParamsDict = CLDefaultParamsDict('gireduce')
-            clParamsDict.update(clPrimParams)
-            clParamsDict.update(clSoftcodedParams)
+            log.debug('Calling gmosScience.overscanSubtract function')
             
-            # Logging the values in the soft and prim parameter dictionaries
-            log.fullinfo('\nParameters set by the CLManager or dictated by '+
-                         'the definition of the primitive:\n', 
-                         category='parameters')
-            gemt.LogDictParams(clPrimParams)
-            log.fullinfo('\nUser adjustable parameters in the parameters '+
-                         'file:\n', category='parameters')
-            gemt.LogDictParams(clSoftcodedParams)
+            adOuts = gmosScience.overscanSubtract(adIns=rc.getInputs(style='AD'), 
+                                        fl_trim=rc['fl_trim'], biassec=rc['biassec'], 
+                                        fl_vardq='AUTO', postpend=rc['postpend'], 
+                                        verbose=rc['logVerbose'])           
             
-            # Taking care of the biasec->nbiascontam param
-            if not rc['biassec'] == '':
-                nbiascontam = clm.nbiascontam()
-                clParamsDict.update({'nbiascontam':nbiascontam})
-                log.fullinfo('nbiascontam parameter was updated to = '+
-                             str(clParamsDict['nbiascontam']),'params')
-
-            log.debug('Calling the gireduce CL script for inputs '+
-                      clm.inputsAsStr())
-            
-            gemini.gmos.gireduce(**clParamsDict)
-
-            if gemini.gmos.gireduce.status:
-                log.critical('gireduce failed for '+rc.inputsAsStr()) 
-                raise GMOSException('gireduce failed')
-            else:
-                log.status('Exited the gireduce CL script successfully')
-         
-            # Renaming CL outputs and loading them back into memory, and 
-            # cleaning up the intermediate tmp files written to disk
-            clm.finishCL()
-            # Wrap up logging
-            i=0
-            for ad in rc.getOutputs(style='AD'):
-                # Verifying gireduce was actually ran on the file
-                if ad.phuGetKeyValue('GIREDUCE'): 
-                    # If gireduce was ran, then log the changes to the files 
-                    # it made
-                    log.fullinfo('File '+clm.preCLNames()[i]+
-                                 ' had its overscan subracted successfully')
-                    log.fullinfo('New file name is: '+ad.filename)
-                i = i+1
-                # Updating GEM-TLM and OVERSUB time stamps in the PHU
-                ad.historyMark(key='OVERSUB', stomp=False)  
+            log.status('gmosScience.overscanSubtract completed successfully')
                 
-                # Updating logger with new GEM-TLM time stamp value
-                log.fullinfo('************************************************'
-                             , category='header')
-                log.fullinfo('File = '+ad.filename, category='header')
-                log.fullinfo('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-                             , category='header')
-                log.fullinfo('PHU keywords updated/added:\n', 'header')
-                log.fullinfo('GEM-TLM = '+ad.phuGetKeyValue('GEM-TLM'), 
-                              category='header')
-                log.fullinfo('OVERSUB = '+ad.phuGetKeyValue('OVERSUB')+'\n', 
-                              category='header')
+            # Reporting the updated files to the reduction context
+            rc.reportOutput(adOuts)
             
             log.status('*FINISHED* subtracting the overscan from the '+
                        'input data')
@@ -653,189 +578,3 @@ class GMOSPrimitives(GEMINIPrimitives):
         
         yield rc
 
-def CLDefaultParamsDict(CLscript):
-    """
-    A function to return a dictionary full of all the 
-    default parameters for each CL script used so far in the Recipe System.
-    
-    """
-    # Loading and bringing the pyraf related modules into the name-space
-    pyraf, gemini, yes, no = pyrafLoader()
-    
-    # Ensuring that if a invalide CLscript was requested, that a critical
-    # log message be made and exception raised.
-    if (CLscript != 'gireduce') and (CLscript != 'giflat') and \
-    (CLscript != 'gmosaic') and (CLscript != 'gdisplay') :
-        log.critical('The CLscript '+CLscript+' does not have a default'+
-                     ' dictionary')
-        raise GMOSException('The CLscript '+CLscript+
-                              ' does not have a default'+' dictionary')
-    
-    if CLscript == 'gireduce':
-        defaultParams = {
-            'inimages'   :'',                # Input GMOS images 
-            'outpref'    :'DEFAULT',         # Prefix for output images
-            'outimages'  :'',                # Output images
-            'fl_over'    :no,                # Subtract overscan level
-            'fl_trim'    :no,                # Trim off the overscan section
-            'fl_bias'    :no,                # Subtract bias image
-            'fl_dark'    :no,                # Subtract (scaled) dark image
-            'fl_flat'    :no,                # Do flat field correction?
-            'fl_vardq'   :no,                # Create variance and data quality frames
-            'fl_addmdf'  :no,                # Add Mask Definition File? (LONGSLIT/MOS/IFU modes)
-            'bias'       :'',                # Bias image name
-            'dark'       :'',                # Dark image name
-            'flat1'      :'',                # Flatfield image 1
-            'flat2'      :'',                # Flatfield image 2
-            'flat3'      :'',                # Flatfield image 3
-            'flat4'      :'',                # Flatfield image 4
-            'key_exptime':'EXPTIME',         # Header keyword of exposure time
-            'key_biassec':'BIASSEC',         # Header keyword for bias section
-            'key_datasec':'DATASEC',         # Header keyword for data section
-            'rawpath'    :'',                # GPREPARE: Path for input raw images
-            'gp_outpref' :'g',               # GPREPARE: Prefix for output images
-            'sci_ext'    :'SCI',             # Name of science extension
-            'var_ext'    :'VAR',             # Name of variance extension
-            'dq_ext'     :'DQ',              # Name of data quality extension
-            'key_mdf'    :'MASKNAME',        # Header keyword for the Mask Definition File
-            'mdffile'    :'',                # MDF file to use if keyword not found
-            'mdfdir'     :'',                # MDF database directory
-            'bpm'        :'',                # Bad pixel mask
-            #'giandb'     :'default',        # Database with gain data
-            'sat'        :65000,             # Saturation level in raw images [ADU]
-            'key_nodcount':'NODCOUNT',       # Header keyword with number of nod cycles
-            'key_nodpix' :'NODPIX',          # Header keyword with shuffle distance
-            'key_filter' :'FILTER2',         # Header keyword of filter
-            'key_ron'    :'RDNOISE',         # Header keyword for readout noise
-            'key_gain'   :'GAIN',            # Header keyword for gain (e-/ADU)
-            'ron'        :3.5,               # Readout noise in electrons
-            'gain'       :2.2,               # Gain in e-/ADU
-            'fl_mult'    :no, #$$$$$$$$$     # Multiply by gains to get output in electrons
-            'fl_inter'   :no,                # Interactive overscan fitting?
-            'median'     :no,                # Use median instead of average in column bias?
-            'function'   :'chebyshev',       # Overscan fitting function
-            'nbiascontam':4, #$$$$$$$        # Number of columns removed from overscan region
-            'biasrows'   :'default',         # Rows to use for overscan region
-            'order'      :1,                 # Order of overscan fitting function
-            'low_reject' :3.0,               # Low sigma rejection factor in overscan fit
-            'high_reject':3.0,               # High sigma rejection factor in overscan fit
-            'niterate'   :2,                 # Number of rejection iterations in overscan fit
-            'logfile'    :'',                # Logfile
-            'verbose'    :yes,               # Verbose?
-            'status'     :0,                 # Exit status (0=good)
-            'Stdout'     :gemt.IrafStdout(),
-            'Stderr'     :gemt.IrafStdout()
-                           }
-    if CLscript == 'giflat':
-        defaultParams = { 
-            'inflats'    :'',            # Input flat field images
-            'outflat'    :'',            # Output flat field image
-            'normsec'    :'default',     # Image section to get the normalization.
-            'fl_scale'   :yes,           # Scale the flat images before combining?
-            'sctype'     :'mean',        # Type of statistics to compute for scaling
-            'statsec'    :'default',     # Image section for relative intensity scaling
-            'key_gain'   :'GAIN',        # Header keyword for gain (e-/ADU)
-            'fl_stamp'   :no,            # Input is stamp image
-            'sci_ext'    :'SCI',         # Name of science extension
-            'var_ext'    :'VAR',         # Name of variance extension
-            'dq_ext'     :'DQ',          # Name of data quality extension
-            'fl_vardq'   :no,            # Create variance and data quality frames?
-            'sat'        :65000,         # Saturation level in raw images (ADU)
-            'verbose'    :yes,           # Verbose output?
-            'logfile'    :'',            # Name of logfile
-            'status'     :0,             # Exit status (0=good)
-            'combine'    :'average',     # Type of combine operation
-            'reject'     :'avsigclip',   # Type of rejection in flat average
-            'lthreshold' :'INDEF',       # Lower threshold when combining
-            'hthreshold' :'INDEF',       # Upper threshold when combining
-            'nlow'       :0,             # minmax: Number of low pixels to reject
-            'nhigh'      :1,             # minmax: Number of high pixels to reject
-            'nkeep'      :1,             # avsigclip: Minimum to keep (pos) or maximum to reject (neg)
-            'mclip'      :yes,           # avsigclip: Use median in clipping algorithm?
-            'lsigma'     :3.0,           # avsigclip: Lower sigma clipping factor
-            'hsigma'     :3.0,           # avsigclip: Upper sigma clipping factor
-            'sigscale'   :0.1,           # avsigclip: Tolerance for clipping scaling corrections
-            'grow'       :0.0,           # minmax or avsigclip: Radius (pixels) for neighbor rejection
-            'gp_outpref' :'g',           # Gprepare prefix for output images
-            'rawpath'    :'',            # GPREPARE: Path for input raw images
-            'key_ron'    :'RDNOISE',     # Header keyword for readout noise
-            'key_datasec':'DATASEC',     # Header keyword for data section
-            #'giandb'     :'default',    # Database with gain data
-            'bpm'        :'',            # Bad pixel mask
-            'gi_outpref' :'r',           # Gireduce prefix for output images
-            'bias'       :'',            # Bias calibration image
-            'fl_over'    :no,            # Subtract overscan level?
-            'fl_trim'    :no,            # Trim images?
-            'fl_bias'    :no,            # Bias-subtract images?
-            'fl_inter'   :no,            # Interactive overscan fitting?
-            'nbiascontam':4, #$$$$$$$    # Number of columns removed from overscan region
-            'biasrows'   :'default',     # Rows to use for overscan region
-            'key_biassec':'BIASSEC',     # Header keyword for overscan image section
-            'median'     :no,            # Use median instead of average in column bias?
-            'function'   :'chebyshev',   # Overscan fitting function.
-            'order'      :1,             # Order of overscan fitting function.
-            'low_reject' :3.0,           # Low sigma rejection factor.
-            'high_reject':3.0,           # High sigma rejection factor.
-            'niterate'   :2,             # Number of rejection iterations.
-            'Stdout'      :gemt.IrafStdout(),
-            'Stderr'      :gemt.IrafStdout()
-                       }      
-    if CLscript == 'gmosaic':
-        defaultParams = { 
-            'inimages'   :'',                     # Input GMOS images 
-            'outimages'  :'',                     # Output images
-            'outpref'    :'DEFAULT',              # Prefix for output images
-            'fl_paste'   :no,                     # Paste images instead of mosaic
-            'fl_vardq'   :no,                     # Propagate the variance and data quality planes
-            'fl_fixpix'  :no,                     # Interpolate across chip gaps
-            'fl_clean'   :yes ,                   # Clean imaging data outside imaging field
-            'geointer'   :'linear',               # Interpolant to use with geotran
-            'gap'        :'default',              # Gap between the CCDs in unbinned pixels
-            'bpmfile'    :'gmos$data/chipgaps.dat',   # Info on location of chip gaps ## HUH??? Why is variable called 'bpmfile' if it for chip gaps??
-            'statsec'    :'default',              # Statistics section for cleaning
-            'obsmode'    :'IMAGE',                # Value of key_obsmode for imaging data
-            'sci_ext'    :'SCI',                  # Science extension(s) to mosaic, use '' for raw data
-            'var_ext'    :'VAR',                  # Variance extension(s) to mosaic
-            'dq_ext'     :'DQ',                   # Data quality extension(s) to mosaic
-            'mdf_ext'    :'MDF',                  # Mask definition file extension name
-            'key_detsec' :'DETSEC',               # Header keyword for detector section
-            'key_datsec' :'DATASEC',              # Header keyword for data section
-            'key_ccdsum' :'CCDSUM',               # Header keyword for CCD binning
-            'key_obsmode':'OBSMODE',              # Header keyword for observing mode
-            'logfile'    :'',                     # Logfile
-            'fl_real'    :no,                     # Convert file to real before transforming
-            'verbose'    :yes,                    # Verbose
-            'status'     :0,                      # Exit status (0=good)
-            'Stdout'     :gemt.IrafStdout(),
-            'Stderr'     :gemt.IrafStdout()
-                       }
-    
-    if CLscript == 'gdisplay':
-        defaultParams = { 
-            'image'         :'',                # GMOS image to display, can use number if current UT
-            'frame'         :1,                 # Frame to write to
-            'output'        :'',                # Save pasted file to this name if not blank
-            'fl_paste'      :'no',              # Paste images to one for imexamine
-            'fl_bias'       :'no',              # Rough bias subtraction
-            'rawpath'       :'',                # Path for input image if not included in name
-            'gap'           :'default',         # Size of the gap between the CCDs (in pixels)
-            'z1'            :0.0,               # Lower limit if not autoscaling
-            'z2'            :0.0,               # Upper limit if not autoscaling
-            'fl_sat'        :'no',              # Flag saturated pixels
-            'fl_imexam'     :'yes',             # If possible, run imexam
-            'signal'        :'INDEF',           # Flag pixels with signal above this limit
-            'sci_ext'       :'SCI',             # Name of extension(s) to display
-            'observatory'   :'',                # Observatory (gemini-north or gemini-south)
-            'prefix'        :'auto',            # File prefix, (N/S)YYYYMMDDS if not auto
-            'key_detsec'    :'DETSEC',          # Header keyword for detector section
-            'key_datasec'   :'DATASEC',         # Header keyword for data section
-            'key_ccdsum'    :'CCDSUM',          # Header keyword for CCD binning
-            'gaindb'        :'default',         # Database with gain data
-            'verbose'       :yes,               # Verbose
-            'status'        :0,                 # Exit status (0=good)
-            'Stdout'        :gemt.IrafStdout(), 
-            'Stderr'        :gemt.IrafStdout()  
-                       }
-                                  
-    return defaultParams    
-    #$$$$$$$$$$$$$$$$$$$$$$$ END OF KYLES NEW STUFF $$$$$$$$$$$$$$$$$$$$$$$$$$
