02ce9fda3c034a992fbc9e2fc23e97323f801d0c
Emma Hogan <ehogan@gemini.edu>
2011-02-08 19:19:24 +0000

    Removed key_numexpos in favour of key_coadds
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1428 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/descriptors/MICHELLE/StandardMICHELLEKeyDict.py
+++ astrodata_Gemini/ADCONFIG_Gemini/descriptors/MICHELLE/StandardMICHELLEKeyDict.py
@@ -1,15 +1,14 @@
 stdkeyDictMICHELLE = {
     "key_central_wavelength":"GRATPOS",
+    "key_coadds":"NUMEXPOS",
     "key_disperser":"GRATNAME",
     "key_dispersion":"GRATDISP",
     "key_exposure":"EXPOSURE",
     "key_filter":"FILTER",
     "key_focal_plane_mask":"SLITNAME",
     "key_numext":"NUMEXT",
-    "key_numexpos":"NUMEXPOS",
     "key_observation_mode":"MODE",
     "key_pixel_scale":"PIXELSIZ",
-    "key_ut_date":"UTDATE",
     "key_read_mode":"MODE",
-    "key_coadds":"NUMEXPOS",
+    "key_ut_date":"UTDATE",
     }
