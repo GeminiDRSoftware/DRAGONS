c41eab4237ff7a5e94105f37dacec4651a07675d
Emma Hogan <ehogan@gemini.edu>
2011-02-08 22:59:06 +0000

    Removed decker, grating, prism and slit entries, since they are already in StandardDescriptorKeyDict.py
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1437 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/descriptors/GNIRS/StandardGNIRSKeyDict.py
+++ astrodata_Gemini/ADCONFIG_Gemini/descriptors/GNIRS/StandardGNIRSKeyDict.py
@@ -1,12 +1,8 @@
 stdkeyDictGNIRS = {
     "key_bias":"DETBIAS",
     "key_central_wavelength":"GRATWAVE",
-    "key_decker":"DECKER",
     "key_filter1":"FILTER1",
     "key_filter2":"FILTER2",
-    "key_grating":"GRATING",
     "key_lnrs":"LNRS",
     "key_ndavgs":"NDAVGS",
-    "key_prism":"PRISM",
-    "key_slit":"SLIT",
     }
