21069d72bd48246a87a171b2c625d170b0b6c271
Kyle Mede <kmede@gemini.edu>
2011-02-23 22:12:06 +0000

    dictionary to hold descriptions of each descriptor for use with mkCalculatoryInterface.py
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1532 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ astrodata_Gemini/ADCONFIG_Gemini/descriptors/descriptorDescriptionDict.py
@@ -0,0 +1,7 @@
+
+descriptorDescDict = {
+                      'airmass':'description of what it does',
+                      
+                      'descriptor_name2':'description of what it does',
+                      
+                      }
