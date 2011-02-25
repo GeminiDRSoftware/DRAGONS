6b82596614d276f5b47e4334c0180bd17b0ea61f
Kyle Mede <kmede@gemini.edu>
2011-02-25 13:57:46 +0000

    upgraded to mimick the new logLevel per prim ability, postpend->suffix and added the params to the docstrings, and removed the display prim as it now lives in primitives_GMOS
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1552 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/primitives_GMOS_SPECT.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/primitives_GMOS_SPECT.py
@@ -32,44 +32,22 @@ class GMOS_SPECTPrimitives(GMOSPrimitives):
     def init(self, rc):
         GMOSPrimitives.init(self, rc)
         return rc
-    
-    def display(self, rc):
-        
-        pyraf,gemini,yes,no = pyrafLoader(rc)
-        
-        from adutils.future import gemDisplay
-        from pyraf import iraf
-        from pyraf.iraf import images
-        
-        iraf.set(stdimage='imtgmos')
-        ds = gemDisplay.getDisplayService()
-        
-        for i in range(0, len(rc.inputs)):   
-            inputRecord = rc.inputs[i]
-            gemini.gmos.gdisplay( inputRecord.filename, i+1, fl_imexam=iraf.no,
-                Stdout = rc.getIrafStdout(), Stderr = rc.getIrafStderr() )
-            # this version had the display id conversion code which we'll need to redo
-            # code above just uses the loop index as frame number
-            #gemini.gmos.gdisplay( inputRecord.filename, ds.displayID2frame(rq.disID), fl_imexam=iraf.no,
-            #    Stdout = coi.getIrafStdout(), Stderr = coi.getIrafStderr() )
-        yield rc
-    
-    #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-       
-#$$$$$$$$$$$$$$$$$$$$ NEW STUFF BY KYLE FOR: PREPARE $$$$$$$$$$$$$$$$$$$$$
-    '''
-    These are the primitives for 'prepare' that are just to check how the general primitives in 
-    primitives_GEMINI will work for a GMOS_SPEC image and its MDF file.  Since these are just to check 
-    how it will work, they are operational but not thuroughly tested or robust and will be needing more
-    work when we start writing the rest of the prims for GMOS_SPEC.  Because of this, sorry for the lack
-    of commenting if you end up reading this code.
-    '''
 
     def standardizeInstrumentHeaders(self,rc):
         '''
-        makes the changes and additions to the headers of the input files that are instrument specific
-        '''
+        This primitive is called by standardizeHeaders to makes the changes and 
+        additions to the headers of the input files that are GMOS_SPEC 
+        specific.
         
+        :param logLevel: Verbosity setting for log messages to the screen.
+        :type logLevel: int. 
+                          This value can be set for each primitive individually 
+                          in a recipe only (ie. not in the parameter file). 
+                          If no value is specified during the recipe, the value 
+                          set during the call to reduce or its default (2) will 
+                          be used.
+        '''
+        log = gemLog.getGeminiLog(logLevel=int(rc['logLevel']))
         try: 
             for ad in rc.getInputs(style="AD"): 
                 log.status('calling stdInstHdrs','status')
@@ -82,12 +60,29 @@ class GMOS_SPECTPrimitives(GMOSPrimitives):
             raise 
         
         yield rc
-    #-----------------------------------------------------------------------
+
     def attachMDF(self,rc):
         '''
-        this works to add an MDF if there is a MASKNAME in the images PHU only.  
+        This primitive is used to add an MDF if there is a MASKNAME in the   
+        images PHU only. It is called by the primitive standardizeStructure
+        during the prepare recipe if the parameter addMDF=True.
+        ***********************************************
         will be upgraded later, early testing complete
+        ***********************************************
+        
+        :param suffix: Value to be post pended onto each input name(s) to 
+                         create the output name(s).
+        :type suffix: string
+        
+        :param logLevel: Verbosity setting for log messages to the screen.
+        :type logLevel: int. 
+                          This value can be set for each primitive individually 
+                          in a recipe only (ie. not in the parameter file). 
+                          If no value is specified during the recipe, the value 
+                          set during the call to reduce or its default (2) will 
+                          be used.
         '''
+        log = gemLog.getGeminiLog(logLevel=int(rc['logLevel']))
         try:           
             for ad in rc.getInputs(style ='AD'):
                 infilename = ad.filename
@@ -109,7 +104,7 @@ class GMOS_SPECTPrimitives(GMOSPrimitives):
                 ad.append(moredata=admdf)  
                 log.status(ad.info(),'status')
                 
-                ad.filename=gemt.fileNameUpdater(ad.filename,postpend=rc["suffix"], strip=False)
+                ad.filename=gemt.fileNameUpdater(ad.filename,suffix=rc["suffix"], strip=False)
                 rc.reportOutput(ad)
                 
                 log.status('finished adding the MDF','status')
@@ -117,11 +112,21 @@ class GMOS_SPECTPrimitives(GMOSPrimitives):
             log.critical("Problem preparing the image.", 'critical')
             raise 
         yield rc
-    #------------------------------------------------------------------------
+
     def validateInstrumentData(self,rc):
         '''
-        instrument specific validations for the input file
+        This primitive is called by validateData to validate the GMOS_SPEC 
+        specific data checks for all input files.
+        
+        :param logLevel: Verbosity setting for log messages to the screen.
+        :type logLevel: int. 
+                          This value can be set for each primitive individually 
+                          in a recipe only (ie. not in the parameter file). 
+                          If no value is specified during the recipe, the value 
+                          set during the call to reduce or its default (2) will 
+                          be used.
         '''
+        log = gemLog.getGeminiLog(logLevel=int(rc['logLevel']))
         try:        
             for ad in rc.getInputs(style="AD"):
                 log.status('validating data for file = '+ad.filename,'status')
@@ -132,7 +137,4 @@ class GMOS_SPECTPrimitives(GMOSPrimitives):
             log.critical("Problem preparing the image.",'critical')
             raise 
         yield rc
-   #----------------------------------------------------------------------------------
-#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Prepare primitives end here $$$$$$$$$$$$$$$$$$$$$$$$$$$$      
-#$$$$$$$$$$$$$$$$$$$$$$$ END OF KYLES NEW STUFF $$$$$$$$$$$$$$$$$$$$$$$$$$
-        
+
