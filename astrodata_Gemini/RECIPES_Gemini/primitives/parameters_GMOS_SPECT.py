bc6f35525293fd92fbada0c6141bdacc691edcba
Kyle Mede <kmede@gemini.edu>
2011-02-25 13:56:22 +0000

    removed showParams and pause primitive params as those prims don't exist in primitives_GMOS_SPEC, and changed parameter postpend->suffix
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1551 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_SPECT.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_SPECT.py
@@ -1,24 +1,4 @@
-{ "showParams": {"test": 
-                 {
-                    "default": "GMOS_IMAGE_SETTING",
-                    "recipeOverride": True,
-                    "uiLevel": "debug",
-                    "userOverride":True
-                    
-                 },
-                 
-                 "otherTest":
-                    {"default": "default",
-                     "userOverride":True}
-                },
-  "pause": {"message":
-            { "default":"Pausing Reduction by Control System Request",
-              "type":"string",
-              "a":"default comes first, the rest alphabetically",
-              "note1":"these are just test parameters...",
-              "note2":"pause doesn't need a 'message' parameter"
-            }
-           },
+{ 
     "attachMDF":{"suffix":
                           {
                            'default': '_mdf' , #value to be post pended to this primitives outputs
