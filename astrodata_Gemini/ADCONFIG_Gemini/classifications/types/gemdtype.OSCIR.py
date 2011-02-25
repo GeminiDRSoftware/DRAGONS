5fa1813e769c44a4a9a8dd9f56143832b5b7db4f
Paul Hirst <phirst@gemini.edu>
2011-02-25 21:39:53 +0000

    oscir can be upper or lower case
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1573 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/gemdtype.OSCIR.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/gemdtype.OSCIR.py
@@ -3,6 +3,6 @@ class OSCIR(DataClassification):
     name="OSCIR"
     usage = "Applies to datasets from the OSCIR instrument"
     parent = "GEMINI"
-    requirement = PHU(INSTRUME='oscir')
+    requirement = OR(PHU(INSTRUME='oscir'), PHU(INSTRUME='OSCIR'))
 
 newtypes.append(OSCIR())
