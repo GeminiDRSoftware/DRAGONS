8080d2c483da6eaaadf492b153a76438db66b54e
Craig Allen <callen@gemini.edu>
2011-03-07 20:36:50 +0000

    improved design
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1609 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/Errors.py
+++ astrodata/Errors.py
@@ -5,29 +5,33 @@
 class Error(Exception):
     """Base class for exceptions in this module."""
     pass
-
+    
+    
+    def __str__(self):
+        return self.message
+        
+    def __init__(self, message = None):
+        if message != None:
+            self.message = message
+            
 class CalcError(Error):
     """
     Exception raised for instances when the keyword is found but the value of
     the keyword cannot be handled by the descriptor code
     """
-
-    def __init__(self, message = "Keyword value outside allowed range"):
-        self.message = message
-
+    message = "Keyword value outside allowed range"
+    
 class ExistError(Error):
     """
     Exception raised for instances when the keyword doesn't exist and the
     value hasn't yet been decided
     """
 
-    def __init__(self, message = "Keyword value not yet determined for this instrument"):
-        self.message = message
+    message = "Keyword value not yet determined for this instrument"
 
 class DescriptorListError(Error):
     """
     Exception raised for instances where the asList=True parameter should be
     used, but isn't
     """
-    def __init__(self, message = "Please use asList=True to obtain a list"):
-        self.message = message
+    message = "Please use asList=True to obtain a list"
