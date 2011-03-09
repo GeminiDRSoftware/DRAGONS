7c1802451e01718f3c3ca727806d842473096671
Emma Hogan <ehogan@gemini.edu>
2011-03-09 23:02:34 +0000

    Added code to create descriptor doc strings
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1633 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/descriptors/descriptorDescriptionDict.py
+++ astrodata_Gemini/ADCONFIG_Gemini/descriptors/descriptorDescriptionDict.py
@@ -1,7 +1,272 @@
+# Use the descriptorDescDict dictionary to fill in the doc string for a
+# descriptor that requires more details than the standard doc string as
+# defined in mkCalculatorInterface:
+#
+#     Return the <descriptor> value
+#     :param dataset: the data set
+#     :type dataset: AstroData
+#     :rtype: string
+#     :return: the <descriptor> for the observation
+#
+# Add to this dictionary using the format <descriptor>:<doc string>,
 
 descriptorDescDict = {
-                      'airmass':'description of what it does',
-                      
-                      'descriptor_name2':'description of what it does',
-                      
+    # central_wavelength
+    'central_wavelength':'Return the central_wavelength value\n' +
+    '        :param dataset: the data set\n' +
+    '        :type dataset: AstroData\n' +
+    '        :param asMicrometers: set to True to return the ' +
+    'central_wavelength \n                              value in units of ' +
+    'Micrometers\n' +
+    '        :type asMicrometers: Python boolean\n' +
+    '        :param asNanometers: set to True to return the ' +
+    'central_wavelength \n                             value in units of ' +
+    'Nanometers\n' +
+    '        :type asNanometers: Python boolean\n' +
+    '        :param asAngstroms: set to True to return the ' +
+    'central_wavelength \n                            value in units of ' +
+    'Angstroms\n' +
+    '        :type asAngstroms: Python boolean\n' +
+    '        :param asDict: set to True to return a dictionary, where the ' +
+    'number of \n                       dictionary elements equals the ' +
+    'number of pixel data \n                       extensions in the image. ' +
+    'The key of the dictionary is \n                       an (EXTNAME, ' +
+    'EXTVER) tuple, if available. Otherwise, \n                       the ' +
+    'key is the integer index of the extension.\n' + \
+    '        :type asDict: Python boolean\n' + \
+    '        :rtype: dictionary containing one or more float(s)\n' + \
+    '        :return: the central wavelength (in meters by default) of the ' +
+    '\n                 observation',
+    # data_section
+    'data_section':'Return the data_section value\n' +
+    '        :param dataset: the data set\n' +
+    '        :type dataset: AstroData\n' +
+    '        :param pretty: set to True to return a human meaningful ' +
+    'data_section \n                       value in the form [x1:x2,y1:y2] ' +
+    'that uses 1-based \n                       indexing\n' +
+    '        :type pretty: Python boolean\n' +
+    '        :param asDict: set to True to return a dictionary, where the ' +
+    'number of \n                       dictionary elements equals the ' +
+    'number of pixel data \n                       extensions in the image. ' +
+    'The key of the dictionary is \n                       an (EXTNAME, ' +
+    'EXTVER) tuple, if available. Otherwise, \n                       the ' +
+    'key is the integer index of the extension.\n' + \
+    '        :type asDict: Python boolean\n' + \
+    '        :rtype: dictionary containing one or more tuple(s) that use ' +
+    '0-based \n                indexing in the form ' + \
+    '(x1 - 1, x2 - 1, y1 - 1, y2 - 1) as \n                default, or one ' +
+    'or more string(s) that use 1-based indexing in \n                the ' +
+    'form [x1:x2,y1:y2] if pretty=True, where x1, x2, y1 and y2 ' +
+    '\n                are integers.\n' + \
+    '        :return: the section of the data of the observation',
+    # detector_section
+    'detector_section':'Return the detector_section value\n' +
+    '        :param dataset: the data set\n' +
+    '        :type dataset: AstroData\n' +
+    '        :param pretty: set to True to return a human meaningful \n' +
+    '                       detector_section value in the form ' +
+    '[x1:x2,y1:y2] that \n                       uses 1-based indexing\n' +
+    '        :type pretty: Python boolean\n' +
+    '        :param asDict: set to True to return a dictionary, where the ' +
+    'number of \n                       dictionary elements equals the ' +
+    'number of pixel data \n                       extensions in the image. ' +
+    'The key of the dictionary is \n                       an (EXTNAME, ' +
+    'EXTVER) tuple, if available. Otherwise, \n                       the ' +
+    'key is the integer index of the extension.\n' + \
+    '        :type asDict: Python boolean\n' + \
+    '        :rtype: dictionary containing one or more tuple(s) that use ' +
+    '0-based \n                indexing in the form ' + \
+    '(x1 - 1, x2 - 1, y1 - 1, y2 - 1) as \n                default, or one ' +
+    'or more string(s) that use 1-based indexing in \n                the ' +
+    'form [x1:x2,y1:y2] if pretty=True, where x1, x2, y1 and y2 ' +
+    '\n                are integers.\n' + \
+    '        :return: the section of the data of the observation',
+    # dispersion
+    'dispersion':'Return the dispersion value\n' +
+    '        :param dataset: the data set\n' +
+    '        :type dataset: AstroData\n' +
+    '        :param asMicrometers: set to True to return the ' +
+    'dispersion \n                              value in units of ' +
+    'Micrometers\n' +
+    '        :type asMicrometers: Python boolean\n' +
+    '        :param asNanometers: set to True to return the ' +
+    'dispersion \n                             value in units of ' +
+    'Nanometers\n' +
+    '        :type asNanometers: Python boolean\n' +
+    '        :param asAngstroms: set to True to return the ' +
+    'dispersion \n                            value in units of ' +
+    'Angstroms\n' +
+    '        :type asAngstroms: Python boolean\n' +
+    '        :param asDict: set to True to return a dictionary, where the ' +
+    'number of \n                       dictionary elements equals the ' +
+    'number of pixel data \n                       extensions in the image. ' +
+    'The key of the dictionary is \n                       an (EXTNAME, ' +
+    'EXTVER) tuple, if available. Otherwise, \n                       the ' +
+    'key is the integer index of the extension.\n' + \
+    '        :type asDict: Python boolean\n' + \
+    '        :rtype: dictionary containing one or more float(s)\n' + \
+    '        :return: the dispersion (in meters per pixel by default) of ' +
+    'the \n                 observation',
                       }
+
+# Use the returnTypeDict dictionary to change the return type of a descriptor
+# when using the standard doc string. The default return type is 'string'. Add
+# to this dictionary using the format <descriptor>:<return type>,
+
+returnTypeDict = {
+    'airmass':'float',
+    'azimuth':'float',
+    'cass_rotator_pa':'float',
+    'central_wavelength':'float',
+    'coadds':'integer',
+    'dec':'float',
+    'detector_x_bin':'integer',
+    'detector_y_bin':'integer',
+    'dispersion_axis':'integer',
+    'elevation':'float',
+    'exposure_time':'float',
+    'gain':'float',
+    'mdf_row_id':'integer',
+    'nod_count':'integer',
+    'nod_pixels':'integer',
+    'non_linear_level':'integer',
+    'pixel_scale':'float',
+    'ra':'float',
+    'read_noise':'float',
+    'saturation_level':'integer',
+    'wavelength_reference_pixel':'float',
+    'x_offset':'float',
+    'y_offset':'float',
+                 }
+
+# Use the detailedNameDict dictionary to use a more detailed description of
+# the descriptor in the return field (include units if applicable) when using
+# the standard doc string. The default is the descriptor name. Add to this
+# dictionary using the format <descriptor>:<detailed descriptor description>,
+
+detailedNameDict = {
+    'airmass':'mean airmass of the observation',
+    'amp_read_area':'composite string containing the name of the detector\n' +
+        '                 amplifier (ampname) and the readout area of the ' +
+        'CCD (detsec) \n                 used for the observation',
+    'azimuth':'azimuth (in degrees between 0 and 360) of the observation',
+    'camera':'camera used for the observation',
+    'cass_rotator_pa':'cassegrain rotator position angle (in degrees between '+
+        '0 \n                 and 360) of the observation',
+    'coadds':'number of coadds used for the observation',
+    'data_label':'data label of the observation',
+    'dec':'declination (in decimal degrees) of the observation',
+    'decker':'decker used for the observation',
+    'detector_x_bin':'binning of the x-axis of the detector used for the ' +
+        '\n                 observation',
+    'detector_y_bin':'binning of the y-axis of the detector used for the ' +
+        '\n                 observation',
+    'disperser':'disperser used for the observation',
+    'dispersion_axis':'dispersion axis (x = 1; y = 2; z = 3) of the ' +
+        'observation',
+    'elevation':'elevation (in degrees) of the observation',
+    'exposure_time':'total exposure time (in seconds) of the observation',
+    'filter_name':'unique, sorted filter name idenifier string used for ' +
+    'the \n                 observation',
+    'focal_plane_mask':'focal plane mask used for the observation',
+    'gain':'gain (in electrons per ADU) of the observation',
+    'grating':'grating used for the observation',
+    'gain_setting':'gain setting of the observation',
+    'instrument':'instrument used for the observation',
+    'local_time':'local time (in HH:MM:SS.S) at the start of the observation',
+    'mdf_row_id':'corresponding reference row in the MDF',
+    'nod_count':'number of nod and shuffle cycles in the nod and shuffle \n' +
+        '                 observation',
+    'nod_pixels':'number of pixel rows the charge is shuffled by in the ' +
+        'nod \n                 and shuffle observation',
+    'non_linear_level':'non linear level in the raw images (in ADU) of the ' +
+        '\n                 observation',
+    'object':'name of the target object observed',
+    'observation_class':'class (either \'science\', \'progCal\', ' +
+        '\'partnerCal\', \'acq\', \n                 \'acqCal\' or ' +
+        '\'dayCal\') of the observation',
+    'observation_epoch':'epoch (in years) at the start of the observation',
+    'observation_id':'ID (e.g., GN-2011A-Q-123-45) of the observation',
+    'observation_type':'type (either \'OBJECT\', \'DARK\', \'FLAT\', ' +
+    '\'ARC\', \'BIAS\' or \n                 \'MASK\') of the observation',
+    'pixel_scale':'pixel scale (in arcsec per pixel) of the observation',
+    'prism':'prism used for the observation',
+    'program_id':'Gemini program ID (e.g., GN-2011A-Q-123) of the \n' +
+    '                 observation',
+    'pupil_mask':'pupil mask used for the observation',
+    'qa_state':'quality assessment state (either \'Undefined\', \'Pass\', \n' +
+        '                 \'Usable\', \'Fail\' or \'CHECK\') of the ' +
+        'observation',
+    'ra':'Right Ascension (in decimal degrees) of the observation',
+    'raw_bg':'raw background (either \'20-percentile\', \'50-percentile\', ' +
+        '\n                 \'80-percentile\' or \'Any\') of the observation',
+    'raw_cc':'raw cloud cover (either \'50-percentile\', \'70-percentile\', ' +
+        '\n                 \'80-percentile\', \'90-percentile\' or ' +
+        '\'Any\') of the observation',
+    'raw_iq':'raw image quality (either \'20-percentile\', \n' +
+        '                 \'70-percentile\', \'85-percentile\' or ' +
+        '\'Any\') of the observation',
+    'raw_wv':'raw water vapour (either \'20-percentile\', \n' +
+        '                 \'50-percentile\', \'80-percentile\' or \'Any\') ' +
+        'of the observation',
+    'read_mode':'read mode (either \'Very Faint Objects\', \n' +
+        '                 \'Faint Object(s)\', \'Medium Object\', ' +
+        '\'Bright Object(s)\', \n                 \'Very Bright Object\', ' +
+        '\'Low Background\', \'Medium Background\', \n                 ' +
+        '\'High Background\' or \'Invalid\') of the observation',
+    'read_noise':'estimated readout noise (in electrons) of the observation',
+    'read_speed_setting':'read speed setting (either \'fast\' or \'slow\') ' +
+        'of the \n                 observation',
+    'saturation_level':'saturation level in the raw images (in ADU) of the ' +
+        '\n                 observation',
+    'slit':'slit used for the observation',
+    'telescope':'telescope used for the observation',
+    'ut_date':'UT date at the start of the observation',
+    'ut_time':'UT time at the start of the observation',
+    'wavefront_sensor':'wavefront sensor (either \'AOWFS\', \'OIWFS\', ' +
+        '\'PWFS1\', \n                 \'PWFS2\', some combination in ' +
+        'alphebetic order separated with \n                 an ampersand or ' +
+        'None) used for the observation',
+    'wavelength_reference_pixel':'reference pixel of the central wavelength ' +
+        'of the \n                 observation',
+    'well_depth_setting':'well depth setting (either \'Shallow\', \'Deep\' ' +
+        'or \n                 \'Invalid\') of the observation',
+    'x_offset':'x offset of the observation',
+    'y_offset':'y offset of the observation',
+                   }
+
+# Use the asDictArgDict dictionary to change the standard doc string of a
+# descriptor to include the asDict argument description for those descriptors
+# that access keywords in the headers of the pixel data extensions. The
+# default return type is 'no'. Add to this dictionary using the format
+# <descriptor>:'yes',
+
+asDictArgDict = {
+    'amp_read_area':'yes',
+    'data_section':'yes',
+    'detector_x_bin':'yes',
+    'detector_y_bin':'yes',
+    'gain':'yes',
+    'mdf_row_id':'yes',
+    'nod_count':'yes',
+    'nod_pixels':'yes',
+    'read_noise':'yes',
+    'wavelength_reference_pixel':'yes',
+                }
+
+# Use the stripIDArgDict dictionary to change the standard doc string of a
+# descriptor to include the stripID and pretty argument descriptions for those
+# descriptors that return values with a Gemini ID associated with them. The
+# default return type is 'no'. Add to this dictionary using the format
+# <descriptor>:'yes',
+
+stripIDArgDict = {
+    'decker':'yes',
+    'disperser':'yes',
+    'filter_name':'yes',
+    'focal_plane_mask':'yes',
+    'grating':'yes',
+    'grating':'yes',
+    'prism':'yes',
+    'slit':'yes',
+                 }
