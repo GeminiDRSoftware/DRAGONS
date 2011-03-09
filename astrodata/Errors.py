5a2f34cd012b55fcf124f0a440eaf17f4fd40dd1
Emma Hogan <ehogan@gemini.edu>
2011-03-09 23:01:01 +0000

    Changed DescriptorListError to DescriptorDictError
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1630 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/Errors.py
+++ astrodata/Errors.py
@@ -4,23 +4,22 @@
 
 class Error(Exception):
     """Base class for exceptions in this module."""
-    pass
-    
-    
-    def __str__(self):
-        return self.message
-        
     def __init__(self, message = None):
         if message != None:
             self.message = message
             
+    def __str__(self):
+        return self.message
+        
     def __repr__(self):
         return self.__class__.__name__+"(%s)" % repr(self.message)
             
 class CalcError(Error):
     """
-    Exception raised for instances when the keyword is found but the value of
-    the keyword cannot be handled by the descriptor code
+    Exception raised for instances when the keyword required for calculation
+    within a descriptor function is found but the value of the keyword has an
+    invalid value (as determined by the validation code within the descriptor
+    function)
     """
     message = "Keyword value outside allowed range"
     
@@ -32,51 +31,42 @@ class ExistError(Error):
 
     message = "Keyword value not yet determined for this instrument"
 
-class DescriptorListError(Error):
+class DescriptorDictError(Error):
     """
-    Exception raised for instances where the asList=True parameter should be
+    Exception raised for instances where the asDict=True parameter should be
     used, but isn't
     """
-    message = "Please use asList=True to obtain a list"
+    message = "Please use asDict=True to obtain a dictionary"
 
 class PrimitiveError(Error):
     """
     Exception raised for general errors inside of a primitive.
     """
-    message = 'An error occurred during a primitive'
-    
+    message = "An error occurred during a primitive"
+
 class ScienceError(Error):
     """
-    Exception raised for general errors inside of a user level 'science' function.
+    Exception raised for general errors inside of a user level 'science'
+    function.
     """
-    message = 'An error occurred during a user level "science" function'   
+    message = "An error occurred during a user level 'science' function"
 
 class ToolboxError(Error):
     """
-    Exception raised for general errors inside of a "toolbox" function.
+    Exception raised for general errors inside of a 'toolbox' function.
     """
-    message = 'An error occurred during a "toolbox" function'
+    message = "An error occurred during a 'toolbox' function"
     
 class UndefinedKeyError(Error):
     """
-    Exception raised for errors when a PHU key has a value that is 
+    Exception raised for errors when a PHU keyword has a value that is 
     pyfits.core.Undefined
     """
-    message = 'Key value was undefined, so returning None'
+    message = "Keyword value undefined, so returning None"
 
 class EmptyKeyError(Error):
     """
-    Exception raised for errors when a PHU key was found to but was empty or ' '.
+    Exception raised for errors when a PHU keyword was found to but was empty
+    or ' '.
     """    
-    message = 'Key value was found but was empty, so returning None'
-
-    
-       
-    
-    
-    
-    
-    
-    
-    
-    
+    message = "Keyword value found but empty, so returning None"
