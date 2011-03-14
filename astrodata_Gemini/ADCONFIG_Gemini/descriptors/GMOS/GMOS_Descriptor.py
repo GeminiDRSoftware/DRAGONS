c8552cface38709aa9a80458f3bd55e716385887
Emma Hogan <ehogan@gemini.edu>
2011-03-14 19:33:30 +0000

    Moved the 'utility member functions' from GMOS_Descriptor.py to GemCalcUtil.py
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1658 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/descriptors/GMOS/GMOS_Descriptor.py
+++ astrodata_Gemini/ADCONFIG_Gemini/descriptors/GMOS/GMOS_Descriptor.py
@@ -1,8 +1,6 @@
 from astrodata import Lookups
 from astrodata import Descriptors
 from astrodata import Errors
-import math
-import re
 
 from astrodata.Calculator import Calculator
 
@@ -387,7 +385,8 @@ class GMOS_DescriptorCalc(GEMINI_DescriptorCalc):
         else:
             # Return a unique, sorted filter name identifier string with an
             # ampersand separating each filter name
-            ret_filter_name = str('&'.join(filters.sort))
+            filters.sort
+            ret_filter_name = str('&'.join(filters))
         
         return ret_filter_name
     
@@ -687,65 +686,3 @@ class GMOS_DescriptorCalc(GEMINI_DescriptorCalc):
                 raise Errors.DescriptorDictError()
         
         return ret_wavelength_reference_pixel
-
-    ## UTILITY MEMBER FUNCTIONS (NOT DESCRIPTORS)
-    
-    def convert_units(self, input_units, input_value, output_units):
-        """
-        :param input_units: the units of the value specified by input_value.
-                            Possible values are 'meters', 'micrometers',
-                            'nanometers' and 'angstroms'.
-        :type input_units: string
-        :param input_value: the input value to be converted from the
-                            input_units to the output_units
-        :type input_value: float
-        :param output_units: the units of the returned value. Possible values
-                             are 'meters', 'micrometers', 'nanometers' and
-                             'angstroms'.
-        :type output_units: string
-        :rtype: float
-        :return: the converted value of input_value from input_units to
-                 output_units
-        """
-        # Determine the factor required to convert the input_value from the 
-        # input_units to the output_units
-        power = self.unitDict[input_units] - self.unitDict[output_units]
-        factor = math.pow(10, power)
-        
-        # Return the converted output value
-        return input_value * factor
-    
-    # The unitDict dictionary defines the factors for the function
-    # convert_units
-    unitDict = {
-        'meters':0,
-        'micrometers':-6,
-        'nanometers':-9,
-        'angstroms':-10,
-               }
-    
-    def section_to_tuple(self, section):
-        """
-        Convert the input section in the form [x1:x2,y1:y2] to a tuple in the
-        form (x1 - 1, x2 - 1, y1 - 1, y2 - 1), where x1, x2, y1 and y2 are
-        integers. The values in the output tuple are converted to use 0-based
-        indexing, making it compatible with numpy.
-        :param section: the section (in the form [x1:x2,y1:y2]) to be
-                        converted to a tuple
-        :type section: string
-        :rtype: tuple
-        :return: the converted section as a tuple that uses 0-based indexing
-                 in the form (x1 - 1, x2 - 1, y1 - 1, y2 - 1)
-        """
-        # Strip the square brackets from the input section and then create a
-        # list in the form ['x1:x2', 'y1:y2']
-        xylist = section.strip('[]').split(',')
-        
-        # Create variables containing the single x1, x2, y1 and y2 values
-        x1 = int(xylist[0].split(':')[0]) - 1
-        x2 = int(xylist[0].split(':')[1]) - 1
-        y1 = int(xylist[1].split(':')[0]) - 1
-        y2 = int(xylist[1].split(':')[1]) - 1
-
-        # Return the tuple in the form (x1, x2, y1, y2)
-        return (x1, x2, y1, y2)
