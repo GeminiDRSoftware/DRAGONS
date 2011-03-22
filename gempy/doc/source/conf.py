02c693b9342b1cd59eb14b2388bac34cb101b2f3
Kyle Mede <kmede@gemini.edu>
2011-03-22 18:24:20 +0000

    LOTS of reworking to structure, now lists of functions that are clickable to show code and individual docstrings.
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1739 e04282df-fb77-4b70-bf7a-c1223586725c

--- gempy/doc/source/conf.py
+++ gempy/doc/source/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'gempy'
-copyright = u'2011, nz'
+project = 'gempy'
+copyright = '2011, nz'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -178,8 +178,8 @@ htmlhelp_basename = 'gempydoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'gempy.tex', u'gempy Documentation',
-   u'nz', 'manual'),
+  ('index', 'gempy.tex', 'gempy Documentation',
+   'nz', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -211,6 +211,6 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'gempy', u'gempy Documentation',
-     [u'nz'], 1)
+    ('index', 'gempy', 'gempy Documentation',
+     ['nz'], 1)
 ]
