653d81dd245c8a14f4c53965ff748e015d7d73a8
Kyle Mede <kmede@gemini.edu>
2011-03-22 17:06:49 +0000

    deleted standardizeInstrumentHeaders and validateInstrumentData primitives as they are GMOS level prims, not GMOS_SPEC.  They can be put back in if any specific GMOS_SPEC stuff needs to be done, but are not needed at the moment.
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1736 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/primitives_GMOS_SPECT.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/primitives_GMOS_SPECT.py
@@ -25,35 +25,6 @@ class GMOS_SPECTPrimitives(GMOSPrimitives):
         GMOSPrimitives.init(self, rc)
         return rc
 
-    def standardizeInstrumentHeaders(self,rc):
-        """
-        This primitive is called by standardizeHeaders to makes the changes and 
-        additions to the headers of the input files that are GMOS_SPEC 
-        specific.
-        
-        :param logLevel: Verbosity setting for log messages to the screen.
-        :type logLevel: integer from 0-6, 0=nothing to screen, 6=everything to 
-                        screen. OR the message level as a string (ie. 'critical'  
-                        , 'status', 'fullinfo'...)
-        """
-        log = gemLog.getGeminiLog(logName=rc['logName'],logLevel=rc['logLevel'])
-        try: 
-            for ad in rc.getInputs(style="AD"): 
-                log.debug('calling stdInstHdrs for '+ad.filename)
-                gmost.stdInstHdrs(ad,logName=rc['logName'],
-                                  logLevel=rc['logLevel']) 
-                
-                log.status('instrument headers fixed') 
-                
-        except:
-            # logging the exact message from the actual exception that was 
-            # raised in the try block. Then raising a general PrimitiveError 
-            # with message.
-            log.critical(repr(sys.exc_info()[1]))
-            raise PrimitiveError("Problem preparing the image.")
-        
-        yield rc
-
     def attachMDF(self,rc):
         """
         This primitive is used to add an MDF if there is a MASKNAME in the   
@@ -110,29 +81,5 @@ class GMOS_SPECTPrimitives(GMOSPrimitives):
             raise PrimitiveError("Problem preparing the image.")
         yield rc
 
-    def validateInstrumentData(self,rc):
-        """
-        This primitive is called by validateData to validate the GMOS_SPEC 
-        specific data checks for all input files.
-        
-        :param logLevel: Verbosity setting for log messages to the screen.
-        :type logLevel: integer from 0-6, 0=nothing to screen, 6=everything to 
-                        screen. OR the message level as a string (ie. 'critical'  
-                        , 'status', 'fullinfo'...)
-        """
-        log = gemLog.getGeminiLog(logName=rc['logName'],logLevel=rc['logLevel'])
-        try:        
-            for ad in rc.getInputs(style="AD"):
-                log.debug('calling gmost.valInstData for '+ad.filename)
-                gmost.valInstData(ad, logName=rc['logName'], 
-                                  logLevel=rc['logLevel'],)
-                log.status('data validated for file = '+ad.filename)
-                
-        except:
-            # logging the exact message from the actual exception that was 
-            # raised in the try block. Then raising a general PrimitiveError 
-            # with message.
-            log.critical(repr(sys.exc_info()[1]))
-            raise PrimitiveError('Problem validating one of  '+rc.inputsAsStr())
-        yield rc
+
 
