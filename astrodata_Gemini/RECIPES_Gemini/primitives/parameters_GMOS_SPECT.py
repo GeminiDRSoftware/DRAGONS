c4e8f618a3760a14138438ba3e34132d207cf484
Kyle Mede <kmede@gemini.edu>
2011-03-29 18:13:22 +0000

    deleted attachMDF primitive and its param dict as that code is now in the standardize.add_mdf science function called during the standardize_structure_gmos utilized in the standardizeInstrumentStructure primitive(s).
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1817 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_SPECT.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_SPECT.py
@@ -1,10 +1,3 @@
-{ 
-    "attachMDF":{"suffix":
-                          {
-                           'default': '_mdf' , #value to be post pended to this primitives outputs
-                          'recipeOverride': True,
-                          'type': 'str',
-                          'userOverride':False    
-                           }
-                        }  
+{
+
 }
--- astrodata_Gemini/RECIPES_Gemini/primitives/primitives_GMOS_SPECT.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/primitives_GMOS_SPECT.py
@@ -25,59 +25,5 @@ class GMOS_SPECTPrimitives(GMOSPrimitives):
         GMOSPrimitives.init(self, rc)
         return rc
 
-    def attachMDF(self,rc):
-        """
-        This primitive is used to add an MDF if there is a MASKNAME in the   
-        images PHU only. It is called by the primitive standardizeStructure
-        during the prepare recipe if the parameter addMDF=True.
-        ***********************************************
-        will be upgraded later, early testing complete
-        ***********************************************
-        
-        :param suffix: Value to be post pended onto each input name(s) to 
-                         create the output name(s).
-        :type suffix: string
-        
-        :param logLevel: Verbosity setting for log messages to the screen.
-        :type logLevel: integer from 0-6, 0=nothing to screen, 6=everything to 
-                        screen. OR the message level as a string (ie. 'critical'  
-                        , 'status', 'fullinfo'...)
-        """
-        log = gemLog.getGeminiLog(logType=rc['logType'],logLevel=rc['logLevel'])
-        try:           
-            for ad in rc.getInputs(style ='AD'):
-                infilename = ad.filename
-                log.status('file having MDF attached= '+infilename)
-                
-                pathname = 'kyles_test_images/' #$$$$ HARDCODED FOR NOW, TILL FIX COMES FROM CRAIG
-                maskname = ad.phuGetKeyValue("MASKNAME")
-                log.stdinfo("maskname = "+maskname,'stdinfo')
-                inMDFname = 'kyles_test_images/'+maskname +'.fits'
-                log.status('input MDF file = '+inMDFname)
-                admdf = AstroData(inMDFname)
-                admdf.renameExt('MDF',1)  #$$$ HARDCODED EXTVER=1 FOR NOW, CHANGE LATER??
-                admdf.extSetKeyValue(len(admdf)-1,'EXTNAME', 'MDF',"Extension name" )
-                admdf.extSetKeyValue(len(admdf)-1,'EXTVER', 1,"Extension version" ) #$$$ HARDCODED EXTVER=1 FOR NOW, CHANGE LATER?? this one is to add the comment
-                
-                log.debug(admdf[0].getHeader())
-                log.debug(admdf.info())
-                
-                ad.append(moredata=admdf)  
-                log.status(ad.info())
-                
-                ad.filename=gemt.fileNameUpdater(ad.filename,
-                                                 suffix=rc["suffix"], 
-                                                 strip=False)
-                rc.reportOutput(ad)
-                
-                log.status('finished adding the MDF')
-        except:
-            # logging the exact message from the actual exception that was 
-            # raised in the try block. Then raising a general PrimitiveError 
-            # with message.
-            log.critical(repr(sys.exc_info()[1]))
-            raise PrimitiveError("Problem preparing the image.")
-        yield rc
-
 
 
