69dee7c0c70195ad9504d4b5755212d22a6e5ad7
Emma Hogan <ehogan@gemini.edu>
2011-04-06 18:51:59 +0000

    Removed exposure_time and ut_date in favour of the descriptor functions in the Gemini specific descriptor file, use phuGetKeyValue and getKeyValue instead of header
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1886 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/descriptors/PHOENIX/PHOENIX_Descriptor.py
+++ astrodata_Gemini/ADCONFIG_Gemini/descriptors/PHOENIX/PHOENIX_Descriptor.py
@@ -1,112 +1,69 @@
 import re
 
-from astrodata import Lookups
 from astrodata import Descriptors
-
-import astrodata
+from astrodata import Lookups
 from astrodata.Calculator import Calculator
-
-import GemCalcUtil 
+from gempy import astrotools
 
 from StandardDescriptorKeyDict import globalStdkeyDict
 from StandardPHOENIXKeyDict import stdkeyDictPHOENIX
 from GEMINI_Descriptor import GEMINI_DescriptorCalc
 
 class PHOENIX_DescriptorCalc(GEMINI_DescriptorCalc):
-    # Updating the global key dict with the local dict of this descriptor class
+    # Updating the global key dictionary with the local key dictionary
+    # associated with this descriptor class
     globalStdkeyDict.update(stdkeyDictPHOENIX)
     
     def __init__(self):
         pass
     
     def dec(self, dataset, **args):
-        """
-        Return the dec value for PHOENIX
-        @param dataset: the data set
-        @type dataset: AstroData
-        @rtype: float
-        @return: the declination in decimal degrees
-        """
-        hdu = dataset.hdulist
-        dec = hdu[0].header[globalStdkeyDict['key_dec']]
-        ret_dec = float(GemCalcUtil.degsextodec(dec))
+        # Get the declination from the header of the PHU
+        dec = dataset.phuGetKeyValue(globalStdkeyDict['key_dec'])
+        if dec is None:
+            # The phuGetKeyValue() function returns None if a value cannot be
+            # found and stores the exception info. Re-raise the exception. It
+            # will be dealt with by the CalculatorInterface.
+            if hasattr(dataset, 'exception_info'):
+                raise dataset.exception_info
+        # Return the declination float
+        ret_dec = float(astrotools.degsextodec(dec))
         
         return ret_dec
     
-    def exposure_time(self, dataset, **args):
-        """
-        Return the exposure_time value for PHOENIX
-        @param dataset: the data set
-        @type dataset: AstroData
-        @rtype: float
-        @return: the total exposure time of the observation (seconds)
-        """
-        hdu = dataset.hdulist
-        exptime = hdu[0].header[globalStdkeyDict['key_exposure_time']]
-        coadds = dataset.coadds()
-        
-        ret_exposure_time = float(exptime * coadds)
-        
-        return ret_exposure_time
-    
     def filter_name(self, dataset, stripID=False, pretty=False, **args):
-        """
-        Return the filter_name value for PHOENIX
-        @param dataset: the data set
-        @type dataset: AstroData
-        @param stripID: set to True to remove the component ID from the
-        returned filter name
-        @param pretty: set to True to return a meaningful filter name
-        @rtype: string
-        @return: the unique filter identifier string
-        """
-        hdu = dataset.hdulist
-        filter_name = hdu[0].header[stdkeyDictPHOENIX['key_filter']]
-        
-        ret_filter_name = str(filter_name)
+        # Get the filter name value from the header of the PHU. The filter name
+        # keyword is defined in the local key dictionary (stdkeyDictPHOENIX)
+        # but are read from the updated global key dictionary
+        # (globalStdkeyDict)
+        filter_name = dataset.phuGetKeyValue(globalStdkeyDict['key_filter'])
+        if filter_name is None:
+            # The phuGetKeyValue() function returns None if a value cannot be
+            # found and stores the exception info. Re-raise the exception. It
+            # will be dealt with by the CalculatorInterface.
+            if hasattr(dataset, 'exception_info'):
+                raise dataset.exception_info
+        if pretty:
+            stripID = True
+        if stripID:
+            # Return the stripped filter name string
+            ret_filter_name = string.removeComponentID(filter_name)
+        else:
+            # Return the filter name string
+            ret_filter_name = str(filter_name)
         
         return ret_filter_name
     
     def ra(self, dataset, **args):
-        """
-        Return the ra value for PHOENIX
-        @param dataset: the data set
-        @type dataset: AstroData
-        @rtype: float
-        @return: the R.A. in decimal degrees
-        """
-        hdu = dataset.hdulist
-        ra = hdu[0].header[globalStdkeyDict['key_ra']]
-        ret_ra = float(GemCalcUtil.rasextodec(ra))
+        # Get the declination from the header of the PHU
+        ra = dataset.phuGetKeyValue(globalStdkeyDict['key_ra'])
+        if ra is None:
+            # The phuGetKeyValue() function returns None if a value cannot be
+            # found and stores the exception info. Re-raise the exception. It
+            # will be dealt with by the CalculatorInterface.
+            if hasattr(dataset, 'exception_info'):
+                raise dataset.exception_info
+        # Return the declination float
+        ret_ra = float(astrotools.degsextodec(ra))
         
         return ret_ra
-    
-    def ut_date(self, dataset, **args):
-        """
-        Return the ut_date value for PHOENIX
-        @param dataset: the data set
-        @type dataset: AstroData
-        @rtype: string
-        @return: the UT date of the observation (YYYY-MM-DD)
-        """
-        hdu = dataset.hdulist
-        ut_date = hdu[0].header[stdkeyDictPHOENIX['key_ut_date']]
-        
-        # Validate the result. The definition is taken from the FITS
-        # standard document v3.0. Must be YYYY-MM-DD or
-        # YYYY-MM-DDThh:mm:ss[.sss]. Here I also do some very basic checks
-        # like ensuring the first digit of the month is 0 or 1, but I
-        # don't do cleverer checks like 01<=M<=12. nb. seconds ss > 59 is
-        # valid when leap seconds occur.
-        
-        match1 = re.match('\d\d\d\d-[01]\d-[0123]\d', ut_date)
-        match2 = re.match('(\d\d\d\d-[01]\d-[0123]\d)(T)([012]\d:[012345]\d:\d\d.*\d*)', ut_date)
-        
-        if match1:
-            ret_ut_date = str(ut_date)
-        elif match2:
-            ret_ut_date = str(match2.group(1))
-        else:
-            ret_ut_date = None
-        
-        return ret_ut_date
