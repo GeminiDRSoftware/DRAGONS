f7be7bdb08f293cceea7f39c94d5f6f84fbc4303
Emma Hogan <ehogan@gemini.edu>
2011-04-06 21:11:41 +0000

    Updated key_filter to key_filter_name
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1893 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/descriptors/PHOENIX/StandardPHOENIXKeyDict.py
+++ astrodata_Gemini/ADCONFIG_Gemini/descriptors/PHOENIX/StandardPHOENIXKeyDict.py
@@ -1,5 +1,5 @@
 stdkeyDictPHOENIX = {
-    "key_filter":"FILT_POS",
+    "key_filter_name":"FILT_POS",
     "key_focal_plane_mask":"SLIT_POS",
     "key_central_wavelength":"GRAT_POS",
     "key_ut_date":"UTDATE",
