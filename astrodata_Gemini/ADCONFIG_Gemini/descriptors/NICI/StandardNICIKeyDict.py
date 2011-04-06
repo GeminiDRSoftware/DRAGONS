c089ba1ebae58aa594958357a2abcdf68617d69a
Emma Hogan <ehogan@gemini.edu>
2011-04-06 22:22:51 +0000

    Updated key_exptime to key_exposure_time
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1900 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/descriptors/NICI/StandardNICIKeyDict.py
+++ astrodata_Gemini/ADCONFIG_Gemini/descriptors/NICI/StandardNICIKeyDict.py
@@ -4,8 +4,8 @@ stdkeyDictNICI = {
     "key_pupil_mask":"PMW",
     "key_filter_r":"FILTER_R",
     "key_filter_b":"FILTER_B",
-    "key_exptime_r":"ITIME_R",
-    "key_exptime_b":"ITIME_B",
+    "key_exposure_time_r":"ITIME_R",
+    "key_exposure_time_b":"ITIME_B",
     "key_coadds_r":"NCOADD_R",
     "key_coadds_b":"NCOADD_B",
     }
