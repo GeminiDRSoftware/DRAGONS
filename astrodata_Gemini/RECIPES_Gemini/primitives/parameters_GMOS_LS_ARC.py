9e2b8456f4ee6d04c8cd620c131ce27af1886f96
Emma Hogan <ehogan@gemini.edu>
2011-04-11 21:34:40 +0000

    Formatted the parameter dictionaries
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1923 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_LS_ARC.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_LS_ARC.py
@@ -1,83 +1,95 @@
-{
- 'wavecal':{'debug': {# debug characters
-                       'default': '' , 
-                       'recipeOverride': True,
-                       'type': 'str',
-                       'userOverride':True
-                      },
-            'reffile': {# Reference file with reference lines
-                       'default': 'cuar.dat' ,
-                       'recipeOverride': True,
-                       'type': 'str',
-                       'userOverride':True
-                      },
-              'wrdb': {# Write database file with wavelength solution
-                       'default': False, 
-                       'recipeOverride': True,
-                       'type': 'bool',
-                       'userOverride':False
-                      },
-       'fitfunction': {# Fitting function to use
-                       'default': 'chebyshev',
-                       'recipeOverride': True,
-                       'type': 'str',
-                       'userOverride':True
-                      },
-          'fitorder': {# Fitting function order
-                       'default': 4,
-                       'recipeOverride': True,
-                       'type': 'int',
-                       'userOverride':True
-                      },
-             'ntmax': {# Number of target features
-                       'default': 50,
-                       'recipeOverride': True,
-                       'type': 'int',
-                       'userOverride':True
-                      },
-             'fwidth': {# Full-width at the base 
-                       'default': 10,
-                       'recipeOverride': True,
-                       'type': 'int',
-                       'userOverride':True
-                      },
-             'match': {# Coordinate list matching limit
-                       'default': -6,
-                       'recipeOverride': True,
-                       'type': 'int',
-                       'userOverride':True
-                      },
-             'cradius': {# The maximum distance, in pixels, allowed when 
-                         # defining a new line.
-                       'default': 12,
-                       'recipeOverride': True,
-                       'type': 'int',
-                       'userOverride':True
-                      },
-             'clip': {# clip*stdeviacion rejection limit for refiting
-                       'default': 5,
-                       'recipeOverride': True,
-                       'type': 'int',
-                       'userOverride':True
-                      },
-             'nsum': {# clip*stdeviacion rejection limit for refiting
-                       'default': 10,
-                       'recipeOverride': True,
-                       'type': 'int',
-                       'userOverride':True
-                      },
-            'minsep': {# peaks: Minimum pixel separation
-                       'default': 2,
-                       'recipeOverride': True,
-                       'type': 'int',
-                       'userOverride':True
-                      },
-            'logfile': {# logfile
-                       'default': '',
-                       'recipeOverride': True,
-                       'type': 'str',
-                       'userOverride':True
-                      },
-                },
-
-}
+{'wavecal':{
+    # debug characters
+    'debug':{
+        'default'       : '',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : True,
+        },
+    # Reference file with reference lines
+    'reffile':{
+        'default'       : 'cuar.dat' ,
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : True,
+        },
+    # Write database file with wavelength solution
+    'wrdb':{
+        'default'       : False, 
+        'recipeOverride': True,
+        'type'          : 'bool',
+        'userOverride'  : False,
+        },
+    # Fitting function to use
+    'fitfunction':{
+        'default'       : 'chebyshev',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : True,
+        },
+    # Fitting function order
+    'fitorder':{
+        'default'       : 4,
+        'recipeOverride': True,
+        'type'          : 'int',
+        'userOverride'  : True,
+        },
+    # Number of target features
+    'ntmax':{
+        'default'       : 50,
+        'recipeOverride': True,
+        'type'          : 'int',
+        'userOverride'  : True,
+        },
+    # Full-width at the base 
+    'fwidth':{
+        'default'       : 10,
+        'recipeOverride': True,
+        'type'          : 'int',
+        'userOverride'  : True,
+        },
+    # Coordinate list matching limit
+    'match':{
+        'default'       : -6,
+        'recipeOverride': True,
+        'type'          : 'int',
+        'userOverride'  : True,
+        },
+    # The maximum distance, in pixels, allowed when 
+    'cradius':{
+        # defining a new line.
+        'default'       : 12,
+        'recipeOverride': True,
+        'type'          : 'int',
+        'userOverride'  : True,
+        },
+    # clip*stdeviacion rejection limit for refiting
+    'clip':{
+        'default'       : 5,
+        'recipeOverride': True,
+        'type'          : 'int',
+        'userOverride'  : True,
+        },
+    # clip*stdeviacion rejection limit for refiting
+    'nsum':{
+        'default'       : 10,
+        'recipeOverride': True,
+        'type'          : 'int',
+        'userOverride'  : True,
+        },
+    # peaks: Minimum pixel separation
+    'minsep':{
+        'default'       : 2,
+        'recipeOverride': True,
+        'type'          : 'int',
+        'userOverride'  : True,
+        },
+    # logfile
+    'logfile':{
+        'default'       : '',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : True,
+        },
+    },
+ }
