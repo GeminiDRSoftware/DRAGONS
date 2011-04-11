de07a0d5305c3c1e6ce03b0d58d56785b6dd8208
Emma Hogan <ehogan@gemini.edu>
2011-04-11 21:40:49 +0000

    Formatted the parameter dictionaries
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1926 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_IMAGE.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS_IMAGE.py
@@ -1,75 +1,65 @@
-{  'fringeCorrect': {
-                'suffix':
-                   {
-                    'default': '_fringecorrected' , #value to be post pended to this primitives outputs
-                      'recipeOverride': True,
-                      'type': 'str',
-                      'userOverride':False 
-                    },
-            'statsec':
-                   {
-                    'default': '' , 
-                      'recipeOverride': True,
-                      'type': 'str',
-                      'userOverride':False 
-                    },
-                      
-                'fl_statscale':
-                   {
-                   'default': True , 
-                      'recipeOverride': True,
-                      'type': 'bool',
-                      'userOverride':True 
-                    },
-                    
-                'scale':
-                   {
-                   'default': 0.0 , 
-                      'recipeOverride': True,
-                      'type': 'float',
-                      'userOverride':False  
-                     },  
-                    },   
-    'makeFringeFrame': {
-                'suffix':
-                   {
-                    'default': '_fringe' , #value to be post pended to this primitives outputs
-                      'recipeOverride': True,
-                      'type': 'str',
-                      'userOverride':False 
-                    },
-                      
-                'fl_vardq':
-                   {
-                   'default': True , 
-                      'recipeOverride': True,
-                      'type': 'bool',
-                      'userOverride':True 
-                    },
-                    
-                'method':
-                   {
-                   'default': 'median' , 
-                      'recipeOverride': True,
-                      'type': 'str',
-                      'userOverride':False  
-                     },  
-                    }, 
-'standardizeInstrumentStructure':
-                    {
-                    'suffix':
-                    {
-                       'default': '_struct' , #value to be post pended to this primitives outputs
-                      'recipeOverride': True,
-                      'type': 'str',
-                      'userOverride':False    
-                    } ,
-                    'addMDF':
-                    {
-                      'default': False , 
-                      'recipeOverride': True,
-                      'type': 'bool',
-                      'userOverride':True                                      
-                     }     
-                     }, 
-}             
+{'fringeCorrect':{
+    'suffix':{
+        # String to be post pended to the output of fringeCorrect
+        'default'       : '_fringeCorrected',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : False,
+        },
+    'statsec':{
+        'default'       : '',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : False,
+        },
+    'fl_statscale':{
+        'default'       : True,
+        'recipeOverride': True,
+        'type'          : 'bool',
+        'userOverride'  : True,
+        },
+    'scale':{
+        'default'       : 0.0,
+        'recipeOverride': True,
+        'type'          : 'float',
+        'userOverride'  : False,
+        },
+    },
+ 'makeFringeFrame':{
+    'suffix':{
+        # String to be post pended to the output of makeFringeFrame
+        'default'       : '_fringe',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : False,
+        },
+    'fl_vardq':{
+        'default'       : True,
+        'recipeOverride': True,
+        'type'          : 'bool',
+        'userOverride'  : True,
+        },
+    'method':{
+        'default'       : 'median',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : False,
+        },
+    },
+ 'standardizeInstrumentStructure':{
+    'suffix':{
+        # String to be post pended to the output of
+        # standardizeInstrumentStructure
+        'default'       : '_sdzInstStruct',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : False,
+        },
+    'addMDF':{
+        'default'       : False,
+        'recipeOverride': True,
+        'type'          : 'bool',
+        'userOverride'  : True,
+        },
+    },
+}
