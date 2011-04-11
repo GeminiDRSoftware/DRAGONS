cf3046b9cb9321ab09f9bdacd552f46a823449e8
Emma Hogan <ehogan@gemini.edu>
2011-04-11 21:29:17 +0000

    Formatted the parameter dictionaries
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1921 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GMOS.py
@@ -1,182 +1,169 @@
-{       'addBPM': {'suffix':
-                          {
-                           'default': '_bpm' , #value to be post pended to this primitives outputs
-                          'recipeOverride': True,
-                          'type': 'str',
-                          'userOverride':False    
-                           }
-                          },
-
-        'biasCorrect':{'fl_over':
-                        {
-                          'default': False , 
-                          'recipeOverride': True,
-                          'type': 'bool',
-                          'userOverride':True                                      
-                         },
-                         'fl_trim':
-                         {
-                          'default': False , 
-                          'recipeOverride': False,
-                          'type': 'bool',
-                          'userOverride':False    
-                          },
-                          'fl_vardq':
-                          {
-                           'default': True ,
-                          'recipeOverride': True,
-                          'type': 'bool',
-                          'userOverride':True    
-                           },
-                          'suffix':
-                          {
-                           'default': '_biassub' , #value to be post pended to this primitives outputs
-                          'recipeOverride': True,
-                          'type': 'str',
-                          'userOverride':False    
-                           }
-                          },
-        'flatCorrect':{
-                       'suffix':
-                       {
-                        'default': '_flatcorrected' , 
-                          'recipeOverride': True,
-                          'type': 'str',
-                          'userOverride':False  
-                        }
-                       },
-        'mosaicDetectors':{
-                  'fl_paste':
-                          {
-                           'default': False ,
-                          'recipeOverride': True,
-                          'type': 'bool',
-                          'userOverride':True,  
-                          'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-                           },
-                  'suffix':
-                       {
-                        'default': '_mosaic' , 
-                          'recipeOverride': True,
-                          'type': 'str',
-                          'userOverride':False  ,
-                        'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-                        },
-                    'interp_function':  #this is the geointer parameters new name for easier reading to the user
-                          {
-                           'default': 'linear' ,
-                          'recipeOverride': True,
-                          'type': 'str',
-                          'userOverride':True,  
-                          'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-                           },
-                           
-                  },
-    'normalizeFlat':{
-                        'fl_over':
-                        {
-                          'default': False , 
-                          'recipeOverride': False,
-                          'type': 'bool',
-                          'userOverride':False,
-                          'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-                         },
-                         'fl_trim':
-                         {
-                          'default': False , 
-                          'recipeOverride': False,
-                          'type': 'bool',
-                          'userOverride':False,    
-                          'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-                         
-                          },
-                          'fl_vardq':
-                          {
-                           'default': True ,
-                          'recipeOverride': True,
-                          'type': 'bool',
-                          'userOverride':True,  
-                          'tag':['cl_iraf','ui_advanced'], #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-                           },
-                          'suffix':
-                          {
-                           'default': '_normalized' , #value to be post pended to this primitives outputs (used by CLManager not CL directly for this prim)
-                          'recipeOverride': True,
-                          'type': 'str',
-                          'userOverride':False    
-                           },
-                          },
-                                                   
-        'overscanSubtract':{'fl_trim':
-                         {
-                          'default': False , 
-                          'recipeOverride': False,
-                          'type': 'bool',
-                          'userOverride':False    
-                          },
-                          'fl_vardq':
-                          {
-                           'default': False ,
-                          'recipeOverride': True,
-                          'type': 'bool',
-                          'userOverride':True    
-                           },
-                          'suffix':
-                          {
-                           'default': '_oversubed' , #value to be post pended to this primitives outputs
-                          'recipeOverride': True,
-                          'type': 'str',
-                          'userOverride':False    
-                           },
-                           'biassec':
-                           {
-                            'default': '[1:25,1:2304],[1:32,1:2304],[1025:1056,1:2304]' ,
-                            'recipeOverride': True,
-                            'type': 'str',
-                            'userOverride':True
-                            }
-                        },
-        'overscanTrim':{'suffix':
-                          {
-                           'default': '_overtrimd' , #value to be post pended to this primitives outputs
-                          'recipeOverride': True,
-                          'type': 'str',
-                          'userOverride':False    
-                           }
-                        },        
-        'standardizeInstrumentStructure':
-                    {
-                    'suffix':
-                    {
-                       'default': '_struct' , #value to be post pended to this primitives outputs
-                      'recipeOverride': True,
-                      'type': 'str',
-                      'userOverride':False    
-                    } ,
-                    'addMDF':
-                    {
-                      'default': True , 
-                      'recipeOverride': True,
-                      'type': 'bool',
-                      'userOverride':True                                      
-                     }     
-                     }, 
-        'validateInstrumentData':
-                    {
-                    'suffix':
-                    {
-                       'default': '_validated' , #value to be post pended to this primitives outputs
-                      'recipeOverride': True,
-                      'type': 'str',
-                      'userOverride':False    
-                    } ,
-                    'repair':
-                    {
-                      'default': True , 
-                      'recipeOverride': True,
-                      'type': 'bool',
-                      'userOverride':True                                      
-                     }     
-                     }, 
-        
+{'addBPM':{
+    'suffix':{
+        # String to be post pended to the output of addBPM
+        'default'       : '_bpmAdded',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : False,
+        },
+    },
+'subtractBias':{
+    'suffix':{
+        # String to be post pended to the output of subtractBias
+        'default'       : '_biasCorrected',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : False,
+        },
+    'fl_over':{
+        'default'       : False,
+        'recipeOverride': True,
+        'type'          : 'bool',
+        'userOverride'  : True,
+        },
+    'fl_trim':{
+        'default'       : False,
+        'recipeOverride': False,
+        'type'          : 'bool',
+        'userOverride'  : False,
+        },
+    'fl_vardq':{
+        'default'       : True,
+        'recipeOverride': True,
+        'type'          : 'bool',
+        'userOverride'  : True    
+         },
+    },
+'mosaicDetectors':{
+    'suffix':{
+        # String to be post pended to the output of mosaicDetectors
+        'default'       : '_mosaiced',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : False,
+        'tag'           : ['cl_iraf','ui_advanced'],
+        },
+    'fl_paste':{
+        'default'       : False,
+        'recipeOverride': True,
+        'type'          : 'bool',
+        'userOverride'  : True,
+        'tag'           : ['cl_iraf','ui_advanced'],
+        },
+    'interp_function':{
+        # This is the new 'user friendly' name for the geointer parameter
+        'default'       : 'linear',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : True,
+        'tag'           : ['cl_iraf','ui_advanced'],
+         },
+    },
+'normalizeFlat':{
+    'suffix':{
+        # String to be post pended to the output of normalizeFlat
+        'default'       : '_normalized',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : False,
+        },
+    'fl_over':{
+        'default'       : False,
+        'recipeOverride': False,
+        'type'          : 'bool',
+        'userOverride'  : False,
+        'tag'           : ['cl_iraf','ui_advanced'],
+        },
+    'fl_trim':{
+        'default'       : False,
+        'recipeOverride': False,
+        'type'          : 'bool',
+        'userOverride'  : False,
+        'tag'           : ['cl_iraf','ui_advanced'],
+        },
+    'fl_vardq':{
+        'default'       : True,
+        'recipeOverride': True,
+        'type'          : 'bool',
+        'userOverride'  : True,
+        'tag'           : ['cl_iraf','ui_advanced'],
+        },
+    },
+'overscanSubtract':{
+    'suffix':{
+        # String to be post pended to the output of overscanSubtract
+        'default'       : '_overscanCorrected',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : False,
+        },
+    'fl_trim':{
+        'default'       : False,
+        'recipeOverride': False,
+        'type'          : 'bool',
+        'userOverride'  : False,
+        },
+    'fl_vardq':{
+        'default'       : False,
+        'recipeOverride': True,
+        'type'          : 'bool',
+        'userOverride'  : True,
+        },
+    'biassec':{
+        'default'       : '[1:25,1:2304],[1:32,1:2304],[1025:1056,1:2304]',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : True,
+        },
+    },
+'overscanTrim':{
+    'suffix':{
+        # String to be post pended to the output of overscanTrim
+        'default'       : '_overscanTrimmed',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : False,
+        },
+    },
+'standardizeInstrumentHeaders':{
+    'suffix':{
+        # String to be post pended to the output of
+        # standardizeInstrumentHeaders
+        'default'       : '_sdzInstHdrs',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : False,
+        },
+    },
+ 'standardizeInstrumentStructure':{
+    'suffix':{
+        # String to be post pended to the output of
+        # standardizeInstrumentStructure
+        'default'       : '_sdzInstStruct',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : False,
+        },
+    'addMDF':{
+        'default'       : True,
+        'recipeOverride': True,
+        'type'          : 'bool',
+        'userOverride'  : True,
+        },
+    },
+'validateInstrumentData':{
+    'suffix':{
+        # String to be post pended to the output of validateInstrumentData
+        'default'       : '_validatedInst',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  :False,
+        },
+    'repair':{
+        'default'       : True,
+        'recipeOverride': True,
+        'type'          : 'bool',
+        'userOverride'  :True,
+        },
+    },
 }
