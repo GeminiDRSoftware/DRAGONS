5b1160bfd9bb7597279d4ae9fb5e308ee301c563
Emma Hogan <ehogan@gemini.edu>
2011-04-11 21:41:44 +0000

    Added parameter files for F2
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1927 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_F2_IMAGE.py
@@ -0,0 +1,17 @@
+{'standardizeInstrumentStructure':{
+    'suffix':{
+        # String to be post pended to the output of
+        # standardizeInstrumentStructure
+        'default'       : '_sdzInstStruct',
+        'recipeOverride': True,
+        'type'          : 'str',
+        'userOverride'  : False,
+        },
+    'addMDF':{
+        'default'       : False,
+        'recipeOverride': True,
+        'type'          : 'bool',
+        'userOverride'  : True,
+        },
+    },
+}
