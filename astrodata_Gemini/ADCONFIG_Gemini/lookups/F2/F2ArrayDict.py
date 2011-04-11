9e91ccf750c2694ddb196f1c68b8bb8f6140d8ad
Emma Hogan <ehogan@gemini.edu>
2011-04-11 16:31:10 +0000

    Added F2 lookup dictionaries and BPM
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1918 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/F2/F2ArrayDict.py
@@ -0,0 +1,8 @@
+f2ArrayDict = {
+    # Taken from f2$data/f2array.fits
+    # Dictionary key is the number of non-destructive read pairs
+    # Dictionary values are in the following order:
+    # readnoise, gain, well, linearlimit, coeff1, coeff2, coeff3,
+    # nonlinearlimit
+    1:(7.215, 5.20, 250000, 0.80, 1.0, 0.0, 0.0, 0.80)
+    }
---
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/F2/F2ConfigDict.py
@@ -0,0 +1,6 @@
+f2ConfigDict = {
+    # Dictionary key is the slit
+    # Dictionary values are in the following order:
+    # lyot, specsec
+    '1pix-slit':('f/16_G5830', '[60:1480,180:1810]')
+    }
