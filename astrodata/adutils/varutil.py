7d4081e39c552a4b9a260f1ac29bb916cd300293
Kyle Mede <kmede@gemini.edu>
2011-04-18 19:35:51 +0000

    re-organized and upgraded varianceArrayCalculator plus filled in its doc strings.
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@1965 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/adutils/varutil.py
+++ astrodata/adutils/varutil.py
@@ -1,6 +1,6 @@
-#Author: Kyle Mede, Aug 2010
-# This module provides functions that perform numpy operations on astrodata
-# objects
+# Author: Kyle Mede, April 2011
+# This module provides functions that perform variance frame calculations
+# and the creation of their headers on AstroData objects
 
 import os
 
@@ -10,24 +10,36 @@ import astrodata
 from astrodata.AstroData import AstroData
 from astrodata.Errors import ArithError
 
-def varianceArrayCalculator(sciExtA=None, sciExtB=None, constB=None, 
-                            varExtA=None, varExtB=None, div=False, mult=False,
-                                                        sub=False, add=False):
-    """
-    ####### make this handle float value for the sciExtB argument and follow
-    ####### outvar = varA*int*int
+def varianceArrayCalculator(sciExtA=None, sciExtB=None, constB=None,  
+                            sciOut=None, varExtA=None, varExtB=None, div=False, 
+                                            mult=False, sub=False, add=False):
+    """    
+    This function will update a currently existing variance plane due to a 
+    mathematical operation performed on the science data.    
+    
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    For multiplications and division operations:
+    --------------------------------------------
+    If sciExtB is an AstroData instance:
+    varOut=sciOut^2 * ( varA/(sciA^2) + varB/(sciB^2) )
+    
+    Else:
+    varOut=varA * constB^2
+    
+    For addition and subtraction operations:
+    ----------------------------------------
+    If sciExtB is an AstroData instance:
+    varOut= varA + varB
+    
+    Else:
+    varOut=varA 
+    variance is not affected if the science data is just consistently raised
+    or lowered by a constant.
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
     The A input MUST have a varExtA defined!
+    
     If both A and B inputs are AstroData's then they must BOTH have varExt's.
-    #######
-    
-    For multiplications and division operations it boils down to:
-    if mult: sciOut = sciA * sciB 
-    for div: sciOut = sciA / sciB 
-    # varOut=sciOut^2 * ( varA/(sciA^2) + varB/(sciB^2) )
-    
-    for addition and subtraction operations:
-    # varOut= varA + varB
     
     Only ONE mathematical operation can be performed at a time!!
     
@@ -39,6 +51,10 @@ def varianceArrayCalculator(sciExtA=None, sciExtB=None, constB=None,
                     by sciExtA OR dividing sciExtA by
     :type sciExtB: AstroData single extension.ex. sciExtB = adB['SCI',#]
     
+    :param sciOut: science extension of an output AstroData instance
+                   ie. the science frame resulting from the operation.
+    :type sciOut: AstroData single extension.ex. sciOut = adOut['SCI',#]
+    
     :param constB: constant multiplying sciExtA by
     :tyep constB: float
     
@@ -47,8 +63,20 @@ def varianceArrayCalculator(sciExtA=None, sciExtB=None, constB=None,
     :type varExtA: AstroData single extension. ex. varExtA = adA['VAR',#]
     
     :param varExtB: variance extension of an AstroData instance being multiplied
-                    or divided by extB
+                    or divided by extB. 
     :type varExtB: AstroData single extension. ex. varExtB = adA['VAR',#]
+    
+    :param mult: was a multiplication performed between A and B?
+    :type mult: Python boolean (True/False)
+    
+    :param div: was a division performed between A and B?
+    :type div: Python boolean (True/False)
+    
+    :param add: was an addition performed between A and B?
+    :type add: Python boolean (True/False)
+    
+    :param sub: was a subtraction performed between A and B?
+    :type sub: Python boolean (True/False)
     """
     try:
         # checking if it more than one math operation is set True
@@ -60,50 +88,81 @@ def varianceArrayCalculator(sciExtA=None, sciExtB=None, constB=None,
                     raise ArithError('only ONE math operation can be True')
                 else:
                     yup = True
-            
-        # Checking all the inputs are AstroData's then grabbing their data arrays
-        if isinstance(sciExtA, astrodata.AstroData) or \
-                            isinstance(sciExtA, astrodata.AstroData.AstroData):
-            sciA = sciExtA.data
-            if isinstance(varExtA, astrodata.AstroData) or \
-                            isinstance(varExtA, astrodata.AstroData.AstroData):
-                varA = varExtA.data
-            else:
-                raise ArithError('varExtA must be an AstroData instances')
-        if isinstance(sciExtB, astrodata.AstroData) or \
-                            isinstance(sciExtB, astrodata.AstroData.AstroData):
-            sciB = sciExtB.data
-            if isinstance(varExtB, astrodata.AstroData) or \
-                            isinstance(varExtB, astrodata.AstroData.AstroData):
-                varB = varExtB.data
-            else:
-                raise ArithError('varExtB must be an AstroData instances')
-        elif isinstance(constB,float):
-            pass
-        else:
-            raise ArithError('Either sciExtB must be an AstroData instances '+
-                             'OR constB is a float, neither case satisfied.')
+        if (sciExtB is not None) and (constB is not None):
+            raise ArithError('sciExtB and constB cannot both be defined')
         
-        # preparing the sciOut array if performing a mult or div
-        if mult:
-            sciOut = np.multiply(sciA,sciB)
-        if div:
-            sciOut = np.divide(sciA,sciB)
+        ## Perform all checks and math needed for mult and div cases
         if mult or div:
-            sciOutSquared = np.multiply(sciOut, sciOut)
-            sciAsquared = np.multiply(sciA, sciA)
-            sciBsquared = np.multiply(sciB, sciB)
-            # Now varA/sciAsquared and varB/sciBsquared
-            varAoverSciASquared = np.divide(varA, sciAsquared)
-            varBoverSciBSquared = np.divide(varB, sciBsquared)
-            # Now varAoverSciASquared + varBoverSciBSquared
-            varOverAplusB = np.add(varAoverSciASquared, varBoverSciBSquared) 
-            # Put it all together 
-            # varOut=sciOut^2 * ( varA/(sciA^2) + varB/(sciB^2) )
-            varOut = np.multiply(sciOutSquared,varOverAplusB)
-        if sub or add:
-            varOut = np.add(varA, varB)
-       
+            # Checking all the inputs are AstroData's then grabbing their 
+            # data arrays
+            if sciExtA is not None:
+                if isinstance(sciExtA, astrodata.AstroData) or \
+                            isinstance(sciExtA, astrodata.AstroData.AstroData):
+                    sciA = sciExtA.data
+                    if isinstance(varExtA, astrodata.AstroData) or \
+                            isinstance(varExtA, astrodata.AstroData.AstroData):
+                        varA = varExtA.data
+                    else:
+                        raise ArithError('varExtA must be an AstroData '+
+                                         'instances')
+            if sciExtB is not None:
+                if isinstance(sciExtB, astrodata.AstroData) or \
+                            isinstance(sciExtB, astrodata.AstroData.AstroData):
+                    sciB = sciExtB.data
+                    if isinstance(varExtB, astrodata.AstroData) or \
+                            isinstance(varExtB, astrodata.AstroData.AstroData):
+                        varB = varExtB.data
+                    else:
+                        raise ArithError('varExtB must be an AstroData '+
+                                         'instances')
+            elif isinstance(constB,float):
+                pass
+            else:
+                raise ArithError('Either sciExtB must be an AstroData '+
+                                 'instances OR constB is a float, neither '+
+                                 'case satisfied.')
+            # calculate the output variance array if mult or div
+            if constB is None:
+                # Science was multiplied/divided by an array so follow:
+                # varOut=sciOut^2 * ( varA/(sciA^2) + varB/(sciB^2) )
+                
+                sciOutSquared = np.multiply(sciOut, sciOut)
+                sciAsquared = np.multiply(sciA, sciA)
+                sciBsquared = np.multiply(sciB, sciB)
+                # Now varA/sciAsquared and varB/sciBsquared
+                varAoverSciASquared = np.divide(varA, sciAsquared)
+                varBoverSciBSquared = np.divide(varB, sciBsquared)
+                # Now varAoverSciASquared + varBoverSciBSquared
+                varOverAplusB = np.add(varAoverSciASquared, varBoverSciBSquared) 
+                # Put it all together 
+                # varOut=sciOut^2 * ( varA/(sciA^2) + varB/(sciB^2) )
+                varOut = np.multiply(sciOutSquared,varOverAplusB)
+            else:
+                # Science was multiplied/divided by constant so follow:
+                # varOut = varA * constB^2
+                varOut = np.multiply(varA,constB*constB)
+                
+        ## Perform all checks and math needed for add and sub cases        
+        elif add or sub:
+            if varExtA is not None:
+                if isinstance(varExtA, astrodata.AstroData) or \
+                            isinstance(varExtA, astrodata.AstroData.AstroData):
+                            varA = varExtA.data
+            else:
+                raise ArithError('varExtA must be not be None')
+            if varExtB is not None:
+                if isinstance(varExtB, astrodata.AstroData) or \
+                            isinstance(varExtB, astrodata.AstroData.AstroData):
+                            varB = varExtB.data
+                else:
+                    raise ArithError('varExtB must be an AstroData instances')
+                # calculate the output variance array varOut=varA+varB
+                varOut = np.add(varA, varB)
+            else:
+                # No second variance array defined so just pass the first 
+                # through to the output
+                varOut = varA
+
         # return final variance data array
         return varOut
     except:
@@ -131,12 +190,12 @@ def calculateInitialVarianceArray(sciExt=None):
         # var = (read noise/gain)**2 + max(data,0.0)/gain
                         
         # Retrieving necessary values (read noise, gain)
-        readNoise=sciExt.read_noise()
+        readNoise=sciExt.read_noise().asPytype()
         gain = sciExt.gain().asPytype()
         # Creating (read noise/gain) constant
         rnOverG=readNoise/gain
         # Convert negative numbers (if they exist) to zeros
-        maxArray=np.where(sciExt.data>0.0,0,sciExt.data)
+        maxArray=np.where(sciExt.data>0.0,sciExt.data,0.0)
         #maxArray=sciExt.data[np.where(sciExt.data>0.0,0)]
         # Creating max(data,0.0)/gain array
         maxOverGain=np.divide(maxArray,gain)
