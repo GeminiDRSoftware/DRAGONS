1a08cda575d9cd34866e891d808f32913bf5dc50
Emma Hogan <ehogan@gemini.edu>
2011-05-02 20:29:17 +0000

    Added preprocessing package
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2029 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ gempy/science/preprocessing/flat.py
@@ -0,0 +1,2 @@
+# This module contains user level functions related to the preprocessing of
+# the input dataset with a flat frame
---
+++ gempy/science/preprocessing/misc.py
@@ -0,0 +1,2 @@
+# This module contains miscellaneous user level functions related to the
+# preprocessing of the input dataset
