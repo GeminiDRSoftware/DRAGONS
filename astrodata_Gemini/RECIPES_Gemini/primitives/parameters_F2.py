bab3dcb95b9f5a2165914b17737c5790ce95fb18
Emma Hogan <ehogan@gemini.edu>
2011-05-02 18:40:42 +0000

    Removed standardizeHeaders, standardizeStructure and validateData parameters from parameters_F2.py in favour of the parameters listed in parameters_GEMINI.py
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2024 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/parameters_F2.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_F2.py
@@ -7,45 +7,4 @@
         'userOverride'  : False,
         },
     },
-'standardizeInstrumentHeaders':{
-    'suffix':{
-        # String to be post pended to the output of
-        # standardizeInstrumentHeaders
-        'default'       : '_sdzInstHdrs',
-        'recipeOverride': True,
-        'type'          : 'str',
-        'userOverride'  : False,
-        },
-    },
- 'standardizeInstrumentStructure':{
-    'suffix':{
-        # String to be post pended to the output of
-        # standardizeInstrumentStructure
-        'default'       : '_sdzInstStruct',
-        'recipeOverride': True,
-        'type'          : 'str',
-        'userOverride'  : False,
-        },
-    'addMDF':{
-        'default'       : True,
-        'recipeOverride': True,
-        'type'          : 'bool',
-        'userOverride'  : True,
-        },
-    },
-'validateInstrumentData':{
-    'suffix':{
-        # String to be post pended to the output of validateInstrumentData
-        'default'       : '_validatedInst',
-        'recipeOverride': True,
-        'type'          : 'str',
-        'userOverride'  : False,
-        },
-    'repair':{
-        'default'       : True,
-        'recipeOverride': True,
-        'type'          : 'bool',
-        'userOverride'  : True,
-        },
-    },
 }
