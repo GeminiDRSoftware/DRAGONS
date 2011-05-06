cbd8709e4207d1ac9e77cdf2cf0588218fbc6c54
Emma Hogan <ehogan@gemini.edu>
2011-05-06 19:08:16 +0000

    Minor updates to comments and formatting
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2065 e04282df-fb77-4b70-bf7a-c1223586725c

--- gempy/string.py
+++ gempy/string.py
@@ -34,11 +34,11 @@ def sectionStrToIntList(section):
     :type section: string
     
     :rtype: list
-    :return: the converted section as a list that uses 0-based and non-inclusive
-             in the form [y1 - 1, y2, x1 - 1, x2]
+    :return: the converted section as a list that uses 0-based and
+             non-inclusive in the form [y1 - 1, y2, x1 - 1, x2]
     """
     # Strip the square brackets from the input section and then create a
-    # list in the form ['x1:x2', 'y1:y2']
+    # list in the form ['x1:x2','y1:y2']
     xylist = section.strip('[]').split(',')
     
     # Create variables containing the single x1, x2, y1 and y2 values
@@ -47,6 +47,6 @@ def sectionStrToIntList(section):
     y1 = int(xylist[1].split(':')[0]) - 1
     y2 = int(xylist[1].split(':')[1]) 
     
-    # Return the tuple in the form [y1 - 1, y2, x1 - 1, x2]
+    # Return the list in the form [y1 - 1, y2, x1 - 1, x2]
     return [y1, y2, x1, x2]
 
