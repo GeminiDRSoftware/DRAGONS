0c6d39478a62ce65360ed0bdc795110cf231b6b3
Kaniela Dement <kdement@gemini.edu>
2011-05-12 03:23:13 +0000

    minor typo fix
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2107 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/scripts/test/adtest_deepcopy.py
+++ astrodata/scripts/test/adtest_deepcopy.py
@@ -12,14 +12,11 @@ parser.add_option('--test1', action='store_true', dest='test1', default=False,
                    help='run test1:')
 parser.add_option('--test2', action='store_true', dest='test2', default=False,
                    help='run test2:')
-parser.add_option('--test3', action='store_true', dest='test3', default=False,
-                   help='run test3:')
 (options,  args) = parser.parse_args()
 
-if not options.test1 and not options.test2 and not options.test3:
+if not options.test1 and not options.test2:
     options.test1 = True
     options.test2 = True
-    options.test3 = True
 
 print "\n", "_"*57,"\n\n\t\tAstroData TEST: deepcopy\n"
 if len(args) is 1:
@@ -64,8 +61,6 @@ if options.test2:
         raise AssertionError
     print "\t>> PASSED: Original name is preserved by deepcopy\n" 
 
-if options.test3:
-    print "Test3: Under Construction\n"
 
 print "_"*57,"\n"
 
