b6cec3375f1bce96d2e6d6b8c45469300b138c6a
Kaniela Dement <kdement@gemini.edu>
2011-05-18 02:41:13 +0000

    configure tests for nose
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2122 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/scripts/test/test_constructor.py
+++ astrodata/scripts/test/test_constructor.py
@@ -1,83 +1,52 @@
-import sys, os
-import subprocess
-from optparse import OptionParser
-from astrodata import Errors
-import time
-#import test_astrodata
-import inspect
+import sys
+import os
 
-parser = OptionParser()
-parser.set_description(
-"""AstroData Self-Test.\n
-Created by C.Allen, K.Dement, 01May2011.""" )
+import pyfits
+import astrodata
+from astrodata import AstroData
+import adtest_utils
 
-parser.add_option('-r','--runall', action='store_true', dest='runall',\
-                    default=False, help='run all AstroData class tests')
-parser.add_option('-v','--verbose', action='store_true', dest='verbose',\
-                    default=False, help='see standard out')
-(options,  args) = parser.parse_args()
-verbose = options.verbose
+testfile = adtest_utils.testdatafile_1
 
-def astrodata_trunner(ad_test):
-    '''Helper function for python nose
-    '''
-    #verbose = False
-    inlist = []
-    inlist.append('python')
-    inlist.append(ad_test)
-    if verbose:
-        retcode = subprocess.call(inlist)
-        if retcode > 0:
-            raise Errors.DescriptorValueTypeError
-    else:
-        retcode = subprocess.call(inlist, stdout=open('/dev/null','w'))
-        if retcode > 0:
-            raise Errors.DescriptorValueTypeError
+def checkad(ad):
+    assert ad.mode != None
+    print("\tad.mode is set to %s" % ad.mode)
+    assert type(ad.phu) is pyfits.core.PrimaryHDU
+    print("\tad.phu type is pyfits.core.PrimaryHDU")
+    for ext in ad:
+        assert type(ext) is astrodata.AstroData
+        print("\tad[%s,%s] type is astrodata.AstroData" % (ext.extname(), ext.extver()))
+    ad.close()
 
-def test_deepcopy():
-    astrodata_trunner('adtest_deepcopy.py')
+def constructor_test_1():
+    """TEST 1:  Create AstroData instance using filename.
+    """
+    print("\n\tTest input file: %s" % testfile)
+    ad = AstroData(testfile)
+    print("\tad = AstroData(%s)" % ad.filename)
+    checkad(ad)
 
-def test_inferred_extname():
-    astrodata_trunner('adtest_inferred_extname.py')
+def constructor_test_2():
+    """TEST 2:  Create AstroData instance using hdulist.
+    """
+    print("\n\tTest input file: %s" % testfile)
+    hdulist = pyfits.open(testfile)
+    ad = AstroData(hdulist)
+    print("\tad = AstroData(hdulist)")
+    checkad(ad)
+    hdulist.close()
 
-def test_dv_operators():
-    astrodata_trunner('adtest_dv_operators.py')
+def constructor_test_3():
+    """TEST 3:  Create AstroData instance using phu and data.
+    """
+    print("\n\tTest input file: %s" % testfile)
+    hdulist = pyfits.open(testfile)
+    ad = AstroData(header=hdulist[0], data=[hdulist[1],hdulist[2],hdulist[3]])
+    print("\tad = AstroData(hdulist)")
+    checkad(ad)
+    hdulist.close()
 
 
 
-# Run tests using -r option in interpretor
-if options.runall:
-    adtests = os.listdir('.') 
-    testlist = []
-    for adtest in adtests:
-        if adtest[:7] == 'adtest_':
-            temp = adtest[7:]
-            testlist.append(temp[:-3])
-    times = []
-    numpass = 0
-    numfail = 0
-    print('\nrunning AstroData class self-tests...\n')
-    for tes in testlist:
-        timerStart = time.time() 
-        try:
-            exec('test_%s()' % tes)
-            print '.',
-            numpass += 1
-        except:
-            print 'F',
-            numfail += 1
-        times.append(time.time() - timerStart)
-    total = 0
-    for tim in times:
-        total += tim
-    print("\n")
-    if verbose:
-        for i in range(len(testlist)):
-            print("%s (%.5fs)" % (testlist[i],times[i]))
-    print('-'*60)
-    print("Ran %i tests in %.2fs" % (len(testlist), total))
-    #print "\nResults: Pass = ", numpass, " Fail = ", numfail
-    if numfail == 0:
-        print('\nOK')
-    else:
-        print('\nFAILED (%i)' % numfail)
+
+     
