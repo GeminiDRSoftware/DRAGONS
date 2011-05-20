2fa8d862e28715fd531792e989a4141dcc94a20a
Emma Hogan <ehogan@gemini.edu>
2011-05-20 22:00:16 +0000

    Removed unused imports from GMOS_SPECT specific primitive file
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2170 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/primitives_GMOS_SPECT.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/primitives_GMOS_SPECT.py
@@ -1,24 +1,13 @@
-#Author: Kyle Mede, June 2010
-# Skeleton originally written by Craig Allen, callen@gemini.edu
-
-import sys
-from astrodata.adutils import gemLog
-from gempy import geminiTools as gt
-from gempy.science import calibrate as cal
-from gempy.science import standardization as sdz
 from primitives_GMOS import GMOSPrimitives
 
 class GMOS_SPECTPrimitives(GMOSPrimitives):
     """
-    This is the class of all primitives for the GMOS_SPECT level of the type 
-    hierarchy tree.  It inherits all the primitives to the level above
-    , 'GMOSPrimitives'.
+    This is the class containing all of the primitives for the F2_IMAGE
+    level of the type hierarchy tree. It inherits all the primitives from the
+    level above, 'GMOSPrimitives'.
     """
     astrotype = "GMOS_SPECT"
     
     def init(self, rc):
         GMOSPrimitives.init(self, rc)
         return rc
-
-
-
