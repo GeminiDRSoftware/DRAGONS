6671e98e3739483c3cbedcd0489da920432eafbc
Kaniela Dement <kdement@gemini.edu>
2011-05-26 01:41:35 +0000

    added new test
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2223 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/scripts/test/test_ad_close.py
+++ astrodata/scripts/test/test_ad_close.py
@@ -1,6 +1,5 @@
 import sys
 import os
-from optparse import OptionParser
 
 from nose.tools import *
 
@@ -10,8 +9,28 @@ from astrodata import AstroData
 testfile = file_urls.testdatafile_1
 
 def ad_close_test1():
-    """AD_CLOSE: test1 -
-    """
-    print("\n\tTest input file: %s" % testfile)
+    '''AD_CLOSE: test1 -calls hdulist.close() and sets ad.hdulist to None  
+    '''
+    print('\n\t* testfile: %s' % testfile)
     ad = AstroData(testfile)
+    print('\tad = AstroData(testfile)')
+    print('\tad.hdulist == %s' % ad.hdulist)
+    print('\tad.close()')
+    ad.close()
+    eq_(ad.hdulist, None, msg='ad.hdulist is not None')
+    print('\tad.hdulist == None')
+
+def ad_close_test2():
+    '''AD_CLOSE: test2 -subdata will not close the original hdulist  
+    '''
+    print('\n\t* testfile: %s' % testfile)
+    ad = AstroData(testfile)
+    print('\tad = AstroData(testfile)')
+    sd = ad['SCI,1']
+    print("\tsd = ad['SCI,1']")
+    sd.close()
+    print('\tsd.close()')
+    assert_not_equal(ad.hdulist, None, msg='original hdulist is None')
+    print('\tad.hdulist != None')
+    ad.close() 
 
