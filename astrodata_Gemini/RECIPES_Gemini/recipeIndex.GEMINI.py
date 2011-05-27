75ed994f3e15d821211df4519b455a417bf43878
Emma Hogan <ehogan@gemini.edu>
2011-05-27 20:10:20 +0000

    Edited comment in recipeIndex.GEMINI.py
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2243 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/recipeIndex.GEMINI.py
+++ astrodata_Gemini/RECIPES_Gemini/recipeIndex.GEMINI.py
@@ -1,4 +1,5 @@
-# adds recipes to the global list for a type
+# This dictionary defines the default recipes to use to process a dataset of a 
+# given AstroData Type
 
 localAstroTypeRecipeIndex = {
                                 "F2_IMAGE_FLAT": ["makeProcessedFlat"],
