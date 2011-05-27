8fe12605e7a38cb95cdd51f6ebbfaad0cd830e59
Emma Hogan <ehogan@gemini.edu>
2011-05-27 20:55:45 +0000

    Moved normalize parameters to F2_IMAGE specific parameter file
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2248 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/parameters_F2_IMAGE.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_F2_IMAGE.py
@@ -1,10 +1,19 @@
 # This parameter file contains the parameters related to the primitives located
 # in the primitives_F2_IMAGE.py file, in alphabetical order.
 
-# The standardizeStructure primitive is actually located in the primtives_F2.py
-# file, but the add_mdf parameter should be set to False as default for data
-# with an AstroData Type of IMAGE.
-{"standardizeStructure":{
+{"normalize":{
+    "suffix":{
+        # String to be post pended to the output of normalize
+        "default"       : "_normalized",
+        "recipeOverride": True,
+        "type"          : "str",
+        "userOverride"  : False,
+        },
+    },
+ # The standardizeStructure primitive is actually located in the
+ # primtives_F2.py file, but the add_mdf parameter should be set to False as
+ # default for data with an AstroData Type of IMAGE. 
+ "standardizeStructure":{
     "suffix":{
         # String to be post pended to the output of standardizeStructure
         "default"       : "_sdzStruct",
