065b15f7f7b58455bec5034e5660278b3a0fbc36
Emma Hogan <ehogan@gemini.edu>
2011-05-31 16:26:39 +0000

    Standardized the subtract_dark ulf in preprocessing/dark.py using the ulf template
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2273 e04282df-fb77-4b70-bf7a-c1223586725c

--- gempy/science/preprocessing/dark.py
+++ gempy/science/preprocessing/dark.py
@@ -2,92 +2,69 @@
 # the input dataset with a dark frame
 
 import sys
+from astrodata import Errors
+from astrodata.adutils import gemLog
 from gempy import geminiTools as gt
-from gempy import managers as man
 
-def subtract_dark(adInputs, darks=None, outNames=None, suffix=None):
+def subtract_dark(adinput=None, dark=None):
     """
-    This function will subtract the SCI of the input darks from each SCI frame 
-    of the inputs and take care of the VAR and DQ frames if they exist.  
+    This function will subtract the science extension of the input dark frames
+    from the science extension of the input science frames. The variance and
+    data quality extension will be updated, if they exist.
     
-    This is all conducted in pure Python through the arith "toolbox" of 
+    This is all conducted in pure Python through the arith 'toolbox' of 
     astrodata. 
        
     Either a 'main' type logger object, if it exists, or a null logger 
     (ie, no log file, no messages to screen) will be retrieved/created in the 
     ScienceFunctionManager and used within this function.
     
-    :param adInputs: Astrodata input science data
-    :type adInputs: Astrodata objects, either a single or a list of objects
+    :param adinput: Astrodata input science data
+    :type adinput: Astrodata
     
-    :param darks: The dark(s) to be added to the input(s).
-    :type darks: AstroData objects in a list, or a single instance.
-                Note: If there are multiple inputs and one dark provided, 
-                then the same dark will be applied to all inputs; else the 
-                darks list must match the length of the inputs.
-    
-    :param outNames: filenames of output(s)
-    :type outNames: String, either a single or a list of strings of same length 
-                    as adInputs.
-    
-    :param suffix:
-            string to add on the end of the input filenames 
-            (or outNames if not None) for the output filenames.
-    :type suffix: string
+    :param dark: The dark(s) to be added to the input(s). The darks can be a
+                 list of AstroData objects or a single AstroData object.
+                 Note: If there are multiple inputs and one dark provided, 
+                 then the same dark will be applied to all inputs; else the 
+                 darks list must match the length of the inputs.
+    :type dark: AstroData
     """
-    # Instantiate ScienceFunctionManager object
-    sfm = man.ScienceFunctionManager(adInputs, outNames, suffix,
-                                                    funcName='subtract_dark') 
-    # Perform start up checks of the inputs, prep/check of outnames, and get log
-    adInputs, outNames, log = sfm.startUp()
-    
-    # casting darks into a list if not one all ready for later indexing
-    if not isinstance(darks, list):
-        darks = [darks]
-    
-    # checking the inputs have matching filters, binning and SCI shapes.
-    gt.checkInputsMatch(adInsA=darks, adInsB=adInputs)
-    
+    # Instantiate the log. This needs to be done outside of the try block,
+    # since the log object is used in the except block 
+    log = gemLog.getGeminiLog()
+    # The validate_input function ensures that the input is not None and
+    # returns a list containing one or more AstroData objects
+    adinput = gt.validate_input(input=adinput)
+    dark = gt.validate_input(input=dark)
+    # Create a dictionary that has the AstroData objects specified by adinput
+    # as the key and the AstroData objects specified by dark as the value
+    dark_dict = gt.make_dict(key_list=adinput, value_list=dark)
+    # Define the keyword to be used for the time stamp for this user level
+    # function
+    keyword = "SUBDARK"
+    # Initialize the list of output AstroData objects
+    adoutput_list = []
     try:
-        # Set up counter for looping through outNames list
-        count=0
-        
-        # Creating empty list of ad's to be returned that will be filled below
-        adOutputs=[]
-        
-        # Loop through the inputs 
-        for ad in adInputs:  
-            # Getting the right dark for this input
-            if len(darks)>1:
-                dark = darks[count]
-            else:
-                dark = darks[0]
-           
-            # sub each dark SCI  from each input SCI and handle the updates to 
-            # the DQ and VAR frames.
-            # the sub function of the arith toolbox performs a deepcopy so
-            # it doesn't need to be done here. 
-            adOut = ad.sub(dark)
-            
-            # renaming the output ad filename
-            adOut.filename = outNames[count]
-                    
-            log.status('File name updated to '+adOut.filename+'\n')
-            
-            # Updating GEM-TLM (automatic) and SUBDARK time stamps to the PHU
-            # and updating logger with updated/added time stamps
-            sfm.mark_history(adOutputs=adOut, historyMarkKey='SUBDARK')
-        
-            # Appending to output list
-            adOutputs.append(adOut)
-    
-            count=count+1
-                
-        log.status('**FINISHED** the subtract_dark function')
-        # Return the outputs (list or single, matching adInputs)
-        return adOutputs
+        # Loop over each input AstroData object in the input list
+        for ad in adinput:
+            # Check whether the subtract_dark user level function has been
+            # run previously
+            if ad.phu_get_key_value(keyword):
+                raise Errors.InputError("%s has already been processed by " \
+                                        "subtract_dark" % (ad.filename))
+            # Subtract the dark from the adinput
+            dark = dark_dict[ad]
+            log.info("Subtracting the dark (%s) from the input AstroData " \
+                     "object %s" % (dark.filename, ad.filename))
+            ad = ad.sub(dark)
+            # Add the appropriate time stamps to the PHU
+            gt.mark_history(adinput=ad, keyword=keyword)
+            # Append the output AstroData object to the list of output
+            # AstroData objects
+            adoutput_list.append(ad)
+        # Return the list of output AstroData objects
+        return adoutput_list
     except:
-        # logging the exact message from the actual exception that was raised
-        # in the try block. Then raising a general ScienceError with message.
+        # Log the message from the exception
         log.critical(repr(sys.exc_info()[1]))
-        raise 
+        raise
