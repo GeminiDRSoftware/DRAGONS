feef571e415114fe7a056b7019ebec97d513883b
Emma Hogan <ehogan@gemini.edu>
2011-06-04 01:16:19 +0000

    Updated GMOS_IMAGE AstroData type so that it includes acquisitions (removed the MASKTYP and MASKNAME dependencies)
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2312 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/GMOS/gemdtype.GMOS_IMAGE.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/GMOS/gemdtype.GMOS_IMAGE.py
@@ -5,8 +5,6 @@ class GMOS_IMAGE(DataClassification):
         """
     parent = "GMOS"
     requirement = AND([  ISCLASS("GMOS"),
-                         PHU(MASKTYP="0"),
-                         PHU(MASKNAME="None"),
                          PHU(GRATING="MIRROR")  ])
 
 newtypes.append(GMOS_IMAGE())
