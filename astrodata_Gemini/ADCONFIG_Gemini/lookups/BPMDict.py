38149430d054becc99017cc4b1f0b6bc75680aef
Emma Hogan <ehogan@gemini.edu>
2011-06-09 23:29:45 +0000

    Added common BPM dictionary (used by the _select_bpm helper function (for add_dq ulf) in extension.py)
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2334 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/BPMDict.py
@@ -0,0 +1,8 @@
+bpm_dict = {
+    # Dictionary key is the instrument, the x binning and the y binning
+    # Dictionary value is the lookup path of the BPM for that instrument with
+    # that binning
+    "GMOS_1_1": "Gemini/GMOS/BPM/GMOS_BPM_11.fits",
+    "GMOS_2_2": "Gemini/GMOS/BPM/GMOS_BPM_22.fits",
+    "F2_1_1": "Gemini/F2/BPM/F2_bpm.fits",
+    }
