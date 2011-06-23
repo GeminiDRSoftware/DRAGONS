ab25de41fe39413b723ce7f1acaf63bc7dd86d51
Emma Hogan <ehogan@gemini.edu>
2011-06-23 00:26:48 +0000

    Updated GMOS BPMs in BPMDict.py
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2377 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/lookups/BPMDict.py
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/BPMDict.py
@@ -2,7 +2,9 @@ bpm_dict = {
     # Dictionary key is the instrument, the x binning and the y binning
     # Dictionary value is the lookup path of the BPM for that instrument with
     # that binning
-    "GMOS_1_1": "Gemini/GMOS/BPM/GMOS_BPM_11.fits",
-    "GMOS_2_2": "Gemini/GMOS/BPM/GMOS_BPM_22.fits",
+    "GMOS-N_1_1": "Gemini/GMOS/BPM/GMOS_BPM_11.fits",
+    "GMOS-N_2_2": "Gemini/GMOS/BPM/GMOS_BPM_22.fits",
+    "GMOS-S_1_1": "Gemini/GMOS/BPM/GMOS_BPM_11.fits",
+    "GMOS-S_2_2": "Gemini/GMOS/BPM/GMOS_BPM_22.fits",
     "F2_1_1": "Gemini/F2/BPM/F2_bpm.fits",
     }
