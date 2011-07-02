98bfd29f66c6b5f3e995bfd6e9c1207c35cb7a6c
Emma Hogan <ehogan@gemini.edu>
2011-07-02 03:21:35 +0000

    Re-ordered the output of the sectionStrToIntList function in string.py
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2447 e04282df-fb77-4b70-bf7a-c1223586725c

--- gempy/string.py
+++ gempy/string.py
@@ -22,12 +22,9 @@ def removeComponentID(instr):
 def sectionStrToIntList(section):
     """
     Convert the input section in the form '[x1:x2,y1:y2]' to a list in the
-    form [y1 - 1, y2, x1 - 1, x2], where x1, x2, y1 and y2 are
+    form [x1 - 1, x2, y1 - 1, y2], where x1, x2, y1 and y2 are
     integers. The values in the output list are converted to use 0-based and 
     non-inclusive indexing, making it compatible with numpy.
-    numpy follows matrix style indexing ([rows, columns]) vs fits header 
-    standards of [x1:x2,y1:y2], so the x's and y's are flipped in the return 
-    list.
     
     :param section: the section (in the form [x1:x2,y1:y2]) to be
                     converted to a list
@@ -35,7 +32,7 @@ def sectionStrToIntList(section):
     
     :rtype: list
     :return: the converted section as a list that uses 0-based and
-             non-inclusive in the form [y1 - 1, y2, x1 - 1, x2]
+             non-inclusive in the form [x1 - 1, x2, y1 - 1, y2]
     """
     # Strip the square brackets from the input section and then create a
     # list in the form ['x1:x2','y1:y2']
@@ -47,6 +44,6 @@ def sectionStrToIntList(section):
     y1 = int(xylist[1].split(':')[0]) - 1
     y2 = int(xylist[1].split(':')[1]) 
     
-    # Return the list in the form [y1 - 1, y2, x1 - 1, x2]
-    return [y1, y2, x1, x2]
+    # Return the list in the form [x1 - 1, x2, y1 - 1, y2]
+    return [x1, x2, y1, y2]
 
