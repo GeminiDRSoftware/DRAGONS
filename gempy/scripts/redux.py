358083cc9c3b11c7417ec00a630966a05e0440eb
Melanie Clarke <mclarke@gemini.edu>
2011-07-08 20:22:54 +0000

    Fixing silly mistake
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2509 e04282df-fb77-4b70-bf7a-c1223586725c

--- gempy/scripts/redux.py
+++ gempy/scripts/redux.py
@@ -77,7 +77,7 @@ if __name__=='__main__':
 
     # Call reduce with generic GMOS reduction recipe
     print "\nBeginning reduction for file %s, %s\n" % (imgname,ad.data_label()) 
-    reduce_cmd = ["qaReduce", 
+    reduce_cmd = ["reduce", 
                   "--logLevel","stdinfo",
                   "-p", "clobber=True,clob=True,add_mdf=False," + \
                         "mask_type='goodvalue'",
