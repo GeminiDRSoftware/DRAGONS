6f9e1ddd207aa97ef16264c3bd1f1ddf43db0cfa
Melanie Clarke <mclarke@gemini.edu>
2011-07-18 21:54:15 +0000

    Combining scale_fringe_to_science and subtract_fringe into a single ULF: remove_fringe
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2527 e04282df-fb77-4b70-bf7a-c1223586725c

--- gempy/science/preprocessing/sky.py
+++ gempy/science/preprocessing/sky.py
@@ -148,25 +148,14 @@ def make_fringe_image_gmos(adinput=None, operation='median',
         log.critical(repr(sys.exc_info()[1]))
         raise 
 
-def scale_fringe_to_science(adinput=None, science=None, 
-                            stats_section=None, stats_scale=True):
+def remove_fringe(adinput=None, fringe=None, 
+                  stats_section=None, stats_scale=True):
+
     """
-    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-    THIS FUNCTION WAS ORIGINALLY GOING TO BE A GENERIC SCALE_A_TO_B, BUT IT WAS
-    REALIZED THAT IT PERFORMED VERY FRINGE SPECIFIC CLIPPING DURING THE SCALING,
-    THUS IT WAS RENAMED SCALE_FRINGE_TO_SCIENCE.  A VERSION OF THIS FUNCTION 
-    THAT PERFORMS SPECIFIC THINGS FOR SKIES NEEDS TO BE CREATED, OR THIS 
-    FUNCTION NEEDS TO BE MODIFIED TO WORK FOR BOTH AND RENAMED.  IDEALLY A 
-    FUNCTION THAT COULD SCALE A TO B WOULD BE GREAT, BUT HARD TO ACCOMPLISH 
-    WITHOUT ADDING A LARGE NUMBER OF PARAMETERS (IE CLUTTER).
-    TO MAKE FUTURE REFACTORING EASIER SCIENCE INPUTS = B AND FRINGE = A, SO JUST
-    THROUGH AND CONVERT PHRASES FOR SCIENCE BACK TO B AND SIMILAR FOR FRINGES.
-    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-    
-    
-    This function will take the SCI extensions of the fringes and scale them
-    up/down to match those of science.  There are two ways to find the 
-    value to scale fringes by:
+    This function will scale the fringe extensions to the science
+    extensions, then subtract them.
+
+    There are two ways to find the value to scale fringes by:
     1. If stats_scale is set to True, the equation:
     (letting science data = b (or B), and fringe = a (or A))
     
@@ -182,25 +171,15 @@ def scale_fringe_to_science(adinput=None, science=None,
     2. If stats_scale=False, then scale will be calculated using:
     exposure time of science / exposure time of fringe
     
-    The outputs of adoutput_list will be the scaled version of adinput.
-    
-    NOTE: There MUST be a matching number of inputs for science and fringes, 
-    AND every pair of inputs MUST have matching size SCI frames.
-    
-    NOTE: If you are looking to simply perform basic scaling by a predetermined 
-    value, rather than calculating it from a second set of inputs inside this
-    function, then the .div(), .mult(), .sub() and .add() functions of the 
-    arith.py toolbox in astrodata are perfect to perform such opperations. 
-    
-    :param adinput: fringe inputs to be scaled to those of science
+    :param adinput: Astrodata input science data
     :type adinput: Astrodata objects, either a single or a list of objects
-                   Note: there must be an equal number of science as adinput
     
-    :param science: Astrodata inputs to have those of adinput scaled to.
-    :type science: AstroData objects in a list, or a single instance.
-                     Note: there must be an equal number of science as adinput
-                     Note: no changes will be made to the science images.
-                     
+    :param fringe: The fringe(s) to be scaled and subtracted from the input(s).
+    :type fringe: AstroData objects in a list, or a single instance.
+                Note: If there are multiple inputs and one fringe provided, 
+                then the same fringe will be applied to all inputs; else the 
+                fringe list must match the length of the inputs.
+
     :param stats_section: sections of detectors to use for calculating
                           the statistics
     :type stats_section: Dictionary of the format:
@@ -216,51 +195,58 @@ def scale_fringe_to_science(adinput=None, science=None,
     # instantiate log
     log = gemLog.getGeminiLog()
 
-    # ensure that adinput and science are not None and make 
+    # ensure that adinput and fringe are not None and make 
     # them into lists if they are not already
     adinput = gt.validate_input(adinput=adinput)
-    science = gt.validate_input(adinput=science)
+    fringe = gt.validate_input(adinput=fringe)
 
     # time stamp keyword
-    keyword = 'SCALEFRG'
+    keyword = 'RMFRINGE'
     
     # initialize output list
     adoutput_list = []
     
     try:
 
-        # check the inputs have matching filters, binning and SCI shapes.
-        gt.checkInputsMatch(adInsA=adinput, adInsB=science)
+        # if only one fringe is provided, make it into a list that
+        # matches adinput
+        if len(fringe)!=len(adinput):
+            if len(fringe)==1:
+                fringe = [fringe[0] for ad in adinput]
+            else:
+                raise Errors.inputError('Length of fringe list is not 1 and ' +
+                                        'does not match length of adinput ' +
+                                        'list.')
 
-        # Loop through the inputs to perform scaling of fringes to the science
+        # check the inputs have matching filters, binning and SCI shapes.
+        gt.checkInputsMatch(adInsA=adinput, adInsB=fringe)
+
+        # Loop through the inputs to perform scaling of fringes to the science 
         # NOTE: for clarity and simplicity, fringes objects are type 'A' and 
         #       science input objects are type 'B'.
         count=0
-        for adA in adinput:  
+        for ad in adinput:  
             
-            # Clean up log and screen if multiple inputs
-            log.fullinfo('+'*50, category='format')
-
             # set up empty dict to hold scale vals for each extension
             scaleDict = {}
-            # get matching B input
-            adB = science[count]
+            # get matching fringe
+            this_fringe = fringe[count]
             log.info('Scaling this fringe to input (%s):\n%s' %
-                        (adB.filename,adA.filename))
+                        (ad.filename,this_fringe.filename))
             
-            for sciExtA in adA['SCI']:
-                # Grab the A and B SCI extensions to operate on
-                curExtver = sciExtA.extver()
-                sciExtB = adB[('SCI', curExtver)]
+            for sciExt in ad['SCI']:
+                # Grab the fringe and science SCI extensions to operate on
+                curExtver = sciExt.extver()
+                fringeExt = this_fringe[('SCI', curExtver)]
                 
                 log.fullinfo('Scaling SCI extension '+str(curExtver))
                 
                 if stats_scale:
                     # use statistics to calculate the scaling factor, following
-                    # arrayB = where({where[sciExtB < 
-                    #                       (sciExtB.median+2.5*sciExtB.std)]} 
-                    #                 > [sciExtB.median-3*sciExtB.std])
-                    # scale = arrayB.std / sciExtA.std
+                    # masked_sci = where({where[sciExt < 
+                    #                    (sciExt.median+2.5*sciExt.std)]} 
+                    #                 > [sciExt.median-3*sciExt.std])
+                    # scale = masked_sci.std / fringeExt.std
                     log.info('Using statistics to calculate the ' +
                                'scaling factor')
                     # Get current SCI's stats_section
@@ -269,11 +255,10 @@ def scale_fringe_to_science(adinput=None, science=None,
                         
                         # Get the data section as a int list of form:
                         # [y1, y2, x1, x2] 0-based and non-inclusive
-                        datsecAlist = sciExtA.data_section().as_pytype()
-                        dAl = datsecAlist
+                        sds = sciExt.data_section().as_pytype()
                         # Take 100 pixels off each side
-                        curStatsecList = [dAl[0]+100,dAl[1]-100,
-                                          dAl[2]+100,dAl[3]-100]
+                        curStatsecList = [sds[0]+100,sds[1]-100,
+                                          sds[2]+100,sds[3]-100]
                     else:
                         # pull value from stats_section dict provided
                         if isinstance(stats_section,dict):
@@ -289,56 +274,58 @@ def scale_fringe_to_science(adinput=None, science=None,
                              'calculate the scaling factor')      
                     # pull the data arrays from the extensions, 
                     # for the stats_section region
-                    A = sciExtA.data[cl[0]:cl[1],cl[2]:cl[3]]
-                    B = sciExtB.data[cl[0]:cl[1],cl[2]:cl[3]]
+                    s = sciExt.data[cl[0]:cl[1],cl[2]:cl[3]]
+                    f = fringeExt.data[cl[0]:cl[1],cl[2]:cl[3]]
                     # Must flatten for compatibility with older
                     # versions of numpy    
 
-                    # B's median
-                    Bmed = np.median(B.flatten()) 
-                    # B's standard deviation
-                    Bstd = B.std()
+                    # science median
+                    smed = np.median(s.flatten()) 
+                    # fringe standard deviation
+                    sstd = s.std()
 
                     # make an array of all the points where the pixel value is 
                     # less than the median value + 2.5 x the standard deviation.
-                    Bbelow = B[np.where(B<(Bmed+(2.5*Bstd)))]  
+                    sbelow = s[np.where(s<(smed+(2.5*sstd)))]  
 
                     # make an array from the previous one where all the pixels  
                     # in it have a value greater than the median -3 x the 
                     # standard deviation. Thus a final array of all the pixels 
                     # with values between (median + 2.5xstd) and (median -3xstd)
-                    Bmiddle = Bbelow[np.where(Bbelow>(Bmed-(3.*Bstd)))]
+                    smiddle = sbelow[np.where(sbelow>(smed-(3.*sstd)))]
 
                     ######## NOTE: kathleen believes the median should #########
                     ########       be used below instead of the std    #########
                     ### This needs real scientific review and discussion with ##
                     ### DA's to make a decision as to what is appropriate/works#
-                    curScale = Bmiddle.std() / A.std() 
+                    curScale = smiddle.std() / f.std() 
                 
                 else:
                     # use the exposure times to calculate the scale
                     log.info('Using exposure times to calculate the scaling'+
                              ' factor')
-                    curScale = sciExtB.exposure_time() / sciExtA.exposure_time()
+                    curScale = sciExt.exposure_time()/fringeExt.exposure_time()
                 
                 log.info('Scale factor found = '+str(curScale))
                 
                 # load determined scale for this extension into scaleDict    
-                scaleDict[('SCI',sciExtA.extver())] = curScale
-                
-            # Use mult from the arith toolbox to perform the scaling of 
-            # A (fringe input) to B (science input), it does deepcopy
-            # so none needed here.
-            ad_out = adA.mult(input_b=scaleDict)          
+                scaleDict[('SCI',sciExt.extver())] = curScale
             
-            # Update GEM-TLM (automatic) and SUBDARK time stamps to the PHU
+            # Use mult from the arith toolbox to perform the scaling of 
+            # the fringe frame
+            scaled_fringe = this_fringe.mult(scaleDict)          
+            
+            # Subtract the scaled fringe from the science
+            ad_out = ad.sub(scaled_fringe)
+
+            # Update GEM-TLM (automatic) and RMFRINGE time stamps to the PHU
             # and update logger with updated/added time stamps
             gt.mark_history(adinput=ad_out, keyword=keyword)
         
             # Append to output list
             adoutput_list.append(ad_out)
     
-            count=count+1
+            count+=1
                 
         # Return the output list
         # These are the scaled fringe ad's
@@ -349,79 +336,3 @@ def scale_fringe_to_science(adinput=None, science=None,
         log.critical(repr(sys.exc_info()[1]))
         raise 
 
-def subtract_fringe(adinput=None, fringe=None):
-    """
-    This function will subtract the SCI of the input fringes from each SCI frame
-    of the inputs and take care of the VAR and DQ frames if they exist.  
-    
-    This is all conducted in pure Python through the arith "toolbox" of 
-    astrodata. 
-
-    Either a 'main' type logger object, if it exists, or a null logger 
-    (ie, no log file, no messages to screen) will be retrieved/created 
-    and used within this function.
-
-    :param adinput: Astrodata input science data
-    :type adinput: Astrodata objects, either a single or a list of objects
-    
-    :param fringe: The fringe(s) to be added to the input(s).
-    :type fringe: AstroData objects in a list, or a single instance.
-                Note: If there are multiple inputs and one fringe provided, 
-                then the same fringe will be applied to all inputs; else the 
-                fringe list must match the length of the inputs.
-    """
-
-    # instantiate log
-    log = gemLog.getGeminiLog()
-
-    # ensure that adinput and fringe are not None and make 
-    # them into lists if they are not already
-    adinput = gt.validate_input(adinput=adinput)
-    fringe = gt.validate_input(adinput=fringe)
-
-    # time stamp keyword
-    keyword = 'SUBFRING'
-    
-    # initialize output list
-    adoutput_list = []  
-    
-    try:
-
-        # check the inputs have matching filters, binning and SCI shapes.
-        gt.checkInputsMatch(adInsA=fringe, adInsB=adinput)
-            
-        # Loop through the input
-        count=0
-        for ad in adinput:  
-
-            # Clean up log and screen if multiple inputs
-            log.fullinfo('+'*50, category='format')    
-
-            # Get the right fringe for this input
-            if len(fringe)>1:
-                this_fringe = fringe[count]
-            else:
-                this_fringe = fringe[0]
-           
-            # sub each fringe SCI  from each input SCI and handle the updates to
-            # the DQ and VAR frames.
-            # the sub function of the arith toolbox performs a deepcopy so
-            # it doesn't need to be done here. 
-            ad_out = ad.sub(this_fringe)
-            
-            # Update GEM-TLM (automatic) and SUBFRINGE time stamps to the PHU
-            # and update logger with updated/added time stamps
-            gt.mark_history(adinput=ad_out, keyword=keyword)
-        
-            # Append to output list
-            adoutput_list.append(ad_out)
-    
-            count=count+1
-                
-        # Return the outputs list
-        return adoutput_list
-    except:
-        # log the exact message from the actual exception that was raised
-        # in the try block. Then raise a general ScienceError with message.
-        log.critical(repr(sys.exc_info()[1]))
-        raise
