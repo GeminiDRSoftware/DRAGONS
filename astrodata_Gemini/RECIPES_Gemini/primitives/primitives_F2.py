9ef2d7a566448c12a38a3d2aa6bc69a4ff60a313
Emma Hogan <ehogan@gemini.edu>
2011-07-27 23:38:43 +0000

    Minor 'clean up' edits to F2 specific primitive file
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2585 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/primitives_F2.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/primitives_F2.py
@@ -19,23 +19,22 @@ class F2Primitives(GEMINIPrimitives):
         """
         This primitive is used to make the changes and additions to the
         keywords in the headers of FLAMINGOS-2 data, specifically.
-        
-        :param loglevel: Verbosity setting for log messages to the screen.
-                         0 = nothing to screen, 6 = everything to screen. OR
-                         the message level as a string (i.e., 'critical',
-                         'status', 'fullinfo' ...)
-        :type loglevel: integer or string
         """
+        
         # Instantiate the log
         log = gemLog.getGeminiLog(logType=rc["logType"],
                                   logLevel=rc["logLevel"])
+        
         # Log the standard "starting primitive" debug message
         log.debug(gt.log_message("primitive", "standardizeHeaders",
                                  "starting"))
+        
         # Initialize the list of output AstroData objects
         adoutput_list = []
+        
         # Loop over each input AstroData object in the input list
         for ad in rc.get_inputs(style="AD"):
+            
             # Check whether the standardizeHeaders primitive has been run
             # previously
             if ad.phu_get_key_value("SDZHDRSI"):
@@ -45,11 +44,14 @@ class F2Primitives(GEMINIPrimitives):
                 # AstroData objects without further processing
                 adoutput_list.append(ad)
                 continue
+            
             # Call the standardize_headers_f2 user level function
             ad = sdz.standardize_headers_f2(adinput=ad)
+            
             # Append the output AstroData object (which is currently in the
             # form of a list) to the list of output AstroData objects
             adoutput_list.append(ad[0])
+        
         # Report the list of output AstroData objects to the reduction
         # context
         rc.report_output(adoutput_list)
@@ -60,23 +62,22 @@ class F2Primitives(GEMINIPrimitives):
         """
         This primitive is used to standardize the structure of FLAMINGOS-2
         data, specifically.
-        
-        :param loglevel: Verbosity setting for log messages to the screen.
-                         0 = nothing to screen, 6 = everything to screen. OR
-                         the message level as a string (i.e., 'critical',
-                         'status', 'fullinfo' ...)
-        :type loglevel: integer or string
         """
+        
         # Instantiate the log
         log = gemLog.getGeminiLog(logType=rc["logType"],
                                   logLevel=rc["logLevel"])
+        
         # Log the standard "starting primitive" debug message
         log.debug(gt.log_message("primitive", "standardizeStructure",
                                  "starting"))
+        
         # Initialize the list of output AstroData objects
         adoutput_list = []
+        
         # Loop over each input AstroData object in the input list
         for ad in rc.get_inputs(style="AD"):
+            
             # Check whether the standardizeStructure primitive has been run
             # previously
             if ad.phu_get_key_value("SDZSTRUC"):
@@ -86,13 +87,16 @@ class F2Primitives(GEMINIPrimitives):
                 # AstroData objects without further processing
                 adoutput_list.append(ad)
                 continue
+            
             # Call the standardize_structure_f2 user level function
             ad = sdz.standardize_structure_f2(adinput=ad,
                                               attach_mdf=rc["attach_mdf"],
                                               mdf=rc["mdf"])
+            
             # Append the output AstroData object (which is currently in the
             # form of a list) to the list of output AstroData objects
             adoutput_list.append(ad[0])
+        
         # Report the list of output AstroData objects to the reduction
         # context
         rc.report_output(adoutput_list)
@@ -102,22 +106,21 @@ class F2Primitives(GEMINIPrimitives):
     def validateData(self, rc):
         """
         This primitive is used to validate FLAMINGOS-2 data, specifically.
-        
-        :param loglevel: Verbosity setting for log messages to the screen.
-                         0 = nothing to screen, 6 = everything to screen. OR
-                         the message level as a string (i.e., 'critical',
-                         'status', 'fullinfo' ...)
-        :type loglevel: integer or string
         """
+        
         # Instantiate the log
         log = gemLog.getGeminiLog(logType=rc["logType"],
                                   logLevel=rc["logLevel"])
+        
         # Log the standard "starting primitive" debug message
         log.debug(gt.log_message("primitive", "validateData", "starting"))
+        
         # Initialize the list of output AstroData objects
         adoutput_list = []
+        
         # Loop over each input AstroData object in the input list
         for ad in rc.get_inputs(style="AD"):
+            
             # Check whether the validateData primitive has been run previously
             if ad.phu_get_key_value("VALDATA"):
                 log.warning("%s has already been processed by validateData" \
@@ -126,11 +129,14 @@ class F2Primitives(GEMINIPrimitives):
                 # AstroData objects without further processing
                 adoutput_list.append(ad)
                 continue
+            
             # Call the validate_data_f2 user level function
             ad = sdz.validate_data_f2(adinput=ad, repair=rc["repair"])
+            
             # Append the output AstroData object (which is currently in the
             # form of a list) to the list of output AstroData objects
             adoutput_list.append(ad[0])
+        
         # Report the list of output AstroData objects to the reduction
         # context
         rc.report_output(adoutput_list)
