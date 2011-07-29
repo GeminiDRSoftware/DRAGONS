a01813af6255fb3134a341961dc857f1bdf5a6c7
Emma Hogan <ehogan@gemini.edu>
2011-07-29 17:04:41 +0000

    Added lookup table containing all the timestamp keywords that will be used in each ulf and corresponding primitive
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2622 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/timestamp_keywords.py
@@ -0,0 +1,36 @@
+timestamp_keys = {
+    # Dictionary key is the name of the user level function (in alphabetical
+    # order)
+    # Dictionary value is the timestamp keyword that should be used when
+    # writing a timestamp keyword to the header once the user level function
+    # has completed
+    "add_dq": "ADDDQ",
+    "add_mdf": "ADDMDF",
+    "add_objcat": "ADDOBCAT",
+    "add_var": "ADDVAR",
+    "adu_to_electrons": "ADUTOELE",
+    "align_to_reference_image": "ALIGN",
+    "correct_wcs_to_reference_image": "CORRWCS",
+    "detect_sources": "DETECSRC",
+    "divide_by_flat": "DIVFLAT",
+    "make_fringe_image_gmos": "FRINGE",
+    "measure_iq": "MEASREIQ",
+    "mosaic_detectors": "MOSAIC",
+    "nonlinearity_correct": "LINCORR",
+    "normalize": "NORMLIZE",
+    "normalize_image": "NORMLIZE",
+    "normalize_image_gmos": "NORMLIZE",
+    "prepare": "PREPARE",
+    "remove_fringe": "RMFRINGE",
+    "standardize_headers_f2": "SDZHDRSI",
+    "standardize_headers_gemini": "SDZHDRSG",
+    "standardize_headers_gmos": "SDZHDRSI",
+    "standardize_structure_f2": "SDZSTRUC",
+    "standardize_structure_gmos": "SDZSTRUC",
+    "subtract_bias": "SUBBIAS",
+    "subtract_dark": "SUBDARK",
+    "subtract_overscan_gmos": "SUBOVER",
+    "trim_overscan": "TRIMOVER",
+    "validate_data_f2": "VALDATA",
+    "validate_data_gmos": "VALDATA",
+    }
