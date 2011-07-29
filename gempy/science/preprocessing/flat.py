b7ec48e18942b3c0e16e54818616c52f9141b91b
Emma Hogan <ehogan@gemini.edu>
2011-07-29 19:12:31 +0000

    Added missing import in flat.py
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2638 e04282df-fb77-4b70-bf7a-c1223586725c

--- gempy/science/preprocessing/flat.py
+++ gempy/science/preprocessing/flat.py
@@ -4,6 +4,7 @@
 import sys
 import numpy as np
 from astrodata import Errors
+from astrodata import Lookups
 from astrodata.adutils import gemLog
 from astrodata.adutils.gemutil import pyrafLoader
 from gempy import geminiTools as gt
