1a2de012a90d0bf2dac91b16e726cb53dc99756a
Emma Hogan <ehogan@gemini.edu>
2011-07-30 01:16:32 +0000

    Added new lines for clarity in F2 specific descriptor file
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2655 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/descriptors/F2/F2_Descriptor.py
+++ astrodata_Gemini/ADCONFIG_Gemini/descriptors/F2/F2_Descriptor.py
@@ -24,6 +24,7 @@ class F2_DescriptorCalc(GEMINI_DescriptorCalc):
     
     def data_section(self, dataset, pretty=False, **args):
         data_section = "[1:2048,1:2048]"
+        
         if pretty:
             # Return the data section string that uses 1-based indexing as the
             # value in the form [x1:x2,y1:y2]
@@ -41,23 +42,26 @@ class F2_DescriptorCalc(GEMINI_DescriptorCalc):
         # (stdkeyDictF2) but are read from the updated global key dictionary
         # (self.get_descriptor_key)
         lnrs = dataset.phu_get_key_value(self.get_descriptor_key("key_lnrs"))
+        
         if lnrs is None:
             # The phu_get_key_value() function returns None if a value cannot
             # be found and stores the exception info. Re-raise the exception.
             # It will be dealt with by the CalculatorInterface.
             if hasattr(dataset, "exception_info"):
                 raise dataset.exception_info
+        
         if lnrs in getattr(self, "f2ArrayDict"):
             gain = self.f2ArrayDict[lnrs][1]
         else:
             raise Errors.TableKeyError()
+        
         # Return the gain float
         ret_gain = float(gain)
         
         return ret_gain
     
     f2ArrayDict = None
-
+    
     def instrument(self, dataset, **args):
         ret_instrument = "F2"
         
@@ -69,24 +73,29 @@ class F2_DescriptorCalc(GEMINI_DescriptorCalc):
         # (stdkeyDictF2) but are read from the updated global key dictionary
         # (self.get_descriptor_key)
         lnrs = dataset.phu_get_key_value(self.get_descriptor_key("key_lnrs"))
+        
         if lnrs is None:
             # The phu_get_key_value() function returns None if a value cannot
             # be found and stores the exception info. Re-raise the exception.
             # It will be dealt with by the CalculatorInterface.
             if hasattr(dataset, "exception_info"):
                 raise dataset.exception_info
+        
         # Get the saturation level using the appropriate descriptor
         saturation_level = dataset.saturation_level()
+        
         if saturation_level is None:
             # The descriptor functions return None if a value cannot be found
             # and stores the exception info. Re-raise the exception. It will be
             # dealt with by the CalculatorInterface.
             if hasattr(dataset, "exception_info"):
                 raise dataset.exception_info
+        
         if lnrs in getattr(self, "f2ArrayDict"):
             non_linear_fraction = self.f2ArrayDict[lnrs][3]
         else:
             raise Errors.TableKeyError()
+        
         # Return the read noise float
         ret_non_linear_level = int(saturation_level * non_linear_fraction)
         
@@ -100,16 +109,19 @@ class F2_DescriptorCalc(GEMINI_DescriptorCalc):
         # (stdkeyDictF2) but are read from the updated global key dictionary
         # (self.get_descriptor_key)
         lnrs = dataset.phu_get_key_value(self.get_descriptor_key("key_lnrs"))
+        
         if lnrs is None:
             # The phu_get_key_value() function returns None if a value cannot
             # be found and stores the exception info. Re-raise the exception.
             # It will be dealt with by the CalculatorInterface.
             if hasattr(dataset, "exception_info"):
                 raise dataset.exception_info
+        
         if lnrs in getattr(self, "f2ArrayDict"):
             read_noise = self.f2ArrayDict[lnrs][0]
         else:
             raise Errors.TableKeyError()
+        
         # Return the read noise float
         ret_read_noise = float(read_noise)
         
@@ -123,16 +135,19 @@ class F2_DescriptorCalc(GEMINI_DescriptorCalc):
         # (stdkeyDictF2) but are read from the updated global key dictionary
         # (self.get_descriptor_key)
         lnrs = dataset.phu_get_key_value(self.get_descriptor_key("key_lnrs"))
+        
         if lnrs is None:
             # The phu_get_key_value() function returns None if a value cannot
             # be found and stores the exception info. Re-raise the exception.
             # It will be dealt with by the CalculatorInterface.
             if hasattr(dataset, "exception_info"):
                 raise dataset.exception_info
+        
         if lnrs in getattr(self, "f2ArrayDict"):
             saturation_level = self.f2ArrayDict[lnrs][2]
         else:
             raise Errors.TableKeyError()
+        
         # Return the read noise float
         ret_saturation_level = int(saturation_level)
         
@@ -144,12 +159,14 @@ class F2_DescriptorCalc(GEMINI_DescriptorCalc):
         # Get the y offset from the header of the PHU.
         y_offset = dataset.phu_get_key_value(
             self.get_descriptor_key("key_y_offset"))
+        
         if y_offset is None:
             # The phu_get_key_value() function returns None if a value cannot
             # be found and stores the exception info. Re-raise the exception.
             # It will be dealt with by the CalculatorInterface.
             if hasattr(dataset, "exception_info"):
                 raise dataset.exception_info
+        
         ret_x_offset = -y_offset
         
         return ret_x_offset
@@ -158,12 +175,14 @@ class F2_DescriptorCalc(GEMINI_DescriptorCalc):
         # Get the x offset from the header of the PHU.
         x_offset = dataset.phu_get_key_value(
             self.get_descriptor_key("key_x_offset"))
+        
         if x_offset is None:
             # The phu_get_key_value() function returns None if a value cannot
             # be found and stores the exception info. Re-raise the exception.
             # It will be dealt with by the CalculatorInterface.
             if hasattr(dataset, "exception_info"):
                 raise dataset.exception_info
+        
         ret_y_offset = -x_offset
         
         return ret_y_offset
