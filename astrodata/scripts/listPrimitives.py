6e976237151ae4233291682feb1f26b09244331a
Kaniela Dement <kdement@gemini.edu>
2011-07-30 02:40:47 +0000

    improved style added dir check for args
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2659 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/scripts/listPrimitives.py
+++ astrodata/scripts/listPrimitives.py
@@ -1,57 +1,64 @@
 #!/usr/bin/env python
-#
-# Author: C. Allen and K. Dement
-# Date: April 2010
-# Desciption: Tool to inspect Primitives
+import os
 
-import os, sys
-from astrodata.PrimInspect import PrimInspect
 from optparse import OptionParser
 
-#Instantiate OptionParser
+from astrodata.PrimInspect import PrimInspect
+
+# set up commandline args and options
 parser = OptionParser()
-parser.set_description( 
-"""The Gemini Primitive Inspection Tool.\n Created by C.Allen, K.Dement, Apr2010.""" )
-
-parser.add_option('-c', '--useColor', action='store_true', dest='useColor', default=False,
-                  help='Apply color output scheme')
-parser.add_option('-f', '--makeOutputFile', action='store_true', dest='makeOutputFile', default=False,
-                  help='Make output textfile (primitives_List.txt).')
-parser.add_option('-i', '--showInfo', action='store_true', dest='showInfo', default=False,
-                  help='Show information on Primitive class name and location')
-parser.add_option('-p', '--showParams', action='store_true', dest='showParams', default=False,
-                  help='include parameter information in output')
-parser.add_option('-s', '--showSetsOnly', action='store_true', dest='showSetsOnly', default=False,
-                  help='Show only Primitives sets')
-parser.add_option('-u', '--showUsage', action='store_true', dest='showUsage', default=False,
-                  help='Include usage information in output')
-parser.add_option('-v', '--verbose', action='store_true', dest='verbose', default=False,
-                  help='Include ALL information about Primitives')
+parser.set_description( "Gemini Observatory Primitive Inspection Tool "
+                       "(v_1.0 2011)")
+parser.add_option('-c', '--use-color', action='store_true', dest='use_color',
+                  default=False, help='apply color output scheme')
+parser.add_option('-f', '--make-file', action='store_true', 
+                  dest='make_file', default=False,
+                  help='write report to file (primitives_list.txt).')
+parser.add_option('-i', '--show-info', action='store_true', dest='show_info',
+                  default=False,
+                  help='show information on Primitive class name and location')
+parser.add_option('-p', '--show-param', action='store_true', dest='show_param',
+                  default=False,
+                  help='show parameters')
+parser.add_option('-s', '--show-set', action='store_true', 
+                  dest='show_set', default=False,
+                  help='show only primitve sets')
+parser.add_option('-u', '--show-usage', action='store_true', dest='show_usage',
+                  default=False, help='show usage')
+parser.add_option('-v', '--verbose', action='store_true', dest='verbose',
+                  default=False, help='set verbose mode')
 (options,  args) = parser.parse_args()
-options.datasets = []
-options.astrotypes = []
-options.args = args
+optc = options.use_color
+optp = options.show_param
+optu = options.show_usage
+opti = options.show_info
+optv = options.verbose
+optf = options.make_file
+
+# distinguish between data and astrotype in args
+datasets = []
+astrotypes = []
 for arg in args:
-    if os.path.exists(arg):
-        options.datasets.append(arg)
+    if os.path.exists(arg) and not os.path.isdir(arg):
+        datasets.append(arg)
     else:
-        options.astrotypes.append(arg)
+        astrotypes.append(arg)
 
-#Instantiate PrimInspectObject
-pin = PrimInspect(options)
-
-#Proceed to show Primitives
-pin.datasets = options.datasets
-pin.astrotypes = options.astrotypes
+# create primitive inspect object
+pin = PrimInspect(use_color=optc, show_param=optp, show_usage=optu,
+                  show_info=opti, make_file=optf, verbose=optv)
+pin.datasets = datasets
+pin.astrotypes = astrotypes
 pin.build_dictionaries()
 primsets = pin.primsdict.keys()
 
 primsets.sort(pin.primsetcmp)
 
 names = []
-if options.showSetsOnly:
-    pin.show("\n\n\t\t\t\t${BOLD}PRIMITIVE SETS${NORMAL}")
-    pin.show("-"*80) 
+if options.show_set:
+    pin.show("\n" + "="*79) 
+    pin.show("${BOLD}PRIMITIVE SETS${NORMAL}")
+    pin.show("="*79) 
     count = 1
 for primset in primsets:
     nam = primset.__class__.__name__
@@ -59,20 +66,16 @@ for primset in primsets:
         continue
     else:
         names.append(nam)    
-    if options.showSetsOnly:
+    if options.show_set:
         cl = pin.name2class[ nam ]
-        pin.show("\n\n%2d. ${BOLD}%s${NORMAL}\n" %(count,cl.astrotype))
+        if len(primsets) == 1:
+            pin.show("\n  ${BOLD}%s${NORMAL}\n" % cl.astrotype)
+        else:
+            pin.show("\n%2d. ${BOLD}%s${NORMAL}\n" %(count,cl.astrotype))
         primlist = pin.primsdict_kbn[nam]
         pin.show_set_info(nam, cl, primlist) 
-        count+=1
+        count += 1
     else:
         pin.showPrims(nam)
-if options.showSetsOnly:
-    pin.show("\n\n") 
-    pin.show("-"*80)
-else:
-    pin.show("_"*80)
-pin.close_fhandler()
+pin.show("\n" + "="*79) 
 
-
-#------------------------------- eof
