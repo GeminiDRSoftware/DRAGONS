f5981651e2be15b36c31ac69ba74524bcb5b997e
Melanie Clarke <mclarke@gemini.edu>
2011-08-02 01:51:45 +0000

    Updating descriptor dictionary
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2669 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/scripts/header_cal_rq.py
+++ astrodata/scripts/header_cal_rq.py
@@ -4,23 +4,23 @@ from astrodata import AstroData
 import urllib2, urllib
 
 # This is a GMOS_N imaging science dataset
-ad = AstroData("/home/callen/SVN-AD/gemini_python/test_data/calsearch/N20110531S0114.fits")
+ad = AstroData("/Users/mclarke/DPD/test/gireduce/N20110313S0188_prepared.fits")
 
 desc_dict = {'instrument':ad.instrument().for_db(),
+             'observation_type': ad.observation_type().for_db(),
              'data_label':ad.data_label().for_db(),
              'detector_x_bin':ad.detector_x_bin().for_db(),
              'detector_y_bin':ad.detector_y_bin().for_db(),
              'read_speed_setting':ad.read_speed_setting().for_db(),
              'gain_setting':ad.gain_setting().for_db(),
-             'amp_read_area':ad.amp_read_area(asDict=True).for_db(),
-             'ut_datetime':str(ad.ut_datetime().for_db()),
+             'amp_read_area':ad.amp_read_area().for_db(),
+             'ut_datetime':ad.ut_datetime().for_db(),
              'exposure_time':ad.exposure_time().for_db(),
-             'nodandshuffle':ad.is_type('GMOS_NODANDSHUFFLE'),
-             'observation_type': ad.observation_type().for_db(),
-             'spectroscopy': ad.is_type("SPECT"),
-             'object': ad.object().for_db()
-             
+             'object': ad.object().for_db(),
+             'filter_name':ad.filter_name().for_db(),
+             'focal_plane_mask':ad.focal_plane_mask().for_db(),
              }
+
 print repr(desc_dict)
 type_list = ad.types
 ad.close()
@@ -29,7 +29,7 @@ sequence = [('descriptors', desc_dict), ('types', type_list)]
 postdata = urllib.urlencode(sequence)
 
 #postdata = urllib.urlencode({"hello":1.})
-url = "http://hbffits2.hi.gemini.edu/calmgr/bias/"
+url = "http://hbffits3.hi.gemini.edu/calmgr/processed_flat/"
 #url = "http://hbffits1/calmgr/processed_bias/"
 # u = urllib.urlopen(url, postdata)
 try:
