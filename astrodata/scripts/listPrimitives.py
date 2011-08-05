40924943d9ee37d972e91611466da2a983052830
Kaniela Dement <kdement@gemini.edu>
2011-08-05 01:14:38 +0000

    added recipe listing
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2687 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/scripts/listPrimitives.py
+++ astrodata/scripts/listPrimitives.py
@@ -11,6 +11,8 @@ parser.set_description( "Gemini Observatory Primitive Inspection Tool "
                        "(v_1.0 2011)")
 parser.add_option("-c", "--use-color", action="store_true", dest="use_color",
                   default=False, help="apply color output scheme")
+parser.add_option("-e", "--engineering", action="store_true", dest="engineering",
+                  default=False, help="include engineering recipes")
 parser.add_option("-f", "--copy-tofile", action="store_true", 
                   dest="copy_tofile", default=False,
                   help="write to file (primitives_list.txt).")
@@ -20,15 +22,20 @@ parser.add_option("-i", "--info", action="store_true", dest="info",
 parser.add_option("-p", "--parameters", action="store_true", dest="parameters",
                   default=False,
                   help="show parameters")
+parser.add_option("-r", "--recipes", action="store_true", dest="recipes",
+                  default=False, help="list top recipes")
 parser.add_option("-s", "--primitive-set", action="store_true", 
                   dest="primitive_set", default=False,
-                  help="show primitive info by set")
+                  help="show primitive by set")
 parser.add_option("-u", "--usage", action="store_true", dest="usage",
                   default=False, help="show usage")
 parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
                   default=False, help="set verbose mode")
 parser.add_option("--primitives", action="store_true", dest="primitives",
                   default=False, help="show all primitives for set")
+parser.add_option("--view-recipe", dest="view_recipe", 
+                  default=None, help="show all primitives for set")
+
 (options,  args) = parser.parse_args()
 optc = options.use_color
 optp = options.parameters
@@ -36,26 +43,29 @@ optu = options.usage
 opti = options.info
 optv = options.verbose
 optf = options.copy_tofile
+opte = options.engineering
+oview = options.view_recipe
 options.primitives = True
 
 # distinguish between data and astrotype in args
 datasets = []
 astrotypes = []
-for arg in args:
-    if os.path.exists(arg) and not os.path.isdir(arg):
-        datasets.append(arg)
-    else:
-        astrotypes.append(arg)
 
-# create primitive inspect object
-pin = PrimInspect(use_color=optc, show_param=optp, show_usage=optu,
-                  show_info=opti, make_file=optf, verbose=optv,
-                  datasets=datasets, astrotypes=astrotypes)
-
-# execution
-if options.primitive_set:
-    pin.show_primitive_sets()
-elif options.primitives:
-    pin.show_primitive_sets(prims=True)
+if options.recipes or oview:
+    pin = PrimInspect(use_color=optc)
+    pin.list_recipes(pkg="Gemini",eng=opte, view=oview)
 else:
-    print("ERROR: Incorrect options to use tool")
+    for arg in args:
+        if os.path.exists(arg) and not os.path.isdir(arg):
+            datasets.append(arg)
+        else:
+            astrotypes.append(arg)
+    pin = PrimInspect(use_color=optc, show_param=optp, show_usage=optu,
+                      show_info=opti, make_file=optf, verbose=optv,
+                      datasets=datasets, astrotypes=astrotypes)
+
+    # execution
+    if options.primitive_set:
+        pin.show_primitive_sets()
+    elif options.primitives:
+        pin.show_primitive_sets(prims=True)
