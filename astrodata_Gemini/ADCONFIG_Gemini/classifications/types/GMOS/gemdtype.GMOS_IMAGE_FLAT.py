58fec40bfdcbd88455e0e71f4394a05699f1f02e
Melanie Clarke <mclarke@gemini.edu>
2011-08-24 20:06:18 +0000

    Adding to the type definition so it will recognize twilight flats
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2762 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/GMOS/gemdtype.GMOS_IMAGE_FLAT.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/GMOS/gemdtype.GMOS_IMAGE_FLAT.py
@@ -4,8 +4,11 @@ class GMOS_IMAGE_FLAT(DataClassification):
         Applies to all imaging flat datasets from the GMOS instruments
         """
     parent = "GMOS_IMAGE"
-    requirement = AND([  ISCLASS("GMOS_IMAGE"),
-                         PHU(OBSTYPE="FLAT"),
-                         NOT(ISCLASS("GMOS_IMAGE_TWILIGHT"))  ])
+    requirement = OR(AND([  ISCLASS("GMOS_IMAGE"),
+                            PHU(OBSTYPE="FLAT"),
+                            NOT(ISCLASS("GMOS_IMAGE_TWILIGHT"))  ]),
+                     AND([  ISCLASS("GMOS_IMAGE"),
+                            OR([PHU(OBJECT="Twilight"),
+                                PHU(OBJECT="twilight")])  ]))
 
 newtypes.append(GMOS_IMAGE_FLAT())
