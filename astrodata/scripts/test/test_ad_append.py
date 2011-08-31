41d4d9406dfebd50c2a70c7f23b582dbf472b684
Kaniela Dement <kdement@gemini.edu>
2011-08-31 00:03:48 +0000

    fixed typo
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@2821 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/scripts/test/test_ad_append.py
+++ astrodata/scripts/test/test_ad_append.py
@@ -10,20 +10,21 @@ def ad_append_test1():
     """
     ad1 = AstroData(file_urls.testdatafile_1) #sci 3
     ad3 = AstroData(file_urls.testdatafile_3) #mdf sci var dq
-    #print "\n             >>>>>>>     AD HOST    <<<<<<<<"
-    #ad1.info()
-    #print "\n             >>>>>>>    AD APPEND   <<<<<<<<"
-    #ad3.info()
-    ad1.append(moredata=ad3, auto_number=True)
-    #print "\n             >>>>>>>  AD HOST (NEW) <<<<<<<<"
+    print "\n             >>>>>>>     AD HOST    <<<<<<<<"
     ad1.info()
-    ok_(ad1[3].extname(), "MDF")
-    ok_(ad1[4].extname(), "SCI")
-    ok_(ad1[5].extname(), "SCI")
-    ok_(ad1[6].extname(), "SCI")
-    ok_(ad1[4].extver(), 4)
-    ok_(ad1[5].extver(), 4)
-    ok_(ad1[6].extver(), 4)
+    print "\n             >>>>>>>    AD APPEND   <<<<<<<<"
+    ad3.info()
+    ad1.append(moredata=ad3, auto_number=True)
+    print "\n             >>>>>>>  AD HOST (NEW) <<<<<<<<"
+    ad1.info()
+    print ad1[3].extname()
+    eq_(ad1[3].extname(), "MDF")
+    eq_(ad1[4].extname(), "SCI")
+    eq_(ad1[5].extname(), "VAR")
+    eq_(ad1[6].extname(), "DQ")
+    eq_(ad1[4].extver(), 4)
+    eq_(ad1[5].extver(), 4)
+    eq_(ad1[6].extver(), 4)
 
 def ad_append_test2():
     """ad_append_test2 -moredata=AD, with auto_number
@@ -37,12 +38,12 @@ def ad_append_test2():
     ad3.append(moredata=ad1, auto_number=True)
     print "\n             >>>>>>>  AD HOST (NEW) <<<<<<<<"
     ad3.info()
-    ok_(ad3[4].extname(), "SCI")
-    ok_(ad3[5].extname(), "SCI")
-    ok_(ad3[6].extname(), "SCI")
-    ok_(ad3[4].extver(), 2)
-    ok_(ad3[5].extver(), 3)
-    ok_(ad3[6].extver(), 4)
+    eq_(ad3[4].extname(), "SCI")
+    eq_(ad3[5].extname(), "SCI")
+    eq_(ad3[6].extname(), "SCI")
+    eq_(ad3[4].extver(), 2)
+    eq_(ad3[5].extver(), 3)
+    eq_(ad3[6].extver(), 4)
 
 def ad_append_test3():
     """ad_append_test3 -moredata=AD, with auto_number
@@ -56,12 +57,12 @@ def ad_append_test3():
     ad4.append(moredata=ad1, auto_number=True)
     print "\n             >>>>>>>  AD HOST (NEW) <<<<<<<<"
     ad4.info()
-    ok_(ad4[9].extname(), "SCI")
-    ok_(ad4[10].extname(), "SCI")
-    ok_(ad4[11].extname(), "SCI")
-    ok_(ad4[9].extver(), 4)
-    ok_(ad4[10].extver(), 5)
-    ok_(ad4[11].extver(), 6)
+    eq_(ad4[9].extname(), "SCI")
+    eq_(ad4[10].extname(), "SCI")
+    eq_(ad4[11].extname(), "SCI")
+    eq_(ad4[9].extver(), 4)
+    eq_(ad4[10].extver(), 5)
+    eq_(ad4[11].extver(), 6)
 
 def ad_append_test4():
     """ad_append_test4 -moredata=AD, with auto_number
@@ -75,24 +76,24 @@ def ad_append_test4():
     ad1.append(moredata=ad4, auto_number=True)
     print "\n             >>>>>>>  AD HOST (NEW) <<<<<<<<"
     ad1.info()
-    ok_(ad1[3].extname(), "SCI")
-    ok_(ad1[4].extname(), "SCI")
-    ok_(ad1[5].extname(), "SCI")
-    ok_(ad1[6].extname(), "DQ")
-    ok_(ad1[7].extname(), "DQ")
-    ok_(ad1[8].extname(), "DQ")
-    ok_(ad1[9].extname(), "VAR")
-    ok_(ad1[10].extname(), "VAR")
-    ok_(ad1[11].extname(), "VAR")
-    ok_(ad1[3].extver(), 4)
-    ok_(ad1[4].extver(), 5)
-    ok_(ad1[5].extver(), 6)
-    ok_(ad1[6].extver(), 4)
-    ok_(ad1[7].extver(), 5)
-    ok_(ad1[8].extver(), 6)
-    ok_(ad1[9].extver(), 4)
-    ok_(ad1[10].extver(), 5)
-    ok_(ad1[11].extver(), 6)
+    eq_(ad1[3].extname(), "SCI")
+    eq_(ad1[4].extname(), "SCI")
+    eq_(ad1[5].extname(), "SCI")
+    eq_(ad1[6].extname(), "DQ")
+    eq_(ad1[7].extname(), "DQ")
+    eq_(ad1[8].extname(), "DQ")
+    eq_(ad1[9].extname(), "VAR")
+    eq_(ad1[10].extname(), "VAR")
+    eq_(ad1[11].extname(), "VAR")
+    eq_(ad1[3].extver(), 4)
+    eq_(ad1[4].extver(), 5)
+    eq_(ad1[5].extver(), 6)
+    eq_(ad1[6].extver(), 4)
+    eq_(ad1[7].extver(), 5)
+    eq_(ad1[8].extver(), 6)
+    eq_(ad1[9].extver(), 4)
+    eq_(ad1[10].extver(), 5)
+    eq_(ad1[11].extver(), 6)
 
 def ad_append_test5():
     """ad_append_test5 -moredata=AD, with auto_number
@@ -106,24 +107,24 @@ def ad_append_test5():
     ad3.append(moredata=ad4, auto_number=True)
     print "\n             >>>>>>>  AD HOST (NEW) <<<<<<<<"
     ad3.info()
-    ok_(ad3[4].extname(), "SCI")
-    ok_(ad3[5].extname(), "SCI")
-    ok_(ad3[6].extname(), "SCI")
-    ok_(ad3[7].extname(), "DQ")
-    ok_(ad3[8].extname(), "DQ")
-    ok_(ad3[9].extname(), "DQ")
-    ok_(ad3[10].extname(), "VAR")
-    ok_(ad3[11].extname(), "VAR")
-    ok_(ad3[12].extname(), "VAR")
-    ok_(ad3[4].extver(), 2)
-    ok_(ad3[5].extver(), 3)
-    ok_(ad3[6].extver(), 4)
-    ok_(ad3[7].extver(), 2)
-    ok_(ad3[8].extver(), 3)
-    ok_(ad3[9].extver(), 4)
-    ok_(ad3[10].extver(), 2)
-    ok_(ad3[11].extver(), 3)
-    ok_(ad3[12].extver(), 4)
+    eq_(ad3[4].extname(), "SCI")
+    eq_(ad3[5].extname(), "SCI")
+    eq_(ad3[6].extname(), "SCI")
+    eq_(ad3[7].extname(), "DQ")
+    eq_(ad3[8].extname(), "DQ")
+    eq_(ad3[9].extname(), "DQ")
+    eq_(ad3[10].extname(), "VAR")
+    eq_(ad3[11].extname(), "VAR")
+    eq_(ad3[12].extname(), "VAR")
+    eq_(ad3[4].extver(), 2)
+    eq_(ad3[5].extver(), 3)
+    eq_(ad3[6].extver(), 4)
+    eq_(ad3[7].extver(), 2)
+    eq_(ad3[8].extver(), 3)
+    eq_(ad3[9].extver(), 4)
+    eq_(ad3[10].extver(), 2)
+    eq_(ad3[11].extver(), 3)
+    eq_(ad3[12].extver(), 4)
 
 def ad_append_test6():
     """ad_append_test6 -moredata=AD, with auto_number
@@ -137,13 +138,13 @@ def ad_append_test6():
     ad4.append(moredata=ad3, auto_number=True)
     print "\n             >>>>>>>  AD HOST (NEW) <<<<<<<<"
     ad4.info()
-    ok_(ad4[9].extname(), "MDF")
-    ok_(ad4[10].extname(), "SCI")
-    ok_(ad4[11].extname(), "VAR")
-    ok_(ad4[12].extname(), "DQ")
-    ok_(ad4[10].extver(), 4)
-    ok_(ad4[11].extver(), 4)
-    ok_(ad4[12].extver(), 4)
+    eq_(ad4[9].extname(), "MDF")
+    eq_(ad4[10].extname(), "SCI")
+    eq_(ad4[11].extname(), "VAR")
+    eq_(ad4[12].extname(), "DQ")
+    eq_(ad4[10].extver(), 4)
+    eq_(ad4[11].extver(), 4)
+    eq_(ad4[12].extver(), 4)
 
 def ad_append_test7():
     """ad_append_test7 -auto_number, add sci1 to existing sci1
@@ -159,8 +160,8 @@ def ad_append_test7():
     print "ad1.append(header=adsci.header, data=adsci.data, auto_number=True)"
     print "\n             >>>>>>>  AD HOST (NEW) <<<<<<<<"
     ad1.info()
-    ok_(ad1[3].hdulist[1].name, "SCI")
-    ok_(ad1[3].extver(), 4)
+    eq_(ad1[3].hdulist[1].name, "SCI")
+    eq_(ad1[3].extver(), 4)
 
 def ad_append_test8():
     """ad_append_test8 -auto_number, append var
@@ -176,8 +177,8 @@ def ad_append_test8():
     print "ad1.append(header=adsci.header, data=adsci.data, auto_number=True)"
     print "\n             >>>>>>>  AD HOST (NEW) <<<<<<<<"
     ad1.info()
-    ok_(ad1[3].extname(), "VAR")
-    ok_(ad1[3].extver(), 2)
+    eq_(ad1[3].extname(), "VAR")
+    eq_(ad1[3].extver(), 4)
 
 
 def ad_append_test9():
@@ -196,8 +197,8 @@ def ad_append_test9():
     ad1.append(header=adsci.header, data=adsci.data, auto_number=True)
     print "\n             >>>>>>>  AD HOST (NEW) <<<<<<<<"
     ad1.info()
-    ok_(ad1[2].extname(), "VAR")
-    ok_(ad1[2].extver(), 3)
+    eq_(ad1[2].extname(), "VAR")
+    eq_(ad1[2].extver(), 3)
 
 @ raises(Errors.AstroDataError)
 def ad_append_test10():
@@ -214,8 +215,8 @@ def ad_append_test10():
     print "ad1.append(data=adsci.data, auto_number=True)"
     print "\n             >>>>>>>  AD HOST (NEW) <<<<<<<<"
     ad1.info()
-    ok_(ad1[3].extname(), "SCI")
-    ok_(ad1[3].extver(), 4)
+    eq_(ad1[3].extname(), "SCI")
+    eq_(ad1[3].extver(), 4)
 
 def ad_append_test11():
     """ad_append_test11 -auto_number, append with high sci extver
@@ -232,8 +233,8 @@ def ad_append_test11():
           auto_number=True)")
     print "\n             >>>>>>>  AD HOST (NEW) <<<<<<<<"
     ad1.info()
-    ok_(ad1[3].extname(), "SCI")
-    ok_(ad1[3].extver(), 10)
+    eq_(ad1[3].extname(), "SCI")
+    eq_(ad1[3].extver(), 10)
 
 @ raises(Errors.AstroDataError)
 def ad_append_test12():
@@ -266,4 +267,30 @@ def ad_append_test13():
           auto_number=True)")
     print "\n             >>>>>>>  AD HOST (NEW) <<<<<<<<"
     ad1.info()
+
+
+def ad_append_test14():
+    """ad_append_test14 -auto_number, build mef from phu instantiation
+    """
+    ad1 = AstroData(file_urls.testdatafile_1) #sci 3
+    ad4 = AstroData(file_urls.testdatafile_4) #sci3 var3 dq3
+    print "\n             >>>>>>>     AD NEW    <<<<<<<<"
+    ad_new = AstroData(phu=ad1.phu)
+    ad_new.info()
+    print "\n             >>>>>>>    AD APPEND   <<<<<<<<"
+    for i in range(1,4):
+        adsci = ad1['SCI', i]
+        print("adsci = ad1['SCI', %d]" % i)
+        ad_new.append(header=adsci.header, data=adsci.data, auto_number=True)
+        mystr = "ad_new.append(header=adsci.header, data=adsci.data,"
+        mystr += " auto_number=True)"
+        print mystr
+    print "\n             >>>>>>>  AD NEW <<<<<<<<"
+    ad_new.info()
+    eq_(ad_new[0].extname(), "SCI")
+    eq_(ad_new[0].extver(), 1)
+    eq_(ad_new[1].extname(), "SCI")
+    eq_(ad_new[1].extver(), 2)
+    eq_(ad_new[2].extname(), "SCI")
+    eq_(ad_new[2].extver(), 3)
 
