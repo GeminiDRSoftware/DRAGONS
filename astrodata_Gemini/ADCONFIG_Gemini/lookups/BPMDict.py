3d2d7d5ce76090053a9988858254f1b7efaea2a6
Emma Hogan <ehogan@gemini.edu>
2011-10-04 20:49:22 +0000

    Updated BPMDict.py to distinguish between GMOS-N and GMOS-S BPMs
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3006 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/lookups/BPMDict.py
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/BPMDict.py
@@ -2,9 +2,9 @@ bpm_dict = {
     # Dictionary key is the instrument, the x binning and the y binning
     # Dictionary value is the lookup path of the BPM for that instrument with
     # that binning
-    "GMOS-N_1_1": "Gemini/GMOS/BPM/GMOS_BPM_11.fits",
-    "GMOS-N_2_2": "Gemini/GMOS/BPM/GMOS_BPM_22.fits",
-    "GMOS-S_1_1": "Gemini/GMOS/BPM/GMOS_BPM_11.fits",
-    "GMOS-S_2_2": "Gemini/GMOS/BPM/GMOS_BPM_22.fits",
+    "GMOS-N_1_1": "Gemini/GMOS/BPM/GMOS-N_BPM_11.fits",
+    "GMOS-N_2_2": "Gemini/GMOS/BPM/GMOS-N_BPM_22.fits",
+    "GMOS-S_1_1": "Gemini/GMOS/BPM/GMOS-S_BPM_11.fits",
+    "GMOS-S_2_2": "Gemini/GMOS/BPM/GMOS-S_BPM_22.fits",
     "F2_1_1": "Gemini/F2/BPM/F2_bpm.fits",
     }
