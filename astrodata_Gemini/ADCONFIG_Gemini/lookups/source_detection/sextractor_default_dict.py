0d2b663de0b92d56eff618c50c8a97de64305515
Melanie Clarke <mclarke@gemini.edu>
2011-10-26 23:28:11 +0000

    Improving sextractor support: checking version before attempting to run, and making changes needed to run it without a DQ plane
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3098 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/lookups/source_detection/sextractor_default_dict.py
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/source_detection/sextractor_default_dict.py
@@ -1,6 +1,14 @@
 sextractor_default_dict = {
-    "conv": "Gemini/source_detection/default.conv",
-    "nnw": "Gemini/source_detection/default.nnw",
-    "param": "Gemini/source_detection/default.param",
-    "sex": "Gemini/source_detection/default.sex",
+    "dq": {
+        "conv": "Gemini/source_detection/default.conv",
+        "nnw": "Gemini/source_detection/default.nnw",
+        "param": "Gemini/source_detection/default.param",
+        "sex": "Gemini/source_detection/default.sex",
+        },
+    "no_dq": {
+        "conv": "Gemini/source_detection/default.conv",
+        "nnw": "Gemini/source_detection/default.nnw",
+        "param": "Gemini/source_detection/default_nodq.param",
+        "sex": "Gemini/source_detection/default.sex",
+        },
     }
