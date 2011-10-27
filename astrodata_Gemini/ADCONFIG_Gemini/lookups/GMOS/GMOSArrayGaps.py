0838a87c5a7c4cb73a37ec37fcaea8ba21c18d8b
Mark Simpson <msimpson@gemini.edu>
2011-10-27 20:16:53 +0000

    Look up dictionary for the unbinned and binned (starting value) for the various GMOS detectors, keywed by tuple of DETTYPE and unbinned / binned
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3106 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/GMOS/GMOSArrayGaps.py
@@ -0,0 +1,21 @@
+# Dictionary of GMOS Chip gap sizes, in pixels, both binned and unbinned
+# for each DETTYPE
+gmosArrayGaps = {
+    # Unbinned
+    # EEV CCDs
+    ("SDSU II CCD", "unbinned") : 37,
+    # e2v CCDs
+    ("SDSU II e2v DD CCD42-90", "unbinned") : 37,
+    # Hamamatsu
+    ("S10892-01", "unbinned") : 37,
+
+    # Binned
+    # EEV CCDs
+    ("SDSU II CCD", "binned") : 36,
+    # e2v CCDs
+    ("SDSU II e2v DD CCD42-90", "binned") : 36,
+    # Hamamatsu
+    ("S10892-01", "binned") : 36
+}
+
+
