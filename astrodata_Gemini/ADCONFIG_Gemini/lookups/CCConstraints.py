410fcc3b61c62d085b846400aec67babf5d2ee5b
Paul Hirst <phirst@gemini.edu>
2011-12-03 02:28:00 +0000

    initial ccConstraints table
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3189 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/CCConstraints.py
@@ -0,0 +1,7 @@
+# Gives CC band constraints as a function of magnitudes of extinction.
+# From http://www.gemini.edu/sciops/telescopes-and-sites/observing-condition-constraints
+# the value defines the maximum amount of extinction that classes as that band.
+
+ccConstraints = {
+'50':0.00, '70':0.3, '80':1.0
+}
