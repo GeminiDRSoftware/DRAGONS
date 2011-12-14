732bf447b63ef0f9bf524f4b7d2ea913b13168e4
Emma Hogan <ehogan@gemini.edu>
2011-12-14 19:58:57 +0000

    Added additional variables to StandardF2KeyDict.py so that the descriptors are backwards compatible with the F2 engineering data
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3204 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/descriptors/F2/StandardF2KeyDict.py
+++ astrodata_Gemini/ADCONFIG_Gemini/descriptors/F2/StandardF2KeyDict.py
@@ -1,6 +1,9 @@
 stdkeyDictF2 = {
     "key_lnrs":"LNRS",
-    "key_filter1":"FILT1POS",
-    "key_filter2":"FILT2POS",
-    "key_grating":"GRISMPOS",
+    "key_filter1":"FILTER1",
+    "key_filter2":"FILTER2",
+    "key_grating":"GRISM",
+    "key_old_filter1":"FILT1POS",
+    "key_old_filter2":"FILT2POS",
+    "key_old_grating":"GRISMPOS",
     }
