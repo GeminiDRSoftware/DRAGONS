d7b13e7bac3d2fa771f2c178ee0991907edb08dc
Melanie Clarke <mclarke@gemini.edu>
2011-12-15 21:30:39 +0000

    Fixing the type to actually apply to GMOS twilight flats.  Also making GMOS_IMAGE_FLAT the parent type
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3217 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/GMOS/gemdtype.GMOS_IMAGE_TWILIGHT.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/GMOS/gemdtype.GMOS_IMAGE_TWILIGHT.py
@@ -3,9 +3,9 @@ class GMOS_IMAGE_TWILIGHT(DataClassification):
     usage = """
         Applies to all imaging twilight flat datasets from the GMOS instruments
         """
-    parent = "GMOS_IMAGE"
-    requirement = AND([  ISCLASS("GMOS_IMAGE"),
-                         PHU(OBSTYPE="FLAT"),
-                         PHU(OBJECT="Twilight")  ])
+    parent = "GMOS_IMAGE_FLAT"
+    requirement = AND([  ISCLASS("GMOS_IMAGE_FLAT"),
+                         OR([PHU(OBJECT="Twilight"),
+                             PHU(OBJECT="twilight")]) ])
 
 newtypes.append(GMOS_IMAGE_TWILIGHT())
