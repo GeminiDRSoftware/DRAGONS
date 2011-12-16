5c922159f2b58f7a045b55ec5bd4d0d9a4db4335
Emma Hogan <ehogan@gemini.edu>
2011-12-16 00:57:42 +0000

    F2_IMAGE_FLAT includes F2_IMAGE_TWILIGHT ... update type definition
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3220 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/F2/gemdtype.F2_IMAGE_FLAT.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/F2/gemdtype.F2_IMAGE_FLAT.py
@@ -5,7 +5,8 @@ class F2_IMAGE_FLAT(DataClassification):
         """
     parent = "F2_IMAGE"
     requirement = AND([  ISCLASS("F2_IMAGE"),
-                         PHU(OBSTYPE="FLAT"), 
-                         NOT(ISCLASS("F2_IMAGE_TWILIGHT"))  ])
+                         OR([  PHU(OBSTYPE="FLAT"),
+                               OR([  PHU(OBJECT="Twilight"),
+                                     PHU(OBJECT="twilight")  ])  ])  ])
 
 newtypes.append(F2_IMAGE_FLAT())
