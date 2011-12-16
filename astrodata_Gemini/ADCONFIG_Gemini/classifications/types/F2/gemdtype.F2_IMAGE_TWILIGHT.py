b4879b172dc66d96868db0d0cc2630bcd360deb5
Emma Hogan <ehogan@gemini.edu>
2011-12-16 00:58:49 +0000

    Update F2_IMAGE_TWILIGHT AstroData type so that it is a child of F2_IMAGE_FLAT
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3221 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/F2/gemdtype.F2_IMAGE_TWILIGHT.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/F2/gemdtype.F2_IMAGE_TWILIGHT.py
@@ -4,9 +4,9 @@ class F2_IMAGE_TWILIGHT(DataClassification):
         Applies to all imaging twilight flat datasets from the FLAMINGOS-2
         instrument
         """
-    parent = "F2_IMAGE"
-    requirement = AND([  ISCLASS("F2_IMAGE"),
-                         PHU(OBSTYPE="FLAT"),
-                         PHU(OBJECT="Twilight")  ])
+    parent = "F2_IMAGE_FLAT"
+    requirement = AND([  ISCLASS("F2_IMAGE_FLAT"),
+                         OR([  PHU(OBJECT="Twilight"),
+                               PHU(OBJECT="twilight")  ])  ])
 
 newtypes.append(F2_IMAGE_TWILIGHT())
