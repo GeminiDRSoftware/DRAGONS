27fb2318eb74a048b45c7754e15dcea47eea4299
Paul Hirst <phirst@gemini.edu>
2012-02-03 23:18:47 +0000

    identify data from gemini instruments as gemini even if they dont have telescope headers, so that the descriptors work on engineering data
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3285 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/gemdtype.GEMINI.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/gemdtype.GEMINI.py
@@ -5,7 +5,20 @@ class GEMINI(DataClassification):
     usage = '''
         Applies to all data from either GMOS-North or GMOS-South instruments in any mode.
         '''
+    # Added the instrument names directly, so that when we get engineering data that does
+    # not have telescope headers in, and thus doesn't identify as GEMINI_NORTH or _SOUTH
+    # then it does identify as GEMINI, so that the gemini descriptors associate with it.
     requirement = OR(ISCLASS("GEMINI_NORTH"),
-                     ISCLASS("GEMINI_SOUTH"))
+                     ISCLASS("GEMINI_SOUTH"),
+                     ISCLASS("GMOS"),
+                     ISCLASS("NIRI"),
+                     ISCLASS("GNIRS"),
+                     ISCLASS("MICHELLE"),
+                     ISCLASS("NICI"),
+                     ISCLASS("F2"),
+                     ISCLASS("NIFS"),
+                     ISCLASS("TRECS"),
+                     ISCLASS("GSAOI"))
+               
 
 newtypes.append( GEMINI())
