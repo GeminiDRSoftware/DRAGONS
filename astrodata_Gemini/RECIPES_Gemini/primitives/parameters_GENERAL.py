01064b9a70c5f70e189632c58d07630f2887a091
Melanie Clarke <mclarke@gemini.edu>
2012-02-09 22:01:04 +0000

    Adding parameters for the add/sub/mult/div primitives
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3289 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_GENERAL.py
@@ -0,0 +1,39 @@
+# This parameter file contains the parameters related to the primitives located
+# in the primitives_GEMINI.py file, in alphabetical order.
+{"addToInput":{
+    "suffix":{
+        "default"       : "_valueAdded",
+        "type"          : "str",
+        "recipeOverride": True,
+        "userOverride"  : True,
+        "uiLevel"       : "UIBASIC",
+        },
+    },
+ "divideInputBy":{
+    "suffix":{
+        "default"       : "_valueSubtracted",
+        "type"          : "str",
+        "recipeOverride": True,
+        "userOverride"  : True,
+        "uiLevel"       : "UIBASIC",
+        },
+    },
+ "multiplyInputBy":{
+    "suffix":{
+        "default"       : "_valueSubtracted",
+        "type"          : "str",
+        "recipeOverride": True,
+        "userOverride"  : True,
+        "uiLevel"       : "UIBASIC",
+        },
+    },
+ "subtractFromInput":{
+    "suffix":{
+        "default"       : "_valueSubtracted",
+        "type"          : "str",
+        "recipeOverride": True,
+        "userOverride"  : True,
+        "uiLevel"       : "UIBASIC",
+        },
+    },
+}
