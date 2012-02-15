cfa3055979d50f3521ebb3adb5479bb94699eec2
Kaniela Dement <kdement@gemini.edu>
2012-02-15 19:57:28 +0000

    restructure
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3309 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/scripts/test/file_urls.py
+++
@@ -1,4 +0,0 @@
-sci123 = "../../../../test_data/infrastructure/N20090703S0163.fits" 
-sci1 = "../../../../test_data/infrastructure/S20100122S0063.fits" 
-mdfscivardq1 = "../../../../test_data/infrastructure/nS20040613S0162.fits" 
-scivardq123 = "../../../../test_data/infrastructure/N20110313S0188_varAdded.fits" 
