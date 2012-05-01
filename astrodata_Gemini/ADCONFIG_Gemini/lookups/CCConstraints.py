0298c4641bf0469452b2d3790dd9e1919ccd0c6a
Paul Hirst <phirst@gemini.edu>
2012-05-01 01:01:10 +0000

    updated CC50 to reflect scatter in GMOS values
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3436 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/lookups/CCConstraints.py
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/CCConstraints.py
@@ -2,6 +2,10 @@
 # From http://www.gemini.edu/sciops/telescopes-and-sites/observing-condition-constraints
 # the value defines the maximum amount of extinction that classes as that band.
 
+# Photometry to better than 5% (0.05 mags) is difficult due to scattered light from the 
+# focal reducer,  also we're using a nominal rather than measured atmospheric extinction
+# so we take the photometric number to be less than 0.08 mags extinction.
+
 ccConstraints = {
-'50':0.00, '70':0.3, '80':1.0
+'50':0.08, '70':0.3, '80':1.0
 }
