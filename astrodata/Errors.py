64e289a8d9d0d776e84b1b84c3245b5efe188417
Emma Hogan <ehogan@gemini.edu>
2012-10-01 15:28:22 +0000

    All descriptor exceptions now inherit from the DescriptorError base class, meaning that those exception can be caught by the CI and return None ... all other errors are NOT caught
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3708 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/Errors.py
+++ astrodata/Errors.py
@@ -1,7 +1,3 @@
-# These exceptions allow the distinction between the None values given when a
-# descriptor either cannot find the keyword, cannot do a calculation with the
-# keyword etc. 
-
 class Error(Exception):
     """Base class for exceptions in this module."""
     def __init__(self, message=None):
@@ -26,38 +22,12 @@ class AstroDataError(Error):
     For general Exceptions raised in the AstroData Class Module
     """
     message = 'Exception Raised in AstroData.py'
+
 class AstroDataReadonlyError(AstroDataError):
     """
     For general Exceptions raised in the AstroData Class Module
     """
     message = 'Readonly Exception Raised in AstroData.py'
-
-class NoLoggerError(AstroDataError):
-    message = "Request for Logger preceded creation of logger."
-    
-class OutputExists(AstroDataError):
-    def __init__(self, msg=None):
-        if msg == None:
-            self.message = "Cannot overwrite existing file."
-        else:
-            self.message = "Cannot overwrite existing file, " + msg
-
-class SingleHDUMemberExcept(AstroDataError):
-    def __init__(self, msg=None):
-        if msg == None:
-            tmpmes = "This member or method can only be called for"
-            tmpmes += " Single HDU AstroData instances"
-            self.message = tmpmes
-        else:
-            self.message = "SingleHDUMember: " + msg     
-
-class CalcError(Error):
-    """
-    Exception raised for instances when the keyword required for calculation
-    within a descriptor function is found but the descriptor code is unable to
-    calculate and return a value
-    """
-    message = "Descriptor unable to calculate value"
 
 class CalibrationDefinitionLibraryError(Error):
     """
@@ -65,26 +35,11 @@ class CalibrationDefinitionLibraryError(Error):
     """
     message = "Exception raised in CalibrationDefinitionLibrary.py"
 
-class CorruptDataError(Error):
-    """
-    Exception raised for instances when an AstroData object was not
-    automatically assigned (EXTNAME, EXTVER) data extensions
-    """
-    message = "The AstroData object was not assigned a 'SCI' extension"
-
-
 class DescriptorsError(Error):
     """
     For general exceptions raised in the Descriptors.py module
     """
     message = "Exception raised in Descriptors.py"
-
-class DescriptorTypeError(Error):
-    """
-    Exception raised for instances when a descriptor function cannot return a
-    value for a given AstroData Type i.e., dispersion_axis for IMAGE data
-    """
-    message = "Unable to return a value for data of this AstroData Type"
 
 class DescriptorValueError(Error):
     pass
@@ -92,20 +47,6 @@ class DescriptorValueError(Error):
 class DescriptorValueTypeError(Error):
     pass
     
-class EmptyKeyError(Error):
-    """
-    Exception raised for errors when a PHU keyword was found to but was empty
-    or ' '.
-    """
-    message = "Keyword found but the value was empty"
-    
-class ExistError(Error):
-    """
-    Exception raised for instances when a descriptor doesn't exist for a
-    particular instrument
-    """
-    message = "Descriptor does not exist for this instrument"
-
 class ExtTableError(Error):
     """
     General exception raised for Errors in ExtTable.py
@@ -132,40 +73,27 @@ class InputError(Error):
     """
     message = "Input is None or empty"
     
-class InvalidValueError(Error):
-    """
-    Exception raised for instances when the keyword required for calculation
-    within a descriptor function is found but the value of the keyword has an
-    invalid value (as determined by the validation code within the descriptor
-    function)
-    """
-    message = "Keyword value outside allowed range"
-
 class MatchShapeError(Error):
     """
     Exception raised for instances when two arrays do not match in shape
     """
     message = "The two arrays do not match in shape"
 
+class NoLoggerError(AstroDataError):
+    message = "Request for Logger preceded creation of logger."
+    
 class OutputError(Error):
     """
     Exception raised for instances when an output value is None or empty
     """
     message = "Output is None or empty"
 
-class TableKeyError(Error):
-    """
-    Exception raised for instances when a key cannot be matched in a lookup
-    table
-    """
-    message = "Key not matched in lookup table"
-
-class TableValueError(Error):
-    """
-    Exception raised for instances when a value cannot be found in a lookup
-    table
-    """
-    message = "Value not found in lookup table"
+class OutputExists(AstroDataError):
+    def __init__(self, msg=None):
+        if msg == None:
+            self.message = "Cannot overwrite existing file."
+        else:
+            self.message = "Cannot overwrite existing file, " + msg
 
 class PrimitiveError(Error):
     """
@@ -193,23 +121,101 @@ class ScienceError(Error):
     """
     message = "An error occurred during a user level 'science' function"
 
+class SingleHDUMemberExcept(AstroDataError):
+    def __init__(self, msg=None):
+        if msg == None:
+            tmpmes = "This member or method can only be called for"
+            tmpmes += " Single HDU AstroData instances"
+            self.message = tmpmes
+        else:
+            self.message = "SingleHDUMember: " + msg     
+
 class ToolboxError(Error):
     """
     Exception raised for general errors inside of a 'toolbox' function.
     """
     message = "An error occurred during a 'toolbox' function"
     
-class UndefinedKeyError(Error):
+class GDPGUtilError(Error):
+    message = "Error in gpdgutil module."
+
+# Descriptor exception; these exceptions allow the distinction between the None
+# values given when a descriptor either cannot find the keyword, cannot do a
+# calculation with the keyword etc. 
+
+class DescriptorError(Error):
+    """
+    Base class for descriptor exceptions
+    """
+    message = "Descriptor error"
+
+class CalcError(DescriptorError):
+    """
+    Exception raised for instances when the keyword required for calculation
+    within a descriptor function is found but the descriptor code is unable to
+    calculate and return a value
+    """
+    message = "Descriptor unable to calculate value"
+
+class CorruptDataError(DescriptorError):
+    """
+    Exception raised for instances when an AstroData object was not
+    automatically assigned (EXTNAME, EXTVER) data extensions
+    """
+    message = "The AstroData object was not assigned a 'SCI' extension"
+
+class DescriptorTypeError(DescriptorError):
+    """
+    Exception raised for instances when a descriptor function cannot return a
+    value for a given AstroData Type i.e., dispersion_axis for IMAGE data
+    """
+    message = "Unable to return a value for data of this AstroData Type"
+
+class EmptyKeyError(DescriptorError):
+    """
+    Exception raised for errors when a PHU keyword was found to but was empty
+    or ' '.
+    """
+    message = "Keyword found but the value was empty"
+    
+class ExistError(DescriptorError):
+    """
+    Exception raised for instances when a descriptor doesn't exist for a
+    particular instrument
+    """
+    message = "Descriptor does not exist for this instrument"
+
+class InvalidValueError(DescriptorError):
+    """
+    Exception raised for instances when the keyword required for calculation
+    within a descriptor function is found but the value of the keyword has an
+    invalid value (as determined by the validation code within the descriptor
+    function)
+    """
+    message = "Keyword value outside allowed range"
+
+class TableKeyError(DescriptorError):
+    """
+    Exception raised for instances when a key cannot be matched in a lookup
+    table
+    """
+    message = "Key not matched in lookup table"
+
+class TableValueError(DescriptorError):
+    """
+    Exception raised for instances when a value cannot be found in a lookup
+    table
+    """
+    message = "Value not found in lookup table"
+
+class UndefinedKeyError(DescriptorError):
     """
     Exception raised for errors when a PHU keyword has a value that is 
     pyfits.core.Undefined
     """
     message = "Keyword found but the value was undefined"
     
-class GDPGUtilError(Error):
-    message = "Error in gpdgutil module."
-
-### CONFIGURATION SYSTEM ERRORS
+# Configuration system errors
 
 class ConfigurationError(Error):
     message = "Configuration Error"
@@ -217,6 +223,7 @@ class ConfigurationError(Error):
 class RecipeImportError(ConfigurationError):
     message = "Recipe Import Error"
     
-# high level errors (e.g. for reduce to throw)
+# High level errors (e.g. for reduce to throw)
+
 class RecipeNotFoundError(Error):
     message = "Recipe not found."
