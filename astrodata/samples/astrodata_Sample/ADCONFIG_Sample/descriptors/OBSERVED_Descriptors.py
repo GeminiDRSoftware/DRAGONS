c5156e6efe74ecefe4df65c7f5004f21bb4e85ac
Craig Allen <callen@gemini.edu>
2012-10-12 20:37:26 +0000

    updated with telescope descriptor example
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3717 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/samples/astrodata_Sample/ADCONFIG_Sample/descriptors/OBSERVED_Descriptors.py
+++ astrodata/samples/astrodata_Sample/ADCONFIG_Sample/descriptors/OBSERVED_Descriptors.py
@@ -1,3 +1,6 @@
 class OBSERVED_DescriptorCalc:
     def observatory(self, dataset, **args):
         return dataset.get_phu_key_value("OBSERVAT")
+
+    def telescope(self, dataset, **args):
+        return dataset.get_phu_key_value("TELESCOP")
