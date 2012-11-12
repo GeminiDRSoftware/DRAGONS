9dea8cf046c7b79b1d57ef4f3d2cf435f94a13f2
Emma Hogan <ehogan@gemini.edu>
2012-11-12 19:55:17 +0000

    Minor formatting updates to primitive_F2.py, removed unnecessary backslashes
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3772 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/RECIPES_Gemini/primitives/primitives_F2.py
+++ astrodata_Gemini/RECIPES_Gemini/primitives/primitives_F2.py
@@ -31,7 +31,7 @@ class F2Primitives(GEMINIPrimitives):
         
         # Define the keyword to be used for the time stamp for this primitive
         timestamp_key = self.timestamp_keys["standardizeInstrumentHeaders"]
-
+        
         # Initialize the list of output AstroData objects
         adoutput_list = []
         
@@ -41,9 +41,10 @@ class F2Primitives(GEMINIPrimitives):
             # Check whether the standardizeInstrumentHeaders primitive
             # has been run previously
             if ad.phu_get_key_value(timestamp_key):
-                log.warning("No changes will be made to %s, since it has " \
-                            "already been processed by "\
-                            "standardizeInstrumentHeaders" % (ad.filename))
+                log.warning("No changes will be made to %s, since it has "
+                            "already been processed by "
+                            "standardizeInstrumentHeaders" % ad.filename)
+                
                 # Append the input AstroData object to the list of output
                 # AstroData objects without further processing
                 adoutput_list.append(ad)
@@ -52,44 +53,44 @@ class F2Primitives(GEMINIPrimitives):
             # Update the keywords in the headers that are specific to
             # FLAMINGOS-2
             log.status("Updating keywords that are specific to FLAMINGOS-2")
-
+            
             # Filter name (required for IRAF?)
             gt.update_key_from_descriptor(
-                adinput=ad, descriptor="filter_name(stripID=True, pretty=True)",
-                keyword="FILTER", extname="PHU")
-
+              adinput=ad, descriptor="filter_name(stripID=True, pretty=True)",
+              keyword="FILTER", extname="PHU")
+            
             # Pixel scale
             gt.update_key_from_descriptor(
-                adinput=ad, descriptor="pixel_scale()", extname="PHU")
-
+              adinput=ad, descriptor="pixel_scale()", extname="PHU")
+            
             # Read noise (new keyword, should it be written?)
             gt.update_key_from_descriptor(
-                adinput=ad, descriptor="read_noise()", extname="SCI")
-
+              adinput=ad, descriptor="read_noise()", extname="SCI")
+            
             # Gain (new keyword, should it be written?)
             gt.update_key_from_descriptor(
-                adinput=ad, descriptor="gain()", extname="SCI")
-
+              adinput=ad, descriptor="gain()", extname="SCI")
+            
             # Non linear level
             gt.update_key_from_descriptor(
-                adinput=ad, descriptor="non_linear_level()", extname="SCI")
-
+              adinput=ad, descriptor="non_linear_level()", extname="SCI")
+            
             # Saturation level
             gt.update_key_from_descriptor(
-                adinput=ad, descriptor="saturation_level()", extname="SCI")
-
+              adinput=ad, descriptor="saturation_level()", extname="SCI")
+            
             # Dispersion axis (new keyword, should it be written?)
             if "IMAGE" not in ad.types:
                 gt.update_key_from_descriptor(
-                    adinput=ad, descriptor="dispersion_axis()", extname="SCI")
-
+                  adinput=ad, descriptor="dispersion_axis()", extname="SCI")
+            
             # Add the appropriate time stamps to the PHU
             gt.mark_history(adinput=ad, keyword=timestamp_key)
             
             # Change the filename
-            ad.filename = gt.filename_updater(adinput=ad, suffix=rc["suffix"], 
-                                              strip=True)
-
+            ad.filename = gt.filename_updater(
+              adinput=ad, suffix=rc["suffix"], strip=True)
+            
             # Append the output AstroData object to the list
             # of output AstroData objects
             adoutput_list.append(ad)
@@ -116,65 +117,63 @@ class F2Primitives(GEMINIPrimitives):
         
         # Define the keyword to be used for the time stamp for this primitive
         timestamp_key = self.timestamp_keys["standardizeStructure"]
-
+        
         # Initialize the list of output AstroData objects
         adoutput_list = []
         
-        # First run addMDF if necessary to attach mdf files to the input
-        for ad in rc.get_inputs_as_astrodata():
-            if rc["attach_mdf"]:
-                # Get the mdf parameter from the RC
-                mdf = rc["mdf"]
-                if mdf is not None:
-                    rc.run("addMDF(mdf=%s)" % mdf)
-                else:
-                    rc.run("addMDF")
-
-                # It only needs to run once, on all input, so break loop
-                break
-
         # Loop over each input AstroData object in the input list
         for ad in rc.get_inputs_as_astrodata():
             
             # Check whether the standardizeStructure primitive has been run
             # previously
             if ad.phu_get_key_value(timestamp_key):
-                log.warning("No changes will be made to %s, since it has " \
-                            "already been processed by standardizeStructure" \
-                            % (ad.filename))
+                log.warning("No changes will be made to %s, since it has "
+                            "already been processed by standardizeStructure"
+                            % ad.filename)
                 # Append the input AstroData object to the list of output
                 # AstroData objects without further processing
                 adoutput_list.append(ad)
                 continue
             
-            # Standardize the structure of the input AstroData
-            # object. Raw FLAMINGOS-2 data have three dimensions (i.e.,
-            # 2048x2048x1), so check whether the third dimension has a length
-            # of one and remove it
+            # Standardize the structure of the input AstroData object. Raw
+            # FLAMINGOS-2 data have three dimensions (e.g., 2048x2048x1), so
+            # check whether the third dimension has a length of one and remove
+            # it.
             for ext in ad:
                 if len(ext.data.shape) == 3:
+                    
                     # Remove the single-dimensional axis from the pixel data
                     ext.data = np.squeeze(ext.data)
                     if len(ext.data.shape) == 2:
-                        log.fullinfo("Removed third dimension from %s" \
+                        log.fullinfo("Removed the third dimension from %s"
                                      % ad.filename)
+                    
                     if len(ext.data.shape) == 3:
                         # The np.squeeze method only removes a dimension from
                         # the array if it is equal to 1. In this case, the
                         # third dimension contains multiple datasets. Need to
                         # deal with this as some point
                         pass
-                    log.debug("Dimensions of %s[%s,%d] = %s" \
-                              % (ad.filename, ext.extname(), ext.extver(), \
-                              str(ext.data.shape)))
+                    
+                    log.debug("Dimensions of %s[%s,%d] = %s" % (
+                      ad.filename, ext.extname(), ext.extver(),
+                      ext.data.shape))
+            
+            # Attach an MDF to each input AstroData object
+            if rc["attach_mdf"]:
+                mdf = rc["mdf"]
+                if mdf is not None:
+                    rc.run("addMDF(mdf=%s)" % mdf)
+                else:
+                    rc.run("addMDF")
             
             # Add the appropriate time stamps to the PHU
             gt.mark_history(adinput=ad, keyword=timestamp_key)
-
+            
             # Change the filename
-            ad.filename = gt.filename_updater(adinput=ad, suffix=rc["suffix"], 
-                                              strip=True)
-
+            ad.filename = gt.filename_updater(
+              adinput=ad, suffix=rc["suffix"], strip=True)
+            
             # Append the output AstroData object to the list
             # of output AstroData objects
             adoutput_list.append(ad)
@@ -188,7 +187,7 @@ class F2Primitives(GEMINIPrimitives):
     def validateData(self, rc):
         """
         This primitive is used to validate FLAMINGOS-2 data, specifically.
-
+        
         :param repair: Set to True to repair the data. Note: this feature does
                        not work yet. 
         :type repair: Python boolean
@@ -200,7 +199,7 @@ class F2Primitives(GEMINIPrimitives):
         
         # Define the keyword to be used for the time stamp for this primitive
         timestamp_key = self.timestamp_keys["validateData"]
-
+        
         # Log the standard "starting primitive" debug message
         log.debug(gt.log_message("primitive", "validateData", "starting"))
         
@@ -212,9 +211,9 @@ class F2Primitives(GEMINIPrimitives):
             
             # Check whether the validateData primitive has been run previously
             if ad.phu_get_key_value(timestamp_key):
-                log.warning("No changes will be made to %s, since it has " \
-                            "already been processed by validateData" \
-                            % (ad.filename))
+                log.warning("No changes will be made to %s, since it has "
+                            "already been processed by validateData"
+                            % ad.filename)
                 # Append the input AstroData object to the list of output
                 # AstroData objects without further processing
                 adoutput_list.append(ad)
@@ -222,17 +221,17 @@ class F2Primitives(GEMINIPrimitives):
             
             # Get the repair parameter from the RC
             repair = rc["repair"]
-
-            # Validate the input AstroData object. ACTUALLY DO SOMETHING HERE?
+            
+            # Validate the input AstroData object.
             log.stdinfo("No validation required for FLAMINGOS-2")
             
             # Add the appropriate time stamps to the PHU
             gt.mark_history(adinput=ad, keyword=timestamp_key)
             
             # Change the filename
-            ad.filename = gt.filename_updater(adinput=ad, suffix=rc["suffix"], 
-                                              strip=True)
-
+            ad.filename = gt.filename_updater(
+              adinput=ad, suffix=rc["suffix"], strip=True)
+            
             # Append the output AstroData object to the list
             # of output AstroData objects
             adoutput_list.append(ad)
