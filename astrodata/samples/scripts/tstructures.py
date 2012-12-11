badf2860bf0b5bccd3f2aafe25bf609bec618c91
Craig Allen <callen@gemini.edu>
2012-12-11 21:14:05 +0000

    script for testing structures
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3804 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ astrodata/samples/scripts/tstructures.py
@@ -0,0 +1,18 @@
+from astrodata import AstroData
+from astrodata.structuredslice import pixel_exts,bintable_exts
+
+# some files for tests, of === the one used
+ffile = "../../../../test_data/gmosspect/gsN20011222S027.fits"
+od = "../../../../test_data/gndeploy1/N20110826S0336.fits"
+of = nf = "../../../../test_data/multibins.fits"
+
+
+od = AstroData(of) #original od
+print od.infostr()
+
+
+pixad = od[pixel_exts]
+print pixad.infostr()
+
+binad = od[bintable_exts]
+print binad.infostr()
