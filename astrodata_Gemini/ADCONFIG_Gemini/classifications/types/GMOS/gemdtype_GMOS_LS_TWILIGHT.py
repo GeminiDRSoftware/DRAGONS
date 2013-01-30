36c0348baaf595364f7d6935f27dd912e8c8eed8
Emma Hogan <ehogan@gemini.edu>
2013-01-30 23:29:27 +0000

    Removed duplicate file for GMOS_LS_TWILIGHT in the GMOS types directory
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3846 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/GMOS/gemdtype_GMOS_LS_TWILIGHT.py
+++
@@ -1,7 +0,0 @@
-class GMOS_LS_TWILIGHT(DataClassification):
-    name="GMOS_LS_TWILIGHT"
-    usage = ""
-    parent = "GMOS_LS"
-    requirement = PHU( OBSMODE='LONGSLIT',OBSTYPE='FLAT',OBJECT='Twilight' )
-    
-newtypes.append(GMOS_LS_TWILIGHT())
