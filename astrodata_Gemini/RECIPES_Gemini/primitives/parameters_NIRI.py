8834d57d47c20c53ba53ccf71cb5e6628095e365
Nelson Zarate <nzarate@gemini.edu>
2013-02-13 13:58:11 +0000

    New NIRI primitives
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3856 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ astrodata_Gemini/RECIPES_Gemini/primitives/parameters_NIRI.py
@@ -0,0 +1,52 @@
+# This parameter file contains the parameters related to the primitives located
+# in the primitives_NIRI.py file, in alphabetical order.
+{"standardizeInstrumentHeaders":{
+    "suffix":{
+        "default"       : "_instrumentHeadersStandardized",
+        "type"          : "str",
+        "recipeOverride": True,
+        "userOverride"  : True,
+        "uiLevel"       : "UIBASIC",
+        },
+    },
+ "standardizeStructure":{
+    "suffix":{
+        "default"       : "_structureStandardized",
+        "type"          : "str",
+        "recipeOverride": True,
+        "userOverride"  : True,
+        "uiLevel"       : "UIBASIC",
+        },
+    "attach_mdf":{
+        "default"       : True,
+        "type"          : "bool",
+        "recipeOverride": True,
+        "userOverride"  : True,
+        "uiLevel"       : "UIBASIC",
+        },
+    "mdf":{
+        "default"       : None,
+        # No default type defined, since the mdf parameter could be a string or
+        # an AstroData object
+        "recipeOverride": True,
+        "userOverride"  : True,
+        "uiLevel"       : "UIBASIC",
+        },
+    },
+ "validateData":{
+    "suffix":{
+        "default"       : "_dataValidated",
+        "type"          : "str",
+        "recipeOverride": True,
+        "userOverride"  : True,
+        "uiLevel"       : "UIBASIC",
+        },
+    "repair":{
+        "default"       : False,
+        "type"          : "bool",
+        "recipeOverride": True,
+        "userOverride"  : True,
+        "uiLevel"       : "UIBASIC",
+        },
+    },
+}
---
+++ astrodata_Gemini/RECIPES_Gemini/primitives/primitivesIndex.NIRI.py
@@ -0,0 +1,8 @@
+# This is added to the reduction object dictionary, but only one reduction
+# object per AstroData Type. NOTE: primitives are the member functions of a
+# Reduction Object.
+
+localPrimitiveIndex = {
+    "NIRI": ("primitives_NIRI.py", "NIRIPrimitives"),
+    "NIRI_IMAGE": ("primitives_NIRI_IMAGE.py", "NIRI_IMAGEPrimitives"),
+    }
