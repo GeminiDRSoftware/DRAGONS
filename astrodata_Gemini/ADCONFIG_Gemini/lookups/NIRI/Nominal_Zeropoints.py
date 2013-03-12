2fde83dcd0642d689938e69f710354460064e641
Nelson Zarate <nzarate@gemini.edu>
2013-03-12 18:35:30 +0000

    For NIRI type
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@3897 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/NIRI/Nominal_Zeropoints.py
@@ -0,0 +1,17 @@
+
+nominal_zeropoints = {
+    # Table of NIRI Nominal Zeropoint magnitudes
+    # By Camera and Filter ID.
+    # From Photometric zero points in "sciops.instruments
+    #   .PerformanceMonitoring.DataProducts.NIRI"
+
+
+        # BAND  CAMERA: Zeropoint (average)
+        ('Y',  'f6'):  22.99,
+        ('J',  'f6'):  23.97,
+        ('J',  'f32'): 23.33,
+        ('H',  'f6'):  24.05,
+        ('H',  'f32'): 23.62,
+        ('K',  'f6'):  23.43,
+        ('K',  'f32'): 22.95,
+}
