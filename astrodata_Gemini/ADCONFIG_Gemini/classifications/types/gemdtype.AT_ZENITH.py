a589be62eb15926734cdaddc45b89f94776d33dc
Paul Hirst <phirst@gemini.edu>
2013-06-28 19:41:11 +0000

    elaborate usage strings
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@4154 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/gemdtype.AT_ZENITH.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/gemdtype.AT_ZENITH.py
@@ -1,6 +1,6 @@
 class AT_ZENITH(DataClassification):
     name="AT_ZENITH"
-    usage = "Data taken at Zenith in the AZEL_TOPO co-ordinate system"
+    usage = "Data taken at Zenith in the AZEL_TOPO co-ordinate system. Normally this means that the telescope was parked at zenith."
     
     parent = "AZEL_TARGET"
     requirement = PHU(FRAME='AZEL_TOPO') & PHU(ELEVATIO='90.*')
