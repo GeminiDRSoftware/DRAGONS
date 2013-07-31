01f47a12041ab563c2286617a7a6e5ef27d5e58a
Ken Anderson <kanderso@gemini.edu>
2013-07-31 19:54:44 +0000

    updated urls w/ qa metrics query, upload_processed_cal, pushed from prsproxyutil
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@4323 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/lookups/calurl_dict.py
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/calurl_dict.py
@@ -1,5 +1,23 @@
+#
+#                                                                     QAP Gemini
+#
+#                                                                 calurl_dict.py
+#                                                                        07-2013
+# ------------------------------------------------------------------------------
+# $Id $
+# ------------------------------------------------------------------------------
+__version__      = '$Revision $'[11:-2]
+__version_date__ = '$Date $'[7:-2]
+# ------------------------------------------------------------------------------
+# FITS Store URLs, etc
+
 calurl_dict = {
      "CALMGR" : "http://fits/calmgr",
 "LOCALCALMGR" : "http://localhost:%(httpport)d/calmgr/%(caltype)s",
-"QAMETRICURL" : "http://fits/qareport"
+#"UPLOADPROCCAL": "http://hbffits3.hi.gemini.edu/upload_processed_cal",
+"UPLOADPROCCAL": "http://fits/upload_processed_cal",
+"QAMETRICURL" : "http://fits/qareport",
+"QAQUERYURL"  : "http://fits/qaforgui",
+#"QAMETRICURL" : "http://cpofits1new/qareport",   # test site
+#"QAQUERYURL"  : "http://cpofits1new/qaforgui"    # test site
               }
