eac0c0f62f2c2bd0baeae183a37d7269ae317579
Paul Hirst <phirst@gemini.edu>
2013-10-10 00:36:15 +0000

    need to kludge _IMAGE types not to include DARKs so that we can assign recipes to DARKs. Bleagh.
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@4419 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/NIRI/gemdtype.NIRI_IMAGE.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/NIRI/gemdtype.NIRI_IMAGE.py
@@ -3,7 +3,7 @@ class NIRI_IMAGE(DataClassification):
     name="NIRI_IMAGE"
     usage = "Applies to any IMAGE dataset from the NIRI instrument."
     parent = "NIRI"
-    requirement = ISCLASS('NIRI') & PHU({"{prohibit}FILTER3":"(.*?)grism(.*?)"})
+    requirement = ISCLASS('NIRI') & PHU({"{prohibit}FILTER3":"(.*?)grism(.*?)"}) & NOT(ISCLASS("NIRI_DARK"))
 
 
 newtypes.append(NIRI_IMAGE())
