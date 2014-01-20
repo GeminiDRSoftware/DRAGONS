86557106ffb6b1da522923b43fc446480e878eec
Ken Anderson <kanderso@gemini.edu>
2014-01-20 18:38:21 +0000

    formalized, purged unnecessary class attributes
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@4535 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/ParamObject.py
+++ astrodata/ParamObject.py
@@ -1,13 +1,20 @@
-class PrimitiveParameter( object ):
-    '''
-    This is the object which contains all relevant variables where primitive parameter data is stored.
-    '''
-    name = None
-    value = None
-    overwrite = None
-    help = ""
-    
-    def __init__(self, name, overwrite=False, helps="", value=None):
+#
+#                                                                 gemini_python
+#
+#                                                                      astrodata
+#                                                                 ParamObject.py
+# ------------------------------------------------------------------------------
+# $Id$
+# ------------------------------------------------------------------------------
+__version__      = '$Rev$'[11:-2]
+__version_date__ = '$Date$'[7:-2]
+# ------------------------------------------------------------------------------
+class PrimitiveParameter(object):
+    """
+    This is the object which contains all relevant variables where primitive 
+    parameter data are stored.
+    """
+    def __init__(self, name, value=None, overwrite=False, helps=""):
         self.name = name
         self.value = value
         self.overwrite = overwrite
@@ -16,9 +23,11 @@ class PrimitiveParameter( object ):
     def __str__(self):
         retstr = "Parameter (" + str(self.name) + '):'
         retstr = retstr + \
-"""
-Help      : %(help)s
-Value     : %(value)s
-Overwrite : %(overwrite)s
-""" % {'value':str(self.value), 'overwrite':str(self.overwrite), 'help':str(self.helps)}
+                 """
+                 Help      : %(help)s
+                 Value     : %(value)s
+                 Overwrite : %(overwrite)s
+                 """ % {'value':str(self.value), 
+                        'overwrite':str(self.overwrite), 
+                        'help':str(self.helps)}
         return retstr
