0f700e2a70c555e82923fa035156c958e1c2af65
Mark Simpson <msimpson@gemini.edu>
2014-03-05 22:42:27 +0000

    Merge of QAP-2014-1 NIRI_SPECT type definition
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@4590 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/NIRI/gemdtype.NIRI_SPECT.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/NIRI/gemdtype.NIRI_SPECT.py
@@ -3,6 +3,10 @@ class NIRI_SPECT(DataClassification):
     name="NIRI_SPECT"
     usage = "Applies to any spectra from the NIRI instrument."
     parent = "NIRI"
-    requirement = ISCLASS('NIRI') & PHU(FILTER3='(.*?)grism(.*?)')
+    requirement = AND([ISCLASS('NIRI'),
+                       PHU(FILTER3='(.*?)grism(.*?)'),
+                       NOT(ISCLASS("NIRI_DARK"))
+                       ])
+                       
 
 newtypes.append(NIRI_SPECT())
