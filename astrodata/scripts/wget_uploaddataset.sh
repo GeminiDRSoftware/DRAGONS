7e51051c9bb5ecd709cf14c1b84a10282c3d0ea8
Ken Anderson <kanderso@gemini.edu>
2014-03-19 15:55:53 +0000

    clean up on Trac #601
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@4614 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/scripts/wget_uploaddataset.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget http://hbffits3.hi.gemini.edu/upload_processed_cal/N20100221S0516_preparedBias.fits --post-file=/home/callen/SVN-AD/gemini_python/test_data/test_cal_files/processed_biases/N20100221S0516_preparedBias.fits
