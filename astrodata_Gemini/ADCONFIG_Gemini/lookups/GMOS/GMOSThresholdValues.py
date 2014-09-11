3c2fd5c0887a1f0bce56ff4df16d3fe1f5d4c511
Kathleen Labrie <klabrie@gemini.edu>
2014-09-11 00:52:16 +0000

    Merge GMOS-S Ham changes. Merge QAP-2014-1 r4859, 4864, 4869, 4871 to trunk.
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@4878 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/lookups/GMOS/GMOSThresholdValues.py
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/GMOS/GMOSThresholdValues.py
@@ -1,6 +1,7 @@
 # Keyed by : ampname for e2vDD and Hamamatsu CCDs. The old EEV CCDs
 # do not have a well-defined saturation level.
 # These limits are in electrons, for 1x1 unbiased data.
+# 2014-09-04: These values are for slow / low (science) mode - MS
 gmosThresholds = {
     # GMOS-N e2v DD CCDs
     "e2v 10031-23-05, left": 110900.0,
@@ -10,18 +11,18 @@ gmosThresholds = {
     "e2v 10031-18-04, left": 115700.0,
     "e2v 10031-18-04, right": 109200.0,
     # GMOS-S Hamamatsu CCDs ##M FULL_WELL
-    "BI5-36-4k-2, 1": 109936.6,
-    "BI5-36-4k-2, 2": 114811.2,
-    "BI5-36-4k-2, 3": 111680.6,
-    "BI5-36-4k-2, 4": 106126.3,
-    "BI11-33-4k-1, 1": 109102.6,
-    "BI11-33-4k-1, 2": 108751.6,
-    "BI11-33-4k-1, 3": 111020.3,
-    "BI11-33-4k-1, 4": 116642.2,
-    "BI12-34-4k-1, 1": 101582.7,
-    "BI12-34-4k-1, 2": 101842.5,
-    "BI12-34-4k-1, 3": 100492.2,
-    "BI12-34-4k-1, 4": 108391.5
+    "BI5-36-4k-2, 1": 108289.0,
+    "BI5-36-4k-2, 2": 113419.0,
+    "BI5-36-4k-2, 3": 111421.0,
+    "BI5-36-4k-2, 4": 106558.0,
+    "BI11-33-4k-1, 1": 109060.0,
+    "BI11-33-4k-1, 2": 110814.0,
+    "BI11-33-4k-1, 3": 110155.0,
+    "BI11-33-4k-1, 4": 113986.0,
+    "BI12-34-4k-1, 1": 99539.4,
+    "BI12-34-4k-1, 2": 98963.6,
+    "BI12-34-4k-1, 3": 98813.4,
+    "BI12-34-4k-1, 4": 105683.0
 }
 
 
