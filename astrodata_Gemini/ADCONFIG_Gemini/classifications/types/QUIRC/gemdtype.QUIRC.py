02c6a835bde178b209e5a2507575bcdfc343a5ad
Paul Hirst <phirst@gemini.edu>
2015-05-07 20:30:46 +0000

    quirc by itself - same types for now
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@5242 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/QUIRC/gemdtype.QUIRC.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/QUIRC/gemdtype.QUIRC.py
@@ -3,6 +3,6 @@ class HOKUPAAQUIRC(DataClassification):
     name="HOKUPAAQUIRC"
     usage = "Applies to datasets from the HOKUPAA+QUIRC instrument"
     parent = "GEMINI"
-    requirement = PHU(INSTRUME='Hokupaa\+QUIRC')
+    requirement = PHU(INSTRUME='Hokupaa\+QUIRC') | PHU(INSTRUME='QUIRC')
 
 newtypes.append(HOKUPAAQUIRC())
