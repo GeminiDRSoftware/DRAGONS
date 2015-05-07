4bfe41859413307f1c82d2ed99fdb061aea8b331
Paul Hirst <phirst@gemini.edu>
2015-05-07 20:30:29 +0000

    some old files have lower case gnirs....
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@5241 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/GNIRS/gemdtype.GNIRS.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/GNIRS/gemdtype.GNIRS.py
@@ -3,6 +3,6 @@ class GNIRS(DataClassification):
     name="GNIRS"
     usage = "Applies to all datasets from the GNIRS instrument."
     parent = "GEMINI"
-    requirement = PHU(INSTRUME='GNIRS')
+    requirement = PHU(INSTRUME='GNIRS') | PHU(INSTRUME='gnirs')
 
 newtypes.append(GNIRS())
