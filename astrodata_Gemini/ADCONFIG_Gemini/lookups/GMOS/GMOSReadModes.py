b9a3826eb853e95f69913204915280c33acbd543
Ken Anderson <kanderso@gemini.edu>
2015-06-05 13:44:03 +0000

    Original read mode lookup table was wrong all along.
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@5270 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/lookups/GMOS/GMOSReadModes.py
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/GMOS/GMOSReadModes.py
@@ -4,16 +4,16 @@
 #
 # It is unclear whether there is an 'Engineering' read mode for 
 # Hamamatsu CCDs. This mode is not defined in the requirements document,
-# GMOS-S Hamamatsu CCD Upgrades, v2.0 - 15 December 2014 (M Simpson)
+# GMOS-S Hamamatsu CCD Upgrades, v2.0 - 15 December 2014 (M Simpson).
 # The mode is left in here for JIC purposes as the final possible combination
-# [ gain_setting, read_speed_setting ]
+# [ gain_setting, read_speed_setting ].
 
 # 04-06-2015, kra
 read_mode_map = {
     "default": { 
         "Normal": ["low", "slow"],
-        "Bright": ["low", "fast"],
-        "Acquisition": ["high", "fast"],
+        "Bright": ["hgh", "fast"],
+        "Acquisition": ["low", "fast"],
         "Engineering": ["high", "slow"],
     },
     "Hamamatsu": { 
