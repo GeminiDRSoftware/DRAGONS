72ace81b16621450397d600ae48bc68d4c33c2cb
Emily Down <edown@gemini.edu>
2015-07-29 01:24:55 +0000

    Bug fix to NON-SIDEREAL astrodata type.
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@5291 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/types/gemdtype.SIDEREAL.py
+++ astrodata_Gemini/ADCONFIG_Gemini/classifications/types/gemdtype.SIDEREAL.py
@@ -4,6 +4,10 @@ class SIDEREAL(DataClassification):
     usage = "Data taken with the telesocope tracking siderealy"
     
     parent = "GEMINI"
-    requirement = PHU(DECTRACK='0.') & PHU(RATRACK='0.') & PHU(FRAME='FK5')
+#    requirement = PHU(DECTRACK='0.') & PHU(RATRACK='0.') & PHU(FRAME='FK5')
+    requirement = AND([PHU(FRAME="FK5"),
+                      OR([PHU(DECTRACK="^0$"), PHU(DECTRACK="^0.(0)*\s*$"),]),
+                      OR([PHU(RATRACK="^0$"), PHU(RATRACK="^0.(0)*\s*$")])
+                     ])   
 
 newtypes.append(SIDEREAL())
