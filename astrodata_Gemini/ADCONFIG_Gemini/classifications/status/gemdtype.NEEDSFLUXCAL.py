1befc9415dffd393457d958d3727152577153f61
Paul Hirst <phirst@gemini.edu>
2015-09-18 02:36:15 +0000

    this was never used
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@5338 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/classifications/status/gemdtype.NEEDSFLUXCAL.py
+++
@@ -1,7 +0,0 @@
-class NEEDSFLUXCAL(DataClassification):
-    editprotect=False
-    name="NEEDSFLUXCAL"
-    usage = 'Applies to data ready for flux calibration.'
-    requirement = ISCLASS("IMAGE") & ISCLASS("PREPARED")
-    
-newtypes.append(NEEDSFLUXCAL())
