db435b5e9df1bc30b6b4428e5043cd529c1dee6f
Kathleen Labrie <klabrie@gemini.edu>
2015-09-30 00:23:50 +0000

    Update path to GN raw data to match new volume name.  wikiwiki is no more.
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@5353 e04282df-fb77-4b70-bf7a-c1223586725c

--- gempy/gemini/opsdefs.py
+++ gempy/gemini/opsdefs.py
@@ -3,7 +3,7 @@ GEMINI_NORTH = 'gemini-north'
 GEMINI_SOUTH = 'gemini-south'
 OPSDATAPATH = { GEMINI_NORTH : '/gemsoft/dhs/perm/',
                 GEMINI_SOUTH : '/gemsoft/dhs/perm/' }
-OPSDATAPATHBKUP = { GEMINI_NORTH : '/net/wikiwiki/vol/dhs_perm/',
+OPSDATAPATHBKUP = { GEMINI_NORTH : '/net/mko-nfs/sci/dhs/',
                     GEMINI_SOUTH : '/net/petrohue/tier1/gem/dhs/perm/' }
 OBSPREF = { GEMINI_NORTH : 'N',
             GEMINI_SOUTH : 'S' }
