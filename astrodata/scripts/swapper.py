9456eb942e641fd8dbd847a5d4eed79fbc09341b
Ken Anderson <kanderso@gemini.edu>
2015-12-14 18:52:44 +0000

    fix indentation problem
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@5454 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata/scripts/swapper.py
+++ astrodata/scripts/swapper.py
@@ -169,16 +169,16 @@ def handleCLArgs():
 # print Faces.BOLD + 'Hello World !' + Faces.END
 
 class Faces(object):
-   PURPLE    = '\033[95m'
-   CYAN      = '\033[96m'
-   DARKCYAN  = '\033[36m'
-   BLUE      = '\033[94m'
-   GREEN     = '\033[92m'
-   YELLOW    = '\033[93m'
-   RED       = '\033[91m'
-   BOLD      = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END       = '\033[0m'
+    PURPLE    = '\033[95m'
+    CYAN      = '\033[96m'
+    DARKCYAN  = '\033[36m'
+    BLUE      = '\033[94m'
+    GREEN     = '\033[92m'
+    YELLOW    = '\033[93m'
+    RED       = '\033[91m'
+    BOLD      = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END       = '\033[0m'
 
 # ------------------------------------------------------------------------------
 class Swap(object):
