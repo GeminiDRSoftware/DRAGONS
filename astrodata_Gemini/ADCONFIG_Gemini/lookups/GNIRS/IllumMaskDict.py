fcecf5ba72f797ce53535b999313aa72511784e1
Emily Down <edown@gemini.edu>
2016-01-28 16:51:27 +0000

    Extra BPM added, and renaming of BPMs so more easily identifiable.
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@5523 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/lookups/GNIRS/IllumMaskDict.py
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/GNIRS/IllumMaskDict.py
@@ -5,17 +5,17 @@ illum_masks = {
     # Updated 2015.11.10
 
     # (CAMERA, FILTERTYPE): Illumination mask
-    ('ShortBlue_G5540', 'Broadband'): 'Gemini/GNIRS/BPM/illum_mask1.fits',
-    ('ShortBlue_G5540', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_mask3.fits',
-    ('ShortBlue_G5538', 'Broadband'): 'Gemini/GNIRS/BPM/illum_mask1.fits',
-    ('ShortBlue_G5538', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_mask3.fits',
-    ('LongBlue_G5542', 'Broadband'): 'Gemini/GNIRS/BPM/illum_mask2.fits',
-    ('LongBlue_G5542', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_mask4.fits',
+    ('ShortBlue_G5540', 'Broadband'): 'Gemini/GNIRS/BPM/illum_mask_ShortBlue_G5540_Broad.fits',
+    ('ShortBlue_G5540', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_mask_ShortBlue_G5540_Narrow.fits',
+    ('ShortBlue_G5538', 'Broadband'): 'Gemini/GNIRS/BPM/illum_mask_ShortBlue_G5540_Broad.fits',
+    ('ShortBlue_G5538', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_mask_ShortBlue_G5540_Narrow.fits',
+    ('LongBlue_G5542', 'Broadband'): 'Gemini/GNIRS/BPM/illum_mask_LongBlue_G5542_Broad.fits',
+    ('LongBlue_G5542', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_mask_LongBlue_G5542_Narrow.fits',
 #    ('ShortRed_G5539', 'Broadband'): 'Gemini/GNIRS/BPM/illum_maskx.fits',
 #    ('ShortRed_G5539', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_maskx.fits',
 #    ('LongRed_G5516', 'Broadband'): 'Gemini/GNIRS/BPM/illum_maskx.fits',
 #    ('LongBlue_G5516', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_maskx.fits',
-#    ('LongRed_G5543', 'Broadband'): 'Gemini/GNIRS/BPM/illum_maskx.fits',
+    ('LongRed_G5543', 'Broadband'): 'Gemini/GNIRS/BPM/illum_mask_LongRed_G5543_Broad.fits',
 #    ('LongBlue_G5543', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_maskx.fits',
 
 }
