9a0a8639f5f99714e0012e9a44cfdb0f9b992516
Emily Down <edown@gemini.edu>
2016-02-03 12:48:58 +0000

    Updated GNIRS BPMs (to include LongRed NoWings and LongBlue NoWings).
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@5541 e04282df-fb77-4b70-bf7a-c1223586725c

--- astrodata_Gemini/ADCONFIG_Gemini/lookups/GNIRS/IllumMaskDict.py
+++ astrodata_Gemini/ADCONFIG_Gemini/lookups/GNIRS/IllumMaskDict.py
@@ -1,22 +1,17 @@
 
 illum_masks = {
-    # Table of GNIRS illumination masks by camera and filter type (broad band
-    # or narrow band).
-    # Updated 2015.11.10
+    # Table of GNIRS illumination masks by camera and filter type (filters with or
+    # without wings, see http://www.gemini.edu/sciops/instruments/gnirs/imaging).
+    # Updated 2016.02.03
 
-    # (CAMERA, FILTERTYPE): Illumination mask
-    ('ShortBlue_G5540', 'Broadband'): 'Gemini/GNIRS/BPM/illum_mask_ShortBlue_G5540_Broad.fits',
-    ('ShortBlue_G5540', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_mask_ShortBlue_G5540_Narrow.fits',
-    ('ShortBlue_G5538', 'Broadband'): 'Gemini/GNIRS/BPM/illum_mask_ShortBlue_G5540_Broad.fits',
-    ('ShortBlue_G5538', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_mask_ShortBlue_G5540_Narrow.fits',
-    ('LongBlue_G5542', 'Broadband'): 'Gemini/GNIRS/BPM/illum_mask_LongBlue_G5542_Broad.fits',
-    ('LongBlue_G5542', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_mask_LongBlue_G5542_Narrow.fits',
-#    ('ShortRed_G5539', 'Broadband'): 'Gemini/GNIRS/BPM/illum_maskx.fits',
-#    ('ShortRed_G5539', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_maskx.fits',
-#    ('LongRed_G5516', 'Broadband'): 'Gemini/GNIRS/BPM/illum_maskx.fits',
-#    ('LongBlue_G5516', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_maskx.fits',
-    ('LongRed_G5543', 'Broadband'): 'Gemini/GNIRS/BPM/illum_mask_LongRed_G5543_Broad.fits',
-    ('LongRed_G5543', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_mask_LongRed_G5543_Narrow.fits',
-#    ('LongBlue_G5543', 'Narrowband'): 'Gemini/GNIRS/BPM/illum_maskx.fits',
+    # (CAMERA, MASKSHAPE): Illumination mask
+    ('ShortBlue_G5540', 'Wings'): 'Gemini/GNIRS/BPM/illum_mask_ShortBlue_G5540_Wings.fits',
+    ('ShortBlue_G5540', 'NoWings'): 'Gemini/GNIRS/BPM/illum_mask_ShortBlue_G5540_NoWings.fits',
+    ('ShortBlue_G5538', 'Wings'): 'Gemini/GNIRS/BPM/illum_mask_ShortBlue_G5540_Wings.fits',
+    ('ShortBlue_G5538', 'NoWings'): 'Gemini/GNIRS/BPM/illum_mask_ShortBlue_G5540_NoWings.fits',
+    ('LongBlue_G5542', 'Wings'): 'Gemini/GNIRS/BPM/illum_mask_LongBlue_G5542_Wings.fits',
+    ('LongBlue_G5542', 'NoWings'): 'Gemini/GNIRS/BPM/illum_mask_LongBlue_G5542_NoWings.fits',
+    ('LongRed_G5543', 'Wings'): 'Gemini/GNIRS/BPM/illum_mask_LongRed_G5543_Wings.fits',
+    ('LongRed_G5543', 'NoWings'): 'Gemini/GNIRS/BPM/illum_mask_LongRed_G5543_NoWings.fits',
 
 }
