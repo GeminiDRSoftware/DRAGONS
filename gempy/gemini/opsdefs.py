549e37a3169fac58fb3a1173457f0761ee87e87d
Kathleen Labrie <klabrie@gemini.edu>
2016-02-12 02:48:33 +0000

    Removes opsdef.py which contains gemini paths. Those paths should not be public.  instrument_config.py in gemaux_python has the required information.  Nothing is left that uses opsdef.py anymore.  Trac #559.
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@5575 e04282df-fb77-4b70-bf7a-c1223586725c

--- gempy/gemini/opsdefs.py
+++
@@ -1,9 +0,0 @@
-# Define some variables that give site information
-GEMINI_NORTH = 'gemini-north'
-GEMINI_SOUTH = 'gemini-south'
-OPSDATAPATH = { GEMINI_NORTH : '/gemsoft/dhs/perm/',
-                GEMINI_SOUTH : '/gemsoft/dhs/perm/' }
-OPSDATAPATHBKUP = { GEMINI_NORTH : '/net/mko-nfs/sci/dhs/',
-                    GEMINI_SOUTH : '/net/cpostonfs-nv1/tier1/dhs/sto/perm/' }
-OBSPREF = { GEMINI_NORTH : 'N',
-            GEMINI_SOUTH : 'S' }
