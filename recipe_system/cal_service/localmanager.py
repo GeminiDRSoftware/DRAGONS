ef82165a689837985447fcb38695f2ba3631293a
Ricardo Cardenes <rcardene@gemini.edu>
2016-04-22 02:49:17 +0000

    First iteration for the on-disk calibration search
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@5760 e04282df-fb77-4b70-bf7a-c1223586725c

---
+++ recipe_system/cal_service/localmanager.py
@@ -0,0 +1,134 @@
+import os
+from os.path import abspath, basename, dirname
+
+from gemini_calmgr import fits_storage_config as fsc
+from gemini_calmgr import gemini_metadata_utils as gmu
+from gemini_calmgr import orm
+from gemini_calmgr.cal import get_cal_object
+
+__all__ = ['LocalManager']
+
+extra_descript = {
+    'GMOS_NODANDSHUFFLE': 'nodandshuffle',
+    'SPECT': 'spectroscopy',
+    'OVERSCAN_SUBTRACTED': 'overscan_subtracted',
+    'OVERSCAN_TRIMMED': 'overscan_trimmed',
+    'PREPARED': 'prepared'
+}
+
+args_for_cals = {
+    # cal_type      : (method_name, {arg_name: value, ...})
+    'processed_arc':  ('arc', {'processed': True}),
+    'processed_bias': ('bias', {'processed': True}),
+    'processed_dark': ('dark', {'processed': True}),
+    'processed_flat': ('flat', {'processed': True})
+}
+
+
+class LocalManager(object):
+    def __init__(self, db_path):
+        self._db_path = db_path
+        self.session = None
+        self._reset()
+
+    def _reset(self):
+        """Modifies the gemini_calmgr setup and reloads the involved config and
+        orm modules. Then it sets a new database session object for this
+        instance.
+
+        It is a bit kludgy, but Fits Storage was not designed to change
+        databases on the fly.
+        """
+        fsc.storage_root = abspath(dirname(self._db_path))
+        fsc.fits_dbname = basename(self._db_path)
+        fsc.db_path = self._db_path
+        fsc.fits_database = 'sqlite:///' + fsc.db_path
+        reload(fsc)
+        reload(orm)
+        self.session = orm.sessionfactory()
+
+    def calibration_search(self, rq, fullResult=False):
+        """Performs a search in the database using the requested criteria.
+
+        Parameters
+        ----------
+        rq : dict
+            Contains the search criteria, including instrument, descriptors,
+            etc.
+        fullResult : bool
+            This is here just for API compatibility. It's not used anywhere
+            in the code, anyway, and should probably be removed altogether.
+
+        Returns
+        -------
+
+        result: tuple
+            A tuple of exactly two elements.
+
+            In the case of success, the tuple contains two strings, the first
+            being the URL to a calibration file, and the second its MD5 sum.
+
+            When an error occurs, the first element in the tuple will be
+            ``None``, and the second a string describing the error.
+        """
+        from datetime import datetime
+
+        print "\n@ppu074: calibration_search() ..."
+
+        caltype = rq["caltype"]
+        descripts = rq["descriptors"]
+        types = rq["types"]
+
+        if "ut_datetime" in descripts:
+            utc = descripts["ut_datetime"]
+            pyutc = datetime.strptime(utc.value, "%Y%m%dT%H:%M:%S")
+            print "@ppu079: OBS UT Date Time:", pyutc
+            descripts.update({"ut_datetime":pyutc})
+
+        for (type_, desc) in extra_descript.items():
+            descripts[desc] = type_ in types
+
+        nones = [desc for (desc, value) in descripts.items() if value is None]
+
+        # Obtain a calibration manager object instantiated according to the
+        # instrument.
+        cal_obj = get_cal_object(self.session, filename=None, header=None,
+                                 descriptors=descripts, types=types)
+
+        caltypes = gmu.cal_types if caltype == '' else [caltype]
+        # The function that downloads an XML returns only the first result,
+        # so we won't bother iterating over the whole thing in case that
+        # caltype was empty.
+        ct = caltypes[0]
+        method, args = args_for_cals.get(ct, (ct, {}))
+        # Obtain a list of calibrations for the specified cal type
+        cals = getattr(cal_obj, method)(**args)
+
+        for cal in cals:
+            if cal.diskfile.present:
+                path = os.path.join(fsc.storage_root, cal.diskfile.file.path,
+                                    cal.diskfile.file.name)
+
+                return ('file://{}'.format(path), cal.diskfile.data_md5)
+
+        # sent_nones = "No Nones Set" if not nones else ", ".join(nones)
+        # preerr = RESPONSESTR % { "sequence": pformat(sequence),
+        #                          "response": response.strip(),
+        #                          "nones"   : sent_nones }
+
+        # try:
+        #     dom = minidom.parseString(response)
+        #     calel = dom.getElementsByTagName("calibration")
+        #     calurlel = dom.getElementsByTagName('url')[0].childNodes[0]
+        #     calurlmd5 = dom.getElementsByTagName('md5')[0].childNodes[0]
+        # except IndexError:
+        #     print "No url for calibration in response, calibration not found"
+        #     return (None, preerr)
+        # except:
+        #     return (None, preerr)
+
+        #print "prs70:", calurlel.data
+
+        #@@TODO: test only 
+        # print "@ppu165: ", repr(calurlel.data)
+        # return (calurl, calurlmd5)
