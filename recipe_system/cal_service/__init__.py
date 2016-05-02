83d55242f50cc5c8c90b6bcd9764f8d0298700c9
Ricardo Cardenes <rcardene@gemini.edu>
2016-05-02 20:55:42 +0000

    Use localmanager only if gemini_calmgr is available
    git-svn-id: http://chara.hi.gemini.edu/svn/DRSoftware/gemini_python/trunk@5792 e04282df-fb77-4b70-bf7a-c1223586725c

--- recipe_system/cal_service/__init__.py
+++ recipe_system/cal_service/__init__.py
@@ -1,6 +1,11 @@
 from ..config import globalConf
 from . import prsproxyutil
-from . import localmanager
+try:
+    from . import localmanager
+    localmanager_available = True
+except ImportError as e:
+    localmanager_available = False
+    import_error = e.message
 
 CONFIG_SECTION = 'calibs'
 
@@ -18,6 +23,10 @@ def cal_search_factory():
     ret = prsproxyutil.calibration_search
     try:
         if calconf.standalone:
+            if not localmanager_available:
+                raise RuntimeError(
+                        "Local calibs manager has been chosen, but there "
+                        "are missing dependencies: {}".format(import_error))
             lm = localmanager.LocalManager(calconf.database_dir)
             ret = lm.calibration_search
     except AttributeError:
