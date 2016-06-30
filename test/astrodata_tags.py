e0b311544c3b45db582d05a8777113b4f61a33ba
Ricardo Cardenes <rcardene@gemini.edu>
2016-06-29 16:09:27 -1000

    Added tests for tags

---
+++ test/astrodata_tags.py
@@ -0,0 +1,19 @@
+import pytest
+import astrodata
+import instruments
+
+from lut_tags import fixture_data as tags_fixture_data
+
+class FixtureIterator(object):
+    def __init__(self, data_dict):
+        self._data = data_dict
+
+    def __iter__(self):
+        for key in sorted(self._data.keys()):
+            (instr, filename) = key
+            ad = astrodata.open('data/{}/{}'.format(instr, filename))
+            yield ad, set(self._data[key])
+
+@pytest.mark.parametrize("ad,tag_set", FixtureIterator(tags_fixture_data))
+def test_descriptor(ad, tag_set):
+    assert ad.tags() == tag_set
---
+++ test/lut_tags.py
@@ -0,0 +1,4 @@
+fixture_data = {
+    ('gnirs', 'N20160523S0191.fits'): ['GEMINI_NORTH', 'SIDEREAL', 'LS', 'SPECT', 'UNPREPARED', 'RAW'],
+    ('nifs', 'N20160428S0174.fits') : ['IFU', 'GEMINI_NORTH', 'AZEL_TARGET', 'NON_SIDEREAL', 'SPECT', 'UNPREPARED', 'RAW']
+}
