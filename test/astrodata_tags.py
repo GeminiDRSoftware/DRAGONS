5a0e0d15d82d88d97651142042d680bd68ccba02
Ricardo Cardenes <rcardenes@gemini.edu>
2016-07-26 10:36:24 -1000

    Make sure that we access the right directory for test data

--- test/astrodata_tags.py
+++ test/astrodata_tags.py
@@ -1,8 +1,11 @@
 import pytest
 import astrodata
 import instruments
+import os
 
 from lut_tags import fixture_data as tags_fixture_data
+
+THIS_DIR=os.path.dirname(__file__)
 
 class FixtureIterator(object):
     def __init__(self, data_dict):
@@ -11,7 +14,7 @@ class FixtureIterator(object):
     def __iter__(self):
         for key in sorted(self._data.keys()):
             (instr, filename) = key
-            ad = astrodata.open('data/{}/{}'.format(instr, filename))
+            ad = astrodata.open(os.path.join(THIS_DIR, 'test_data', instr, filename))
             yield filename, ad, set(self._data[key])
 
 @pytest.mark.parametrize("fn,ad,tag_set", FixtureIterator(tags_fixture_data))
