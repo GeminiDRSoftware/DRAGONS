2b5b0851451fd815cf4490fc9e9bb38b89b2a66c
Ricardo Cardenes <rcardenes@gemini.edu>
2016-09-26 10:34:31 -1000

    remove astrodata core tests (not significant at the moment)

--- test/astrodata_core.py
+++
@@ -1,7 +0,0 @@
-import astrodata
-import instruments
-
-def test_gemini_with_no_instrument():
-    # This should
-    obj = astrodata.create(phu={'OBSERVAT': 'GEMINI-NORTH', 'TELESCOP': 'GEMINI'})
-    assert isinstance(obj, instruments.gemini.AstroDataGemini)
