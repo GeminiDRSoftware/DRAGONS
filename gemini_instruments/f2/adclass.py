dd41c7b82ebd69d2574057f3d2d597cdce72ef2e
Ricardo Cardenes <rcardenes@gemini.edu>
2016-10-07 10:09:19 -1000

    Revert 4849e9f9f3f73ed8b0940436dab583b5a31cf009. This was not indented

--- gemini_instruments/f2/adclass.py
+++ gemini_instruments/f2/adclass.py
@@ -39,7 +39,7 @@ class AstroDataF2(AstroDataGemini):
         if self.phu.get('GRISM') == 'Open':
             return TagSet(['IMAGE'])
 
-    @astro_data_tag
+    # Do not tag this as astro_data_tag. It's a helper function
     def _tag_is_spect(self):
         grism = self.phu.get('GRISM', '')
         grpos = self.phu.get('GRISMPOS', '')
