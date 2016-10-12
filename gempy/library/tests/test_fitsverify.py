4ba4d42e5611c7d289a0f5d52de365bdd2e7c054
Kathleen Labrie <klabrie@gemini.edu>
2016-10-11 15:47:52 -1000

    Adds unit test for fitsverify.

---
+++ gempy/library/tests/test_fitsverify.py
@@ -0,0 +1,50 @@
+# pytest suite
+
+"""
+Tests for the fitsverify module.
+
+This is a suite of tests to be run with pytest.
+
+To run:
+   1) Set the environment variable GEMPYTHON_TESTDATA to the path that contains
+      the file N20130510S0178_forStack.fits.
+      Eg. /net/chara/data2/pub/ad_testdata/GMOS
+   2) From the ??? (location): py.test -v
+"""
+
+import os
+import os.path
+from gempy.library import fitsverify
+
+TESTDATAPATH = os.getenv('GEMPYTHON_TESTDATA', '.')
+TESTFITS = 'N20130510S0178_forStack.fits'
+
+class TestFitsverify:
+    """
+    Suite of tests for the functions in the fitsverify module.
+    """
+
+    @classmethod
+    def setup_class(cls):
+        """Run once at the beginning."""
+        TestFitsverify.fits_file = os.path.join(TESTDATAPATH, TESTFITS)
+
+    @classmethod
+    def teardown_class(cls):
+        """Run once at the end."""
+        pass
+
+    def setup_method(self, method):
+        """Run once before every test."""
+        pass
+
+    def teardown_method(self, method):
+        """Run once after every test."""
+        pass
+
+    def test_fitsverify(self):
+        """
+        Test the return values of fitsverify on our test file.
+        """
+        returned_values = fitsverify.fitsverify(TestFitsverify.fits_file)
+        assert returned_values[:3] == [1, '21', '0']
