cd791b842553b3452fbfe4a7f434a58fab05f660
Kathleen Labrie <klabrie@gemini.edu>
2016-10-28 01:55:37 +0200

    And do not import the etis that were removed

--- gempy/gemini/eti/__init__.py
+++ gempy/gemini/eti/__init__.py
@@ -5,11 +5,3 @@ __version__ = '0.1.0 (May 2010)'
 import gemcombineeti
 import gireduceeti
 import gmosaiceti
-import gsappwaveeti
-import gscrrejeti
-import gsextracteti
-import gsflateti
-import gsskysubeti
-import gstransformeti
-import gswavelengtheti
-import sploteti
