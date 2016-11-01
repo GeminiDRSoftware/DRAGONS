9d6aa55546c4cc392e57b68e495e5aa43606ad8c
Kathleen Labrie <klabrie@gemini.edu>
2016-11-01 10:35:59 -1000

    Refactor the gmos bias recipe.

--- geminidr/gmos/recipes/QA/recipes_BIAS.py
+++ geminidr/gmos/recipes/QA/recipes_BIAS.py
@@ -1,13 +1,25 @@
-# This recipe performs the standardization and corrections needed to convert 
-# the raw input bias images into a single stacked bias image. This output 
-# processed bias is stored on disk using storeProcessedBias and has a name 
-# equal to the name of the first input bias image with "_bias.fits" appended.
+"""
+Recipes available to data with tags ['GMOS', 'CAL', 'BIAS'].
+Default is "makeProcessedBias".
+"""
+recipe_tags = set(['GMOS', 'CAL', 'BIAS'])
 
-prepare
-addDQ
-addVAR(read_noise=True)
-overscanCorrect
-addToList(purpose="forStack")
-getList(purpose="forStack")
-stackFrames
-storeProcessedBias
+default = makeProcessedBias
+
+def makeProcessedBias(p):
+    """
+    This recipe performs the standardization and corrections needed to convert
+    the raw input bias images into a single stacked bias image. This output
+    processed bias is stored on disk using storeProcessedBias and has a name
+    equal to the name of the first input bias image with "_bias.fits" appended.
+    """
+
+    p.prepare()
+    p.addDQ()
+    p.addVAR(read_noise=True)
+    p.overscanCorrect()
+    p.addToList(purpose="forStack")
+    p.getList(purpose="forStack")
+    p.stackFrames()
+    p.storeProcessedBias()
+    return
