b89a978c5f1b6f1ef440dd4ce0bcecf51829825b
Kathleen Labrie <klabrie@gemini.edu>
2016-11-01 13:57:42 -1000

    Refactor GMOS SQ recipes.

--- geminidr/gmos/recipes/SQ/recipes_BIAS.py
+++ geminidr/gmos/recipes/SQ/recipes_BIAS.py
@@ -1,7 +1,29 @@
-prepare
-addDQ
-addVAR(read_noise=True)
-overscanCorrect
-stackFrames
-makeIRAFCompatible
-storeProcessedBias
+"""
+Recipes available to data with tags ['GMOS', 'CAL', 'BIAS'].
+Default is "makeProcessedBias".
+"""
+recipe_tags = set(['GMOS', 'CAL', 'BIAS'])
+
+default = makeProcessedBias
+
+def makeProcessedBias(p):
+    """
+     This recipe performs the standardization and corrections needed to convert
+     the raw input bias images into a single stacked bias image. This output
+     processed bias is stored on disk using storeProcessedBias and has a name
+     equal to the name of the first input bias image with "_bias.fits" appended.
+
+     Parameters
+     ----------
+     p : PrimitivesCORE object
+         A primitive set matching the recipe_tags.
+     """
+
+    p.prepare()
+    p.addDQ()
+    p.addVAR(read_noise=True)
+    p.overscanCorrect()
+    p.stackFrames()
+    p.makeIRAFCompatible()
+    p.storeProcessedBias()
+    return
--- geminidr/gmos/recipes/SQ/recipes_IMAGE.py
+++ geminidr/gmos/recipes/SQ/recipes_IMAGE.py
@@ -1,15 +1,33 @@
-# This recipe performs the standardization and corrections needed to convert
-# the raw input science images into a single stacked science image
+"""
+Recipes available to data with tags ['GMOS', 'IMAGE'].
+Default is "reduce".
+"""
+recipe_tags = set(['GMOS', 'IMAGE'])
 
-prepare
-addDQ
-addVAR(read_noise=True)
-overscanCorrect
-biasCorrect
-ADUToElectrons
-addVAR(poisson_noise=True)
-flatCorrect
-mosaicDetectors
-makeFringe
-fringeCorrect
-alignAndStack
+default = reduce
+
+def reduce(p):
+    """
+    This recipe performs the standardization and corrections needed to
+    convert the raw input science images into a stacked image.
+
+    Parameters
+    ----------
+    p : PrimitivesCORE object
+        A primitive set matching the recipe_tags.
+    """
+
+    p.prepare()
+    p.addDQ()
+    p.addVAR(read_noise=True)
+    p.overscanCorrect()
+    p.biasCorrect()
+    p.ADUToElectrons()
+    p.addVAR(poisson_noise=True)
+    p.flatCorrect()
+    p.mosaicDetectors()
+    p.makeFringe()
+    p.fringeCorrect()
+    p.alignAndStack()
+    return
+
