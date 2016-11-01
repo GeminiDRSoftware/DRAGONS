fb283341febe4e541f4264b09b15dd599afee913
Kathleen Labrie <klabrie@gemini.edu>
2016-11-01 13:51:46 -1000

    One more GMOS QA recipe, N&S

--- geminidr/gmos/recipes/QA/recipes_NS.py
+++ geminidr/gmos/recipes/QA/recipes_NS.py
@@ -1,10 +1,30 @@
-prepare
-addDQ
-addVAR(read_noise=True)
-overscanCorrect
-#biasCorrect
-ADUToElectrons
-addVAR(poisson_noise=True)
-findAcquisitionSlits
-skyCorrectNodShuffle
-measureIQ(display=True)
+"""
+Recipes available to data with tags ['GMOS', 'NODANDSHUFFLE']
+Default is "reduce".
+"""
+recipe_tags = set(['GMOS', 'NODANDSHUFFLE'])
+
+default = reduce
+
+def reduce(p):
+    """
+    This recipe does a quick reduction of GMOS nod and shuffle data.
+    The data is left in its 2D form, and only a sky correction is done.
+    The seeing from the spectra cross-section is measured when possible.
+
+    Parameters
+    ----------
+    p : PrimitivesCORE object
+        A primitive set matching the recipe_tags.
+    """
+    p.prepare()
+    p.addDQ()
+    p.addVAR(read_noise=True)
+    p.overscanCorrect()
+    p.biasCorrect()
+    p.ADUToElectrons()
+    p.addVAR(poisson_noise=True)
+    p.findAcquisitionSlits()
+    p.skyCorrectNodShuffle()
+    p.measureIQ(display=True)
+    return
