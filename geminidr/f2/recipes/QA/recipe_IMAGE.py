ea51bf41c3b1d938ad67e7dce921b11319245456
Kathleen Labrie <klabrie@gemini.edu>
2016-11-01 14:39:57 -1000

    moves and refactor F2 makeProcessedFlat and QA reduce.

---
+++ geminidr/f2/recipes/QA/recipe_IMAGE.py
@@ -0,0 +1,81 @@
+"""
+Recipes available to data with tags ['F2', 'IMAGE', 'CAL', 'FLAT']
+Default is "reduce_nostack".
+"""
+recipe_tags = set(['F2', 'IMAGE', 'CAL', 'FLAT'])
+
+# The nostack version is used because stacking was too slow for QAP.
+# KL: Is this still true with gemini_python 2.0?
+default = reduce_nostack
+
+def reduce(p):
+    """
+    This recipe process F2 data up to and including alignement and stacking.
+    A single stacked output image is produced.
+    It will attempt to do dark and flat correction if a processed calibration
+    is available.  Sky subtraction is done when possible.  QA metrics are
+    measured.
+
+    Parameters
+    ----------
+    p : PrimitivesCORE object
+        A primitive set matching the recipe_tags.
+    """
+
+    p.prepare()
+    p.addDQ()
+    p.ADUToElectrons()
+    p.addVAR(read_noise=True, poisson_noise=True)
+    p.darkCorrect()
+    p.flatCorrect()
+    p.detectSources()
+    p.measureIQ(display=True)
+    p.measureBG()
+    p.measureCCAndAstrometry()
+    p.subtractSkyBackground()
+    p.addToList(purpose=forSky)
+    p.getList(purpose=forSky)
+    p.makeSky()
+    p.skyCorrect()
+    p.detectSources()
+    p.measureIQ(display=True)
+    p.measureCCAndAstrometry()
+    p.alignAndStack()
+    p.detectSources()
+    p.measureIQ(display=True)
+    p.measureCCAndAstrometry()
+    return
+
+
+def reduce_nostack(p):
+    """
+    This recipe process F2 data up to but not including alignment and stacking.
+    It will attempt to do dark and flat correction if a processed calibration
+    is available.  Sky subtraction is done when possible.  QA metrics are
+    measured.
+
+    Parameters
+    ----------
+    p : PrimitivesCORE object
+        A primitive set matching the recipe_tags.
+    """
+
+    p.prepare()
+    p.addDQ()
+    p.ADUToElectrons()
+    p.addVAR(read_noise=True, poisson_noise=True)
+    p.darkCorrect()
+    p.flatCorrect()
+    p.detectSources()
+    p.measureIQ(display=True)
+    p.measureBG()
+    p.measureCCAndAstrometry()
+    p.subtractSkyBackground()
+    p.addToList(purpose=forSky)
+    p.getList(purpose=forSky)
+    p.makeSky()
+    p.skyCorrect()
+    p.detectSources()
+    p.measureIQ(display=True)
+    p.measureCCAndAstrometry()
+    return
