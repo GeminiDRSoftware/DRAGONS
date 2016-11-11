7531f0c905df3f4dde5e4cb66b94ae3c838d9228
kanderso <kanderso@gemini.edu>
2016-11-11 17:25:26 -0300

    calibration dl helper

--- gempy/utils/netutil.py
+++ gempy/utils/netutil.py
@@ -1,64 +1,73 @@
 #
 #                                                                  gemini_python
 #
-#                                                                astrodata.utils
 #                                                                     netutil.py
-# ------------------------------------------------------------------------------
-# $Id: netutil.py 5274 2015-06-11 14:39:37Z kanderson $
-# ------------------------------------------------------------------------------
-__version__      = '$Revision: 5274 $'[11:-2]
-__version_date__ = '$Date: 2015-06-11 11:39:37 -0300 (Thu, 11 Jun 2015) $'[7:-2]
 # ------------------------------------------------------------------------------
 import os
 import urllib2
 import urlparse
-import tempfile
 
-import logutils
-        
-def urlfetch(url, store = None, clobber = False):
+from tempfile import NamedTemporaryFile
+
+from . import logutils
+
+CHUNK = 65536
+def urlfetch(url, store=None, clobber=False):
+    """
+    Get and write data from a url.
+
+    Parameters
+    ----------
+    url : <str>
+        The url pointing to a downloadable file
+
+    store : <str>
+        A path to write and store the file.
+        E.g., 
+            store=/path/to/cache
+
+    clobber : <bool>
+        overwrite an existing file
+
+    """
     log = logutils.get_logger(__name__)
     purl = urlparse.urlparse(url)
     host = "fits" 
     npurl = urlparse.ParseResult(purl.scheme,
                                  purl.hostname,
-                                 purl.path,
+                                 purl.path, 
                                  purl.params,
                                  purl.query,
                                  purl.fragment)
-    
-    url = npurl.geturl()
-    
-    try:
-        res = urllib2.urlopen(url)
-    except urllib2.HTTPError, error:
-        print "ERROR"
-        print error.read()
-        raise 
 
-    CHUNK = 65536
     if store:
         outname = os.path.join(store, os.path.basename(url))
     else:
-        outf = tempfile.NamedTemporaryFile("w", prefix = "uftmp", 
-                                           suffix=".fits", dir =".")
+        outf = NamedTemporaryFile("w", prefix="uftmp", suffix=".fits", dir=".")
         outname = outf.name
         outf.close()
 
     if os.path.exists(outname):
         if not clobber:
-            raise "File Already Exists:" + outname
+            err = "File Already Exists: {}".format(outname)
+            raise IOError(err)
         else:
-            log.debug("File exists, clobber == True, will overwrite/update")
-    f = open(outname,"w")
+            log.debug("File exists but clobber requested; will overwrite/update")
+
+    url = npurl.geturl()
+    try:
+        res = urllib2.urlopen(url)
+    except urllib2.HTTPError as error:
+        log.error(str(error))
+        raise error
 
     log.debug("netutil: downloading +", url)
-    while True:
-        chunk = res.read(CHUNK)
-        if chunk == "":
-            break
-        f.write(chunk)
+    with open(outname, "w") as f:
+        while True:
+            chunk = res.read(CHUNK)
+            if not chunk:
+                break
+            f.write(chunk)
     
-    f.close()
     res.close()
     return outname
