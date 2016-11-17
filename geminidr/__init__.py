bb6c5dad0915f12599954ab92fe399bee1fa10e0
csimpson <csimpson@gemini.edu>
2016-11-16 17:04:12 -1000

    Added functionality to cope with streams and stacks

--- geminidr/__init__.py
+++ geminidr/__init__.py
@@ -19,11 +19,11 @@ class ParametersBASE(object):
     pass
 # ------------------------------------------------------------------------------
 
-
 from inspect import stack
+import os
+import pickle
 
 from gempy.utils import logutils
-
 # new system imports - 10-06-2016 kra
 # NOTE: imports of these and other tables will be moving around ...
 from gemini.lookups import calurl_dict
@@ -57,5 +57,35 @@ class PrimitivesBASE(object):
         self.keyword_comments = keyword_comments.keyword_comments
         self.sx_default_dict  = sextractor_default_dict.sextractor_default_dict
 
+        self.streams          = {}
+        self.cachedir         = './.reducecache/'
+        self.stacks           = self.load_cache('stkindex.pkl')
+
         # This lambda will return the name of the current caller.
         self.myself           = lambda: stack()[1][3]
+
+
+    def load_cache(self, filename):
+        """
+        Tries to load a cached, pickled object. Also creates the cache
+        directory if it doesn't exist
+
+        Parameters
+        ----------
+        filename: str
+            name of the file in the cache directory
+
+        Returns
+        -------
+        dict:
+            the unpickled contents of the file
+        """
+        if not os.path.exists(self.cachedir):
+            os.mkdir(self.cachedir)
+        cachefile = os.path.join(self.cachedir, filename)
+        if os.path.exists(cachefile):
+            try:
+                return pickle.load(open(cachefile, 'r'))
+            except:
+                pass
+        return {}
