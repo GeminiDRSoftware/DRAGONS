29c65918a94edcbd08a39f5eb70057cbd9256941
csimpson <csimpson@gemini.edu>
2016-11-21 14:23:57 -1000

    Applied similar inheritance as for Gemini primitives class

--- geminidr/gemini/parameters_gemini.py
+++ geminidr/gemini/parameters_gemini.py
@@ -1,9 +1,13 @@
 # This parameter file contains the parameters related to the primitives located
 # in the primitives_gemini.py file, in alphabetical order.
 
-from geminidr import ParametersBASE
+from geminidr.core.parameters_bookkeeping import ParametersBookkeeping
+from geminidr.core.parameters_preprocess import ParametersPreprocess
+from geminidr.core.parameters_standardize import ParametersStandardize
+from geminidr.core.parameters_visualize import ParametersVisualize
 
-class ParametersGemini(ParametersBASE):
+class ParametersGemini(ParametersBookkeeping, ParametersPreprocess,
+                       ParametersStandardize, ParametersVisualize):
     standardizeObservatoryHeaders = {
         "suffix"            : "_observatoryHeadersStandardized",
     }
