a38634498c5ef89a67dd853848bab6e899ffed59
csimpson <csimpson@gemini.edu>
2016-11-23 16:49:57 -1000

    Put array_gaps dict in geminidr.gmos.lookups

--- gemini_instruments/gmos/lookup.py
+++ gemini_instruments/gmos/lookup.py
@@ -1217,20 +1217,3 @@ gmosThresholds = {
     "BI12-34-4k-1, 4": 105683.0
 }
 
-gmosArrayGaps = {
-    # Unbinned
-    # EEV CCDs
-    ("SDSU II CCD", "unbinned") : 37,
-    # e2v CCDs
-    ("SDSU II e2v DD CCD42-90", "unbinned") : 37,
-    # GMOS-S Hamamatsu
-    ("S10892", "unbinned") : 61,
-
-    # Binned
-    # EEV CCDs
-    ("SDSU II CCD", "binned") : 36,
-    # e2v CCDs
-    ("SDSU II e2v DD CCD42-90", "binned") : 36,
-    # GMOS-S Hamamatsu
-    ("S10892", "binned") : 60
-}
