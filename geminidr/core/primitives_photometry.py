37460c5238fde84a689e0895df8bc02b7f5742d5
csimpson <csimpson@gemini.edu>
2016-12-06 15:26:23 -1000

    deleted max_sources parameter from detectSources(), since it's daofind-specific

--- geminidr/core/primitives_photometry.py
+++ geminidr/core/primitives_photometry.py
@@ -132,8 +132,6 @@ class Photometry(PrimitivesBASE):
             suffix to be added to output files
         mask: bool
             apply DQ plane as a mask before detection?
-        max_sources: int
-            maximum number of sources to analyse for seeing estimate
         set_saturation: bool
             set the saturation level of the data for SExtractor?
         """
@@ -141,7 +139,6 @@ class Photometry(PrimitivesBASE):
         log.debug(gt.log_message("primitive", self.myself(), "starting"))
         timestamp_key = self.timestamp_keys[self.myself()]
         pars = getattr(self.parameters, self.myself())
-        max_sources = pars["max_sources"]
         mask_bits = pars["replace_flags"]
         set_saturation = pars["set_saturation"]
 
