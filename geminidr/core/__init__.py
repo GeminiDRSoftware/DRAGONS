7aa920d514dec8d0bb3d0a1adc0b482ee140d6cc
csimpson <csimpson@gemini.edu>
2016-12-15 10:44:17 -1000

    changed to absolute imports

--- geminidr/core/__init__.py
+++ geminidr/core/__init__.py
@@ -1,15 +1,13 @@
-from __future__ import absolute_import
-
-from .primitives_bookkeeping import Bookkeeping
-from .primitives_calibdb import CalibDB
-from .primitives_ccd import CCD
-from .primitives_image import Image
-from .primitives_photometry import Photometry
-from .primitives_preprocess import Preprocess
-from .primitives_stack import Stack
-from .primitives_standardize import Standardize
-from .primitives_visualize import Visualize
+from geminidr.core.primitives_bookkeeping import Bookkeeping
+from geminidr.core.primitives_calibdb import CalibDB
+from geminidr.core.primitives_ccd import CCD
+from geminidr.core.primitives_image import Image
+from geminidr.core.primitives_photometry import Photometry
+from geminidr.core.primitives_preprocess import Preprocess
+from geminidr.core.primitives_stack import Stack
+from geminidr.core.primitives_standardize import Standardize
+from geminidr.core.primitives_visualize import Visualize
 
 # These are currently inherited by Image
-#from .primitives_register import Register
-#from .primitives_resample import Resample
+#from geminidr.core.primitives_register import Register
+#from geminidr.core.primitives_resample import Resample
