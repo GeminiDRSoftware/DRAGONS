16598cf43b2d9fe2f9759d8fa693f408727f6aad
csimpson <csimpson@gemini.edu>
2016-12-29 14:05:55 -1000

    updated parameter lists

--- geminidr/f2/parameters_f2_image.py
+++ geminidr/f2/parameters_f2_image.py
@@ -5,4 +5,16 @@ from ..core.parameters_image import ParametersImage
 from ..core.parameters_photometry import ParametersPhotometry
 
 class ParametersF2Image(ParametersF2, ParametersImage, ParametersPhotometry):
+    associateSky = {
+        "distance"              : 1.,
+        "time"                  : 600.,
+    }
     makeLampFlat = {}
+    stackSkyFrames = {
+        "suffix"            : "_skyStacked",
+        "mask"              : True,
+        "nhigh"             : 1,
+        "nlow"              : 0,
+        "operation"         : "median",
+        "reject_method"     : "minmax",
+    }
