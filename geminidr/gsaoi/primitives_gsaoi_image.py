db1feb45a502c4c00b3493b9ce9546b947b4f1ba
csimpson <csimpson@gemini.edu>
2016-12-30 14:19:57 -1000

    gsaoi and gsaoi_image refactored

---
+++ geminidr/gsaoi/primitives_gsaoi_image.py
@@ -0,0 +1,40 @@
+from gempy.gemini import gemini_tools as gt
+
+from .primitives_gsaoi import GSAOI
+from ..core import Image, Photometry
+from .parameters_gsaoi_image import ParametersGSAOIImage
+
+from recipe_system.utils.decorators import parameter_override
+# ------------------------------------------------------------------------------
+@parameter_override
+class GSAOIImage(GSAOI, Image, Photometry):
+    """
+    This is the class containing all of the preprocessing primitives
+    for the F2Image level of the type hierarchy tree. It inherits all
+    the primitives from the level above
+    """
+    tagset = set(["GEMINI", "GSAOI", "IMAGE"])
+
+    def __init__(self, adinputs, **kwargs):
+        super(GSAOIImage, self).__init__(adinputs, **kwargs)
+        self.parameters = ParametersGSAOIImage
+
+    def makeLampFlat(self, adinputs=None, **params):
+        """
+        This produces an appropriate stacked GSAOI imaging flat, based on
+        the inputs, since one of two procedures must be followed.
+
+        In the standard recipe, the inputs will have come from getList and
+        so will all have the same filter and will all need the same recipe.
+        """
+        log = self.log
+        log.debug(gt.log_message("primitive", self.myself(), "starting"))
+
+        if adinputs[0].wavelength_band() in ['Z', 'J']:
+            log.stdinfo('Using stackFrames to make flatfield')
+            adinputs = self.stackFrames(adinputs)
+        else:
+            log.stdinfo('Using lampOnLampOff to make flatfield')
+            adinputs = self.lampOnLampOff(adinputs)
+
+        return adinputs
