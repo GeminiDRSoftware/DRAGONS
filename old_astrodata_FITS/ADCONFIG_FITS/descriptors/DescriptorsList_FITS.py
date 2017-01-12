dfb7e578e72153780d9b2c45b6b69310d0df9bdc
Kathleen Labrie <klabrie@gemini.edu>
2017-01-11 16:10:20 -1000

    Removing old code no longer needed for the refactoring work.

--- old_astrodata_FITS/ADCONFIG_FITS/descriptors/DescriptorsList_FITS.py
+++
@@ -1,6 +0,0 @@
-[
-    DD("instrument", pytype=str),
-    DD("object", pytype=str),
-    DD("telescope", pytype=str),
-    DD("ut_date", pytype=datetime),
-]
--- old_astrodata_FITS/ADCONFIG_FITS/descriptors/FITS_Keywords.py
+++
@@ -1,9 +0,0 @@
-# This dictionary contains '"key_<name>":"KEYWORD"' pairs that associate a
-# variable to a FITS standard keyword
-
-FITS_KeyDict = {
-    "key_instrument":"INSTRUME",
-    "key_object":"OBJECT",
-    "key_telescope":"TELESCOP",
-    "key_ut_date":"DATE-OBS",
-    }
