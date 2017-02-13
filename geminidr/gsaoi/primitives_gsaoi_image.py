96d7c926fa07c5e8375805056ab2ee99267e9fe6
kanderso <kanderso@gemini.edu>
2017-02-13 13:55:10 -0300

    fixes to context, rmapper

--- geminidr/gsaoi/primitives_gsaoi_image.py
+++ geminidr/gsaoi/primitives_gsaoi_image.py
@@ -35,6 +35,10 @@ class GSAOIImage(GSAOI, Image, Photometry):
         log = self.log
         log.debug(gt.log_message("primitive", self.myself(), "starting"))
 
+        # Since this primitive needs a reference, it must no-op without any
+        if not adinputs:
+            return adinputs
+
         if adinputs[0].wavelength_band() in ['Z', 'J']:
             log.stdinfo('Using stackFrames to make flatfield')
             adinputs = self.stackFrames(adinputs)
