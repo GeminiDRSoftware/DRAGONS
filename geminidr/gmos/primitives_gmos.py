c7019bbe93ef95bbe1d35756070d18a26da62311
csimpson <csimpson@gemini.edu>
2017-07-06 11:50:17 -1000

    rejigged overscanCorrect() and parameters to also allow a functionless (row-by-row) subtraction

--- geminidr/gmos/primitives_gmos.py
+++ geminidr/gmos/primitives_gmos.py
@@ -254,8 +254,8 @@ class GMOS(Gemini, CCD):
             comma-separated list of IRAF-style overscan sections
         nbiascontam: int/None
             number of columns adjacent to the illuminated region to reject
-        fit_spline: bool
-            fit a piecewise cubic spline rather than polynomial?
+        function: str
+            function to fit ("polynomial" | "spline" | "none")
         order: int
             order of Chebyshev fit or spline/None
         """
@@ -268,17 +268,21 @@ class GMOS(Gemini, CCD):
         lo_rej = params["low_reject"]
         hi_rej = params["high_reject"]
         order = params["order"]
-        fit_spline = params["fit_spline"]
+        func = (params["function"] or 'none').lower()
         nbiascontam = params["nbiascontam"]
 
         if lo_rej < 0:
-            log.warning("Low rejection threshold set to invalid value {}. "
+            log.warning("Low rejection threshold set to invalid value. "
                         "Ignorning.")
             lo_rej = None
         if hi_rej < 0:
-            log.warning("High rejection threshold set to invalid value {}. "
+            log.warning("High rejection threshold set to invalid value. "
                         "Ignorning.")
             hi_rej = None
+
+        if not(func.startswith('poly') or func == 'spline' or func=='none'):
+            log.warning("Unrecognized function {}.".format(func))
+            func = 'none'
 
         for ad in adinputs:
             if ad.phu.get(timestamp_key):
@@ -289,7 +293,7 @@ class GMOS(Gemini, CCD):
 
             # Use gireduce defaults if values aren't specified
             detname = ad.detector_name(pretty=True)
-            if order is None and not fit_spline:
+            if order is None and func.startswith('poly'):
                 order = 6 if detname.startswith('Hamamatsu') else 0
             if nbiascontam is None:
                 nbiascontam = 5 if detname == 'e2vDD' else 4
@@ -297,7 +301,7 @@ class GMOS(Gemini, CCD):
             osec_list = ad.overscan_section()
             dsec_list = ad.data_section()
             ybinning = ad.detector_y_bin()
-            for ext, osec, dsec in zip(ad, osec_list, dsec_list):
+            for i, (ext, osec, dsec) in enumerate(zip(ad, osec_list, dsec_list)):
                 x1, x2, y1, y2 = osec.x1, osec.x2, osec.y1, osec.y2
                 if x1 > dsec.x1:  # Bias on right
                     x1 += nbiascontam
@@ -306,10 +310,11 @@ class GMOS(Gemini, CCD):
                     x1 += 1
                     x2 -= nbiascontam
 
-                if detname.startswith('Hamamatsu'):
-                    log.fullinfo('Ignoring bottom 48 rows of {}'.
-                                 format(ad.filename))
+                if detname.startswith('Hamamatsu') and func.startswith('poly'):
                     y1 = max(y1, 48 // ybinning)
+                    if i == 0:  # Don't log for every extension
+                        log.fullinfo('Ignoring bottom 48 rows of {}'.
+                                    format(ad.filename))
 
                 row = np.arange(y1, y2)
                 data = np.mean(ext.data[y1:y2, x1:x2], axis=1)
@@ -319,51 +324,58 @@ class GMOS(Gemini, CCD):
                 if ext.hdr.get('BUNIT', 'adu').lower() == 'adu':
                     wt *= ext.gain()
 
-                # The UnivariateSpline will make reduced-chi^2=1 so it will
-                # fit bad rows. Need to mask these before starting, so use a
-                # running median. Probably a good starting point for all fits.
                 medboxsize = 2  # really 2n+1 = 5
-                medarray = np.full((medboxsize*2+1, y2-y1), np.nan)
-                for i in range(-medboxsize, medboxsize+1):
-                    mx1 = max(i, 0)
-                    mx2 = min(y2-y1, y2-y1+i)
-                    medarray[medboxsize+i, mx1:mx2] = data[:mx2-mx1]
-                residuals = data - np.ma.median(np.ma.masked_where(np.isnan(medarray),
-                                                medarray), axis=0)
-                sigma = np.sqrt(x2-x1+1) / wt  # read noise
-                mask = np.where(np.logical_or(residuals > hi_rej * sigma
-                                if hi_rej is not None else False,
-                                residuals < -lo_rej * sigma
-                                if lo_rej is not None else False), True, False)
-
                 for iter in range(niterate+1):
-                    if fit_spline:
-                        if order:
-                            # Equally-spaced knots (like IRAF)
-                            knots = np.linspace(row[0], row[-1], order+1)[1:-1]
-                            bias = LSQUnivariateSpline(row[~mask], data[~mask], knots)
+                    # The UnivariateSpline will make reduced-chi^2=1 so it will
+                    # fit bad rows. Need to mask these before starting, so use a
+                    # running median. Probably a good starting point for all fits.
+                    if iter == 0 or func == 'none':
+                        medarray = np.full((medboxsize * 2 + 1, y2 - y1), np.nan)
+                        for i in range(-medboxsize, medboxsize + 1):
+                            mx1 = max(i, 0)
+                            mx2 = min(y2 - y1, y2 - y1 + i)
+                            medarray[medboxsize + i, mx1:mx2] = data[:mx2 - mx1]
+                        runmed = np.ma.median(np.ma.masked_where(np.isnan(medarray),
+                                                                 medarray), axis=0)
+                        residuals = data - runmed
+                        sigma = np.sqrt(x2 - x1 + 1) / wt  # read noise
+
+                    mask = np.where(np.logical_or(residuals > hi_rej * sigma
+                                        if hi_rej is not None else False,
+                                        residuals < -lo_rej * sigma
+                                        if lo_rej is not None else False), True, False)
+
+                    # Don't clip any pixels if iter==0
+                    if func == 'none' and iter < niterate:
+                        # Replace bad data with running median
+                        data = np.where(mask, runmed, data)
+                    elif func != 'none':
+                        if func == 'spline':
+                            if order:
+                                # Equally-spaced knots (like IRAF)
+                                knots = np.linspace(row[0], row[-1], order+1)[1:-1]
+                                bias = LSQUnivariateSpline(row[~mask], data[~mask], knots)
+                            else:
+                                bias = UnivariateSpline(row[~mask], data[~mask],
+                                                        w=[wt]*np.sum(~mask))
                         else:
-                            bias = UnivariateSpline(row[~mask], data[~mask],
-                                                    w=[wt]*np.sum(~mask))
-                    else:
-                        bias_init = models.Chebyshev1D(degree=order,
-                                                       c0=np.median(data[~mask]))
-                        fit_f = fitting.LinearLSQFitter()
-                        bias = fit_f(bias_init, row[~mask], data[~mask])
-                    residuals = data - bias(row)
-                    sigma = np.std(residuals[~mask])
-                    mask = np.where(np.logical_or(residuals > hi_rej*sigma
-                                    if hi_rej is not None else False,
-                                    residuals < -lo_rej*sigma
-                                    if lo_rej is not None else False), True, False)
+                            bias_init = models.Chebyshev1D(degree=order,
+                                                           c0=np.median(data[~mask]))
+                            fit_f = fitting.LinearLSQFitter()
+                            bias = fit_f(bias_init, row[~mask], data[~mask])
+
+                        residuals = data - bias(row)
+                        sigma = np.std(residuals[~mask])
 
                 # using "-=" won't change from int to float
-                ext.data = ext.data - np.tile(bias(np.arange(0, ext.data.shape[0])),
+                if func != 'none':
+                    data = bias(np.arange(0, ext.data.shape[0]))
+                ext.data = ext.data - np.tile(data,
                                         (ext.data.shape[1],1)).T.astype(np.float32)
 
                 ext.hdr.set('OVERSEC', '[{}:{},{}:{}]'.format(x1+1,x2,y1+1,y2),
                             self.keyword_comments['OVERSEC'])
-                ext.hdr.set('OVERSCAN', np.mean(bias(row)),
+                ext.hdr.set('OVERSCAN', np.mean(data),
                             self.keyword_comments['OVERSCAN'])
                 ext.hdr.set('OVERRMS', sigma, self.keyword_comments['OVERRMS'])
 
