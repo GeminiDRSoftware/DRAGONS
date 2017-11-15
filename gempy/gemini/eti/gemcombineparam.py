0a20c419a664c60de6e43c1c0e6faa7790239c81
= <marc.white@anu.edu.au>
2017-11-15 15:50:02 +1100

    Rectified bug that would cause crash if stackFrames called without apply_dq argument
    The provided default parameters for GHOST slit frame stacking failed
    to provide a default value for the apply_dq option to stackFrames (this
    has now been fixed). This resulted in the RecipeSystem crashing at the
    line modified in this commit, as the key 'apply_dq' did not exist in
    the params dictionary. The new get call will return None if the key is
    not defined (although as good practice, it probably should be anyway).

--- gempy/gemini/eti/gemcombineparam.py
+++ gempy/gemini/eti/gemcombineparam.py
@@ -86,7 +86,7 @@ class Masktype(GemcombineParam):
     def __init__(self, inputs=None, params=None):
         log.debug("Masktype __init__")
         GemcombineParam.__init__(self, inputs, params)
-        if params["apply_dq"]:
+        if params.get("apply_dq"):
             self.masktype = "goodvalue"
         else:
             self.masktype = "none"
