2668293c464432b99cde12d0a9a0b1abc76339e7
csimpson <csimpson@gemini.edu>
2018-04-03 13:24:53 -1000

    delete stuff from testing

--- geminidr/gmos/parameters_gmos.py
+++ geminidr/gmos/parameters_gmos.py
@@ -17,17 +17,5 @@ class subtractOverscanConfig(parameters_ccd.subtractOverscanConfig):
         self.function = None
 
 class validateDataConfig(parameters_standardize.validateDataConfig):
-#class validateDataConfig(config.Config):
-    #num_exts = config.ListField("Allowed number of extensions", int, [1, 2, 3, 4, 6, 12], optional=True, single=True)
-    #suffix = config.Field("Filename suffix", str, "_dataValidated")
-    #num_exts = config.ListField("Allowed number of extensions", int, 1, optional=True, single=True)
-    #repair = config.Field("Repair data?", bool, False)
     def setDefaults(self):
-        print "setDefaults"
-        print self._fields
         self.num_exts = [1,2,3,4,6,12]
-        del self.fake_param
-        #print self.items()
-        #print self._fields
-        #self.__delattr__('fake_param')
-
