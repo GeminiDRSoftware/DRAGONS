a72415e4abc2bff3b5e7d06b62ba722b72d10ac9
Kathleen Labrie <kathleen.labrie.phd@gmail.com>
2018-04-02 16:59:04 -1000

    Fixes the fix as it will make other things barf.  The imports are out again and the filename handling is back to its original implementation.

--- recipe_system/config.py
+++ recipe_system/config.py
@@ -4,11 +4,11 @@
 #                                                                  recipe_system
 #                                                                      config.py
 # ------------------------------------------------------------------------------
-from future import standard_library
-standard_library.install_aliases()
-from builtins import map
-from builtins import str
-from builtins import object
+#from future import standard_library
+#standard_library.install_aliases()
+#from builtins import map
+#from builtins import str
+#gitfrom builtins import object
 
 # ------------------------------------------------------------------------------
 __version__      = '2.0 (beta)'
@@ -206,7 +206,7 @@ class ConfigObject(object):
 
         cp = SafeConfigParser()
 
-        cp.read(list(map(os.path.expanduser, (filenames,))))
+        cp.read(list(map(os.path.expanduser, filenames)))
 
         translate = Converter(self._conv.copy(), cp)
 
