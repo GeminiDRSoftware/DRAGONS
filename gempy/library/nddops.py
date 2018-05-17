0b18daea42eb847d4a5f8d655f9b7ace3822e20d
csimpson <csimpson@gemini.edu>
2018-05-17 10:03:04 -1000

    fix mask=None issues

--- gempy/library/nddops.py
+++ gempy/library/nddops.py
@@ -339,10 +339,11 @@ class NDStacker(object):
     def sigclip(data, mask=None, variance=None, mclip=True, lsigma=3.0, hsigma=3.0):
         # Iterative sigma-clipping based on input pixel scatter
         cenfunc = np.ma.median if mclip else np.ma.mean
-        data = np.ma.masked_array(data, mask=mask & BAD)
+        if mask is not None:
+            data = np.ma.masked_array(data, mask=mask & BAD)
         clipped_data = sigma_clip(data, sigma_lower=lsigma, sigma_upper=hsigma,
                                   cenfunc=cenfunc, iters=None, axis=0, copy=False)
-        return clipped_data.data, clipped_data.mask | mask, variance
+        return clipped_data.data, clipped_data.mask | (mask or 0), variance
 
     @staticmethod
     @rejector
@@ -354,7 +355,7 @@ class NDStacker(object):
                                      lsigma=lsigma, hsigma=hsigma)
 
         cenfunc = np.ma.median  # Always median for first pass
-        clipped_data = np.ma.masked_array(data, mask=mask & BAD)
+        clipped_data = np.ma.masked_array(data, mask=(mask or 0) & BAD)
         nmasked = np.sum(clipped_data.mask)
         while True:  # Write it this way in case we decide to have maxiters
             avg = cenfunc(clipped_data, axis=0)
