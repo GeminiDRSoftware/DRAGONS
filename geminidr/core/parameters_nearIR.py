3e426f61e1a0449945beb3f466b524007ebe6654
James E.H. Turner <jturner@gemini.edu>
2018-07-20 11:42:28 -0400

    Add some threshold parameters like in niflat for makeBPM, before converting it to use the IRAF algorithm for now (current one problematic).

--- geminidr/core/parameters_nearIR.py
+++ geminidr/core/parameters_nearIR.py
@@ -12,7 +12,15 @@ class addDQConfig(parameters_standardize.addDQConfig, addLatencyToDQConfig):
     latency = config.Field("Apply latency for saturated pixels?", bool, False)
 
 class makeBPMConfig(config.Config):
-    pass
+    dark_lo_thresh = config.Field("Low rejection threshold for dark (ADU)", float, -20.)
+    dark_hi_thresh = config.Field("High rejection threshold for dark (ADU)", float, 100.)
+    flat_lo_thresh = config.RangeField("Low rejection threshold for normalized flat", float, 0.8, max=1.0)
+    flat_hi_thresh = config.RangeField("High rejection threshold for normalized flat", float, 1.25, min=1.0)
+
+    def validate(self):
+        config.Config.validate(self)
+        if self.dark_lo_thresh >= self.dark_hi_thresh:
+            raise ValueError("dark_hi_thresh must be greater than dark_lo_thresh")
 
 class makeLampFlatConfig(parameters_stack.core_stacking_config):
     pass
