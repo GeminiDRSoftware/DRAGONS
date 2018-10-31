2bef469b57be1060ee3d595276fc1ee29300e347
James E.H. Turner <jturner@gemini.edu>
2018-10-31 17:58:45 -0300

    Change parameter default from NaN to None and delete unused parameters in GSAOI's makeBPM (having figured out how to do these things). Add some sky subtraction defaults for GSAOI.

--- geminidr/gsaoi/parameters_gsaoi.py
+++ geminidr/gsaoi/parameters_gsaoi.py
@@ -1,7 +1,7 @@
 # This parameter file contains the parameters related to the primitives located
 # in the primitives_gsaoi.py file, in alphabetical order.
 from gempy.library import config
-from geminidr.core import parameters_photometry, parameters_preprocess, parameters_visualize, parameters_nearIR
+from geminidr.core import parameters_photometry, parameters_preprocess, parameters_visualize, parameters_nearIR, parameters_stack
 
 class addReferenceCatalogConfig(parameters_photometry.addReferenceCatalogConfig):
     def setDefaults(self):
@@ -19,11 +19,25 @@ class associateSkyConfig(parameters_preprocess.associateSkyConfig):
 
 class makeBPMConfig(parameters_nearIR.makeBPMConfig):
     def setDefaults(self):
-        self.dark_lo_thresh = float('NaN')  # not used
-        self.dark_hi_thresh = float('NaN')  # means use default
+        del self.dark_lo_thresh, self.flat_hi_thresh
+        self.dark_hi_thresh = None  # (use default)
         self.flat_lo_thresh = 0.5
-        self.flat_hi_thresh = float('Inf')  # not used, limits disallow NaN
 
     def validate(self):
         config.Config.validate(self)
+
+class stackSkyFramesConfig(parameters_stack.stackSkyFramesConfig):
+    def setDefaults(self):
+        self.suffix = "_skyStacked"
+        self.operation = "median"
+        self.reject_method = "minmax"
+        self.nhigh = 1
+        self.scale = False  # darks not taken routinely for GSAOI
+        self.zero = True
+
+class subtractSkyConfig(parameters_preprocess.subtractSkyConfig):
+    def setDefaults(self):
+        self.reset_sky = False
+        self.scale_sky = False  # darks not taken routinely for GSAOI
+        self.offset_sky = False
 
