bbf47141b65e49549e18038e516ac1f5174d53fe
csimpson <csimpson@gemini.edu>
2018-11-15 16:53:33 -1000

    real bug fix

--- gemini_instruments/gemini/adclass.py
+++ gemini_instruments/gemini/adclass.py
@@ -738,8 +738,10 @@ class AstroDataGemini(AstroDataFits):
             dispersion = self.hdr[self._keyword_for('dispersion')]
         except KeyError:
             try:
-                dispersion = [self.phu[self._keyword_for(
-                    'dispersion')]] * len(self)
+                dispersion = self.phu[self._keyword_for(
+                    'dispersion')]
+                if not self.is_single:
+                    dispersion = [dispersion] * len(self)
             except KeyError:
                 return None
 
