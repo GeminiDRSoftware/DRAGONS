9a0392711edf952d017aedcf343a3269aaf30791
csimpson <csimpson@gemini.edu>
2019-01-11 16:13:12 -1000

    wind resampleToCommonFrame() back

--- geminidr/core/parameters_resample.py
+++ geminidr/core/parameters_resample.py
@@ -3,12 +3,6 @@
 from gempy.library import config
 
 class resampleToCommonFrameConfig(config.Config):
-    suffix = config.Field("Filename suffix", str, "_align", optional=True)
-    order = config.RangeField("Order of interpolation", int, 3, min=0, max=5)
-    trim_data = config.Field("Trim to field of view of reference image?", bool, False)
-    clean_data = config.Field("Clean bad pixels before interpolation?", bool, False)
-
-class oldResampleToCommonFrameConfig(config.Config):
     suffix = config.Field("Filename suffix", str, "_oldalign", optional=True)
     interpolator = config.ChoiceField("Type of pixel interpolation", str,
                                       allowed={"nearest": "nearest pixel",
@@ -19,5 +13,3 @@ class oldResampleToCommonFrameConfig(config.Config):
                                                "spline5": "qunitic spline"},
                                       default="linear")
     trim_data = config.Field("Trim to field of view of reference image?", bool, False)
-    clean_data = config.Field("Clean bad pixels before interpolation?", bool, False)
-    order = config.RangeField("Order of interpolation", int, 3, min=0, max=5)
