d5ceb8a7b5bcc13b0ad61e1f8e01488b20d1c5cf
Bruno Quint <bquint@gemini.edu>
2019-01-25 16:35:21 -0300

    Replaced tests to match requirements

--- astrodata/test/test_nddata.py
+++ astrodata/test/test_nddata.py
@@ -13,22 +13,38 @@ def test_new_variance_uncertainty_instance_returns_none():
     assert result is None
 
 
-def test_new_variance_uncertainty_instance_warns_negative_values():
+def test_new_variance_uncertainty_instance_no_warning_if_the_array_is_zeros():
 
     array = np.zeros((500, 500), dtype=np.float32)
-    array[200, 300] = -0.001
 
     with warnings.catch_warnings(record=True) as w:
 
         result = nddata.new_variance_uncertainty_instance(array)
         warnings.simplefilter("always")
 
+        for _w in w:
+            print(str(_w.message))
+
+        assert (array >= 0).all()
+        assert len(w) == 0
+
+
+def test_new_variance_uncertainty_instance_warn_if_there_are_any_negative_numbers():
+
+    array = np.zeros((500, 500), dtype=np.float32)
+    array[250, 250] = -0.001
+
+    with warnings.catch_warnings(record=True) as w:
+        result = nddata.new_variance_uncertainty_instance(array)
+        warnings.simplefilter("always")
 
         for _w in w:
             print(str(_w.message))
 
+        assert not (array >= 0).all()
         assert len(w) == 1
-        assert issubclass(w[-1].category, RuntimeWarning)
+        assert isinstance(result, nddata.StdDevUncertainty)
+        assert result.array[250, 250] == 0
 
 
 def test_new_variance_uncertainty_instance():
@@ -41,6 +57,7 @@ def test_new_variance_uncertainty_instance():
         warnings.simplefilter("always")
 
         assert len(w) == 0
+        assert isinstance(result, nddata.StdDevUncertainty)
 
 
 if __name__ == '__main__':
