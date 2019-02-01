214043bba053587436c0bc0b1b3e1a14cddf9ea1
Bruno Quint <bquint@gemini.edu>
2019-02-01 17:18:21 -0300

    Skipping a test that was supposed to fail

--- recipe_system/cal_service/tests/test_caldb_command_line.py
+++ recipe_system/cal_service/tests/test_caldb_command_line.py
@@ -1,18 +1,35 @@
-
+#!python
 import pytest
+import os
+import subprocess
 
 from recipe_system.cal_service import CalibrationService
 
+@pytest.fixture
+def caldb():
+    return CalibrationService()
 
-def test_can_call_caldb_config():
 
-    caldb = CalibrationService()
+def test_can_call_caldb_config(caldb):
+
     caldb.config()
 
 
-def test_can_call_caldb_init():
+def test_can_call_caldb_init(caldb):
 
-    caldb = CalibrationService()
+    caldb.config()
+    caldb.init()
+
+
+@pytest.mark.skip('This method should fail when calling init twice but it is '
+                  'not. But it fails if you call "caldb init" from the command '
+                  "line. I don't know how to solve this!")
+def test_fail_init_if_called_twice(caldb):
+
+    os.remove(os.path.expanduser("~/.geminidr/cal_manager.db"))
+
+    caldb.config()
+    caldb.init()
     caldb.init()
 
 
