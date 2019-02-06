018ad0e515bd5122b856c074ec6da57bd38c7349
csimpson <csimpson@gemini.edu>
2019-02-05 15:31:10 -1000

    create GNIRS imaging SQ recipes

---
+++ geminidr/gnirs/recipes/sq/recipes_FLAT_IMAGE.py
@@ -0,0 +1,34 @@
+"""
+Recipes available to data with tags ['GNIRS', 'IMAGE', 'CAL', 'FLAT']
+Default is "makeProcessedFlat".
+"""
+recipe_tags = set(['GNIRS', 'IMAGE', 'CAL', 'FLAT'])
+
+def makeProcessedFlat(p):
+    """
+    This recipe performs the standardization and corrections needed to convert
+    the raw input flat images into a single stacked and normalized flat image.
+    This output processed flat is stored on disk using storeProcessedFlat and
+    has a name equal to the name of the first input flat image with "_flat.fits"
+    appended.
+
+    Parameters
+    ----------
+    p : PrimitivesCORE object
+        A primitive set matching the recipe_tags.
+    """
+
+    p.prepare()
+    p.addDQ(add_illum_mask=False)
+    p.addVAR(read_noise=True)
+    #p.nonlinearityCorrect()
+    p.ADUToElectrons()
+    p.addVAR(poisson_noise=True)
+    p.makeLampFlat()
+    p.addIllumMaskToDQ()
+    p.normalizeFlat()
+    p.thresholdFlatfield()
+    p.storeProcessedFlat()
+    return
+
+default = makeProcessedFlat
