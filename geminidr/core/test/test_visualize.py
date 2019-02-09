28e8d83c702cb2760c49e38dd6dcbd81e8043059
csimpson <csimpson@gemini.edu>
2019-02-08 14:02:58 -1000

    test for mosaicDetectors() with different GMOS binnings (uses AstroFaker)

---
+++ geminidr/core/test/test_visualize.py
@@ -0,0 +1,32 @@
+import numpy as np
+from geminidr.gmos.primitives_gmos_image import GMOSImage
+import AstroFaker
+
+def test_mosaicDetectors_gmos_binning():
+    """
+    Tests that the spacing between amplifier centres for NxN binned data
+    is precisely N times smaller than for unbinned data when run through
+    mosaicDetectors()
+    """
+    for hemi in 'NS':
+        for ccd in ('EEV', 'e2v', 'Ham'):
+            for binning in (1, 2, 4):
+                try:
+                    ad = AstroFaker.create('GMOS-{}'.format(hemi), ['IMAGE', ccd])
+                except ValueError:  # No e2v for GMOS-S
+                    continue
+                ad.init_default_extensions(binning=binning, overscan=False)
+                for ext in ad:
+                    shape = ext.data.shape
+                    ext.add_star(amplitude=10000, x=0.5*(shape[1]-1),
+                                 y=0.5*(shape[0]-1), fwhm=0.5*binning)
+                p = GMOSImage([ad])
+                ad = p.mosaicDetectors([ad])
+                ad = p.detectSources([ad])
+                x = np.array(sorted(ad[0].OBJCAT['X_IMAGE']))
+                if binning == 1:
+                    unbinned_positions = x
+                else:
+                    diffs = np.diff(unbinned_positions) -  binning * np.diff(x)
+                    assert np.max(abs(diffs)) < 0.01
+
