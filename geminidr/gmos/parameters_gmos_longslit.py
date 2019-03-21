497e955d9053dc0c9f5bd587c74fc633312756d4
csimpson <csimpson@gemini.edu>
2019-03-21 13:23:15 -1000

    addIllumMask() for GMOS LS

--- geminidr/gmos/parameters_gmos_longslit.py
+++ geminidr/gmos/parameters_gmos_longslit.py
@@ -1,2 +1,7 @@
 # This parameter file contains the parameters related to the primitives located
 # in the primitives_gmos_longslit.py file, in alphabetical order.
+from geminidr.core import parameters_standardize
+
+class addDQConfig(parameters_standardize.addDQConfig):
+    def setDefaults(self):
+        self.add_illum_mask = True
--- geminidr/gmos/primitives_gmos_longslit.py
+++ geminidr/gmos/primitives_gmos_longslit.py
@@ -3,9 +3,17 @@
 #
 #                                                     primtives_gmos_longslit.py
 # ------------------------------------------------------------------------------
+import numpy as np
+from gempy.gemini import gemini_tools as gt
+
+from geminidr.gemini.lookups import DQ_definitions as DQ
+
 from .primitives_gmos_spect import GMOSSpect
 from .primitives_gmos_nodandshuffle import GMOSNodAndShuffle
 from . import parameters_gmos_longslit
+
+from astropy.modeling import models, fitting
+from astropy.stats import sigma_clip
 
 from recipe_system.utils.decorators import parameter_override
 # ------------------------------------------------------------------------------
@@ -21,3 +29,56 @@ class GMOSLongslit(GMOSSpect, GMOSNodAndShuffle):
     def __init__(self, adinputs, **kwargs):
         super(GMOSLongslit, self).__init__(adinputs, **kwargs)
         self._param_update(parameters_gmos_longslit)
+
+    def addIllumMaskToDQ(self, adinputs=None, suffix=None, illum_mask=None):
+        """
+        Adds an illumination mask to each AD object
+
+        Parameters
+        ----------
+        suffix: str
+            suffix to be added to output files
+        illum_mask: str/None
+            name of illumination mask mask (None -> use default)
+        """
+        log = self.log
+        log.debug(gt.log_message("primitive", self.myself(), "starting"))
+        timestamp_key = self.timestamp_keys[self.myself()]
+
+        for ad, illum in zip(*gt.make_lists(adinputs, illum_mask, force_ad=True)):
+            if ad.phu.get(timestamp_key):
+                log.warning('No changes will be made to {}, since it has '
+                    'already been processed by addIllumMaskToDQ'.
+                            format(ad.filename))
+                continue
+
+            if illum is None:
+                # Default operation for GMOS LS
+                row_medians = np.sum(np.array([np.median(ext.data, axis=1) for ext in ad]), axis=0)
+                rows = np.arange(len(row_medians))
+                m_init = models.Polynomial1D(degree=1)
+                fit_it = fitting.FittingWithOutlierRemoval(fitting.LinearLSQFitter(),
+                                                           outlier_func=sigma_clip)
+                m_final, mask = fit_it(m_init, rows, row_medians)
+                mask &= (row_medians < m_final(rows))
+                for ext in ad:
+                    ext.mask |= (mask * DQ.unilluminated).astype(DQ.datatype)[:, np.newaxis]
+
+            else:
+                log.fullinfo("Using {} as illumination mask".format(illum.filename))
+                final_illum = gt.clip_auxiliary_data(ad, aux=illum, aux_type='bpm',
+                                          return_dtype=DQ.datatype)
+
+                for ext, illum_ext in zip(ad, final_illum):
+                    if illum_ext is not None:
+                        # Ensure we're only adding the unilluminated bit
+                        iext = np.where(illum_ext.data > 0, DQ.unilluminated,
+                                        0).astype(DQ.datatype)
+                        ext.mask = iext if ext.mask is None else ext.mask | iext
+
+            # Timestamp and update filename
+            gt.mark_history(ad, primname=self.myself(), keyword=timestamp_key)
+            ad.update_filename(suffix=suffix, strip=True)
+
+        return adinputs
+
