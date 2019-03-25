a5d5a97b8d261890191d733bcda80dd2aab2d02a
Bruno Quint <bquint@gemini.edu>
2019-03-25 15:47:45 -0300

    Removed reference to the rsys.cfg file in first stage

--- .jenkins/test_env_and_install_missing_libs.sh
+++ .jenkins/test_env_and_install_missing_libs.sh
@@ -16,8 +16,3 @@ cd -
 cd .jenkins
 pip install --quiet GeminiCalMgr-0.9.11-py3-none-any.whl
 cd -
-
-mkdir -p ${HOME}/.geminidr
-cp recipe_system/cal_service/tests/rsys.cfg ${HOME}/.geminidr
-cd -
-
