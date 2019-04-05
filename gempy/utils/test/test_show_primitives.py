a43a1085e3994177d3c7c7b15600e17a70b741dd
Ivan <isharankov@gemini.edu>
2019-04-05 17:49:06 -0300

    Completed tests for show_primitives

--- gempy/utils/test/test_show_primitives.py
+++ gempy/utils/test/test_show_primitives.py
@@ -6,7 +6,10 @@ import warnings
 import geminidr
 import astrodata
 import gemini_instruments
+from recipe_system.utils.errors import ModeError
+from recipe_system.utils.errors import RecipeNotFound
 from gempy.utils.show_primitives import show_primitives
+
 
 
 try:
@@ -37,6 +40,9 @@ GSAOI_IMAGE = 'S20170505S0095.fits'
 GSAOI_FLAT = 'S20170505S0031.fits'
 
 
+# # # # # #  GNIRS  # # # # # #
+
+
 gnirs_answer = \
     "Recipe not provided, default recipe (makeProcessedFlat) will be used.\n" \
     "Input file: {}\n".format(os.path.normpath(os.path.join(path, GNIRS))) + \
@@ -63,118 +69,358 @@ gnirs_answer = \
 
 
 
+def test_show_primitives_on_gnirs(test_path):
+    file_location = test_path + 'geminidr/' + GNIRS
+    answer = show_primitives(file_location)
+    assert gnirs_answer == answer
 
-gnirs_spect_answer = \
-    "Input file: {}\n".format(os.path.normpath(
-        os.path.join(path, GNIRS_SPECT))) + \
-    "Input tags: set(['RAW', 'GEMINI', 'NORTH', 'SIDEREAL', 'GNIRS', " \
-    "'UNPREPARED', 'SPECT', 'XD'])\n" \
-    "!!! No recipes were found for this file !!!"
+
+def test_show_primitives_on_gnirs_spect(test_path):
+    try:
+        file_location = test_path + 'geminidr/' + GNIRS_SPECT
+        answer = show_primitives(file_location, 'qa')
+        assert "RecipeNotFound Error" == answer
+    except RecipeNotFound:
+        pass
+
+
+# # # # # #  GMOS  # # # # # #
 
 
 gmos_answer = \
+    "Recipe not provided, default recipe (reduce) will be used.\n" \
     "Input file: {}\n".format(os.path.normpath(os.path.join(path, GMOS))) + \
     "Input tags: set(['SOUTH', 'RAW', 'GMOS', 'GEMINI', 'SIDEREAL', " \
     "'UNPREPARED', 'IMAGE', 'MASK', 'ACQUISITION'])\n" \
-    "Recipes available for the input file: \n" \
-    "   geminidr.gmos.recipes.sq.recipes_IMAGE::makeProcessedFringe\n" \
-    "   geminidr.gmos.recipes.sq.recipes_IMAGE::reduce\n" \
-    "   geminidr.gmos.recipes.qa.recipes_IMAGE::makeProcessedFringe\n" \
-    "   geminidr.gmos.recipes.qa.recipes_IMAGE::reduce\n" \
-    "   geminidr.gmos.recipes.qa.recipes_IMAGE::reduce_nostack\n" \
-    "   geminidr.gmos.recipes.qa.recipes_IMAGE::stack"
+    "Input mode: sq\n" \
+    "Input recipe: reduce\n" \
+    "Matched recipe: geminidr.gmos.recipes.sq.recipes_IMAGE::reduce\n" \
+    "Recipe location: {}\n".format(os.path.normpath(os.path.join(
+        dragons_location + "gmos/recipes/sq/recipes_IMAGE.py"))) + \
+    "Recipe tags: set(['GMOS', 'IMAGE'])\n" \
+    "Primitives used: \n" \
+    "   p.prepare()\n" \
+    "   p.addDQ()\n" \
+    "   p.addVAR(read_noise=True)\n" \
+    "   p.overscanCorrect()\n" \
+    "   p.biasCorrect()\n" \
+    "   p.ADUToElectrons()\n" \
+    "   p.addVAR(poisson_noise=True)\n" \
+    "   p.flatCorrect()\n" \
+    "   p.makeFringe()\n" \
+    "   p.fringeCorrect()\n" \
+    "   p.mosaicDetectors()\n" \
+    "   p.adjustWCSToReference()\n" \
+    "   p.resampleToCommonFrame()\n" \
+    "   p.stackFrames()\n" \
+    "   p.writeOutputs()"
+
+
+def test_show_primitives_on_gmos(test_path):
+    file_location = test_path + 'geminidr/' + GMOS
+    answer = show_primitives(file_location)
+    assert gmos_answer == answer
+
+
+def test_show_primitives_on_gmos_spect(test_path):
+    try:
+        file_location = test_path + 'geminidr/' + GMOS_SPECT
+        answer = show_primitives(file_location)
+        assert "RecipeNotFound Error" == answer
+    except RecipeNotFound:
+        pass
+
 
 gmos_ns_answer = \
+    "Recipe not provided, default recipe (reduce) will be used.\n" \
     "Input file: {}\n".format(os.path.normpath(os.path.join(path, GMOS_NS))) + \
     "Input tags: set(['RAW', 'GMOS', 'GEMINI', 'LS', 'UNPREPARED', " \
     "'SPECT', 'NODANDSHUFFLE', 'SOUTH', 'SIDEREAL'])\n" \
-    "Recipes available for the input file: \n" \
-    "   geminidr.gmos.recipes.qa.recipes_NS::reduce"
+    "Input mode: qa\n" \
+    "Input recipe: reduce\n" \
+    "Matched recipe: geminidr.gmos.recipes.qa.recipes_NS::reduce\n" \
+    "Recipe location: {}\n".format(os.path.normpath(os.path.join(
+        dragons_location + "gmos/recipes/qa/recipes_NS.py"))) + \
+    "Recipe tags: set(['GMOS', 'NODANDSHUFFLE', 'SPECT'])\n" \
+    "Primitives used: \n" \
+    "   p.prepare()\n" \
+    "   p.addDQ()\n" \
+    "   p.addVAR(read_noise=True)\n" \
+    "   p.overscanCorrect()\n" \
+    "   p.biasCorrect()\n" \
+    "   p.ADUToElectrons()\n" \
+    "   p.addVAR(poisson_noise=True)\n" \
+    "   p.findAcquisitionSlits()\n" \
+    "   p.skyCorrectNodAndShuffle()\n" \
+    "   p.measureIQ(display=True)\n" \
+    "   p.writeOutputs()"
+
+
+def test_show_primitives_on_gmos_ns(test_path):
+    file_location = test_path + 'geminidr/' + GMOS_NS
+    answer = show_primitives(file_location, 'qa')
+    assert gmos_ns_answer == answer
+
+
+def test_show_primitives_on_gmos_spect_default_mode(test_path):
+    try:
+        file_location = test_path + 'geminidr/' + GMOS_NS
+        answer = show_primitives(file_location)
+        assert "RecipeNotFound Error" == answer
+    except RecipeNotFound:
+        pass
+
+
+# # # # # #  GSAOI  # # # # # #
+
 
 gsaoi_dark_answer = \
+    "Recipe not provided, default recipe (makeProcessedDark) will be used.\n" \
     "Input file: {}\n".format(os.path.normpath(
         os.path.join(path, GSAOI_DARK))) + \
     "Input tags: set(['DARK', 'RAW', 'AT_ZENITH', 'AZEL_TARGET', 'CAL', " \
     "'UNPREPARED', 'SOUTH', 'GEMINI', 'GSAOI', 'NON_SIDEREAL'])\n" \
-    "Recipes available for the input file: \n" \
-    "   geminidr.gsaoi.recipes.sq.recipes_DARK::makeProcessedDark"
+    "Input mode: sq\n" \
+    "Input recipe: makeProcessedDark\n" \
+    "Matched recipe: geminidr.gsaoi.recipes.sq.recipes_DARK" \
+    "::makeProcessedDark\n" \
+    "Recipe location: {}\n".format(os.path.normpath(os.path.join(
+        dragons_location + "gsaoi/recipes/sq/recipes_DARK.py"))) + \
+    "Recipe tags: set(['DARK', 'GSAOI', 'CAL'])\n" \
+    "Primitives used: \n" \
+    "   p.prepare()\n" \
+    "   p.addDQ(add_illum_mask=False)\n" \
+    "   p.addVAR(read_noise=True)\n" \
+    "   p.nonlinearityCorrect()\n" \
+    "   p.ADUToElectrons()\n" \
+    "   p.addVAR(poisson_noise=True)\n" \
+    "   p.stackDarks()\n" \
+    "   p.storeProcessedDark()"
 
 
-gsaoi_image_answer = \
+def test_show_primitives_on_gsaoi_dark(test_path):
+    file_location = test_path + 'geminidr/' + GSAOI_DARK
+    answer = show_primitives(file_location, 'sq', 'default')
+    assert gsaoi_dark_answer == answer
+
+
+def test_show_primitives_on_gsaoi_dark_qa_mode(test_path):
+    try:
+        file_location = test_path + 'geminidr/' + GSAOI_DARK
+        answer = show_primitives(file_location, 'qa')
+        assert "RecipeNotFound Error" == answer
+    except RecipeNotFound:
+        pass
+
+
+gsaoi_image_answer_sq = \
+    "Recipe not provided, default recipe (reduce_nostack) will be used.\n" \
     "Input file: {}\n".format(os.path.normpath(
         os.path.join(path, GSAOI_IMAGE))) + \
-    "Input tags: set(['SOUTH', 'RAW', 'GEMINI', 'SIDEREAL', 'UNPREPARED', " \
-    "'IMAGE', 'GSAOI'])\n" \
-    "Recipes available for the input file: \n" \
-    "   geminidr.gsaoi.recipes.sq.recipes_IMAGE::reduce_nostack\n" \
-    "   geminidr.gsaoi.recipes.qa.recipes_IMAGE::reduce_nostack"
+    "Input tags: set(['SOUTH', 'RAW', 'GEMINI', 'SIDEREAL', " \
+    "'UNPREPARED', 'IMAGE', 'GSAOI'])\n" \
+    "Input mode: sq\n" \
+    "Input recipe: reduce_nostack\n" \
+    "Matched recipe: geminidr.gsaoi.recipes.sq.recipes_IMAGE::reduce_nostack\n"\
+    "Recipe location: {}\n".format(os.path.normpath(os.path.join(
+        dragons_location + "gsaoi/recipes/sq/recipes_IMAGE.py"))) + \
+    "Recipe tags: set(['IMAGE', 'GSAOI'])\n" \
+    "Primitives used: \n" \
+    "   p.prepare()\n" \
+    "   p.addDQ()\n" \
+    "   p.nonlinearityCorrect()\n" \
+    "   p.ADUToElectrons()\n" \
+    "   p.addVAR(read_noise=True, poisson_noise=True)\n" \
+    "   p.flatCorrect()\n" \
+    "   p.flushPixels()\n" \
+    "   p.separateSky()\n" \
+    "   p.associateSky(stream='sky')\n" \
+    "   p.skyCorrect(instream='sky', mask_objects=False, outstream='skysub')\n"\
+    "   p.detectSources(stream='skysub')\n" \
+    "   p.transferAttribute(stream='sky', source='skysub', " \
+    "attribute='OBJMASK')\n" \
+    "   p.clearStream(stream='skysub')\n" \
+    "   p.associateSky()\n" \
+    "   p.skyCorrect(mask_objects=True)\n" \
+    "   p.writeOutputs()"
+
+
+def test_show_primitives_on_gsaoi_image_sq_mode(test_path):
+    file_location = test_path + 'geminidr/' + GSAOI_IMAGE
+    answer = show_primitives(file_location)
+    assert gsaoi_image_answer_sq == answer
+
+
+gsaoi_image_answer_qa = \
+    "Input file: {}\n".format(os.path.normpath(
+        os.path.join(path, GSAOI_IMAGE))) + \
+    "Input tags: set(['SOUTH', 'RAW', 'GEMINI', 'SIDEREAL', " \
+    "'UNPREPARED', 'IMAGE', 'GSAOI'])\n" \
+    "Input mode: qa\n" \
+    "Input recipe: reduce_nostack\n" \
+    "Matched recipe: geminidr.gsaoi.recipes.qa.recipes_IMAGE::reduce_nostack\n"\
+    "Recipe location: {}\n".format(os.path.normpath(os.path.join(
+        dragons_location + "gsaoi/recipes/qa/recipes_IMAGE.py"))) + \
+    "Recipe tags: set(['IMAGE', 'GSAOI'])\n" \
+    "Primitives used: \n" \
+    "   p.prepare()\n" \
+    "   p.addDQ()\n" \
+    "   p.nonlinearityCorrect()\n" \
+    "   p.ADUToElectrons()\n" \
+    "   p.addVAR(read_noise=True, poisson_noise=True)\n" \
+    "   p.flatCorrect()\n" \
+    "   p.detectSources(detect_thresh=5., analysis_thresh=5., back_size=128)\n"\
+    "   p.measureIQ(display=True)\n" \
+    "   p.writeOutputs()\n" \
+    "   p.measureBG()\n" \
+    "   p.addReferenceCatalog()\n" \
+    "   p.determineAstrometricSolution()\n" \
+    "   p.measureCC()\n" \
+    "   p.addToList(purpose='forSky')\n" \
+    "   p.getList(purpose='forSky', max_frames=9)\n" \
+    "   p.separateSky()\n" \
+    "   p.associateSky()\n" \
+    "   p.skyCorrect()\n" \
+    "   p.detectSources(detect_thresh=5., analysis_thresh=5., back_size=128)\n"\
+    "   p.measureIQ(display=True)\n" \
+    "   p.determineAstrometricSolution()\n" \
+    "   p.measureCC()\n" \
+    "   p.writeOutputs()"
+
+
+def test_show_primitives_on_gsaoi_image_qa_mode(test_path):
+    file_location = test_path + 'geminidr/' + GSAOI_IMAGE
+    answer = show_primitives(file_location, 'qa', 'reduce_nostack')
+    assert gsaoi_image_answer_qa == answer
 
 
 gsaoi_flat_answer = \
+    "Recipe not provided, default recipe (makeProcessedFlat) will be used.\n" \
     "Input file: {}\n".format(os.path.normpath(
         os.path.join(path, GSAOI_FLAT))) + \
-    "Input tags: set(['FLAT', 'AZEL_TARGET', 'IMAGE', 'DOMEFLAT', " \
-    "'GSAOI', 'LAMPON', 'RAW', 'GEMINI', 'NON_SIDEREAL', 'CAL', " \
-    "'UNPREPARED', 'SOUTH'])\n" \
-    "Recipes available for the input file: \n" \
-    "   geminidr.gsaoi.recipes.sq.recipes_FLAT_IMAGE::makeProcessedBPM\n" \
-    "   geminidr.gsaoi.recipes.sq.recipes_FLAT_IMAGE::makeProcessedFlat\n" \
-    "   geminidr.gsaoi.recipes.qa.recipes_FLAT_IMAGE::makeProcessedFlat"
+    "Input tags: set(['FLAT', 'AZEL_TARGET', 'IMAGE', " \
+    "'DOMEFLAT', 'GSAOI', 'LAMPON', 'RAW', 'GEMINI', 'NON_SIDEREAL', " \
+    "'CAL', 'UNPREPARED', 'SOUTH'])\n" \
+    "Input mode: sq\n" \
+    "Input recipe: makeProcessedFlat\n" \
+    "Matched recipe: geminidr.gsaoi.recipes.sq.recipes_FLAT_IMAGE::" \
+    "makeProcessedFlat\n" \
+    "Recipe location: {}\n".format(os.path.normpath(os.path.join(
+        dragons_location + "gsaoi/recipes/sq/recipes_FLAT_IMAGE.py"))) + \
+    "Recipe tags: set(['FLAT', 'IMAGE', 'GSAOI', 'CAL'])\n" \
+    "Primitives used: \n" \
+    "   p.prepare()\n" \
+    "   p.addDQ()\n" \
+    "   p.nonlinearityCorrect()\n" \
+    "   p.ADUToElectrons()\n" \
+    "   p.addVAR(read_noise=True, poisson_noise=True)\n" \
+    "   p.makeLampFlat()\n" \
+    "   p.normalizeFlat()\n" \
+    "   p.thresholdFlatfield()\n" \
+    "   p.storeProcessedFlat()"
 
 
-gmos_spect_answer = \
-    "Input file: {}\n".format(os.path.normpath(
-        os.path.join(path, GMOS_SPECT))) + \
-    "Input tags: set(['RAW', 'GMOS', 'GEMINI', 'NORTH', 'SIDEREAL', " \
-    "'UNPREPARED', 'SPECT', 'MOS'])\n" \
-    "!!! No recipes were found for this file !!!"
+def test_show_primitives_on_gsaoi_flat(test_path):
+    file_location = test_path + 'geminidr/' + GSAOI_FLAT
+    answer = show_primitives(file_location)
+    assert gsaoi_flat_answer == answer
+
+
+def test_show_primitives_on_gsaoi_flat_ql_mode(test_path):
+    try:
+        file_location = test_path + 'geminidr/' + GSAOI_FLAT
+        answer = show_primitives(file_location, 'ql')
+        assert "ModuleNotFoundError" == answer
+    except ModeError:
+        pass
+
+
+# # # # # #  NIRI  # # # # # #
+
 
 niri_answer = \
+    "Recipe not provided, default recipe (reduce) will be used.\n" \
     "Input file: {}\n".format(os.path.normpath(os.path.join(path, NIRI))) + \
-    "Input tags: set(['RAW', 'GEMINI', 'NORTH', 'SIDEREAL', 'UNPREPARED', " \
-    "'IMAGE', 'NIRI'])\n" \
-    "Recipes available for the input file: \n" \
-    "   geminidr.niri.recipes.sq.recipes_IMAGE::makeSkyFlat\n" \
-    "   geminidr.niri.recipes.sq.recipes_IMAGE::reduce\n" \
-    "   geminidr.niri.recipes.qa.recipes_IMAGE::makeSkyFlat\n" \
-    "   geminidr.niri.recipes.qa.recipes_IMAGE::reduce"
+    "Input tags: set(['RAW', 'GEMINI', 'NORTH', 'SIDEREAL', " \
+    "'UNPREPARED', 'IMAGE', 'NIRI'])\n" \
+    "Input mode: sq\n" \
+    "Input recipe: reduce\n" \
+    "Matched recipe: geminidr.niri.recipes.sq.recipes_IMAGE::reduce\n" \
+    "Recipe location: {}\n".format(os.path.normpath(os.path.join(
+        dragons_location + "niri/recipes/sq/recipes_IMAGE.py"))) + \
+    "Recipe tags: set(['IMAGE', 'NIRI'])\n" \
+    "Primitives used: \n" \
+    "   p.prepare()\n" \
+    "   p.addDQ()\n" \
+    "   p.removeFirstFrame()\n" \
+    "   p.ADUToElectrons()\n" \
+    "   p.addVAR(read_noise=True, poisson_noise=True)\n" \
+    "   p.nonlinearityCorrect()\n" \
+    "   p.darkCorrect()\n" \
+    "   p.flatCorrect()\n" \
+    "   p.separateSky()\n" \
+    "   p.associateSky(stream='sky')\n" \
+    "   p.skyCorrect(instream='sky', mask_objects=False, outstream='skysub')\n"\
+    "   p.detectSources(stream='skysub')\n" \
+    "   p.transferAttribute(stream='sky', source='skysub', " \
+    "attribute='OBJMASK')\n" \
+    "   p.clearStream(stream='skysub')\n" \
+    "   p.associateSky()\n" \
+    "   p.skyCorrect(mask_objects=True)\n" \
+    "   p.detectSources()\n" \
+    "   p.adjustWCSToReference()\n" \
+    "   p.resampleToCommonFrame()\n" \
+    "   p.stackFrames()\n" \
+    "   p.writeOutputs()"
+
+
+def test_show_primitives_on_niri(test_path):
+    file_location = test_path + 'geminidr/' + NIRI
+    answer = show_primitives(file_location)
+    assert niri_answer == answer
+
+
+# # # # # #  F2  # # # # # #
+
 
 f2_answer = \
+    "Recipe not provided, default recipe (reduce) will be used.\n" \
     "Input file: {}\n".format(os.path.normpath(os.path.join(path, F2))) + \
     "Input tags: set(['SOUTH', 'RAW', 'F2', 'GEMINI', 'SIDEREAL', " \
     "'UNPREPARED', 'IMAGE', 'ACQUISITION'])\n" \
-    "Recipes available for the input file: \n" \
-    "   geminidr.f2.recipes.sq.recipes_IMAGE::makeSkyFlat\n" \
-    "   geminidr.f2.recipes.sq.recipes_IMAGE::reduce\n" \
-    "   geminidr.f2.recipes.qa.recipes_IMAGE::reduce\n" \
-    "   geminidr.f2.recipes.qa.recipes_IMAGE::reduce_nostack"
+    "Input mode: sq\n" \
+    "Input recipe: reduce\n" \
+    "Matched recipe: geminidr.f2.recipes.sq.recipes_IMAGE::reduce\n" \
+    "Recipe location: {}\n".format(os.path.normpath(os.path.join(
+        dragons_location + "f2/recipes/sq/recipes_IMAGE.py"))) + \
+    "Recipe tags: set(['F2', 'IMAGE'])\n" \
+    "Primitives used: \n" \
+    "   p.prepare()\n" \
+    "   p.addDQ()\n" \
+    "   p.ADUToElectrons()\n" \
+    "   p.addVAR(read_noise=True, poisson_noise=True)\n" \
+    "   p.nonlinearityCorrect()\n" \
+    "   p.darkCorrect()\n" \
+    "   p.flatCorrect()\n" \
+    "   p.flushPixels()\n" \
+    "   p.separateSky()\n" \
+    "   p.associateSky(stream='sky')\n" \
+    "   p.skyCorrect(instream='sky', mask_objects=False, outstream='skysub')\n"\
+    "   p.detectSources(stream='skysub')\n" \
+    "   p.transferAttribute(stream='sky', source='skysub', " \
+    "attribute='OBJMASK')\n" \
+    "   p.clearStream(stream='skysub')\n" \
+    "   p.associateSky()\n" \
+    "   p.skyCorrect(mask_objects=True)\n" \
+    "   p.flushPixels()\n" \
+    "   p.detectSources()\n" \
+    "   p.adjustWCSToReference()\n" \
+    "   p.resampleToCommonFrame()\n" \
+    "   p.stackFrames()\n" \
+    "   p.writeOutputs()"
 
 
-list_of_answers = [gnirs_answer, gnirs_spect_answer, gmos_spect_answer,
-                   niri_answer]
-
-
-@pytest.mark.parametrize('filename, str_answer', [
-    # (GNIRS_SPECT, gnirs_spect_answer),
-    # (GMOS_SPECT, gmos_spect_answer),
-    # (GSAOI_DARK, gsaoi_dark_answer),
-    # (GSAOI_IMAGE, gsaoi_image_answer),
-    # (GSAOI_FLAT, gsaoi_flat_answer),
-    # (GMOS_NS, gmos_ns_answer),
-    (GNIRS, gnirs_answer),
-    # (GMOS, gmos_answer),
-    # (NIRI, niri_answer),
-    # (F2, f2_answer),
-    # (NIFS, "ImportError"),
-    # (GRACES, "ImportError")
-    ])
-def test_show_primitives(test_path, filename, str_answer):
-    try:
-        file_location = test_path + 'geminidr/' + filename
-        answer = show_primitives(file_location)
-        assert str_answer == answer
-    except ImportError:
-        if str_answer == 'ImportError':
-            pass
-        else:
-            raise ImportError
+def test_show_primitives_on_f2(test_path):
+    file_location = test_path + 'geminidr/' + F2
+    answer = show_primitives(file_location)
+    assert f2_answer == answer
