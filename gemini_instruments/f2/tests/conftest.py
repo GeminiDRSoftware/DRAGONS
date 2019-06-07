be400a6f547566aa9d8da8dd6abc337075859dad
Bruno Quint <bquint@gemini.edu>
2019-06-07 17:24:00 -0400

    Added tests to check descriptors for F2 gemini instruments

---
+++ gemini_instruments/f2/tests/conftest.py
@@ -0,0 +1,18 @@
+
+import glob
+import pytest
+import os
+
+
+@pytest.fixture(scope="module")
+def f2_files():
+
+    try:
+        path = os.environ['TEST_PATH']
+    except KeyError:
+        pytest.skip("Could not find environment variable: $TEST_PATH")
+
+    if not os.path.exists(path):
+        pytest.skip("Could not find path stored in $TEST_PATH: {}".format(path))
+
+    yield glob.glob(os.path.join(path, "F2/", "*fits"))
