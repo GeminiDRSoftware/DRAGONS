0efd3b0dfadf718f0e5bb24f86e82dcf29e248d8
Bruno Quint <bquint@gemini.edu>
2019-06-11 17:48:50 -0400

    Updated download and install anaconda - let me remove anaconda for now

--- .jenkins/scripts/download_and_install_anaconda.sh
+++ .jenkins/scripts/download_and_install_anaconda.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
+# Remove anaconda for re-installation
+conda install anaconda-clean
+anaconda-clean --yes
+
+
+LINUX_URL="https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh"
+MACOS_URL="https://repo.anaconda.com/archive/Anaconda3-2019.03-MacOSX-x86_64.sh"
+
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    ANACONDA_URL="${MACOS_URL}"
+elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
+    ANACONDA_URL="${LINUX_URL}"
+fi
+
+
 if ! [[ "$(command -v conda)" ]]; then
 
     echo "Conda is not installed - Downloading and installing"
-
-    curl https://repo.anaconda.com/archive/Anaconda3-5.3.1-Linux-x86_64.sh \\
-        --output anaconda.sh --silent
+    curl "${ANACONDA_URL}" --output anaconda.sh --silent
 
     chmod a+x anaconda.sh
     ./anaconda.sh -u -b -p $JENKINS_HOME/anaconda3/
