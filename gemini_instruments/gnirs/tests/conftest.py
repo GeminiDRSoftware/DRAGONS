29b460142a75678c2aafb45fc045218ff56b1e6c
Bruno Quint <bquint@gemini.edu>
2019-06-13 10:26:38 -0400

    Fix GNIRS tests

--- gemini_instruments/gnirs/tests/conftest.py
+++ gemini_instruments/gnirs/tests/conftest.py
@@ -1,19 +1,14 @@
-
 import glob
-import pytest
 import os
+
+import pytest
+
+from astrodata.test.conftest import input_test_path
 
 
 @pytest.fixture(scope="module")
 def gemini_files():
-
-    try:
-        path = os.environ['TEST_PATH']
-    except KeyError:
-        pytest.skip("Could not find environment variable: $TEST_PATH")
-
-    if not os.path.exists(path):
-        pytest.skip("Could not find path stored in $TEST_PATH: {}".format(path))
+    path = input_test_path()
 
     def get_files(instrument):
         return glob.glob(os.path.join(path, instrument, "*fits"))
--- gemini_instruments/gnirs/tests/test_gnirs.py
+++ gemini_instruments/gnirs/tests/test_gnirs.py
@@ -1,107 +1,78 @@
 import os
-import pytest
-import warnings
 
 import astrodata
 import gemini_instruments
-from astrodata.test.conftest import test_path
-
-
-try:
-    path = os.environ['TEST_PATH']
-except KeyError:
-    path = ''
-
-if not os.path.exists(path):
-    path = ''
-
 
 filename = 'N20190206S0279.fits'
 
 
-@pytest.fixture(scope='class')
-def setup_gnirs(request):
-    print('setup TestGNIRS')
-
-    def fin():
-        print('\nteardown TestGNIRS')
-    request.addfinalizer(fin)
-    return
+def test_is_right_type(input_test_path):
+    ad = astrodata.open(os.path.join(input_test_path, filename))
+    assert type(ad) == gemini_instruments.gnirs.adclass.AstroDataGnirs
 
 
-@pytest.mark.usefixtures('setup_gnirs')
-class TestF2:
+def test_is_right_instance(input_test_path):
+    ad = astrodata.open(os.path.join(input_test_path, filename))
+    # YES, this *can* be different from test_is_right_type. Metaclasses!
+    assert isinstance(ad, gemini_instruments.gnirs.adclass.AstroDataGnirs)
 
-    def test_is_right_type(self, test_path):
 
-        ad = astrodata.open(os.path.join(test_path, filename))
-        assert type(ad) ==  gemini_instruments.gnirs.adclass.AstroDataGnirs
+def test_extension_data_shape(input_test_path):
+    ad = astrodata.open(os.path.join(input_test_path, filename))
+    data = ad[0].data
 
-    def test_is_right_instance(self, test_path):
+    assert data.shape == (1022, 1024)
 
-        ad = astrodata.open(os.path.join(test_path, filename))
-        # YES, this *can* be different from test_is_right_type. Metaclasses!
-        assert isinstance(ad, gemini_instruments.gnirs.adclass.AstroDataGnirs)
 
-    def test_extension_data_shape(self, test_path):
+def test_tags(input_test_path):
+    ad = astrodata.open(os.path.join(input_test_path, filename))
+    tags = ad.tags
+    expected = {'RAW', 'GEMINI', 'NORTH',
+                'SIDEREAL', 'GNIRS',
+                'UNPREPARED', 'SPECT', 'XD'}
 
-        ad = astrodata.open(os.path.join(test_path, filename))
-        data = ad[0].data
+    assert expected.issubset(tags)
 
-        assert data.shape == (1022, 1024)
 
-    def test_tags(self, test_path):
+def test_can_return_instrument(input_test_path):
+    ad = astrodata.open(os.path.join(input_test_path, filename))
+    assert ad.phu['INSTRUME'] == 'GNIRS'
+    assert ad.instrument() == ad.phu['INSTRUME']
 
-        ad = astrodata.open(os.path.join(test_path, filename))
-        tags = ad.tags
-        expected = {'RAW', 'GEMINI', 'NORTH',
-                    'SIDEREAL', 'GNIRS',
-                    'UNPREPARED', 'SPECT', 'XD'}
 
-        assert expected.issubset(tags)
+def test_can_return_ad_length(input_test_path):
+    ad = astrodata.open(os.path.join(input_test_path, filename))
+    assert len(ad) == 1
 
-    def test_can_return_instrument(self, test_path):
 
-        ad = astrodata.open(os.path.join(test_path, filename))
-        assert ad.phu['INSTRUME'] == 'GNIRS'
-        assert ad.instrument() == ad.phu['INSTRUME']
+def test_slice_range(input_test_path):
+    ad = astrodata.open(os.path.join(input_test_path, filename))
+    metadata = ('SCI', 2), ('SCI', 3)
+    slc = ad[1:]
 
-    def test_can_return_ad_length(self, test_path):
+    assert len(slc) == 0
 
-        ad = astrodata.open(os.path.join(test_path, filename))
+    for ext, md in zip(slc, metadata):
+        assert (ext.hdr['EXTNAME'], ext.hdr['EXTVER']) == md
+
+
+# def test_read_a_keyword_from_phu(input_test_path):
+#
+#     ad = astrodata.open(os.path.join(input_test_path, filename))
+#     assert ad.phu['DETECTOR'] == 'GNIRS'
+
+def test_read_a_keyword_from_hdr(input_test_path):
+    ad = astrodata.open(os.path.join(input_test_path, filename))
+
+    try:
+        assert ad.hdr['CCDNAME'] == ['EEV 9273-16-03', 'EEV 9273-20-04', 'EEV 9273-20-03']
+    except KeyError:
+        # KeyError only accepted if it's because headers out of range
         assert len(ad) == 1
 
-
-    def test_slice_range(self, test_path):
-
-        ad = astrodata.open(os.path.join(test_path, filename))
-        metadata = ('SCI', 2), ('SCI', 3)
-        slc = ad[1:]
-
-        assert len(slc) == 0
-
-        for ext, md in zip(slc, metadata):
-            assert (ext.hdr['EXTNAME'], ext.hdr['EXTVER']) == md
-
-
-    # def test_read_a_keyword_from_phu(self, test_path):
-    #
-    #     ad = astrodata.open(os.path.join(test_path, filename))
-    #     assert ad.phu['DETECTOR'] == 'GNIRS'
-
-    def test_read_a_keyword_from_hdr(self, test_path):
-
-        ad = astrodata.open(os.path.join(test_path, filename))
-
-        try:
-            assert ad.hdr['CCDNAME'] == ['EEV 9273-16-03', 'EEV 9273-20-04', 'EEV 9273-20-03']
-        except KeyError:
-            # KeyError only accepted if it's because headers out of range
-            assert len(ad) == 1
-
-        # with pytest.raises(AssertionError):
-        #     ad.phu.DETECTOR = 'FooBar'
-        #     ad.phu.ARBTRARY = 'BarBaz'
-        #     assert ad.phu.DETECTOR == 'FooBar'
-        #     assert ad.phu.ARBTRARY == 'BarBaz'
-        #     assert ad.phu['DETECTOR'] == 'FooBar'
+    # with pytest.raises(AssertionError):
+    #     ad.phu.DETECTOR = 'FooBar'
+    #     ad.phu.ARBTRARY = 'BarBaz'
+    #     assert ad.phu.DETECTOR == 'FooBar'
+    #     assert ad.phu.ARBTRARY == 'BarBaz'
+    #     assert ad.phu['DETECTOR'] == 'FooBar'
