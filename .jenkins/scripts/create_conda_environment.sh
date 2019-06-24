e0c5852963384dff71343893cfc43c0b840164fe
Bruno Quint <bquint@gemini.edu>
2019-06-24 12:20:52 -0400

    Updated create_conda_environment.sh

--- .jenkins/scripts/create_conda_environment.sh
+++ .jenkins/scripts/create_conda_environment.sh
@@ -5,11 +5,12 @@
 # that the `$CONDA_ENV_NAME` virtual environment exists and its creating step
 # can be skipped
 #
+echo " Removing ${CONDA_ENV_NAME} ---"
+conda env remove -n "${CONDA_ENV_NAME}" || echo 0
+
 
 if conda info --envs | grep -q $CONDA_ENV_NAME; then
     echo " Skipping cretiong of existing conda environment: ${CONDA_ENV_NAME}";
-
 else
     conda env create --quiet --file ${CONDA_ENV_FILE} -n "${CONDA_ENV_NAME}";
-
 fi
