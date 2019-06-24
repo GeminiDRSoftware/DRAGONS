e3d8b3f793543451129a1c684f8e3436ca9cba4e
Bruno Quint <bquint@gemini.edu>
2019-06-24 10:37:48 -0400

    Added conditional installation for the gemini_calmgr

--- .jenkins/scripts/install_missing_packages.sh
+++ .jenkins/scripts/install_missing_packages.sh
@@ -2,8 +2,19 @@
 
 source activate ${CONDA_ENV_NAME}
 
+version=$(python -V 2>&1 | grep -Po '(?<=Python )(.+)')
+parsedVersion=$(echo "${version//./}")
+echo "Installing missing packages for Python ${parsedVersion}"
+
+
 cd .jenkins/local_calibration_manager/
 
-pip install --quiet GeminiCalMgr-0.9.11-py3-none-any.whl
+if [[ "$parsedVersion" -lt "300" && "$parsedVersion" -gt "270" ]]; then
+    pip install --quiet -r requirements_py2.txt
+    pip install --quiet GeminiCalMgr-0.9.11-py2-none-any.whl
+else
+    pip install --quiet -r requirements_py3.txt
+    pip install --quiet GeminiCalMgr-0.9.11-py3-none-any.whl
+fi
 
 cd -
