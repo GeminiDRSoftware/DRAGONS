12cf967e7c774981a00ed4487915b4cffe547f95
Bruno Quint <bquint@gemini.edu>
2019-06-24 10:36:40 -0400

    Added print messages for when env tests are passing

--- .jenkins/scripts/test_environment.sh
+++ .jenkins/scripts/test_environment.sh
@@ -3,14 +3,19 @@
 source activate ${CONDA_ENV_NAME}
 
 python -c "import astropy"
+echo "Test import astropy passed"
 
 python -c "import stsci.numdisplay"
+echo "Test import stsci.numdisplay passed"
 
 python -c "import astrodata"
+echo "Test import astrodata passed"
 
 python -c "import gemini_instruments"
+echo "Test import gemini_instruments passed"
 
 python -c "import gemini_calmgr"
+echo "Test import gemini_calmgr passed"
 
 # TODO: This is not actually working
 # python -c "from gempy.library import cyclip"
