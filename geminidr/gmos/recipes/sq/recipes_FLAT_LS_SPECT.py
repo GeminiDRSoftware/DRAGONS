47ea96d68d3be301ebc3f6ebe08c151124d6abe5
csimpson <csimpson@gemini.edu>
2019-07-02 11:15:42 -1000

    initial LS FLAT recipe

---
+++ geminidr/gmos/recipes/sq/recipes_FLAT_LS_SPECT.py
@@ -0,0 +1,22 @@
+"""
+Recipes available to data with tags ['GMOS', 'SPECT', 'LS', 'FLAT'].
+These are GMOS longslit observations.
+Default is "reduce".
+"""
+recipe_tags = set(['GMOS', 'SPECT', 'LS', 'FLAT'])
+
+def reduce(p):
+    p.prepare()
+    p.addDQ(static_bpm=None)
+    p.addVAR(read_noise=True)
+    p.overscanCorrect()
+    #p.biasCorrect()
+    p.ADUToElectrons()
+    p.addVAR(poisson_noise=True)
+    #p.stackFlats()
+    p.normalizeFlat()
+    #p.makeIRAFCompatible()
+    #p.storeProcessedFlat()
+    p.writeOutputs()
+
+default = reduce
