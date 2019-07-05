40939ecbfe9fbc5539c9958c1a3522ef802922f7
Bruno Quint <bquint@gemini.edu>
2019-07-05 10:38:25 -0400

    Added git hash to version

--- astrodata/version.py
+++ astrodata/version.py
@@ -9,10 +9,25 @@ API = 2
 FEATURE = 1
 BUG = 1
 TAG = ''
-# --------------------------
 
 
-def version(short=False):
+def get_git_hash():
+    """
+    Returns the current git hash to be used in the versioning system.
+
+    Returns
+    -------
+    str : git hash
+    """
+    from subprocess import check_output
+
+    git_hash = str(check_output(["git", "describe", "--always"]).strip())
+    git_hash = git_hash.split("-")[-1]
+
+    return git_hash
+
+
+def version(short=False, tag=TAG):
     """
     Returns DRAGONS's version based on the api,
     feature and bug numbers.
@@ -21,12 +36,13 @@ def version(short=False):
     -------
     str : formatted version
     """
+    tag = get_git_hash() if not tag else tag
 
     if short:
         _version = "{:d}.{:d}".format(API, FEATURE)
 
     else:
-        _tag = '-{:s}'.format(TAG) if TAG else ''
+        _tag = '_{:s}'.format(tag)
         _version = "{:d}.{:d}.{:d}".format(API, FEATURE, BUG) + _tag
 
     return _version
