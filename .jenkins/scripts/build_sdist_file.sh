d27febf9b64f59f21cf2d6e5a28a30b4f2b040e6
Bruno Quint <bquint@gemini.edu>
2019-07-05 10:59:31 -0400

    Add git hash to sdist .tar.gz file

--- .jenkins/scripts/build_sdist_file.sh
+++ .jenkins/scripts/build_sdist_file.sh
@@ -4,3 +4,8 @@ source activate ${CONDA_ENV_NAME}
 
 python setup.py sdist --formats=gztar
 
+DRAGONS_VERSION=$(python setup.py --version)
+GIT_SHA=$(git log --pretty=format:'%h' -n 1)
+
+mv dist/dragons-${DRAGONS_VERSION}.tar.gz \
+    dist/dragons-${DRAGONS_VERSION}_${BUILD_NUMBER}_${GIT_SHA}.tar.gz
