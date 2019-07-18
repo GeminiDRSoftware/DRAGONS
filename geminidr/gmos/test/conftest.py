09c72d402e88cd7240ba4ea48509f0367bbb9212
James E.H. Turner <jturner@gemini.edu>
2019-07-17 22:32:13 -0400

    Update testing fixtures for b1quint's changes from master (accompanying tests still to be checked in).

--- geminidr/gmos/test/conftest.py
+++ geminidr/gmos/test/conftest.py
@@ -1,28 +1,10 @@
 import pytest
 import os.path
 
-from astrodata.test import conftest
-
-input_test_path = conftest.input_test_path
-output_test_path = conftest.output_test_path
+from astrodata import testing
 
 
-@pytest.fixture
-def path_to_raw_files(input_test_path):
-
-    return os.path.join(input_test_path, 'raw')
-
-@pytest.fixture
-def path_to_ref_files(input_test_path):
-
-    return os.path.join(input_test_path, 'ref')
-
-@pytest.fixture
-def proc_dir(output_test_path):
-
-    # Append proc/ to environment-defined path unless we're just using the CWD:
-    if output_test_path:
-        path = os.path.join(output_test_path, 'proc')
-
-    return path
+path_to_inputs = testing.path_to_inputs
+path_to_refs = testing.path_to_refs
+path_to_outputs = testing.path_to_outputs
 
