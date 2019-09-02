9b9355f630ac5d8505af0ab7058a53d935a59668
bquint <bquint@gemini.edu>
2019-09-02 16:59:55 -0400

    Refactored test for determine wavelength solution and determine distortion and added more files

--- geminidr/core/test/test_spect.py
+++ geminidr/core/test/test_spect.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
+"""
+Tests applied to primitives_spect.py
 
-import glob
+"""
 import pytest
 import os
-import warnings
 
 import astrodata
 import astrodata.testing as ad_test
@@ -12,24 +13,105 @@ import geminidr
 
 from gempy.utils import logutils
 
-
 logutils.config(file_name='foo.log')
 
+file_list = [
+    "process_arcs/N20170530S0006.fits",
+    "process_arcs/N20180119S0232.fits",
+    "process_arcs/N20181114S0512.fits",
+    "process_arcs/N20180120S0417.fits",
+    # "process_arcs/N20180911S0156.fits", # File failed loading
+    # "process_arcs/S20181022S0349.fits", # File failed loading
+    # "process_arcs/N20181011S0220.fits", # File failed loading
+    # "process_arcs/N20181115S0175.fits", # File failed loading
+    # "process_arcs/S20181128S0099.fits", # File failed loading
+]
 
-def prepare_for_wavelength_calibration(p):
+
+@pytest.fixture(params=file_list)
+def test_files(request, path_to_inputs, path_to_outputs, path_to_refs):
+    """
+    Fixture that parses the `file_list` parameter list into a Namespace in a
+    cleaner fashion. A single fixture is created in order to avoid a parameter
+    matrix that would be created if one fixture was done for each variable.
+
+    Parameters
+    ----------
+    request :
+        This is an internal variable from PyTest and it is used to access each
+        parameter in the parameter list.
+
+    path_to_inputs : str
+        This is the path to where the input files are stored. It is accessed
+        via `path_to_inputs` fixture, defined in the conftest.py file
+
+    path_to_outputs : str
+        This is the path to where the outputs files are stored. It is accessed
+        via `path_to_outputs` fixture, defined in the conftest.py file
+
+    path_to_refs : str
+        This is the path to where the reference files are stored. It is accessed
+        via `path_to_refs` fixture, defined in the conftest.py file
+
+    Returns
+    -------
+    Namespace :
+        An object that holds the full path for the input, output and reference
+        files.
+    """
+    class TestFiles:
+        pass
+
+    TestFiles.input = os.path.join(path_to_inputs, request.param)
+    TestFiles.output = os.path.join(path_to_outputs, request.param)
+    TestFiles.reference = os.path.join(path_to_refs, request.param)
+
+    return TestFiles
+
+
+def add_suffix(file_name, suffix):
+    """
+    Adds a suffix to the input file_name.
+
+    Parameters
+    ----------
+    file_name : str
+        Input file name
+
+    suffix : str
+        Suffix to be appended to the file name
+
+
+    Returns
+    -------
+    str
+        new file name with suffix properly appended
+    """
+    return "".join([os.path.splitext(file_name)[0], suffix, ".fits"])
+
+
+def prepare_for_wavelength_calibration(ad):
     """
     Runs the primitives that come before running the actual wavelength calibration
     one.
 
     Parameters
     ----------
-        p : :class:`~geminidr.core.primitives_spect.Spect` or subclass
+        ad : :class:`~astrodata.AstroData` or subclass
 
     Returns
     -------
         :class:`~geminidr.core.primitives_spect.Spect` or subclass : returns
         the same object that entered this function after processing the data.
     """
+
+    if 'GMOS' in ad.tags:
+        from geminidr.gmos import primitives_gmos_spect
+        p = primitives_gmos_spect.GMOSSpect([ad])
+    else:
+        pytest.fail("Input data tags do not match expected tags for tests.\n"
+                    "    {}".format(ad.tags))
+
     p.viewer = geminidr.dormantViewer(p, None)
     p.prepare()
     p.addDQ(static_bpm=None)
@@ -43,155 +125,98 @@ def prepare_for_wavelength_calibration(p):
     return p
 
 
-@pytest.mark.parametrize("input_file", ["GMOS/GN-2017A-FT-19/N20170530S0006.fits"])
-def test_determine_wavelength_solution(input_file, path_to_inputs, path_to_outputs, path_to_refs):
+class TestArcProcessing:
     """
-    Regression test for determine wavelength solution. It checks if the
-    :class:`~astrodata.AstroData` object contains extension(s) with the "WAVECAL"
-    attribute and compare them.
-
-    It processes an input raw file, writes the processed data to an output file
-    and compare the file with a reference file.
-
-    Parameters
-    ----------
-    input_file : str
-        Sub-directory and file name of the input file provided by `parametrize`.
-
-    path_to_inputs : str
-        Path to input data provided by fixtures and defined in `conftest.py`.
-
-    path_to_outputs : str
-        Path to output data provided by fixtures and defined in `conftest.py`.
-
-    path_to_refs : str
-        Path to reference data provided by fixtures and defined in `conftest.py`.
+    This class structure simply holds together tests inside a single context.
+    There is no actual functionality.
     """
-    _suffix = "_wavelength_solution_determined"
-    _input = os.path.join(path_to_inputs, input_file)
 
-    _output = os.path.join(path_to_outputs, "".join(
-        [os.path.splitext(input_file)[0], _suffix, ".fits"]))
+    def test_determine_wavelength_solution(self, test_files):
+        """
+        Regression test for determine wavelength solution. It checks if the
+        :class:`~astrodata.AstroData` object contains extension(s) with the "WAVECAL"
+        attribute and compare them.
 
-    _reference = os.path.join(path_to_refs, "".join(
-        [os.path.splitext(input_file)[0], _suffix, ".fits"]))
+        It processes an input raw file, writes the processed data to an output file
+        and compare the file with a reference file.
 
-    if not os.path.exists(_input):
-        pytest.mark.xfail(reason="Could not access input file:\n"
-                                 "     {}".format(_input))
+        Parameters
+        ----------
+        test_files : Namespace
+            An object that contains an `.input`, an `.output` and a `.reference`
+            reference containing strings with the paths to the respective
+            input, output and reference files.
+        """
+        _suffix = "_wavelength_solution_determined"
 
-    if not os.path.exists(os.path.dirname(_output)):
-        os.makedirs(os.path.dirname(_output), exist_ok=True)
+        _input = test_files.input
+        _output = add_suffix(test_files.output, _suffix)
+        _reference = add_suffix(test_files.reference, _suffix)
 
-    if not os.path.exists(os.path.dirname(_reference)):
-        os.makedirs(os.path.dirname(_reference), exist_ok=True)
+        if not os.path.exists(_input):
+            pytest.mark.xfail(
+                reason="Could not access input file:\n     {}".format(_input))
 
-    ad = astrodata.open(_input)
+        if not os.path.exists(os.path.dirname(_output)):
+            os.makedirs(os.path.dirname(_output), exist_ok=True)
 
-    if 'GMOS' in ad.tags:
-        from geminidr.gmos import primitives_gmos_spect
-        p = primitives_gmos_spect.GMOSSpect([ad])
-    else:
-        pytest.fail("Input data tags do not match expected tags for tests.\n"
-                    "    {}".format(ad.tags))
+        if not os.path.exists(os.path.dirname(_reference)):
+            os.makedirs(os.path.dirname(_reference), exist_ok=True)
 
-    p = prepare_for_wavelength_calibration(p)
-    p.determineWavelengthSolution()
-    p.writeOutputs(outfilename=_output, overwrite=True)
+        ad = astrodata.open(_input)
 
-    output_ad = astrodata.open(_output)
-    reference_ad = astrodata.open(_reference)
+        p = prepare_for_wavelength_calibration(ad)
+        p.determineWavelengthSolution()
+        p.writeOutputs(outfilename=_output, overwrite=True)
 
-    ad_test.assert_same_class(output_ad, reference_ad)
-    ad_test.assert_wavelength_solutions_are_close(output_ad, reference_ad)
+        output_ad = astrodata.open(_output)
+        reference_ad = astrodata.open(_reference)
 
+        ad_test.assert_same_class(output_ad, reference_ad)
+        ad_test.assert_wavelength_solutions_are_close(output_ad, reference_ad)
 
-@pytest.mark.parametrize("input_file", ["GMOS/GN-2017A-FT-19/N20170530S0006.fits"])
-def test_determine_distortion(input_file, path_to_inputs, path_to_outputs, path_to_refs):
-    """
-    Regression test for determine distortion.
+    def test_determine_distortion(self, test_files):
+        """
+        Regression test for determine distortion.
 
-    It processes an input raw file, writes the processed data to an output file
-    and compare the file with a reference file.
+        It processes an input raw file, writes the processed data to an output file
+        and compare the file with a reference file.
 
-    Parameters
-    ----------
-    input_file : str
-        Sub-directory and file name of the input file provided by `parametrize`.
+        Parameters
+        ----------
+        test_files : Namespace
+            An object that contains an `.input`, an `.output` and a `.reference`
+            reference containing strings with the paths to the respective
+            input, output and reference files.
+        """
+        _suffix = "_distortionDetermined"
 
-    path_to_inputs : str
-        Path to input data provided by fixtures and defined in `conftest.py`.
+        _input = test_files.input
+        _output = add_suffix(test_files.output, _suffix)
+        _reference = add_suffix(test_files.reference, _suffix)
 
-    path_to_outputs : str
-        Path to output data provided by fixtures and defined in `conftest.py`.
+        if not os.path.exists(_input):
+            pytest.mark.xfail(
+                reason="Could not access input file:\n     {}".format(_input))
 
-    path_to_refs : str
-        Path to reference data provided by fixtures and defined in `conftest.py`.
-    """
-    _suffix = "_distortionDetermined"
-    _input = os.path.join(path_to_inputs, input_file)
+        if not os.path.exists(os.path.dirname(_output)):
+            os.makedirs(os.path.dirname(_output), exist_ok=True)
 
-    _input = os.path.join(path_to_inputs, input_file)
+        if not os.path.exists(os.path.dirname(_reference)):
+            os.makedirs(os.path.dirname(_reference), exist_ok=True)
 
-    _output = os.path.join(path_to_outputs, "".join(
-        [os.path.splitext(input_file)[0], _suffix, ".fits"]))
+        ad = astrodata.open(_input)
 
-    _reference = os.path.join(path_to_refs, "".join(
-        [os.path.splitext(input_file)[0], _suffix, ".fits"]))
+        p = prepare_for_wavelength_calibration(ad)
+        p.determineWavelengthSolution()
+        p.determineDistortion()
+        p.writeOutputs(outfilename=_output, overwrite=True)
 
-    if not os.path.exists(_input):
-        pytest.mark.xfail(reason="Could not access input file:\n"
-                                 "     {}".format(_input))
+        output_ad = astrodata.open(_output)
+        reference_ad = astrodata.open(_reference)
 
-    if not os.path.exists(os.path.dirname(_output)):
-        os.makedirs(os.path.dirname(_output), exist_ok=True)
-
-    if not os.path.exists(os.path.dirname(_reference)):
-        os.makedirs(os.path.dirname(_reference), exist_ok=True)
-
-    ad = astrodata.open(_input)
-
-    if 'GMOS' in ad.tags:
-        from geminidr.gmos import primitives_gmos_spect
-        p = primitives_gmos_spect.GMOSSpect([ad])
-    else:
-        pytest.fail("Input data tags do not match expected tags for tests.\n"
-                    "    {}".format(ad.tags))
-
-    p = prepare_for_wavelength_calibration(p)
-    p.determineWavelengthSolution()
-    p.determineDistortion()
-    p.writeOutputs(outfilename=_output, overwrite=True)
-
-    output_ad = astrodata.open(_output)
-    reference_ad = astrodata.open(_reference)
-
-    ad_test.assert_same_class(output_ad, reference_ad)
-    ad_test.assert_have_same_distortion(output_ad, reference_ad)
-
-
-@pytest.mark.parametrize("input_file", ["GMOS/GN-2017A-FT-19/N20170530S0006.fits"])
-def test_store_processed_arc(input_file, path_to_inputs, path_to_outputs, path_to_refs):
-    """
-    Regression test for store processed arcs.
-
-    If all assert tests pass, the outputs overwrite the reference.
-
-    Parameters
-    ----------
-    input_file : str
-        Sub-directory and file name of the input file provided by `parametrize`.
-
-    path_to_inputs : str
-        Path to input data provided by fixtures and defined in `conftest.py`.
-
-    path_to_outputs : str
-        Path to output data provided by fixtures and defined in `conftest.py`.
-
-    path_to_refs : str
-        Path to reference data provided by fixtures and defined in `conftest.py`.
-    """
-    assert True
+        ad_test.assert_same_class(output_ad, reference_ad)
+        ad_test.assert_have_same_distortion(output_ad, reference_ad)
 
 
 if __name__ == '__main__':
