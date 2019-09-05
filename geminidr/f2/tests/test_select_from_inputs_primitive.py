2bfc866a0528492ec3a0052e625b8e61b0fc00e0
bquint <bquint@gemini.edu>
2019-09-05 10:16:03 -0400

    Removed try/except that was protecting RecursionError

--- geminidr/f2/tests/test_select_from_inputs_primitive.py
+++ geminidr/f2/tests/test_select_from_inputs_primitive.py
@@ -22,20 +22,16 @@ def test_select_from_inputs_primitive(path_to_inputs):
             p.prepare()
             p.addVAR(read_noise=True)
 
-            try:
+            print('Running tests on file: {:s}'.format(f))
+            p.selectFromInputs(tags="DARK", outstream="darks")
+            p.showInputs(stream="darks")
+            p.showInputs()
 
-                print('Running tests on file: {:s}'.format(f))
-                p.selectFromInputs(tags="DARK", outstream="darks")
-                p.showInputs(stream="darks")
-                p.showInputs()
+            print("{:15s} OK".format(f))
+            _log.write("\n{:15s} OK".format(f))
 
-                print("{:15s} OK".format(f))
-                _log.write("\n{:15s} OK".format(f))
-
-            except RecursionError as re:
-
-                print("{:15s} FAIL".format(f))
-                _log.write("\n{:15s} FAIL".format(f))
+            print("{:15s} FAIL".format(f))
+            _log.write("\n{:15s} FAIL".format(f))
 
             del ad
             del p
