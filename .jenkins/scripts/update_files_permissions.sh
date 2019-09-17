a900cc844951901b7b743ea2a51b6ec679498194
bquint <bquint@gemini.edu>
2019-09-17 15:26:09 -0300

    Display folder name in `update_files_permissions`

--- .jenkins/scripts/update_files_permissions.sh
+++ .jenkins/scripts/update_files_permissions.sh
@@ -2,6 +2,12 @@
 # Updates the files permissions for refs/ and outputs/
 
 chmod -R 775 reports/ || echo 1
+
+echo "Update permissions inside ${DRAGONS_TEST_INPUTS} ---"
 cd $DRAGONS_TEST_INPUTS; chmod -Rv 775 ./* || echo 1
+
+echo "Update permissions inside ${DRAGONS_TEST_OUTPUTS} ---"
 cd $DRAGONS_TEST_OUTPUTS; chmod -Rv 775 ./* || echo 1
+
+echo "Update permissions inside ${DRAGONS_TEST_REFS} ---"
 cd $DRAGONS_TEST_REFS; chmod -Rv 775 ./* || echo 1
