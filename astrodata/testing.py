bc39ce9bd9aff63e1580b4a019660648720867ee
bquint <bquint@gemini.edu>
2019-10-04 09:49:14 -0300

    Refactored astrodata.testing

--- astrodata/testing.py
+++ astrodata/testing.py
@@ -8,104 +8,31 @@ import pytest
 import warnings
 
 
-@pytest.fixture(scope='module')
-def path_to_inputs():
+def assert_have_same_distortion(ad, ad_ref):
     """
-    PyTest fixture that reads the environment variable $DRAGONS_TEST_INPUTS that
-    should contains input data for testing.
+    Checks if two :class:`~astrodata.AstroData` (or any subclass) have the
+    same distortion.
 
-    If the environment variable does not exist, it marks the test to be skipped.
-
-    If the environment variable exists but it not accessible, it also marks the
-    test to be skipped.
-
-    The skip reason changes depending on which situation causes it to be skipped.
-
-    Returns
-    -------
-        str : path to the input data
+    Parameters
+    ----------
+        ad : :class:`astrodata.AstroData`
+            AstroData object to be checked.
+        ad_ref : :class:`astrodata.AstroData`
+            AstroData object used as reference
     """
+    from gempy.library.astromodels import dict_to_chebyshev
+    from numpy.testing import assert_allclose
 
-    try:
-        path = os.path.expanduser(os.environ['DRAGONS_TEST_INPUTS'])
-        path = path.strip()
-    except KeyError:
-        pytest.skip(
-            "Could not find environment variable: $DRAGONS_TEST_INPUTS")
+    for ext, ext_ref in zip(ad, ad_ref):
 
-    if not os.path.exists(path):
-        pytest.skip(
-            "Could not access path stored in $DRAGONS_TEST_INPUTS: "
-            "{}".format(path)
-        )
+        distortion = dict(zip(ext.FITCOORD["name"], ext.FITCOORD["coefficients"]))
+        distortion = dict_to_chebyshev(distortion)
 
-    return path
+        distortion_ref = dict(zip(ext.FITCOORD["name"], ext.FITCOORD["coefficients"]))
+        distortion_ref = dict_to_chebyshev(distortion_ref)
 
-
-@pytest.fixture(scope='module')
-def path_to_refs():
-    """
-    PyTest fixture that reads the environment variable $DRAGONS_TEST_REFS that
-    should contains reference data for testing.
-
-    If the environment variable does not exist, it marks the test to be skipped.
-
-    If the environment variable exists but it not accessible, it also marks the
-    test to be skipped.
-
-    The skip reason changes depending on which situation causes it to be skipped.
-
-    Returns
-    -------
-        str : path to the reference data
-    """
-    try:
-        path = os.path.expanduser(os.environ['DRAGONS_TEST_REFS'])
-    except KeyError:
-        pytest.skip(
-            "Could not find environment variable: $DRAGONS_TEST_REFS")
-
-    if not os.path.exists(path):
-        pytest.skip(
-            "Could not access path stored in $DRAGONS_TEST_REFS: "
-            "{}".format(path)
-        )
-
-    return path
-
-
-@pytest.fixture(scope='module')
-def path_to_outputs():
-    """
-    PyTest fixture that reads the environment variable $DRAGONS_TEST_OUTPUTS
-    where output data will be stored during the tests.
-
-    If the environment variable does not exist, it marks the test to be skipped.
-
-    If the environment variable exists but it not accessible, it also marks the
-    test to be skipped.
-
-    The skip reason changes depending on which situation causes it to be skipped.
-
-    Returns
-    -------
-        str : path to the output data
-    """
-    try:
-        path = os.path.expanduser(os.environ['DRAGONS_TEST_OUTPUTS'])
-    except KeyError:
-        warnings.warn("Could not find environment variable: $DRAGONS_TEST_OUTPUTS"
-                      "\n Using current working directory")
-        path = os.getcwd()
-
-    if not os.path.exists(path):
-        warnings.warn(
-            "Could not access path stored in $DRAGONS_TEST_OUTPUTS: "
-            "{}".format(path) +
-            "\n Using current working directory")
-        path = os.getcwd()
-
-    return path
+        assert isinstance(distortion, type(distortion_ref))
+        assert_allclose(distortion.parameters, distortion_ref.parameters)
 
 
 def assert_same_class(ad, ad_ref):
@@ -156,29 +83,103 @@ def assert_wavelength_solutions_are_close(ad, ad_ref):
         assert_allclose(wcal.parameters, wcal_ref.parameters)
 
 
-def assert_have_same_distortion(ad, ad_ref):
+@pytest.fixture(scope='module')
+def path_to_inputs():
     """
-    Checks if two :class:`~astrodata.AstroData` (or any subclass) have the
-    same distortion.
+    PyTest fixture that reads the environment variable $DRAGONS_TEST_INPUTS that
+    should contains input data for testing.
 
-    Parameters
-    ----------
-        ad : :class:`astrodata.AstroData`
-            AstroData object to be checked.
-        ad_ref : :class:`astrodata.AstroData`
-            AstroData object used as reference
+    If the environment variable does not exist, it marks the test to be skipped.
+
+    If the environment variable exists but it not accessible, it also marks the
+    test to be skipped.
+
+    The skip reason changes depending on which situation causes it to be skipped.
+
+    Returns
+    -------
+        str : path to the input data
     """
-    from gempy.library.astromodels import dict_to_chebyshev
-    from numpy.testing import assert_allclose
 
-    for ext, ext_ref in zip(ad, ad_ref):
+    try:
+        path = os.path.expanduser(os.environ['DRAGONS_TEST_INPUTS'])
+        path = path.strip()
+    except KeyError:
+        pytest.skip(
+            "Could not find environment variable: $DRAGONS_TEST_INPUTS")
 
-        distortion = dict(zip(ext.FITCOORD["name"], ext.FITCOORD["coefficients"]))
-        distortion = dict_to_chebyshev(distortion)
+    # noinspection PyUnboundLocalVariable
+    if not os.path.exists(path):
+        pytest.skip(
+            "Could not access path stored in $DRAGONS_TEST_INPUTS: "
+            "{}".format(path)
+        )
 
-        distortion_ref = dict(zip(ext.FITCOORD["name"], ext.FITCOORD["coefficients"]))
-        distortion_ref = dict_to_chebyshev(distortion_ref)
+    return path
 
-        assert isinstance(distortion, type(distortion_ref))
-        assert_allclose(distortion.parameters, distortion_ref.parameters)
 
+@pytest.fixture(scope='module')
+def path_to_refs():
+    """
+    PyTest fixture that reads the environment variable $DRAGONS_TEST_REFS that
+    should contains reference data for testing.
+
+    If the environment variable does not exist, it marks the test to be skipped.
+
+    If the environment variable exists but it not accessible, it also marks the
+    test to be skipped.
+
+    The skip reason changes depending on which situation causes it to be skipped.
+
+    Returns
+    -------
+        str : path to the reference data
+    """
+    try:
+        path = os.path.expanduser(os.environ['DRAGONS_TEST_REFS'])
+    except KeyError:
+        pytest.skip(
+            "Could not find environment variable: $DRAGONS_TEST_REFS")
+
+    # noinspection PyUnboundLocalVariable
+    if not os.path.exists(path):
+        pytest.skip(
+            "Could not access path stored in $DRAGONS_TEST_REFS: "
+            "{}".format(path)
+        )
+
+    return path
+
+
+@pytest.fixture(scope='module')
+def path_to_outputs():
+    """
+    PyTest fixture that reads the environment variable $DRAGONS_TEST_OUTPUTS
+    where output data will be stored during the tests.
+
+    If the environment variable does not exist, it marks the test to be skipped.
+
+    If the environment variable exists but it not accessible, it also marks the
+    test to be skipped.
+
+    The skip reason changes depending on which situation causes it to be skipped.
+
+    Returns
+    -------
+        str : path to the output data
+    """
+    try:
+        path = os.path.expanduser(os.environ['DRAGONS_TEST_OUTPUTS'])
+    except KeyError:
+        warnings.warn("Could not find environment variable: $DRAGONS_TEST_OUTPUTS"
+                      "\n Using current working directory")
+        path = os.getcwd()
+
+    if not os.path.exists(path):
+        warnings.warn(
+            "Could not access path stored in $DRAGONS_TEST_OUTPUTS: "
+            "{}".format(path) +
+            "\n Using current working directory")
+        path = os.getcwd()
+
+    return path
