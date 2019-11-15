ca3b738d04cbf37f03d5d22fe415975cec286d1c
bquint <bquint@gemini.edu>
2019-11-15 17:35:49 -0300

    Moved tests to core/tests

--- geminidr/core/tests/test_spect.py
+++ geminidr/core/tests/test_spect.py
@@ -2,20 +2,45 @@
 """
 Tests applied to primitives_spect.py
 
+Notes
+-----
+
+    For extraction tests, your input wants to be a 2D image with an `APERTURE`
+    table attached. You'll see what happens if you take a spectrophotometric
+    standard and run it through the standard reduction recipe, but the
+    `APERTURE` table has one row per aperture with the following columns:
+
+    - number : sequential list of aperture number
+
+    - ndim, degree, domain_start, domain_end, c0, [c1, c2, c3...] : standard
+    Chebyshev1D definition of the aperture centre (in pixels) as a function of
+    pixel in the dispersion direction
+
+    - aper_lower : location of bottom of aperture relative to centre (always
+    negative)
+
+    - aper_upper : location of top of aperture relative to centre (always
+    positive)
+
+    The ndim column will always be 1 since it's always 1D Chebyshev, but the
+    `model_to_dict()` and `dict_to_model()` functions that convert the Model
+    instance to a dict create/require this.
 """
 import pytest
-import os
 import numpy as np
+
+from astropy import table
+from astropy.io import fits
 from scipy import optimize
 
-import astrodata
-import astrodata.testing as ad_test
-import geminidr
+from geminidr.core import primitives_spect
 
-# noinspection PyUnresolvedReferences
-import gemini_instruments
+try:
+    import astrofaker
 
-from gempy.utils import logutils
+    HAS_ASTROFAKER = True
+except ImportError:
+    HAS_ASTROFAKER = False
 
 
 # noinspection PyPep8Naming
@@ -54,6 +79,108 @@ def test_QESpline_optimization():
     )
 
     np.testing.assert_allclose(real_coeffs, 1./result.x, atol=0.01)
+
+
+@pytest.mark.skipif("not HAS_ASTROFAKER")
+def test_find_apertures():
+    data = np.zeros((100, 200))
+    data[50] = 10.
+
+    hdu = fits.ImageHDU()
+    hdu.header['CCDSUM'] = "1 1"
+    hdu.data = data
+
+    ad = astrofaker.create('GMOS-S')
+    ad.add_extension(hdu, pixel_scale=1.0)
+
+    _p = primitives_spect.Spect([])
+    _p.findSourceApertures(ad)
+
+
+@pytest.mark.skipif("not HAS_ASTROFAKER")
+def test_sky_correct_from_slit():
+    data = np.zeros((100, 200))
+    data[50] = 100.
+
+    n_lines = 50
+    sky_lines = np.random.randint(low=0, high=data.shape[1], size=n_lines)
+    sky_intensity = 30. * np.random.random(size=n_lines)
+
+    data[:, sky_lines] = sky_intensity
+
+    aperture = table.Table(
+        [[1],  # Number
+         [1],  # ndim
+         [0],  # degree
+         [0],  # domain_start
+         [data.shape[1] - 1],  # domain_end
+         [50],  # c0
+         [-3],  # aper_lower
+         [3],  # aper_upper
+         ],
+        names=[
+            'number',
+            'ndim',
+            'degree',
+            'domain_start',
+            'domain_end',
+            'c0',
+            'aper_lower',
+            'aper_upper'],
+    )
+
+    hdu = fits.ImageHDU()
+    hdu.header['CCDSUM'] = "1 1"
+    hdu.data = data
+
+    ad = astrofaker.create('GMOS-S')
+    ad.add_extension(hdu, pixel_scale=1.0)
+    ad[0].APERTURE = aperture
+
+    _p = primitives_spect.Spect([])
+    ade = _p.skyCorrectFromSlit(ad)[0]
+
+
+@pytest.mark.skipif("not HAS_ASTROFAKER")
+def test_extract_1d_spectra():
+
+    data = np.zeros((100, 200))
+    data[50] = 10.
+
+    aperture = table.Table(
+        [[1],  # Number
+         [1],  # ndim
+         [0],  # degree
+         [0],  # domain_start
+         [data.shape[1] - 1],  # domain_end
+         [50],  # c0
+         [-3],  # aper_lower
+         [3],  # aper_upper
+         ],
+        names=[
+            'number',
+            'ndim',
+            'degree',
+            'domain_start',
+            'domain_end',
+            'c0',
+            'aper_lower',
+            'aper_upper'],
+    )
+
+    hdu = fits.ImageHDU()
+    hdu.header['CCDSUM'] = "1 1"
+    hdu.data = data
+
+    ad = astrofaker.create('GMOS-S')
+    ad.add_extension(hdu, pixel_scale=1.0)
+    ad[0].APERTURE = aperture
+
+    _p = primitives_spect.Spect([])
+    ade = _p.extract1DSpectra(ad)[0]
+
+    np.testing.assert_equal(ade[0].shape[0], data.shape[1])
+    np.testing.assert_equal(ade[0].data, data[50])
 
 
 if __name__ == '__main__':
--- geminidr/gmos/tests/test_gmos_spect_longslit.py
+++ geminidr/gmos/tests/test_gmos_spect_longslit.py
@@ -1,55 +1,10 @@
 #!/usr/bin/env python
 """
-Tests for GMOS Spect LS Extraction.
-
-Notes
------
-
-    For extraction tests, your input wants to be a 2D image with an `APERTURE`
-    table attached. You'll see what happens if you take a spectrophotometric
-    standard and run it through the standard reduction recipe, but the
-    `APERTURE` table has one row per aperture with the following columns:
-
-    - number : sequential list of aperture number
-
-    - ndim, degree, domain_start, domain_end, c0, [c1, c2, c3...] : standard
-    Chebyshev1D definition of the aperture centre (in pixels) as a function of
-    pixel in the dispersion direction
-
-    - aper_lower : location of bottom of aperture relative to centre (always
-    negative)
-
-    - aper_upper : location of top of aperture relative to centre (always
-    positive)
-
-    The ndim column will always be 1 since it's always 1D Chebyshev, but the
-    `model_to_dict()` and `dict_to_model()` functions that convert the Model
-    instance to a dict create/require this.
+Tests for GMOS Spect LS primitives.
 """
 
-import os
 import pytest
 
-import astrofaker
-import astrodata
-import geminidr
-import numpy as np
-
-from astropy import table
-from astropy.io import fits
-from astrodata import testing
-from gempy.adlibrary import dataselect
-from gempy.utils import logutils
-from geminidr.gmos import primitives_gmos_spect
-from recipe_system.reduction.coreReduce import Reduce
-from recipe_system.utils.reduce_utils import normalize_ucals
-
-try:
-    import astrofaker
-
-    HAS_ASTROFAKER = True
-except ImportError:
-    HAS_ASTROFAKER = False
 
 # ToDo @bquint: These files are not used for now but I am keeping them for future regression tests
 test_cases = [
@@ -74,106 +29,6 @@ test_cases = [
     ]),
 
 ]
-
-
-@pytest.mark.skipif("not HAS_ASTROFAKER")
-def test_find_apertures():
-
-    data = np.zeros((100, 200))
-    data[50] = 10.
-
-    hdu = fits.ImageHDU()
-    hdu.header['CCDSUM'] = "1 1"
-    hdu.data = data
-
-    ad = astrofaker.create('GMOS-S')
-    ad.add_extension(hdu, pixel_scale=1.0)
-
-    _p = primitives_gmos_spect.GMOSSpect([ad])
-    _p.findSourceApertures()
-
-
-@pytest.mark.skipif("not HAS_ASTROFAKER")
-def test_sky_correct_from_slit():
-
-    data = np.zeros((100, 200))
-    data[50] = 10.
-
-    aperture = table.Table(
-        [[1],  # Number
-         [1],  # ndim
-         [0],  # degree
-         [0],  # domain_start
-         [data.shape[1] - 1],  # domain_end
-         [50],  # c0
-         [-3],  # aper_lower
-         [3],  # aper_upper
-         ],
-        names=[
-            'number',
-            'ndim',
-            'degree',
-            'domain_start',
-            'domain_end',
-            'c0',
-            'aper_lower',
-            'aper_upper'],
-    )
-
-    hdu = fits.ImageHDU()
-    hdu.header['CCDSUM'] = "1 1"
-    hdu.data = data
-
-    ad = astrofaker.create('GMOS-S')
-    ad.add_extension(hdu, pixel_scale=1.0)
-    ad[0].APERTURE = aperture
-
-    _p = primitives_gmos_spect.GMOSSpect([])
-    ade = _p.skyCorrectFromSlit(ad)[0]
-
-
-@pytest.mark.skipif("not HAS_ASTROFAKER")
-def test_extract_1d_spectra():
-
-    data = np.zeros((100, 200))
-    data[50] = 10.
-
-    aperture = table.Table(
-        [[1],  # Number
-         [1],  # ndim
-         [0],  # degree
-         [0],  # domain_start
-         [data.shape[1] - 1],  # domain_end
-         [50],  # c0
-         [-3],  # aper_lower
-         [3],  # aper_upper
-         ],
-        names=[
-            'number',
-            'ndim',
-            'degree',
-            'domain_start',
-            'domain_end',
-            'c0',
-            'aper_lower',
-            'aper_upper'],
-    )
-
-    hdu = fits.ImageHDU()
-    hdu.header['CCDSUM'] = "1 1"
-    hdu.data = data
-
-    ad = astrofaker.create('GMOS-S')
-    ad.add_extension(hdu, pixel_scale=1.0)
-    ad[0].APERTURE = aperture
-
-    _p = primitives_gmos_spect.GMOSSpect([])
-    ade = _p.extract1DSpectra(ad)[0]
-
-    np.testing.assert_equal(ade[0].shape[0], data.shape[1])
-    np.testing.assert_equal(ade[0].data, data[50])
-
-
 
 
 if __name__ == '__main__':
