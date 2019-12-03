210f2e2303c58775503f04dab80186a4abfb7652
bquint <bquint@gemini.edu>
2019-12-03 16:09:40 -0300

    Added test_estimate_peak_width_with_one_line_with_noise

--- gempy/library/tests/test_tracing.py
+++ gempy/library/tests/test_tracing.py
@@ -25,6 +25,26 @@ def test_estimate_peak_width_with_one_line(x0, fwhm):
     np.testing.assert_allclose(fwhm, measured_fwhm, rtol=0.10)
 
 
+@pytest.mark.parametrize("noise", np.arange(0, 0.5, 0.1))
+def test_estimate_peak_width_with_one_line_with_noise(noise):
+
+    x0 = 1600
+    fwhm = 4
+
+    stddev_to_fwhm = 2 * np.sqrt(2 * np.log(2))
+    stddev = fwhm / stddev_to_fwhm
+
+    x = np.arange(0, 3200)
+    g = models.Gaussian1D(mean=x0, stddev=stddev)
+
+    np.random.seed(0)
+    y = g(x) + noise * (np.random.rand(x.size) - 0.5)
+
+    measured_fwhm = tracing.estimate_peak_width(y)
+
+    np.testing.assert_allclose(fwhm, measured_fwhm, rtol=0.10 + 0.4 * noise)
+
+
 def test_find_peaks():
     x = np.arange(0, 3200)
     y = np.zeros_like(x, dtype=float)
