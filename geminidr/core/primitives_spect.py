72cda4e5bc8492c50414a1c0c2215ac7bb48bc18
bquint <bquint@gemini.edu>
2020-01-08 12:14:13 -0300

    changed determineWavelengthSolution to inspect failing tests

--- geminidr/core/primitives_spect.py
+++ geminidr/core/primitives_spect.py
@@ -803,7 +803,11 @@ class Spect(PrimitivesBASE):
                     # Set some bounds; this may need to be abstracted for
                     # different instruments? TODO
                     dw = abs(2 * m_init.c1 / np.diff(m_init.domain)[0])
-                    c0_unc = 0.02 * cenwave
+
+                    # FixMe - changed line below to inspect failing tests
+                    c0_unc = 0.05 * cenwave
+                    # c0_unc = 0.02 * cenwave
+
                     m_init.c0.bounds = (m_init.c0 - c0_unc, m_init.c0 + c0_unc)
                     c1_unc = 0.005 * abs(m_init.c1)
                     m_init.c1.bounds = tuple(sorted([m_init.c1 - c1_unc, m_init.c1 + c1_unc]))
