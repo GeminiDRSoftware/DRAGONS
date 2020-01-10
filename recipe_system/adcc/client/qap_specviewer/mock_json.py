c1c0971dc282cf8d6dcdad31b8f7238432659c34
bquint <bquint@gemini.edu>
2020-01-10 16:24:54 -0300

    Updated mock_json to return wavelength, intensity and variance

--- recipe_system/adcc/client/qap_specviewer/mock_json.py
+++ recipe_system/adcc/client/qap_specviewer/mock_json.py
@@ -9,38 +9,78 @@ development.
 
 import json
 
+import numpy as np
+
 
 def main():
-    filename = "data.json"
+    # Create fake source
+    np.random.seed(0)
 
-    data = {
-        "filename": "N20001231S001_suffix.fits",
-        "programId": "GX-2000C-Q-001",
-    }
+    image_width = 4000
+    image_height = 2000
 
-    apertures = [
-        {
-            "center": 515,
-            "lower": -5,
-            "upper": +10,
-            "dispersion": 0.15,
-        }, {
-            "center": 123,
-            "lower": -7,
-            "upper": +15,
-            "dispersion": 0.15,
-        }, {
-            "center": 321,
-            "lower": -14,
-            "upper": +12,
-            "dispersion": 0.15,
-        },
-    ]
+    obj_max_weight = 300.
+    obj_continnum = 300. + 0.01 * np.arange(image_width)
 
+    # Create aperture data
+    apertures = []
+    for i in range(3):
+        center = np.random.randint(100, image_height-100)
+        lower = np.random.randint(-15, -1)
+        upper = np.random.randint(1, 15)
+        dispersion = 0.15
+        intensity = create_1d_spectrum(
+            image_width,
+            int(0.1 * image_width), obj_max_weight) + obj_continnum
+        variance = np.sqrt(intensity)
+        wavelength = np.arange(image_width) * dispersion + 4000.
+
+        apertures.append(
+            {
+                "center": center,
+                "lower": lower,
+                "upper": upper,
+                "dispersion": dispersion,
+                "wavelength": list(wavelength),
+                "intensity": list(intensity),
+                "variance": list(variance),
+             }
+        )
+
+    # Create dict with all the data
+    data = dict(filename="N20001231S001_suffix.fits", programId="GX-2000C-Q-001")
     data["apertures"] = apertures
 
+    filename = "data.json"
     with open(filename, 'w') as json_file:
         json.dump(data, json_file)
+
+
+def create_1d_spectrum(width, n_lines, max_weight):
+    """
+    Generates a 1D NDArray with the sky spectrum.
+
+    Parameters
+    ----------
+    width : int
+        Number of array elements.
+    n_lines : int
+        Number of artificial lines.
+    max_weight : float
+        Maximum weight (or flux, or intensity) of the lines.
+
+    Returns
+    -------
+    sky_1d_spectrum : numpy.ndarray
+
+    """
+    lines = np.random.randint(low=0, high=width, size=n_lines)
+    weights = max_weight * np.random.random(size=n_lines)
+
+    spectrum = np.zeros(width)
+    spectrum[lines] = weights
+
+    return spectrum
 
 
 if __name__ == '__main__':
