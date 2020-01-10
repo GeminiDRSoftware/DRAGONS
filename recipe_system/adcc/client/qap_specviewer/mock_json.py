4d84b76d1ad788213df9f15cdfbda0107a96a96e
bquint <bquint@gemini.edu>
2020-01-10 12:53:13 -0300

    Updated mock_json to return several apertures

--- recipe_system/adcc/client/qap_specviewer/mock_json.py
+++ recipe_system/adcc/client/qap_specviewer/mock_json.py
@@ -14,10 +14,30 @@ def main():
     filename = "data.json"
 
     data = {
-        "apertures": 3,
         "filename": "N20001231S001_suffix.fits",
         "programId": "GX-2000C-Q-001",
     }
+
+    apertures = [
+        {
+            "center": 515,
+            "lower": -5,
+            "upper": +10,
+            "dispersion": 0.15,
+        }, {
+            "center": 123,
+            "lower": -7,
+            "upper": +15,
+            "dispersion": 0.15,
+        }, {
+            "center": 321,
+            "lower": -14,
+            "upper": +12,
+            "dispersion": 0.15,
+        },
+    ]
+
+    data["apertures"] = apertures
 
     with open(filename, 'w') as json_file:
         json.dump(data, json_file)
