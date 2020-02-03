2f114b565c7a59dfad4d41eb703aa7c2027e1e3e
bquint <bquint@gemini.edu>
2020-02-03 13:08:41 -0300

    wip: now page refreshes when getting new data and display aperture info

--- recipe_system/adcc/client/qap_specviewer/js/specviewer.js
+++ recipe_system/adcc/client/qap_specviewer/js/specviewer.js
@@ -13,22 +13,23 @@ const specViewerJsonName = "/specqueue.json";
  * @param {string} id - name of the ID of the SpecViewer div container.
  */
 function SpecViewer(parentElement, id) {
-    'use strict';
+  'use strict';
 
-    // Creating empty object
-    this.parentElement = parentElement;
-    this.id = id;
+  // Creating empty object
+  this.parentElement = parentElement;
+  this.id = id;
 
-    // Placeholders for different elements
-    this.activeTab = null;
-    this.framePlots = [];
-    this.stackPlots = [];
+  // Placeholders for different elements
+  this.activeTab = null;
+  this.framePlots = [];
+  this.stackPlots = [];
 
-    this.dataLabel = null;
-    this.groupId = null;
+  this.aperturesCenter = [];
+  this.dataLabel = null;
+  this.groupId = null;
 
-    // Create empty page
-    this.parentElement.html(`
+  // Create empty page
+  this.parentElement.html(`
       <div id="${id}">
         <div class="loading">
           Waiting data from server ...
@@ -36,52 +37,21 @@ function SpecViewer(parentElement, id) {
       </div>
     `);
 
-    // Call function to activate the tabs
-    $(`#${id}`).tabs();
+  // Call function to activate the tabs
+  $(`#${id}`).tabs();
 
-    // Placeholder for adcc command pump
-    this.gjs = null;
-    this.start();
+  // Placeholder for adcc command pump
+  this.gjs = null;
+  this.start();
 
 }
-    //this.specPump = new SpectroPump();
+//this.specPump = new SpectroPump();
 //} // end SpecViewer
 
 // Add methods to prototype
 SpecViewer.prototype = {
 
-    constructor: SpecViewer,
-
-    start: function() {
-
-        console.log("Starting SpecViewer");
-
-     	// Make an AJAX request to the server for the current
-     	// time and the server site information.
-     	// The callback for this request will call the init function
-     	var sv = this;
-
-     	$.ajax(
-          {
-            type: "GET",
-     		url: "/rqsite.json",
-     		success: function (data) {
-     		    sv.site = data.local_site;
-		    sv.timestamp = data.unxtime;
-     		}, // end success
-     		error: function() {
-     		    sv.site = undefined;
-     		    sv.tzname = "LT";
-     		    sv.timestamp = new Date();
-     		} // end error
-          }
-        ); // end ajax
-
-        this.gjs = new GJSCommandPipe();
-        this.gjs.registerCallback("specjson", function(msg){sv.loadData(msg);});
-        this.gjs.startPump(sv.timestamp, "specjson");
-
-    }, // end start
+  constructor: SpecViewer,
 
   /**
    * Add navigation tabs based on how many apertures there is inside the
@@ -157,9 +127,11 @@ SpecViewer.prototype = {
     };
 
     // Call function to activate the tabs
-    $( `#${parentId}` ).tabs('refresh');
-    $( `#${parentId}` ).tabs('option', 'active', 0);
-    $( `#${parentId}` ).tabs( {'activate': selectTab} );
+    $(`#${parentId}`).tabs('refresh');
+    $(`#${parentId}`).tabs('option', 'active', 0);
+    $(`#${parentId}`).tabs({
+      'activate': selectTab
+    });
 
     // Allow plot area to be resized
     $('.ui-widget-content.resizable:has(.framePlot)').map(
@@ -181,7 +153,7 @@ SpecViewer.prototype = {
 
       });
 
-        $('.ui-widget-content.resizable:has(.stackPlot)').map(
+    $('.ui-widget-content.resizable:has(.stackPlot)').map(
       function onResizeStackStop(index, element) {
 
         $(element).resizable({
@@ -226,46 +198,40 @@ SpecViewer.prototype = {
   },
 
   /**
-   * Resizes frame plots on different situations, like window resizing or
-   * when changing tabs.
-   *
-   * @param activeTabIndex {number}
+   * Add/refresh tab content for incomming data.
+   * @type {object} data Incomming JSON data
    */
-  resizeFramePlots: function (activeTabIndex) {
+  addTabContent: function(data) {
 
-    console.log(`Resizing frame plot ${activeTabIndex}`);
+    for (let i = 0; i < data.apertures.length; i++) {
 
-    $(`framePlot${activeTabIndex}`).height(
-      $(`framePlot${activeTabIndex}-resizable`).height() * 0.96
-    );
+      let aperture = data.apertures[i];
 
-    $(`framePlot${activeTabIndex}`).width(
-      $(`framePlot${activeTabIndex}-resizable`).width() * 0.96
-    );
+      console.log(`Adding content for aperture at ${aperture.center} px`);
+      console.log(this.aperturesCenter)
 
-    this.framePlots[activeTabIndex].replot({ resetAxes: true });
+      // Add empty tab container
+      if (!this.aperturesCenter.includes(aperture.center)) {
+        $(`#${this.id}`).append(
+          `<div id="aperture${aperture.center}" class="tabcontent"></div>`
+        );
+      }
 
-  },
+      $(`#aperture${aperture.center}`).html(`
+          <div class="apertureInfo">
+            <span>
+              <b>Aperture definition:</b>
+                <span class="app-info-field" title="Aperture center"> ${aperture.center} px </span> (
+                <span class="app-info-field" title="Lower aperture limit"> ${aperture.lower} px </span>,
+                <span class="app-info-field" title="Upper aperture limit"> ${aperture.upper} px </span>)
+            </span>
+            <span style="padding-left: 10%">
+              <b>Dispersion:</b> ${aperture.dispersion} nm/px
+            </span>
+          </div>
+        `);
 
-  /**
-   * Resizes frame plots on different situations, like window resizing or
-   * when changing tabs.
-   *
-   * @param activeTabIndex {number}
-   */
-  resizeStackPlots: function (activeTabIndex) {
-
-    console.log(`Resizing stack plot ${activeTabIndex}`);
-
-    $(`stackPlot${activeTabIndex}`).height(
-      $(`stackPlot${activeTabIndex}-resizable`).height() * 0.96
-    );
-
-    $(`stackPlot${activeTabIndex}`).width(
-      $(`stackPlot${activeTabIndex}-resizable`).width() * 0.96
-    );
-
-    this.stackPlots[activeTabIndex].replot({ resetAxes: true });
+    }
 
   },
 
@@ -346,10 +312,11 @@ SpecViewer.prototype = {
 
   }, // end addTabs
 
-
   /**
    * Query server for JSON file and start to populate page.
    * This function is the registered callback on the command pump.
+   *
+   * @param jsonData {object}
    */
   loadData: function(jsonData) {
     'use restrict';
@@ -362,6 +329,7 @@ SpecViewer.prototype = {
     $('.loading').remove();
 
     for (let i = 0; i < jsonData.length; i++) {
+
       if (jsonData[i].data_label === this.dataLabel) {
         console.log(`- OLD data label: ${jsonData[i].data_label}`);
       } else {
@@ -374,31 +342,135 @@ SpecViewer.prototype = {
         if (jsonElement.group_id === this.groupId) {
           console.log(`- MATCHING group id: ${jsonElement.group_id}`);
         } else {
+
           console.log(`- NEW group id: ${jsonElement.group_id}`);
           this.groupId = jsonElement.group_id;
+
+          // Clear navigation tabs
+          $(`#${this.id} ul`).empty();
+
+          // Clear tab contents
+          $('.tabcontent').remove();
+
+          // Add tab content
+          this.addTabContent(jsonElement);
+
         }
 
       }
     }
 
     // Reference to self to use in functions inside load
-//	var sViewer = this;
-//	var jsonData = sdata;
-//	var data = JSON.stringify(jsonData);
+    //	var sViewer = this;
+    //	var jsonData = sdata;
+    //	var data = JSON.stringify(jsonData);
 
-//  sViewer.addNavigationTab(sViewer.id, data.apertures.length);
-//	sViewer.addTabs(sViewer.id, data);
-//	sViewer.addPlots(sViewer.id, data);
+    //  sViewer.addNavigationTab(sViewer.id, data.apertures.length);
+    //	sViewer.addTabs(sViewer.id, data);
+    //	sViewer.addPlots(sViewer.id, data);
 
     // Remove loading
-	$('.loading').remove();
+    $('.loading').remove();
 
   }, // end load
+
+  /**
+   * Resizes frame plots on different situations, like window resizing or
+   * when changing tabs.
+   *
+   * @param activeTabIndex {number}
+   */
+  resizeFramePlots: function(activeTabIndex) {
+
+    console.log(`Resizing frame plot ${activeTabIndex}`);
+
+    $(`framePlot${activeTabIndex}`).height(
+      $(`framePlot${activeTabIndex}-resizable`).height() * 0.96
+    );
+
+    $(`framePlot${activeTabIndex}`).width(
+      $(`framePlot${activeTabIndex}-resizable`).width() * 0.96
+    );
+
+    this.framePlots[activeTabIndex].replot({
+      resetAxes: true
+    });
+
+  },
+
+  /**
+   * Resizes frame plots on different situations, like window resizing or
+   * when changing tabs.
+   *
+   * @param activeTabIndex {number}
+   */
+  resizeStackPlots: function(activeTabIndex) {
+
+    console.log(`Resizing stack plot ${activeTabIndex}`);
+
+    $(`stackPlot${activeTabIndex}`).height(
+      $(`stackPlot${activeTabIndex}-resizable`).height() * 0.96
+    );
+
+    $(`stackPlot${activeTabIndex}`).width(
+      $(`stackPlot${activeTabIndex}-resizable`).width() * 0.96
+    );
+
+    this.stackPlots[activeTabIndex].replot({
+      resetAxes: true
+    });
+
+  },
+
+  start: function() {
+
+    console.log("Starting SpecViewer");
+
+    // Make an AJAX request to the server for the current
+    // time and the server site information.
+    // The callback for this request will call the init function
+    var sv = this;
+
+    $.ajax({
+      type: "GET",
+      url: "/rqsite.json",
+      success: function(data) {
+        sv.site = data.local_site;
+        sv.timestamp = data.unxtime;
+      }, // end success
+      error: function() {
+        sv.site = undefined;
+        sv.tzname = "LT";
+        sv.timestamp = new Date();
+      } // end error
+    }); // end ajax
+
+    this.gjs = new GJSCommandPipe();
+    this.gjs.registerCallback("specjson", function(msg) {
+      sv.loadData(msg);
+    });
+    this.gjs.startPump(sv.timestamp, "specjson");
+
+  }, // end start
+
+  /**
+   * Updates the navigation tab based on matching aperture center.
+   *
+   * @param apertures {array} Aperture objects.
+   */
+  updateNavigationTab: function(apertures) {
+
+    // Add navigation tab container
+    let listOfTabs = $(`#${parentId} ul`);
+
+  }
 
 }; // end prototype
 
 
 /**
+ * @deprecated Now that input JSON file comes in the correct format.
+ *
  * Read two arrays and convert then into a single [x, y] array to be used in
  * plots.
  *
