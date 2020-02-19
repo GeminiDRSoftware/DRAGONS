c5da9e2023b2f7b9e71306f4bb582cdd8a0aef40
Oliver <oly@oberdorf.org>
2020-02-19 13:28:17 -1000

    fixed description of --upload to just add science

--- recipe_system/utils/reduce_utils.py
+++ recipe_system/utils/reduce_utils.py
@@ -152,7 +152,7 @@ def buildParser(version):
                         action=UnitaryArgumentAction, nargs="*",
                         help="Send these pipeline products to fitsstore."
                         "Default is None."
-                        "Eg., --upload metrics calibs qa ql sq")
+                        "Eg., --upload metrics calibs science")
 
     parser.add_argument("--user_cal", dest='user_cal', default=None,
                         nargs="*", action=CalibrationAction,
