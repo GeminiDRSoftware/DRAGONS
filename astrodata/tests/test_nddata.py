7fa94f6370b5feb8e0bf66e57a1d7d547d5eb470
Simon Conseil <sconseil@gemini.edu>
2020-02-27 17:31:29 -0300

    Add test for no warning with zeros

--- astrodata/tests/test_nddata.py
+++ astrodata/tests/test_nddata.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pytest
 
-from astrodata import nddata
+from astrodata.nddata import ADVarianceUncertainty
 
 
 def test_variance_uncertainty_warn_if_there_are_any_negative_numbers():
@@ -11,22 +11,29 @@ def test_variance_uncertainty_warn_if_there_are_any_negative_numbers():
     arr[2, 2] = -0.001
 
     with pytest.warns(RuntimeWarning, match='Negative variance values found.'):
-        result = nddata.ADVarianceUncertainty(arr)
+        result = ADVarianceUncertainty(arr)
 
     assert not np.all(arr >= 0)
-    assert isinstance(result, nddata.ADVarianceUncertainty)
+    assert isinstance(result, ADVarianceUncertainty)
     assert result.array[2, 2] == 0
 
     # check that it always works with a VarianceUncertainty instance
     result.array[2, 2] = -0.001
 
     with pytest.warns(RuntimeWarning, match='Negative variance values found.'):
-        result2 = nddata.ADVarianceUncertainty(result)
+        result2 = ADVarianceUncertainty(result)
 
     assert not np.all(arr >= 0)
     assert not np.all(result.array >= 0)
-    assert isinstance(result2, nddata.ADVarianceUncertainty)
+    assert isinstance(result2, ADVarianceUncertainty)
     assert result2.array[2, 2] == 0
+
+
+def test_new_variance_uncertainty_instance_no_warning_if_the_array_is_zeros():
+    arr = np.zeros((5, 5))
+    with pytest.warns(None) as w:
+        ADVarianceUncertainty(arr)
+    assert len(w) == 0
 
 
 if __name__ == '__main__':
