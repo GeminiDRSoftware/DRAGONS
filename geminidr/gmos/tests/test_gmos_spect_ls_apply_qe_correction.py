fe357d5f3b066ed28a94a965f19fa645869b38be
Bruno Quint <bquint@gemini.edu>
2020-03-20 12:30:34 -0300

    Created empty tests

--- geminidr/gmos/tests/test_gmos_spect_ls_apply_qe_correction.py
+++ geminidr/gmos/tests/test_gmos_spect_ls_apply_qe_correction.py
@@ -2,5 +2,24 @@
 
 import pytest
 
+# -- Tests --------------------------------------------------------------------
+@pytest.mark.gmosls
+@pytest.mark.dragons_remote_data
+def test_applied_qe_is_locally_continuous():
+    pass
+
+
+@pytest.mark.gmosls
+@pytest.mark.dragons_remote_data
+def test_applied_qe_is_globally_continuous():
+    pass
+
+
+@pytest.mark.gmosls
+@pytest.mark.dragons_remote_data
+def test_applied_qe_is_stable():
+    pass
+
+
 if __name__ == '__main__':
     pytest.main()
