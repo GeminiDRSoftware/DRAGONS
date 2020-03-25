3725ee2c8db0e7fed6f26c607142866437847559
Simon Conseil <sconseil@gemini.edu>
2020-03-25 14:21:23 -0300

    Mock download_file to avoid a new request each time

--- astrodata/tests/test_testing.py
+++ astrodata/tests/test_testing.py
@@ -21,14 +21,30 @@ def test_download_from_archive_raises_IOError_if_path_is_not_accessible():
         download_from_archive('N20180304S0126.fits', env_var=env_var)
 
 
-@pytest.mark.dragons_remote_data
-def test_download_from_archive(path_to_outputs):
-    fname = download_from_archive('N20170529S0168.fits', path=path_to_outputs)
+def test_download_from_archive(monkeypatch, tmpdir):
+
+    ncall = 0
+
+    def mock_download(remote_url, **kwargs):
+        nonlocal ncall
+        ncall += 1
+        fname = remote_url.split('/')[-1]
+        tmpdir.join(fname).write('')  # create fake file
+        return str(tmpdir.join(fname))
+
+    monkeypatch.setattr("astrodata.testing.download_file", mock_download)
+    monkeypatch.setenv("DRAGONS_TEST_INPUTS", str(tmpdir))
+
+    # first call will use our mock function above
+    fname = download_from_archive('N20170529S0168.fits', path='subdir')
     assert os.path.exists(fname)
-    # make sure that download_from_archive works when the file does not exists
-    os.remove(fname)
-    fname = download_from_archive('N20170529S0168.fits', path=path_to_outputs)
+    assert ncall == 1
+
+    # second call will use the cache so we check that our mock function is not
+    # called twice
+    fname = download_from_archive('N20170529S0168.fits', path='subdir')
     assert os.path.exists(fname)
+    assert ncall == 1
 
 
 def test_path_to_test_data(path_to_test_data):
