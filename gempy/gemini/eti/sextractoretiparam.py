c7e95b7fc5242c34b3eb2ad390cb871d4b95836f
Simon Conseil <sconseil@gemini.edu>
2020-03-31 11:29:33 -0300

    Fix issue with SExtractorETIParam signature

--- gempy/gemini/eti/sextractoretiparam.py
+++ gempy/gemini/eti/sextractoretiparam.py
@@ -8,6 +8,9 @@ __PARAMETERS_SET_INTERNALLY__ = ["FLAG_IMAGE",
 
 class SExtractorETIParam(ETIParam):
 
+    def __init__(self, params):
+        super().__init__(params=params)
+
     # Delete any parameters from the dict that need to be set interally
     def prepare(self):
         if self.params:
