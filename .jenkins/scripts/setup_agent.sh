6724857403abfe08162a5a6084262f6014aec478
Simon Conseil <sconseil@gemini.edu>
2020-04-08 11:00:15 -0400

    Cleanup previous test results

--- .jenkins/scripts/setup_agent.sh
+++ .jenkins/scripts/setup_agent.sh
@@ -3,7 +3,14 @@
 
 set -eux
 
+# Cleanup
 git clean -fxd
 mkdir plots reports
+if [[ -n "${DRAGONS_TEST_OUTPUTS}" ]]; then
+    echo "Cleaning previous test results in ${DRAGONS_TEST_OUTPUTS}"
+    rm -r ${DRAGONS_TEST_OUTPUTS}
+fi
+
 source .jenkins/scripts/download_and_install_anaconda.sh
+
 pip install tox tox-conda
