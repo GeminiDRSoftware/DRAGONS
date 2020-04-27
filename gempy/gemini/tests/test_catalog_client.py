5b32aa6f1ea551ea4bf60d1f08c9bd5b4cdfdc0c
Simon Conseil <sconseil@gemini.edu>
2020-04-27 17:25:00 -0400

    Reactivate gempy tests, except test_catalog_client

--- gempy/gemini/tests/test_catalog_client.py
+++ gempy/gemini/tests/test_catalog_client.py
@@ -7,13 +7,17 @@ This is a suite of tests to be run with pytest.
 To run:
     1) py.test -v --capture=no
 """
-from gempy.gemini.gemini_catalog_client import get_fits_table
-from astropy.coordinates import SkyCoord, Angle
+
+import pytest
+
 import astropy.units as u
+from astropy.coordinates import Angle, SkyCoord
+from gempy.gemini.gemini_catalog_client import get_fits_table
 
 
 # These just check that all sources are within the search radius
 # and that the number is the same as when it was written
+@pytest.mark.skip(reason='requires internal network access')
 def test_get_fits_table_2mass():
     ra = 180.0
     dec = 0.0
@@ -24,6 +28,8 @@ def test_get_fits_table_2mass():
     assert all(center.separation(SkyCoord(ret['RAJ2000'],
                     ret['DEJ2000'], unit='deg')) < Angle(radius * u.deg))
 
+
+@pytest.mark.skip(reason='requires internal network access')
 def test_get_fits_table_sdss9():
     ra = 160.0
     dec = 30.0
@@ -34,6 +40,8 @@ def test_get_fits_table_sdss9():
     assert all(center.separation(SkyCoord(ret['RAJ2000'],
                     ret['DEJ2000'], unit='deg')) < Angle(radius * u.deg))
 
+
+@pytest.mark.skip(reason='requires internal network access')
 def test_get_fits_table_gmos():
     ra = 200.0
     dec = -60.0
