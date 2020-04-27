bcc2bb785f22a23cc507eb1ec469e7045f6ff20b
Simon Conseil <sconseil@gemini.edu>
2020-04-27 10:57:17 -0400

    Add tests for NDStacker.(l)median

--- gempy/library/tests/test_nddops.py
+++ gempy/library/tests/test_nddops.py
@@ -1,18 +1,40 @@
 import numpy as np
 import pytest
-from gempy.library.nddops import NDStacker, sum1d
-from geminidr.gemini.lookups import DQ_definitions as DQ
+from numpy.testing import assert_allclose, assert_array_almost_equal
+
 from astrodata import NDAstroData
+from geminidr.gemini.lookups import DQ_definitions as DQ
+from gempy.library.nddops import NDStacker, sum1d
+
+
+@pytest.fixture
+def testdata():
+    return np.repeat(np.arange(5, dtype=float), 5, axis=0).reshape(5, 5)
+
+
+@pytest.fixture
+def testvar():
+    data = np.repeat(np.arange(5, dtype=float), 5, axis=0).reshape(5, 5)
+    return (data+1) / 2
+
+
+@pytest.fixture
+def testmask():
+    mask = np.zeros((5, 5), dtype=np.uint16)
+    mask[:2, 1:-1] = 1
+    return mask
 
 
 def test_process_mask():
     # List of input DQ pixels, with correct output DQ
     # and the indices of pixels that should be used in the combination
-    tests_and_results = (([5, 9, 8, 8], 8, [2, 3]),
-                         ([0, 1, 2, 3], 2, [0, 2]),
-                         ([2, 4, 0, 1], 6, [0, 1, 2]),
-                         ([2, 4, 8, 8], 6, [0, 1]),
-                         ([8, 1, 9, 1], 8, [0]))
+    tests_and_results = (
+        ([5, 9, 8, 8], 8, [2, 3]),
+        ([0, 1, 2, 3], 2, [0, 2]),
+        ([2, 4, 0, 1], 6, [0, 1, 2]),
+        ([2, 4, 8, 8], 6, [0, 1]),
+        ([8, 1, 9, 1], 8, [0]),
+    )
 
     for mask_pixels, correct_output, good_pixels in tests_and_results:
         pixel_usage = np.full_like(mask_pixels, 32768).astype(DQ.datatype)
@@ -26,7 +48,8 @@ def test_process_mask():
 
         # Second test to confirm that additional iterations (required to
         # process the other output pixel) do not change output
-        in_mask = np.array([[x, DQ.no_data] for x in mask_pixels]).astype(DQ.datatype)
+        in_mask = np.array([[x, DQ.no_data]
+                            for x in mask_pixels]).astype(DQ.datatype)
         mask, out_mask = NDStacker._process_mask(in_mask)
         assert out_mask[0] == correct_output
         assert np.array_equal(mask.T[0], pixel_usage)
@@ -45,10 +68,78 @@ def test_varclip():
     np.testing.assert_array_equal(result.mask, [0])
 
 
+def test_mean(testdata, testvar, testmask):
+    out_data, out_mask, out_var = NDStacker.mean(testdata)
+    assert_allclose(out_data, 2)
+    assert_allclose(out_var, 0.5)
+    assert out_mask is None
+
+    out_data, out_mask, out_var = NDStacker.mean(testdata, variance=testvar)
+    assert_allclose(out_data, 2)
+    assert_allclose(out_var, 0.3)
+
+    out_data, out_mask, out_var = NDStacker.mean(testdata, mask=testmask)
+    assert_allclose(out_data, [2., 3., 3., 3., 2.])
+    assert_array_almost_equal(out_var, [0.5, 0.33, 0.33, 0.33, 0.5], decimal=2)
+    assert_allclose(out_mask, 0)
+
+    out_data, out_mask, out_var = NDStacker.mean(testdata,
+                                                 mask=testmask,
+                                                 variance=testvar)
+    assert_allclose(out_data, [2., 3., 3., 3., 2.])
+    assert_array_almost_equal(out_var, [0.3, 0.66, 0.66, 0.66, 0.3], decimal=2)
+    assert_allclose(out_mask, 0)
+
+
+@pytest.mark.parametrize('func', [NDStacker.median, NDStacker.lmedian])
+def test_median_odd(func, testdata, testvar, testmask):
+    out_data, out_mask, out_var = func(testdata)
+    assert_allclose(out_data, 2)
+    assert_array_almost_equal(out_var, 0.79, decimal=2)
+    assert out_mask is None
+
+    out_data, out_mask, out_var = func(testdata, variance=testvar)
+    assert_allclose(out_data, 2)
+    # FIXME: median and lmedian do not give the same result:
+    # assert_allclose(out_var, 1.5)
+
+    out_data, out_mask, out_var = func(testdata, mask=testmask)
+    assert_allclose(out_data, [2., 3., 3., 3., 2.])
+    assert_array_almost_equal(out_var, [0.79, 0.52, 0.52, 0.52, 0.79],
+                              decimal=2)
+    assert_allclose(out_mask, 0)
+
+    out_data, out_mask, out_var = func(testdata,
+                                       mask=testmask,
+                                       variance=testvar)
+    assert_allclose(out_data, [2., 3., 3., 3., 2.])
+    assert_array_almost_equal(out_var, [0.47, 1.05, 1.05, 1.05, 0.47],
+                              decimal=2)
+    assert_allclose(out_mask, 0)
+
+
+@pytest.mark.parametrize('func,expected_median,expected_var',
+                         [(NDStacker.median, 2.5, 0.65),
+                          (NDStacker.lmedian, 2, 0.79)])
+def test_median_even(func, expected_median, expected_var, testdata, testvar,
+                     testmask):
+    testdata = testdata[1:]
+
+    out_data, out_mask, out_var = func(testdata)
+    assert_allclose(out_data, expected_median)
+    assert_array_almost_equal(out_var, expected_var, decimal=2)
+    assert out_mask is None
+
+    out_data, out_mask, out_var = func(testdata, variance=testvar)
+    assert_allclose(out_data, expected_median)
+    # FIXME: median and lmedian do not give the same result:
+    # assert_allclose(out_var, 1.5)
+
+
 @pytest.mark.xfail(reason='review this')
 def test_sum1d():
     big_value = 10
-    data = np.ones((10,))
+    data = np.ones((10, ))
     ndd = NDAstroData(data)
     ndd.mask = np.zeros_like(data, dtype=DQ.datatype)
     ndd.mask[1] = 1
@@ -59,7 +150,8 @@ def test_sum1d():
     for x2 in np.arange(0., 4.5, 0.5):
         result = sum1d(ndd, x1, x2, proportional_variance=True)
         if x2 > 3.5:
-            np.testing.assert_almost_equal(4 + big_value * (x2 - 3.5), result.data)
+            np.testing.assert_almost_equal(4 + big_value * (x2-3.5),
+                                           result.data)
         else:
             np.testing.assert_almost_equal(x2 - x1, result.data)
         np.testing.assert_almost_equal(x2 - x1, result.variance)
