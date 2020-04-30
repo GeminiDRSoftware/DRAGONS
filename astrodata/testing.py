60232279ad1c1ba59aa5de9ef69bdce576a10846
Bruno Quint <bquint@gemini.edu>
2020-04-30 09:44:59 -0400

    sorted fixtures in astrodata.testing

--- astrodata/testing.py
+++ astrodata/testing.py
@@ -14,6 +14,25 @@ from astropy.utils.data import download_file
 URL = 'https://archive.gemini.edu/file/'
 
 
+def assert_same_class(ad, ad_ref):
+    """
+    Compare if two :class:`~astrodata.AstroData` (or any subclass) have the
+    same class.
+
+    Parameters
+    ----------
+        ad : :class:`astrodata.AstroData` or any subclass
+            AstroData object to be checked.
+        ad_ref : :class:`astrodata.AstroData` or any subclass
+            AstroData object used as reference
+    """
+    from astrodata import AstroData
+
+    assert isinstance(ad, AstroData)
+    assert isinstance(ad_ref, AstroData)
+    assert isinstance(ad, type(ad_ref))
+
+
 @pytest.fixture(scope='module')
 def cache_file_from_archive(path_to_inputs, path_to_outputs):
     """
@@ -108,23 +127,38 @@ def download_from_archive(filename, path=None, env_var='DRAGONS_TEST_INPUTS'):
     return local_path
 
 
-def assert_same_class(ad, ad_ref):
+def get_associated_calibrations(filename, nbias=5):
     """
-    Compare if two :class:`~astrodata.AstroData` (or any subclass) have the
-    same class.
+    Queries Gemini Observatory Archive for associated calibrations to reduce the
+    data that will be used for testing.
 
     Parameters
     ----------
-        ad : :class:`astrodata.AstroData` or any subclass
-            AstroData object to be checked.
-        ad_ref : :class:`astrodata.AstroData` or any subclass
-            AstroData object used as reference
+    filename : str
+        Input file name
     """
-    from astrodata import AstroData
+    pd = pytest.importorskip("pandas", minversion='1.0.0')
+    url = "https://archive.gemini.edu/calmgr/{}".format(filename)
 
-    assert isinstance(ad, AstroData)
-    assert isinstance(ad_ref, AstroData)
-    assert isinstance(ad, type(ad_ref))
+    tree = et.parse(urllib.request.urlopen(url))
+    root = tree.getroot()
+    prefix = root.tag[:root.tag.rfind('}') + 1]
+
+    def iter_nodes(node):
+        cal_type = node.find(prefix + 'caltype').text
+        cal_filename = node.find(prefix + 'filename').text
+        return cal_filename, cal_type
+
+    cals = pd.DataFrame(
+        [iter_nodes(node) for node in tree.iter(prefix + 'calibration')],
+        columns=['filename', 'caltype'])
+
+    cals = cals.sort_values(by='filename')
+    cals = cals[~cals.caltype.str.contains('processed_')]
+    cals = cals[~cals.caltype.str.contains('specphot')]
+    cals = cals.drop(cals[cals.caltype.str.contains('bias')][nbias:].index)
+
+    return cals
 
 
 @pytest.fixture(scope='module')
@@ -228,10 +262,10 @@ def path_to_test_data(env_var='DRAGONS_TEST'):
     return path
 
 
-@pytest.fixture(scope='session')
-def path_to_outputs(tmp_path_factory):
+@pytest.fixture(scope='module')
+def path_to_outputs(request, tmp_path_factory):
     """
-    PyTest fixture that creates a temporary folder to save tests ouputs.
+    PyTest fixture that creates a temporary folder to save tests outputs.
 
     This output folder can be override via $DRAGONS_TEST_OUTPUTS environment
     variable or via `--basetemp` argument.
@@ -246,49 +280,18 @@ def path_to_outputs(tmp_path_factory):
     IOError
         If output path does not exits.
     """
-    path = tmp_path_factory.mktemp('dragons_tests', numbered=False)
-
     if os.getenv('DRAGONS_TEST_OUTPUTS'):
-        path = Path(os.path.expanduser(os.getenv('DRAGONS_TEST_OUTPUTS')))
+        path = os.path.expanduser(os.getenv('DRAGONS_TEST_OUTPUTS'))
+        if not os.path.exists(path):
+            raise OSError(
+                "Could not access path stored in $DRAGONS_TEST_OUTPUTS: "
+                "{}\n Using current working directory".format(path))
+    else:
+        path = str(tmp_path_factory.mktemp('dragons_tests', numbered=False))
 
-    path.mkdir(exist_ok=True, parents=True)
+    module_path = request.module.__name__.split('.')
+    module_path = [item for item in module_path if item not in "tests"]
+    path = os.path.join(path, *module_path)
+    os.makedirs(path, exist_ok=True)
 
-    if not os.path.exists(path):
-        raise OSError("Could not access path stored in $DRAGONS_TEST_OUTPUTS: "
-                      "{}\n Using current working directory".format(path))
-
-    return str(path)  # todo: should astrodata be compatible with pathlib?
-
-
-def get_associated_calibrations(filename, nbias=5):
-    """
-    Queries Gemini Observatory Archive for associated calibrations to reduce the
-    data that will be used for testing.
-
-    Parameters
-    ----------
-    filename : str
-        Input file name
-    """
-    pd = pytest.importorskip("pandas", minversion='1.0.0')
-    url = "https://archive.gemini.edu/calmgr/{}".format(filename)
-
-    tree = et.parse(urllib.request.urlopen(url))
-    root = tree.getroot()
-    prefix = root.tag[:root.tag.rfind('}') + 1]
-
-    def iter_nodes(node):
-        cal_type = node.find(prefix + 'caltype').text
-        cal_filename = node.find(prefix + 'filename').text
-        return cal_filename, cal_type
-
-    cals = pd.DataFrame(
-        [iter_nodes(node) for node in tree.iter(prefix + 'calibration')],
-        columns=['filename', 'caltype'])
-
-    cals = cals.sort_values(by='filename')
-    cals = cals[~cals.caltype.str.contains('processed_')]
-    cals = cals[~cals.caltype.str.contains('specphot')]
-    cals = cals.drop(cals[cals.caltype.str.contains('bias')][nbias:].index)
-
-    return cals
+    return path
