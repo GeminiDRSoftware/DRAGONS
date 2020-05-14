12ffa900964d0ea93e64bbd433cb5e705a0c45a5
Bruno Quint <b1quint@gmail.com>
2020-05-14 16:27:42 -0400

    Refactored tests in geminidr.gemini

--- geminidr/gemini/tests/test_gemini.py
+++ geminidr/gemini/tests/test_gemini.py
@@ -2,8 +2,8 @@ import copy
 import pytest
 
 import astrodata
+from gempy.utils import logutils
 from geminidr.gemini import primitives_gemini as gemini
-
 
 # --- Delete me? ---
 # @pytest.fixture(scope='module')
@@ -28,30 +28,8 @@ STAR_POSITIONS = [(200., 200.), (300.5, 800.5)]
 
 
 # --- Fixtures ---
-@pytest.fixture(scope='module')
-def setup_module(request):
-    print('setup test_gemini module')
-
-    def fin():
-        print('\nteardown test_gemini module')
-
-    request.addfinalizer(fin)
-    return
-
-
-@pytest.fixture(scope='class')
-def setup_testgemini(request):
-    print('setup TestGemini')
-
-    def fin():
-        print('\nteardown TestGemini')
-
-    request.addfinalizer(fin)
-    return
-
-
 @pytest.fixture()
-def geminiimage():
+def gemini_image():
     astrofaker = pytest.importorskip('astrofaker')
     af = astrofaker.create('NIRI', 'IMAGE')
     af.init_default_extensions()
@@ -62,28 +40,31 @@ def geminiimage():
     return af  # geminiimage([af])
 
 
+@pytest.fixture(scope='module', autouse=True)
+def setup_log(change_working_dir):
+    with change_working_dir():
+        logutils.config(file_name='test_gemini.log')
+
+
 # --- Tests ---
-@pytest.mark.usefixtures('setup_testgemini')
-class TestGemini:
+def test_standardize_observatory_headers(gemini_image):
 
-    def test_standardize_observatory_headers(self, geminiimage):
+    test_gemini = gemini.Gemini([gemini_image])
+    processed_af = test_gemini.standardizeObservatoryHeaders()[0]
+    expected_timestamp = processed_af.phu['SDZHDRSG']
 
-        test_gemini = gemini.Gemini([geminiimage])
-        processed_af = test_gemini.standardizeObservatoryHeaders()[0]
-        expected_timestamp = processed_af.phu['SDZHDRSG']
+    assert isinstance(expected_timestamp, str), "phu SDZHDRSG tag not found!"
 
-        assert isinstance(expected_timestamp, str), "phu SDZHDRSG tag not found!"
+    numb_of_extensions = processed_af.phu['NSCIEXT']
 
-        numb_of_extensions = processed_af.phu['NSCIEXT']
+    assert isinstance(numb_of_extensions, int), "phu NSCIEXT tag not found!"
+    assert (numb_of_extensions == 1), "one science extension expected, more/less found"
 
-        assert isinstance(numb_of_extensions, int), "phu NSCIEXT tag not found!"
-        assert (numb_of_extensions == 1), "one science extension expected, more/less found"
+    new_af = copy.deepcopy(processed_af)
+    test_gemini2 = gemini.Gemini([new_af])
+    processed_af2 = test_gemini2.standardizeObservatoryHeaders()[0]
+    expected_timestamp2 = processed_af2.phu['SDZHDRSG']
+    numb_of_extensions2 = processed_af2.phu['NSCIEXT']
 
-        new_af = copy.deepcopy(processed_af)
-        test_gemini2 = gemini.Gemini([new_af])
-        processed_af2 = test_gemini2.standardizeObservatoryHeaders()[0]
-        expected_timestamp2 = processed_af2.phu['SDZHDRSG']
-        numb_of_extensions2 = processed_af2.phu['NSCIEXT']
-
-        assert (expected_timestamp == expected_timestamp2)
-        assert (numb_of_extensions == numb_of_extensions2)
+    assert (expected_timestamp == expected_timestamp2)
+    assert (numb_of_extensions == numb_of_extensions2)
