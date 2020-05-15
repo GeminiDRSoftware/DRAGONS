2aecddde6dddc48953f5482b77f166214c8578fd
Bruno Quint <b1quint@gmail.com>
2020-05-15 18:42:38 -0400

    added final input files in test_gmos_spect_ls_apply_qe_correction.py

--- geminidr/gmos/tests/test_gmos_spect_ls_apply_qe_correction.py
+++ geminidr/gmos/tests/test_gmos_spect_ls_apply_qe_correction.py
@@ -21,15 +21,14 @@ DPI = 90
 datasets = [
 
     # --- Good datasets ---
-    # GN-2017B-FT-20-13-001 B600 0.505um
-    ("N20180109S0287_flatCorrected.fits", "N20180109S0315_arc.fits"),
-    ("N20190302S0089_flatCorrected.fits", ""),  # GN-2019A-Q-203-7-001 B600 0.550um
-    ("N20190313S0114_flatCorrected.fits", ""),  # GN-2019A-Q-325-13-001 B600 0.482um
-    ("N20190427S0123_flatCorrected.fits", ""),  # GN-2019A-FT-206-7-001 R400 0.525um
-    ("N20190427S0126_flatCorrected.fits", ""),  # GN-2019A-FT-206-7-004 R400 0.625um
-    ("N20190910S0028_flatCorrected.fits", ""),  # GN-2019B-Q-313-5-001 B600 0.550um
-    ("S20180919S0139_flatCorrected.fits", ""),  # GS-2018B-Q-209-13-003 B600 0.45um
-    ("S20191005S0051_flatCorrected.fits", ""),  # GS-2019B-Q-132-35-001 R400 0.73um
+    ("N20180109S0287_flatCorrected.fits", "N20180109S0315_arc.fits"),  # GN-2017B-FT-20-13-001 B600 0.505um
+    ("N20190302S0089_flatCorrected.fits", "N20190302S0274_arc.fits"),  # GN-2019A-Q-203-7-001 B600 0.550um
+    ("N20190313S0114_flatCorrected.fits", "N20190313S0132_arc.fits"),  # GN-2019A-Q-325-13-001 B600 0.482um
+    ("N20190427S0123_flatCorrected.fits", "N20190427S0266_arc.fits"),  # GN-2019A-FT-206-7-001 R400 0.525um
+    ("N20190427S0126_flatCorrected.fits", "N20190427S0267_arc.fits"),  # GN-2019A-FT-206-7-004 R400 0.625um
+    ("N20190910S0028_flatCorrected.fits", "N20190910S0279_arc.fits"),  # GN-2019B-Q-313-5-001 B600 0.550um
+    ("S20180919S0139_flatCorrected.fits", "S20180919S0141_arc.fits"),  # GS-2018B-Q-209-13-003 B600 0.45um
+    ("S20191005S0051_flatCorrected.fits", "S20191005S0151_arc.fits"),  # GS-2019B-Q-132-35-001 R400 0.73um
 
     # --- QE Correction Needs improvement ---
     # "N20180721S0444.fits",  # GN-2018B-Q-313-5-002 B1200 0.44um
