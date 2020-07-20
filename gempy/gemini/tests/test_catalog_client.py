b2f512aabf14bf27b591125cf7c23285b872e4f9
Simon Conseil <sconseil@gemini.edu>
2020-07-20 19:33:59 -0400

    Fix 2mass_vizier and sdss9_vizier

--- gempy/gemini/tests/test_catalog_client.py
+++ gempy/gemini/tests/test_catalog_client.py
@@ -16,7 +16,11 @@ TESTS = [
 # These just check that all sources are within the search radius
 # and that the number is the same as when it was written
 @pytest.mark.dragons_remote_data
-@pytest.mark.parametrize('catalog, ra, dec, radius, nres', TESTS)
+@pytest.mark.parametrize('catalog, ra, dec, radius, nres', [
+    ('2mass', 180., 0., 0.02, 5),
+    ('sdss9', 160., 30., 0.02, 9),
+    ('gmos', 200., -60., 0.02, 8),
+])
 def test_get_fits_table(catalog, ra, dec, radius, nres):
     """Test some requests with get_fits_table, which by default will use the
     internal server. So we skip the test if this server is no reachable.
@@ -38,7 +42,10 @@ def test_get_fits_table(catalog, ra, dec, radius, nres):
 
 
 @pytest.mark.dragons_remote_data
-@pytest.mark.parametrize('catalog, ra, dec, radius, nres', TESTS[:2])
+@pytest.mark.parametrize('catalog, ra, dec, radius, nres', [
+    ('2mass', 180., 0., 0.02, 5),
+    ('sdss9', 160., 30., 0.02, 28),
+])
 def test_get_fits_table_vizier(catalog, ra, dec, radius, nres):
     """Tests the same requests but on Vizier this time, specifying explicitly
     the server.
