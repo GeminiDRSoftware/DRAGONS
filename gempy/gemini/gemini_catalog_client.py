368f95d7b00a3108772f09e0a51034141c121d30
Simon Conseil <sconseil@gemini.edu>
2020-07-20 18:31:41 -0400

    Refactor gemini_catalog_client and test for easier testing
    Also add a verbose option to get_fits_table*

--- gempy/gemini/gemini_catalog_client.py
+++ gempy/gemini/gemini_catalog_client.py
@@ -27,185 +27,185 @@ from ..utils import logutils
 
 log = logutils.get_logger(__name__)
 
+# List of available servers for each catalog.
+CAT_SERVERS = {
+    # 'sdss9' : ['sdss9_mko', 'sdss9_cpo', 'sdss9_vizier'],
+    # '2mass' : ['2mass_mko', '2mass_cpo', '2mass_vizier'],
+    'sdss9': ['sdss9_mko', 'sdss9_vizier'],
+    '2mass': ['2mass_mko', '2mass_vizier'],
+    'ukidss9': ['ukidss9_mko', 'ukidss9_cpo'],
+    'gmos': ['gmos_mko', 'gmos_cpo'],
+}
 
-def get_fits_table(catalog, ra, dec, sr, server=None):
+# This defines the URL for each server
+# There must be an entry in this dictionary for each server
+# listed in cat_servers above
+SERVER_URLS = {
+    'sdss9_mko': "http://mkocatalog2/cgi-bin/conesearch.py?CATALOG=sdss9&",
+    'sdss9_cpo': "http://cpocatalog2/cgi-bin/conesearch.py?CATALOG=sdss9&",
+    'sdss9_vizier': "http://vizier.u-strasbg.fr/viz-bin/votable/-A?-source=sdss9&",
+    '2mass_mko': "http://mkocatalog2/cgi-bin/conesearch.py?CATALOG=twomass_psc&",
+    '2mass_cpo': "http://cpocatalog2/cgi-bin/conesearch.py?CATALOG=twomass_psc&",
+    '2mass_vizier': "http://vizier.u-strasbg.fr/viz-bin/votable/-A?-source=B/2mass&",
+    'ukidss9_mko': "http://mkocatalog2/cgi-bin/conesearch.py?CATALOG=ukidss&",
+    'ukidss9_cpo': "http://cpocatalog2/cgi-bin/conesearch.py?CATALOG=ukidss&",
+    'gmos_mko': "http://mkocatalog2/cgi-bin/conesearch.py?CATALOG=gmos&",
+    'gmos_cpo': "http://cpocatalog2/cgi-bin/conesearch.py?CATALOG=gmos&",
+}
+
+# This defines the column names *we* will use for that catalog.
+# There must be one entry in this list for each catalog listed
+# in cat_servers above
+CAT_COLS = {
+    'sdss9': ['catid', 'raj2000', 'dej2000', 'umag', 'umag_err',
+              'gmag', 'gmag_err', 'rmag', 'rmag_err', 'imag',
+              'imag_err', 'zmag', 'zmag_err'],
+    '2mass': ['catid', 'raj2000', 'dej2000', 'jmag', 'jmag_err',
+              'hmag', 'hmag_err', 'kmag', 'kmag_err'],
+    'ukidss9': ['catid', 'raj2000', 'dej2000', 'ymag', 'ymag_err',
+                'zmag', 'zmag_err', 'jmag', 'jmag_err',
+                'hmag', 'hmag_err', 'kmag', 'kmag_err'],
+    'gmos': ['name', 'raj2000', 'dej2000', 'umag', 'umag_err',
+             'gmag', 'gmag_err', 'rmag', 'rmag_err', 'imag',
+             'imag_err', 'zmag', 'zmag_err']
+}
+
+# This defines the column name mapping for each catalog server to our
+# column names. This copes with both variable server conventions, and
+# also allows us to point to different columns in the upstream catalog
+# - eg different model fits magnitides - if we wish
+# ***** These need to be in the same order as the list in our_cols *****
+SERVER_COLMAP = {
+    'sdss9_mko': ['objid', 'raj2000', 'dej2000', 'umag', 'umag_err',
+                  'gmag', 'gmag_err', 'rmag', 'rmag_err', 'imag',
+                  'imag_err', 'zmag', 'zmag_err'],
+    'sdss9_cpo': ['objid', 'raj2000', 'dej2000', 'umag', 'umag_err',
+                  'gmag', 'gmag_err', 'rmag', 'rmag_err', 'imag',
+                  'imag_err', 'zmag', 'zmag_err'],
+    'sdss9_vizier': ['objID', 'RAJ2000', 'DEJ2000', 'umag', 'e_umag',
+                     'gmag', 'e_gmag', 'rmag', 'e_rmag', 'imag', 'e_imag',
+                     'zmag', 'e_zmag'],
+    '2mass_mko': ['designation', 'ra', 'decl', 'j_m', 'j_cmsig',
+                  'h_m', 'h_cmsig', 'k_m', 'k_cmsig'],
+    '2mass_cpo': ['designation', 'ra', 'decl', 'j_m', 'j_cmsig',
+                  'h_m', 'h_cmsig', 'k_m', 'k_cmsig'],
+    '2mass_vizier': ['_2MASS', 'RAJ2000', 'DEJ2000', 'Jmag', 'Jcmsig',
+                     'Hmag', 'Hcmsig', 'Kmag', 'Kcmsig'],
+    'ukidss9_mko': ['id', 'raj2000', 'dej2000', 'y_mag', 'y_mag_err',
+                    'z_mag', 'z_mag_err', 'j_mag', 'j_mag_err',
+                    'h_mag', 'h_mag_err', 'k_mag', 'k_mag_err'],
+    'ukidss9_cpo': ['id', 'raj2000', 'dej2000', 'y_mag', 'y_mag_err',
+                    'z_mag', 'z_mag_err', 'j_mag', 'j_mag_err',
+                    'h_mag', 'h_mag_err', 'k_mag', 'k_mag_err'],
+    'gmos_mko': ['name', 'raj2000', 'dej2000', 'umag', 'umag_err',
+                 'gmag', 'gmag_err', 'rmag', 'rmag_err', 'imag',
+                 'imag_err', 'zmag', 'zmag_err'],
+    'gmos_cpo': ['name', 'raj2000', 'dej2000', 'umag', 'umag_err',
+                 'gmag', 'gmag_err', 'rmag', 'rmag_err', 'imag',
+                 'imag_err', 'zmag', 'zmag_err']
+}
+
+
+def get_fits_table(catalog, ra, dec, sr, server=None, verbose=False):
     """
-    This function returns a QAP style REFCAT in the form of an astropy Table
+    This function returns a QAP style REFCAT in the form of an astropy Table.
 
     Parameters
     ----------
-    catalog: str [sdss9 | 2mass | ukidss9 | gmos]
-        name of catalog to search
-    ra: float
-        right ascension of search center, decimal degrees
-    dec: float
-        declination of search center, decimal degrees
-    sr: float
-        search radius, decimal degrees
-    server: str/None
-        name of server to query [sdss9_mko | sdss9_cpo | sdss9_vizier |
-        2mass_mko | 2mass_cpo | 2mass_vizier | ukidss9_mko | ukidss9_cpo]
+    catalog : {'sdss9', '2mass', 'ukidss9', 'gmos'}
+        Name of catalog to search.
+    ra : float
+        Right ascension of search center, decimal degrees.
+    dec : float
+        Declination of search center, decimal degrees.
+    sr : float
+        Search radius, decimal degrees.
+    server : str, optional
+        Name of server to query. Must be a valid server name in the following
+        list: sdss9_mko, sdss9_cpo, sdss9_vizier, 2mass_mko, 2mass_cpo,
+        2mass_vizier, ukidss9_mko, ukidss9_cpo.
+    verbose : bool
+        Verbose output.
+
     Returns
     -------
-    Table
-        sources within the search cone
-    """
-    # This defines the list of available servers for each catalog.
-    cat_servers = {
-        # 'sdss9' : ['sdss9_mko', 'sdss9_cpo', 'sdss9_vizier'],
-        # '2mass' : ['2mass_mko', '2mass_cpo', '2mass_vizier'],
-        'sdss9': ['sdss9_mko', 'sdss9_vizier'],
-        '2mass': ['2mass_mko', '2mass_vizier'],
-        'ukidss9': ['ukidss9_mko', 'ukidss9_cpo'],
-        'gmos': ['gmos_mko', 'gmos_cpo'],
-    }
+    `astropy.table.Table`
+        Sources within the search cone.
 
+    """
     # Check catalog given is valid
-    assert catalog in cat_servers, 'Invalid Catalog'
+    if catalog not in CAT_SERVERS:
+        raise ValueError('Invalid Catalog')
 
     # Check server if given is valid
-    assert server is None or server in cat_servers[catalog], 'Invalid Server'
+    if server is not None and server not in CAT_SERVERS[catalog]:
+        raise ValueError('Invalid Server')
 
-    if server:
-        cat_servers[catalog] = [server]
+    servers = [server] if server else CAT_SERVERS[catalog]
 
-    fits_table = None
-    for server in cat_servers[catalog]:
-        fits_table = get_fits_table_from_server(catalog, server, ra, dec, sr)
-        if fits_table:
-            break
-    return fits_table
+    for server in servers:
+        tbl = get_fits_table_from_server(catalog, server, ra, dec, sr,
+                                         verbose=verbose)
+        if tbl is not None:
+            return tbl
 
 
-def get_fits_table_from_server(catalog, server, ra, dec, sr):
+def get_fits_table_from_server(catalog, server, ra, dec, sr, verbose=False):
     """
     This function fetches sources from the specified catalog from the specified
     server within the search radius and returns a Table containing them.
 
     Parameters
     ----------
-    catalog: str [sdss9 | 2mass | ukidss9 | gmos]
-        name of catalog to search
-    server: str
-        name of server to query [sdss9_mko | sdss9_cpo | sdss9_vizier |
+    catalog : {'sdss9', '2mass', 'ukidss9', 'gmos'}
+        Name of catalog to search.
+    server : str
+        Name of server to query [sdss9_mko | sdss9_cpo | sdss9_vizier |
         2mass_mko | 2mass_cpo | 2mass_vizier | ukidss9_mko | ukidss9_cpo]
-    ra: float
-        right ascension of search center, decimal degrees
-    dec: float
-        declination of search center, decimal degrees
-    sr: float
-        search radius, decimal degrees
+    ra : float
+        Right ascension of search center, decimal degrees.
+    dec : float
+        Declination of search center, decimal degrees.
+    sr : float
+        Search radius, decimal degrees.
+    verbose : bool
+        Verbose output.
+
     Returns
     -------
-    Table
-        sources within the search cone
+    `astropy.table.Table`
+        Sources within the search cone.
+
     """
-    # This defines the URL for each server
-    # There must be an entry in this dictionary for each server
-    # listed in cat_servers above
-    server_urls = {
-        'sdss9_mko': "http://mkocatalog2/cgi-bin/conesearch.py?CATALOG=sdss9&",
-        'sdss9_cpo': "http://cpocatalog2/cgi-bin/conesearch.py?CATALOG=sdss9&",
-        'sdss9_vizier':
-            "http://vizier.u-strasbg.fr/viz-bin/votable/-A?-source=sdss9&",
-        '2mass_mko':
-            "http://mkocatalog2/cgi-bin/conesearch.py?CATALOG=twomass_psc&",
-        '2mass_cpo':
-            "http://cpocatalog2/cgi-bin/conesearch.py?CATALOG=twomass_psc&",
-        '2mass_vizier':
-            "http://vizier.u-strasbg.fr/viz-bin/votable/-A?-source=B/2mass&",
-        'ukidss9_mko':
-            "http://mkocatalog2/cgi-bin/conesearch.py?CATALOG=ukidss&",
-        'ukidss9_cpo':
-            "http://cpocatalog2/cgi-bin/conesearch.py?CATALOG=ukidss&",
-        'gmos_mko': "http://mkocatalog2/cgi-bin/conesearch.py?CATALOG=gmos&",
-        'gmos_cpo': "http://cpocatalog2/cgi-bin/conesearch.py?CATALOG=gmos&",
-    }
-
-    # This defines the column names *we* will use for that catalog.
-    # There must be one entry in this list for each catalog listed
-    # in cat_servers above
-    cat_cols = {
-        'sdss9': ['catid', 'raj2000', 'dej2000', 'umag', 'umag_err',
-                  'gmag', 'gmag_err', 'rmag', 'rmag_err', 'imag',
-                  'imag_err', 'zmag', 'zmag_err'],
-        '2mass': ['catid', 'raj2000', 'dej2000', 'jmag', 'jmag_err',
-                  'hmag', 'hmag_err', 'kmag', 'kmag_err'],
-        'ukidss9': ['catid', 'raj2000', 'dej2000', 'ymag', 'ymag_err',
-                    'zmag', 'zmag_err', 'jmag', 'jmag_err',
-                    'hmag', 'hmag_err', 'kmag', 'kmag_err'],
-        'gmos': ['name', 'raj2000', 'dej2000', 'umag', 'umag_err',
-                 'gmag', 'gmag_err', 'rmag', 'rmag_err', 'imag',
-                 'imag_err', 'zmag', 'zmag_err']
-    }
-
-    # This defines the column name mapping for each catalog server to our
-    # column names. This copes with both variable server conventions, and
-    # also allows us to point to different columns in the upstream catalog
-    # - eg different model fits magnitides - if we wish
-    # ***** These need to be in the same order as the list in our_cols *****
-    server_colmap = {
-        'sdss9_mko': ['objid', 'raj2000', 'dej2000', 'umag', 'umag_err',
-                      'gmag', 'gmag_err', 'rmag', 'rmag_err', 'imag',
-                      'imag_err', 'zmag', 'zmag_err'],
-        'sdss9_cpo': ['objid', 'raj2000', 'dej2000', 'umag', 'umag_err',
-                      'gmag', 'gmag_err', 'rmag', 'rmag_err', 'imag',
-                      'imag_err', 'zmag', 'zmag_err'],
-        'sdss9_vizier': ['objID', 'RAJ2000', 'DEJ2000', 'umag', 'e_umag',
-                         'gmag', 'e_gmag', 'rmag', 'e_rmag', 'imag', 'e_imag',
-                         'zmag', 'e_zmag'],
-        '2mass_mko': ['designation', 'ra', 'decl', 'j_m', 'j_cmsig',
-                      'h_m', 'h_cmsig', 'k_m', 'k_cmsig'],
-        '2mass_cpo': ['designation', 'ra', 'decl', 'j_m', 'j_cmsig',
-                      'h_m', 'h_cmsig', 'k_m', 'k_cmsig'],
-        '2mass_vizier': ['_2MASS', 'RAJ2000', 'DEJ2000', 'Jmag', 'Jcmsig',
-                         'Hmag', 'Hcmsig', 'Kmag', 'Kcmsig'],
-        'ukidss9_mko': ['id', 'raj2000', 'dej2000', 'y_mag', 'y_mag_err',
-                        'z_mag', 'z_mag_err', 'j_mag', 'j_mag_err',
-                        'h_mag', 'h_mag_err', 'k_mag', 'k_mag_err'],
-        'ukidss9_cpo': ['id', 'raj2000', 'dej2000', 'y_mag', 'y_mag_err',
-                        'z_mag', 'z_mag_err', 'j_mag', 'j_mag_err',
-                        'h_mag', 'h_mag_err', 'k_mag', 'k_mag_err'],
-        'gmos_mko': ['name', 'raj2000', 'dej2000', 'umag', 'umag_err',
-                     'gmag', 'gmag_err', 'rmag', 'rmag_err', 'imag',
-                     'imag_err', 'zmag', 'zmag_err'],
-        'gmos_cpo': ['name', 'raj2000', 'dej2000', 'umag', 'umag_err',
-                     'gmag', 'gmag_err', 'rmag', 'rmag_err', 'imag',
-                     'imag_err', 'zmag', 'zmag_err']
-    }
-
     # OK, do the query
-    url = server_urls[server]
-    cols = cat_cols[catalog]
-    server_cols = server_colmap[server]
+    url = SERVER_URLS[server]
+    cols = CAT_COLS[catalog]
+    server_cols = SERVER_COLMAP[server]
 
-    # print "RA, Dec, radius:", ra, dec, sr
-    # print "catalog: %s" % catalog
-    # print "server: %s" % server
-    # print "url: %s" % url
-    # print "cols       : %s" % cols
-    # print "server_cols: %s" % server_cols
-    # print "\n\n"
+    if verbose:
+        print("RA, Dec, radius :", ra, dec, sr)
+        print("catalog         :", catalog)
+        print("server          :", server)
+        print("url             :", url)
+        print("cols            :", cols)
+        print("server_cols     :", server_cols)
+        print("\n\n")
 
     # turn on verbose for debug to stdout.
     # Need verb=3 to get the right cols from vizier
 
-    # The following phrase is implemented to handle differing function
-    # signatures and return behaviours of vo conesearch function. Under
-    # astropy, conesearch throws a VOSError exception on no results. Which
-    # seems a bit extreme. See the import phrase at top.
+    # astroquery 0.4 removed the pedantic keyword in favor of the config
+    # item, and switched to returning an astropy table by default (hence
+    # return_astropy_table=False below).
+    # Another change is that conesearch returns None and issue of
+    # NoResultsWarning if no results are found, instead of raising a
+    # VOSError: https://github.com/astropy/astroquery/pull/1528
     try:
         try:
-            # astroquery 0.4 removed the pedantic keyword in favor of the
-            # config item, and switched to returning an astropy table by
-            # default (hence return_astropy_table=False below).
-            # Another change is that conesearch returns None and issue of
-            # NoResultsWarning if no results are found, instead of raising
-            # a VOSError.
-            # https://github.com/astropy/astroquery/pull/1528
             table = conesearch((ra, dec), sr, verb=3, catalog_db=url,
                                return_astropy_table=False, verbose=False)
         except TypeError:
+            # astroquery < 0.4
             table = conesearch((ra, dec), sr, verb=3, catalog_db=url,
                                pedantic=False, verbose=False)
     except VOSError:
@@ -213,7 +213,7 @@ def get_fits_table_from_server(catalog, server, ra, dec, sr):
         return
 
     # Did we get any results?
-    if(table is None or table.is_empty() or len(table.array) == 0):
+    if table is None or table.is_empty() or len(table.array) == 0:
         log.stdinfo(f"No results returned from {server}")
         return
 
@@ -238,11 +238,11 @@ def get_fits_table_from_server(catalog, server, ra, dec, sr):
     header = add_header_to_table(ret_table)
     header['CATALOG'] = (catalog.upper(), 'Origin of source catalog')
     # Add comments to the header to describe it
-    header.add_comment('Source catalog derived from the {} catalog'.
-                       format(catalog))
+    header.add_comment('Source catalog derived from the {} catalog'
+                       .format(catalog))
     header.add_comment('Source catalog fetched from server at {}'.format(url))
-    header.add_comment('Delivered Table name from server:  {}'.
-                       format(table.name))
+    header.add_comment('Delivered Table name from server:  {}'
+                       .format(table.name))
     for col in range(len(cols)):
         header.add_comment('UCD for field {} is {}'.format(
             cols[col], table.get_field_by_id(server_cols[col]).ucd))
--- gempy/gemini/tests/test_catalog_client.py
+++ gempy/gemini/tests/test_catalog_client.py
@@ -1,53 +1,26 @@
-# pytest suite
-"""
-Tests for gemini_catalog_client.
-
-This is a suite of tests to be run with pytest.
-
-To run:
-    1) py.test -v --capture=no
-"""
-
 import pytest
-
-import astropy.units as u
+from astropy import units as u
 from astropy.coordinates import Angle, SkyCoord
 from gempy.gemini.gemini_catalog_client import get_fits_table
+
+
+TESTS = [
+    # (catalog, ra, dec, radius, nbresults)
+    ('2mass', 180., 0., 0.02, 5),
+    ('sdss9', 160., 30., 0.02, 9),
+    ('gmos', 200., -60., 0.02, 8),
+]
 
 
 # These just check that all sources are within the search radius
 # and that the number is the same as when it was written
 @pytest.mark.skip(reason='requires internal network access')
-def test_get_fits_table_2mass():
-    ra = 180.0
-    dec = 0.0
-    radius = 0.02
+@pytest.mark.dragons_remote_data
+@pytest.mark.parametrize('catalog, ra, dec, radius, nres', TESTS)
+def test_get_fits_table(catalog, ra, dec, radius, nres):
+    ret = get_fits_table(catalog, ra, dec, radius)
+    assert len(ret) == nres
+
     center = SkyCoord(ra, dec, unit='deg')
-    ret = get_fits_table('2mass', ra, dec, radius)
-    assert len(ret) == 5
-    assert all(center.separation(SkyCoord(ret['RAJ2000'],
-                    ret['DEJ2000'], unit='deg')) < Angle(radius * u.deg))
-
-
-@pytest.mark.skip(reason='requires internal network access')
-def test_get_fits_table_sdss9():
-    ra = 160.0
-    dec = 30.0
-    radius = 0.02
-    center = SkyCoord(ra, dec, unit='deg')
-    ret = get_fits_table('sdss9', ra, dec, radius)
-    assert len(ret) == 9
-    assert all(center.separation(SkyCoord(ret['RAJ2000'],
-                    ret['DEJ2000'], unit='deg')) < Angle(radius * u.deg))
-
-
-@pytest.mark.skip(reason='requires internal network access')
-def test_get_fits_table_gmos():
-    ra = 200.0
-    dec = -60.0
-    radius = 0.02
-    center = SkyCoord(ra, dec, unit='deg')
-    ret = get_fits_table('gmos', ra, dec, radius)
-    assert len(ret) == 8
-    assert all(center.separation(SkyCoord(ret['RAJ2000'],
-                    ret['DEJ2000'], unit='deg')) < Angle(radius * u.deg))
+    coord = SkyCoord(ret['RAJ2000'], ret['DEJ2000'], unit='deg')
+    assert all(center.separation(coord) < Angle(radius * u.deg))
