615555b8af997cf9adbbd9e878070dd5dc2cc19f
Bruno Quint <b1quint@gmail.com>
2020-08-07 12:25:34 -0400

    Refactored gmos.recipes.sq.test_image.py for GN_HAM_2x2_z-band test case

--- geminidr/gmos/recipes/sq/tests/test_image.py
+++ geminidr/gmos/recipes/sq/tests/test_image.py
@@ -14,110 +14,124 @@ from recipe_system.utils.reduce_utils import normalize_ucals
 
 from gempy.utils import logutils
 
-test_cases = [
-    # "GN HAM 2x2 z-band",
-    "GN EEV 2x2 g-band",
-]
 
 datasets = {
 
-    "GN HAM 2x2 z-band": {
+    "GN_HAM_2x2_z-band": {
         "bias": [f"N20170912S{n:04d}.fits" for n in range(295, 300)] +
                 [f"N20170914S{n:04d}.fits" for n in range(481, 486)] +
                 [f"N20170915S{n:04d}.fits" for n in range(337, 342)],
         "flat": [f"N20170915S{n:04d}.fits" for n in range(274, 288)],
         "sci": [f"N20170913S{n:04d}.fits" for n in range(153, 159)],
+        "ucals": [],
     },
 
-    "GN EEV 2x2 g-band": {
-        # Only three files to avoid memory errors or to speed up the test
-        "bias": [f"N20020214S{n:03d}.fits" for n in range(22, 27)][:3],
-        "flat": [f"N20020211S{n:03d}.fits" for n in range(156, 160)][:3],
-        "sci": [f"N20020214S{n:03d}.fits" for n in range(59, 64)][:3],
-    },
+    # "GN_EEV_2x2_g-band": {
+    #     # Only three files to avoid memory errors or to speed up the test
+    #     "bias": [f"N20020214S{n:03d}.fits" for n in range(22, 27)][:3],
+    #     "flat": [f"N20020211S{n:03d}.fits" for n in range(156, 160)][:3],
+    #     "sci": [f"N20020214S{n:03d}.fits" for n in range(59, 64)][:3],
+    # },
 
-    "GS HAM 1x1 i-band": {
-        "bias": [f"S20171204S{n:04d}.fits" for n in range(22, 27)] +
-                [f"S20171206S{n:04d}.fits" for n in range(128, 133)],
-        "flat": [f"S20171206S{n:04d}.fits" for n in range(120, 128)],
-        "sci": [f"S20171205S{n:04d}.fits" for n in range(62, 77)],
-    },
+    # "GS_HAM_1x1_i-band": {
+    #     "bias": [f"S20171204S{n:04d}.fits" for n in range(22, 27)] +
+    #             [f"S20171206S{n:04d}.fits" for n in range(128, 133)],
+    #     "flat": [f"S20171206S{n:04d}.fits" for n in range(120, 128)],
+    #     "sci": [f"S20171205S{n:04d}.fits" for n in range(62, 77)],
+    # },
 
-    "GS HAM 2x2 i-band std": {
-        "bias": [f"S20171204S{n:04d}.fits" for n in range(37, 42)],
-        "flat": [f"S20171120S{n:04d}.fits" for n in range(129, 140)],
-        "std": ["S20171205S0077.fits"],
-    },
+    # "GS_HAM_2x2_i-band_std": {
+    #     "bias": [f"S20171204S{n:04d}.fits" for n in range(37, 42)],
+    #     "flat": [f"S20171120S{n:04d}.fits" for n in range(131, 140)],
+    #     "std": ["S20171205S0077.fits"],
+    # },
 
 }
 
 
 @pytest.mark.parametrize("test_case", datasets.keys())
 def test_reduce_image(change_working_dir, test_case):
-    print(test_case, '-' * 50)
-    for key, filenames in datasets[test_case].items():
-        for filename in filenames:
-            path = download_from_archive(filename)
-            ad = astrodata.open(path)
-            print(ad.filename, ad.observation_class(),
-                  ad.object(), ad.detector_x_bin(), ad.detector_y_bin())
+    """
+    Tests that we can run all the data reduction steps on a complete dataset.
 
-    with change_working_dir():
-        pass
+    Parameters
+    ----------
+    change_working_dir : fixture
+    test_case : str
+    """
+    with change_working_dir(test_case):
+
+        cals = []
+
+        # Reducing bias
+        bias_filenames = datasets[test_case]["bias"]
+        bias_paths = [download_from_archive(f) for f in bias_filenames]
+        cals = reduce(bias_paths, f"bias_{test_case}", cals, save_to="processed_bias")
+
+        # Reducing flats
+        flat_filenames = datasets[test_case]["flat"]
+        flat_paths = [download_from_archive(f) for f in flat_filenames]
+        cals = reduce(flat_paths, f"flat_{test_case}", cals, save_to="processed_flat")
+
+        # Reducing standard stars
+        if "std" in datasets[test_case]:
+            std_filenames = datasets[test_case]["std"]
+            std_paths = [download_from_archive(f) for f in std_filenames]
+            cals = reduce(std_paths, f"std_{test_case}", cals)
+
+        # Reducing science
+        if "sci" in datasets[test_case]:
+            sci_filenames = datasets[test_case]["sci"]
+            sci_paths = [download_from_archive(f) for f in sci_filenames]
+            cals = reduce(
+                sci_paths, f"fringe_{test_case}", cals,
+                recipe_name='makeProcessedFringe', save_to="processed_fringe")
+            _ = reduce(sci_paths, f"sci_{test_case}", cals)
 
 
-# noinspection PyPep8Naming
-@pytest.mark.skip('TODO: Reactivate me')
-@pytest.mark.integtest
-def test_reduce_image_GN_HAM_2x2_z(path_to_inputs):
+def reduce(file_list, label, calib_files, recipe_name=None, save_to=None,
+           user_pars=[]):
+    """
+    Helper function used to prevent replication of code.
+
+    Parameters
+    ----------
+    file_list : list
+        List of files that will be reduced.
+    label : str
+        Labed used on log files name.
+    calib_files : list
+        List of calibration files properly formatted for DRAGONS Reduce().
+    recipe_name : str, optional
+        Name of the recipe used to reduce the data.
+    save_to : str, optional
+        Stores the calibration files locally in a list.
+    user_pars : list, optional
+        List of user parameters
+
+    Returns
+    -------
+    list : an updated list of calibration files
+    """
     objgraph = pytest.importorskip("objgraph")
 
-    logutils.config(file_name='gmos_test_reduce_image_GN_HAM_2x2_z.log')
+    logutils.config(file_name=f"test_image_{label}.log")
+    r = Reduce()
+    r.files = file_list
+    r.ucals = normalize_ucals(r.files, calib_files)
 
-    calib_files = []
+    if recipe_name:
+        r.recipename = recipe_name
 
-    raw_subdir = 'GMOS/GN-2017B-LP-15'
-    all_files = sorted(glob.glob(
-        os.path.join(path_to_inputs, raw_subdir, '*.fits')))
-    assert len(all_files) > 1
+    r.runr()
 
-    list_of_bias = dataselect.select_data(all_files, ['BIAS'], [])
+    if save_to:
+        calib_files.append(f'{save_to}:{r.output_filenames[0]}')
 
-    expr = dataselect.expr_parser('filter_name=="z"')
-    list_of_z_flats = dataselect.select_data(all_files, ['TWILIGHT'], [], expr)
+    # check that we are not leaking objects
+    assert len(objgraph.by_type('NDAstroData')) == 0
 
-    expr = dataselect.expr_parser(
-        'observation_class=="science" and filter_name=="z"'
-    )
-    list_of_science = dataselect.select_data(all_files, [], ['CAL'], expr)
-
-    def reduce(filelist, saveto=None, label='', calib_files=None,
-               recipename=None):
-        red = Reduce()
-        assert len(red.files) == 0
-        red.files.extend(filelist)
-        assert len(red.files) == len(filelist)
-        if calib_files:
-            red.ucals = normalize_ucals(red.files, calib_files)
-        if recipename:
-            red.recipename = recipename
-        red.runr()
-        if saveto:
-            calib_files.append(f'{saveto}:{red.output_filenames[0]}')
-
-        # check that we are not leaking objects
-        assert len(objgraph.by_type('NDAstroData')) == 0
-
-    reduce(list_of_bias, saveto='processed_bias', label='bias',
-           calib_files=calib_files)
-    reduce(list_of_z_flats, saveto='processed_flat', label='flat',
-           calib_files=calib_files)
-
-    # If makeFringe is included in the science recipe, this can be omitted:
-    reduce(list_of_science, saveto='processed_fringe', label='fringe',
-           calib_files=calib_files, recipename='makeProcessedFringe')
-
-    reduce(list_of_science, label='science', calib_files=calib_files)
+    return calib_files
 
 
 # noinspection PyPep8Naming
