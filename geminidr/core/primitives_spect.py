ac66df66e4a7c133cb1205cac080f8e582d21a75
chris-simpson <csimpson@gemini.edu>
2020-08-12 15:19:44 -1000

    calculateSensitivity(): don't add datapoints to sensitivity function if there are zero or negative counts

--- geminidr/core/primitives_spect.py
+++ geminidr/core/primitives_spect.py
@@ -322,10 +322,11 @@ class Spect(PrimitivesBASE):
                         # Regardless of whether FLUX column is f_nu or f_lambda
                         flux = fluxdens.to(u.Unit('erg cm-2 s-1 nm-1'),
                                            equivalencies=u.spectral_density(w0)) * dw.to(u.nm)
-                        wave.append(w0)
-                        # This is (counts/s) / (erg/cm^2/s), in magnitudes (like IRAF)
-                        zpt.append(u.Magnitude(data / flux))
-                        zpt_err.append(u.Magnitude(1 + np.sqrt(variance) / data))
+                        if data > 0:
+                            wave.append(w0)
+                            # This is (counts/s) / (erg/cm^2/s), in magnitudes (like IRAF)
+                            zpt.append(u.Magnitude(data / flux))
+                            zpt_err.append(u.Magnitude(1 + np.sqrt(variance) / data))
 
                 # TODO: Abstract to interactive fitting
                 wave = array_from_list(wave, unit=u.nm)
