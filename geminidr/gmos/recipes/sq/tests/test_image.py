17f65b37a5557353f2fcaa62f8228fff3c67a9c6
Bruno Quint <b1quint@gmail.com>
2020-08-31 16:48:28 -0400

    Updated test_image.py - Skip test that can't run in Jenkins for now

--- geminidr/gmos/recipes/sq/tests/test_image.py
+++ geminidr/gmos/recipes/sq/tests/test_image.py
@@ -56,6 +56,7 @@ datasets = {
 }
 
 
+@pytest.mark.skip("Can't run in Jenkins - Need more investigation")
 @pytest.mark.integration_test
 @pytest.mark.dragons_remote_data
 @pytest.mark.parametrize("test_case", datasets.keys())
