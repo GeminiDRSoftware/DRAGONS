7d14773862518b72a0a23f90cca34474c613fb03
chris-simpson <csimpson@gemini.edu>
2020-09-25 10:34:34 -1000

    test for shiftImages()

--- geminidr/core/tests/test_resample.py
+++ geminidr/core/tests/test_resample.py
@@ -7,7 +7,11 @@ import os
 from copy import deepcopy
 import numpy as np
 
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
 from geminidr.niri.primitives_niri_image import NIRIImage
+
 
 @pytest.mark.parametrize('trim_data', (False, True))
 @pytest.mark.parametrize('file_write', (False, True))
@@ -29,11 +33,14 @@ def test_shift_images(trim_data, file_write, path_to_outputs):
     max_yoff = max(off[1] for off in offsets)
 
     orig_adinputs = []
-    for xoff, yoff in offsets:
-        ad = astrofaker.create('NIRI')
+    coords = []
+    for i, (xoff, yoff) in enumerate(offsets, start=1):
+        ad = astrofaker.create('NIRI', filename=f'test{i}.fits')
         ad.init_default_extensions()
-        ad[0].add_star(amplitude=1000, x=512-xoff, y=512-yoff)
+        x, y = 512 - xoff, 512 - yoff
+        ad[0].add_star(amplitude=1000, x=x, y=y)
         orig_adinputs.append(ad)
+        coords.append(SkyCoord(*ad[0].wcs(x, y)), unit=u.deg)
 
     if file_write:
         shifts_par = os.path.join(path_to_outputs, 'shifts.lis')
@@ -48,12 +55,15 @@ def test_shift_images(trim_data, file_write, path_to_outputs):
     p = NIRIImage(adinputs)
     p.shiftImages(shifts=shifts_par, trim_data=trim_data)
 
-    for ad in p.streams['main']:
+    for ad, coord in zip(p.streams['main'], coords):
         shape = (1024, 1024) if trim_data else (1024 + max_yoff - min_yoff,
                                                 1024 + max_xoff - min_xoff)
         assert ad[0].shape == shape
         peak_loc = (512, 512) if trim_data else (512 - min_yoff, 512 - min_xoff)
-        assert np.unravel_index(ad[0].data.argmax(), ad[0].shape) == peak_loc
+        y, x = np.unravel_index(ad[0].data.argmax(), ad[0].shape)
+        assert (y, x) == peak_loc
+        new_coord = SkyCoord(*ad[0].wcs(x, y), unit=u.deg)
+        assert coord.separation(new_coord) < 1e-6 * u.arcsec  # should be identical
 
     if file_write:
         os.remove(shifts_par)
