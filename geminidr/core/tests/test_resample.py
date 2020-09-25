d2c007f6a9502ef002e1dc971b2f456f2c0e2acf
chris-simpson <csimpson@gemini.edu>
2020-09-24 17:08:57 -1000

    test for shiftImages()

---
+++ geminidr/core/tests/test_resample.py
@@ -0,0 +1,59 @@
+"""
+Tests for primitives_resample
+"""
+import pytest
+
+import os
+from copy import deepcopy
+import numpy as np
+
+from geminidr.niri.primitives_niri_image import NIRIImage
+
+@pytest.mark.parametrize('trim_data', (False, True))
+@pytest.mark.parametrize('file_write', (False, True))
+def test_shift_images(trim_data, file_write, path_to_outputs):
+    """
+    Creates several fake AD objects with a single source in different
+    locations and then shifts them. Checks that the output image sizes are
+    correct and shifts are correct from the location of the brightest pixel
+
+    This tests trimming and not trimming images, and passing the shifts
+    via a string and a file on disk.
+    """
+    astrofaker = pytest.importorskip("astrofaker")
+
+    offsets = ((-10, -10), (-10, 10), (10, 10), (10, -10), (0, 0))
+    min_xoff = min(off[0] for off in offsets)
+    min_yoff = min(off[1] for off in offsets)
+    max_xoff = max(off[0] for off in offsets)
+    max_yoff = max(off[1] for off in offsets)
+
+    orig_adinputs = []
+    for xoff, yoff in offsets:
+        ad = astrofaker.create('NIRI')
+        ad.init_default_extensions()
+        ad[0].add_star(amplitude=1000, x=512-xoff, y=512-yoff)
+        orig_adinputs.append(ad)
+
+    if file_write:
+        shifts_par = os.path.join(path_to_outputs, 'shifts.lis')
+        f = open(shifts_par, 'w')
+        for xoff, yoff in offsets:
+            f.write(f'{xoff} {yoff}\n')
+        f.close()
+    else:
+        shifts_par = ':'.join([f'{xoff},{yoff}' for xoff, yoff in offsets])
+
+    adinputs = [deepcopy(ad) for ad in orig_adinputs]
+    p = NIRIImage(adinputs)
+    p.shiftImages(shifts=shifts_par, trim_data=trim_data)
+
+    for ad in p.streams['main']:
+        shape = (1024, 1024) if trim_data else (1024 + max_yoff - min_yoff,
+                                                1024 + max_xoff - min_xoff)
+        assert ad[0].shape == shape
+        peak_loc = (512, 512) if trim_data else (512 - min_yoff, 512 - min_xoff)
+        assert np.unravel_index(ad[0].data.argmax(), ad[0].shape) == peak_loc
+
+    if file_write:
+        os.remove(shifts_par)
