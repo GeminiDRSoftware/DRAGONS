0a509ab0ed7618c99c30415363d9d66473ec29e0
Simon Conseil <contact@saimon.org>
2020-10-05 20:10:25 -0300

    Parametrize parameters

--- geminidr/gmos/tests/spect/test_sky_correct_from_slit.py
+++ geminidr/gmos/tests/spect/test_sky_correct_from_slit.py
@@ -13,28 +13,26 @@ import geminidr
 import matplotlib.pyplot as plt
 import numpy as np
 import pytest
+from astrodata.testing import download_from_archive
 from astropy import table
 from geminidr.gmos import primitives_gmos_spect
 from gempy.utils import logutils
+from recipe_system.reduction.coreReduce import Reduce
 from recipe_system.testing import ref_ad_factory  # noqa
 
 # Test parameters -------------------------------------------------------------
 # Each test input filename contains the original input filename with
 # "_aperturesTraced" suffix
 test_datasets = [
-    "N20180508S0021_aperturesTraced.fits",  # B600 720
-    "N20180509S0010_aperturesTraced.fits",  # R400 900
-    "N20180516S0081_aperturesTraced.fits",  # R600 860
-    "N20190201S0163_aperturesTraced.fits",  # B600 530
-    "N20190313S0114_aperturesTraced.fits",  # B600 482
-    "N20190427S0123_aperturesTraced.fits",  # R400 525
-    "N20190427S0126_aperturesTraced.fits",  # R400 625
-    "N20190427S0127_aperturesTraced.fits",  # R400 725
-    "N20190427S0141_aperturesTraced.fits",  # R150 660
+    ("N20180508S0021_aperturesTraced.fits", dict(order=5, grow=2)),  # B600 720
+    ("N20180509S0010_aperturesTraced.fits", dict(order=5, grow=2)),  # R400 900
+    ("N20180516S0081_aperturesTraced.fits", dict(order=5, grow=2)),  # R600 860
+    ("N20190427S0126_aperturesTraced.fits", dict(order=5, grow=2)),  # R400 625
+    ("N20190427S0127_aperturesTraced.fits", dict(order=5, grow=2)),  # R400 725
+    ("N20190427S0141_aperturesTraced.fits", dict(order=5, grow=2)),  # R150 660
 ]
 
 PLOT = os.getenv('PLOT')
-
 
 # Tests Definitions -----------------------------------------------------------
 
@@ -117,18 +115,18 @@ def test_sky_correct_with_region(path_to_inputs):
 
 @pytest.mark.gmosls
 @pytest.mark.preprocessed_data
-@pytest.mark.parametrize("ad", test_datasets, indirect=True)
-def test_regression_extract_1d_spectra(ad, change_working_dir,
-                                       ref_ad_factory):  # noqa
+@pytest.mark.parametrize("filename, params", test_datasets)
+def test_regression_extract_1d_spectra(filename, params, change_working_dir,
+                                       path_to_inputs, ref_ad_factory):  # noqa
+
+    path = os.path.join(path_to_inputs, filename)
+    ad = astrodata.open(path)
 
     with change_working_dir():
-
-        logutils.config(
-            file_name='log_regression_{}.txt'.format(ad.data_label()))
-
+        logutils.config(file_name=f'log_regression_{ad.data_label()}.txt')
         p = primitives_gmos_spect.GMOSSpect([ad])
         p.viewer = geminidr.dormantViewer(p, None)
-        p.skyCorrectFromSlit(order=5, grow=2)
+        p.skyCorrectFromSlit(**params)
         sky_subtracted_ad = p.writeOutputs().pop()
 
     ref_ad = ref_ad_factory(sky_subtracted_ad.filename)
@@ -138,34 +136,6 @@ def test_regression_extract_1d_spectra(ad, change_working_dir,
 
 
 # Local Fixtures and Helper Functions -----------------------------------------
-@pytest.fixture(scope='function')
-def ad(path_to_inputs, request):
-    """
-    Returns the pre-processed spectrum file.
-
-    Parameters
-    ----------
-    path_to_inputs : pytest.fixture
-        Fixture defined in :mod:`astrodata.testing` with the path to the
-        pre-processed input file.
-    request : pytest.fixture
-        PyTest built-in fixture containing information about parent test.
-
-    Returns
-    -------
-    AstroData
-        Input spectrum processed up to right before the `calculateSensitivity`
-        primitive.
-    """
-    filename = request.param
-    path = os.path.join(path_to_inputs, filename)
-
-    if os.path.exists(path):
-        ad = astrodata.open(path)
-    else:
-        raise FileNotFoundError(path)
-
-    return ad
 
 
 def _add_aperture_table(ad, center):
@@ -184,25 +154,20 @@ def _add_aperture_table(ad, center):
     width = ad[0].shape[1]
 
     aperture = table.Table(
-        [[1],  # Number
-         [1],  # ndim
-         [0],  # degree
-         [0],  # domain_start
-         [width - 1],  # domain_end
-         [center],  # c0
-         [-5],  # aper_lower
-         [5],  # aper_upper
-         ],
+        [
+            [1],  # Number
+            [1],  # ndim
+            [0],  # degree
+            [0],  # domain_start
+            [width - 1],  # domain_end
+            [center],  # c0
+            [-5],  # aper_lower
+            [5],  # aper_upper
+        ],
         names=[
-            'number',
-            'ndim',
-            'degree',
-            'domain_start',
-            'domain_end',
-            'c0',
-            'aper_lower',
-            'aper_upper']
-    )
+            'number', 'ndim', 'degree', 'domain_start', 'domain_end', 'c0',
+            'aper_lower', 'aper_upper'
+        ])
 
     ad[0].APERTURE = aperture
     return ad
@@ -219,22 +184,32 @@ def create_inputs_recipe():
     a new folder called "dragons_test_inputs". The sub-directory structure
     should reflect the one returned by the `path_to_inputs` fixture.
     """
-    import os
-
-    from astrodata.testing import download_from_archive
-    from gempy.utils import logutils
-    from recipe_system.reduction.coreReduce import Reduce
 
     input_data = {
-        "N20180508S0021.fits": {"arc": "N20180615S0409.fits", "center": 244},
-        "N20180509S0010.fits": {"arc": "N20180509S0080.fits", "center": 259},
-        "N20180516S0081.fits": {"arc": "N20180516S0214.fits", "center": 255},
-        "N20190201S0163.fits": {"arc": "N20190201S0162.fits", "center": 255},
-        "N20190313S0114.fits": {"arc": "N20190313S0132.fits", "center": 254},
-        "N20190427S0123.fits": {"arc": "N20190427S0266.fits", "center": 260},
-        "N20190427S0126.fits": {"arc": "N20190427S0267.fits", "center": 259},
-        "N20190427S0127.fits": {"arc": "N20190427S0268.fits", "center": 258},
-        "N20190427S0141.fits": {"arc": "N20190427S0270.fits", "center": 264},
+        "N20180508S0021.fits": {
+            "arc": "N20180615S0409.fits",
+            "center": 244
+        },
+        "N20180509S0010.fits": {
+            "arc": "N20180509S0080.fits",
+            "center": 259
+        },
+        "N20180516S0081.fits": {
+            "arc": "N20180516S0214.fits",
+            "center": 255
+        },
+        "N20190427S0126.fits": {
+            "arc": "N20190427S0267.fits",
+            "center": 259
+        },
+        "N20190427S0127.fits": {
+            "arc": "N20190427S0268.fits",
+            "center": 258
+        },
+        "N20190427S0141.fits": {
+            "arc": "N20190427S0270.fits",
+            "center": 264
+        },
     }
 
     root_path = os.path.join("./dragons_test_inputs/")
@@ -276,7 +251,10 @@ def create_inputs_recipe():
         _ad = _add_aperture_table(_ad, pars['center'])
 
         p = primitives_gmos_spect.GMOSSpect([_ad])
-        p.traceApertures(trace_order=2, nsum=20, step=10, max_shift=0.09,
+        p.traceApertures(trace_order=2,
+                         nsum=20,
+                         step=10,
+                         max_shift=0.09,
                          max_missed=5)
 
         os.chdir("inputs/")
