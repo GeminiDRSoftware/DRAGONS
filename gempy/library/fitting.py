090b42002ea72426afbb446d18ced84f1c0fa552
James E.H. Turner <jturner@gemini.edu>
2020-11-11 18:25:09 -0300

    Reimplement fit_1D function as a class with an identical API.
    This work-in-progress commit exists purely to separate indentation-related
    reformatting from functional changes in a way that is easy to check using the
    existing tests.

--- gempy/library/fitting.py
+++ gempy/library/fitting.py
@@ -22,17 +22,15 @@ function_map = {
 }
 
 
-def fit_1D(image, weights=None, function='legendre', order=1, axis=-1,
-           lsigma=3.0, hsigma=3.0, iterations=0, grow=False, regions=None,
-           plot=False):
+class fit_1D:
     """
-    A routine for evaluating the result of fitting 1D polynomials to each
+    A class for evaluating the result of fitting 1D polynomials to each
     vector along some axis of an N-dimensional image array, with iterative
     pixel rejection and re-fitting, similar to IRAF's fit1d.
 
-    Interactivity is currently not supported, other than plotting for
-    debugging purposes. This still needs to be made to work for a single 1D
-    input array.
+    The only interactivity currently supported is plotting for debugging
+    purposes; DRAGONS implements interaction via a separate wrapper.
+
 
     Parameters
     ----------
@@ -96,194 +94,205 @@ def fit_1D(image, weights=None, function='legendre', order=1, axis=-1,
 
     """
 
-    # Convert array-like input to a MaskedArray internally, to ensure it's an
-    # `ndarray` instance and that any mask gets passed through to the fitter:
-    image = np.ma.masked_array(image)
+    def __new__(cls, image, weights=None, function='legendre', order=1,
+                axis=-1, lsigma=3.0, hsigma=3.0, iterations=0, grow=False,
+                regions=None, plot=False):
 
-    # Determine how many pixels we're fitting each vector over:
-    try:
-        npix = image.shape[axis]
-    except IndexError:
-        raise ValueError('axis={0} out of range for input shape {1}'
-                         .format(axis, image.shape))
+        # Convert array-like input to a MaskedArray internally, to ensure it's
+        # an `ndarray` instance and that any mask gets passed through to the
+        # fitter:
+        image = np.ma.masked_array(image)
 
-    # Define pixel grid to fit on:
-    points = np.arange(npix, dtype=np.int16)
+        # Determine how many pixels we're fitting each vector over:
+        try:
+            npix = image.shape[axis]
+        except IndexError:
+            raise ValueError('axis={0} out of range for input shape {1}'
+                             .format(axis, image.shape))
 
-    # Define the model to be fitted:
-    func, funcargs = function_map[function]
-    try:
-        astropy_model = issubclass(func, Model)
-    except TypeError:
-        astropy_model = False
+        # Define pixel grid to fit on:
+        points = np.arange(npix, dtype=np.int16)
 
-    # The spline fitting uses an adaptive criterion when order=None, but the
-    # AstroPy polynomials require an integer degree so map None to default:
-    if order is None and func is not UnivariateSplineWithOutlierRemoval:
-        order = 1
+        # Define the model to be fitted:
+        func, funcargs = function_map[function]
+        try:
+            astropy_model = issubclass(func, Model)
+        except TypeError:
+            astropy_model = False
 
-    # Parse the sample regions:
-    slices = cartesian_regions_to_slices(regions)
+        # The spline fitting uses an adaptive criterion when order=None, but
+        # AstroPy polynomials require an integer degree so map None to default:
+        if order is None and func is not UnivariateSplineWithOutlierRemoval:
+            order = 1
 
-    # Convert user regions to a Boolean mask for slicing:
-    user_reg = np.zeros(npix, dtype=np.bool)
-    for _slice in slices:
-        user_reg[_slice] = True
+        # Parse the sample regions:
+        slices = cartesian_regions_to_slices(regions)
 
-    # To support fitting any axis of an N-dimensional array, we must flatten
-    # all the other dimensions into a single "model set axis" first; it's
-    # about 10-15% more efficient to stack astropy models along the second
-    # axis (for a 3k x 2k image), because that's what the linear fitter does
-    # internally, but for general callable functions we stack along the first
-    # axis in order to loop over the fits easily:
-    if astropy_model:
-        ax_before = 0
-        stack_shape = (npix,) if image.size == npix else (npix, -1)
-    else:
-        ax_before = image.ndim
-        stack_shape = (-1, npix)
-    image = np.rollaxis(image, axis, ax_before)
-    tmpshape = image.shape
-    image = image.reshape(stack_shape)
-    if weights is not None:
-        weights = np.rollaxis(weights, axis, ax_before).reshape(stack_shape)
+        # Convert user regions to a Boolean mask for slicing:
+        user_reg = np.zeros(npix, dtype=np.bool)
+        for _slice in slices:
+            user_reg[_slice] = True
 
-    # Create an empty, full-sized mask within which the fitter will populate
-    # only the user-specified region(s):
-    mask = np.zeros(image.shape, dtype=bool)
-
-    # Initialize an array with same dtype as input to accumulate fit values
-    # into when looping & because modelling always seems to return float64:
-    fitvals = np.zeros_like(image.data)
-
-    # Branch pending on whether we're using an AstroPy model or some other
-    # supported fitting function (principally splines):
-    if astropy_model:
-
-        # A single model is treated specially because FittingWithOutlierRemoval
-        # fails for "1-model sets" and it should be more efficient anyway:
-        image_to_fit = image
-        if image.ndim == 1:
-            n_models = 1
-        elif image.mask is np.ma.nomask:
-            n_models = image.shape[1]
-        else:
-            # remove fully masked columns otherwise this will lead to
-            # Runtime warnings from Numpy because of divisions by zero.
-            good_cols = (~image.mask).sum(axis=0) > order
-            n_models = np.sum(good_cols)
-            if n_models < image.shape[1]:
-                image_to_fit = image[:, good_cols]
-                weights = weights[:, good_cols]
-
-        model_set = func(degree=(order - 1), n_models=n_models,
-                         model_set_axis=(None if n_models == 1 else 1),
-                         **funcargs)
-
-        # Configure iterative linear fitter with rejection:
-        fitter = fitting.FittingWithOutlierRemoval(
-            fitting.LinearLSQFitter(),
-            sigma_clip,
-            niter=iterations,
-            # additional args are passed to the outlier_func, i.e. sigma_clip
-            sigma_lower=lsigma,
-            sigma_upper=hsigma,
-            maxiters=1,
-            cenfunc='mean',
-            stdfunc='std',
-            grow=grow  # requires AstroPy 4.2 (#10613)
-        )
-
-        # Fit the pixel data with rejection of outlying points:
-        fitted_model, fitted_mask = fitter(
-            model_set,
-            points[user_reg], image_to_fit[user_reg],
-            weights=None if weights is None else weights[user_reg]
-        )
-
-        # Determine the evaluated model values we want to return:
-        if image.ndim > 1 and n_models < image.shape[1]:
-            # If we removed bad columns, we now need to fill them properly
-            # in the output array
-            fitvals[:, good_cols] = fitted_model(points, model_set_axis=False)
-            # this is quite ugly, but seems the best way to assign to an
-            # array with a mask on both dimensions. This is equivalent to:
-            #   mask[user_reg, masked_cols] = fitted_mask
-            mask[user_reg[:, None] & good_cols] = fitted_mask.flat
-        else:
-            fitvals[:] = fitted_model(points, model_set_axis=False)
-            mask[user_reg] = fitted_mask
-
-    else:
-
-        # If there are no weights, produce a None for every row:
-        weights = iter(lambda: None, True) if weights is None else weights
-
-        user_masked = ~user_reg
-
-        for n, (imrow, wrow) in enumerate(zip(image, weights)):
-
-            # Deal with weights being None or all undefined in regions without
-            # data (should we allow for NaNs as well?):
-            wrow = wrow[user_reg] if wrow is not None and np.any(wrow) else None
-
-            # Could generalize this a bit further using another dict to map
-            # parameter names in function_map, eg. weights -> w?
-            fitted_model = func(points[user_reg], imrow[user_reg], order=order,
-                                w=wrow, niter=iterations, grow=int(grow),
-                                lsigma=lsigma, hsigma=hsigma, maxiters=1,
-                                **funcargs)
-
-            # Determine model values to be returned. This is somewhat specific
-            # to the spline fitter and using fitted_model.data only improves
-            # performance by ~1% compared with just re-evaluating everything,
-            # but it's the only way to get the mask:
-            fitvals[n][user_reg] = fitted_model.data
-            mask[n][user_reg] = fitted_model.mask
-            fitvals[n][user_masked] = fitted_model(points[user_masked])
-            # fitvals[n] = fitted_model(points)
-
-    # TEST: Plot the fit:
-    if plot:
-        import matplotlib.pyplot as plt
-        fig, ax = plt.subplots()
-        points1 = points+1
+        # To support fitting any axis of an N-dimensional array, we must
+        # flatten all the other dimensions into a single "model set axis"
+        # first; it's about 10-15% more efficient to stack astropy models along
+        # the second axis (for a 3k x 2k image), because that's what the linear
+        # fitter does internally, but for general callable functions we stack
+        # along the first axis in order to loop over the fits easily:
         if astropy_model:
-            idx = slice(None) if n_models == 1 else \
-                  (slice(None), image.shape[1] // 4)
+            ax_before = 0
+            stack_shape = (npix,) if image.size == npix else (npix, -1)
         else:
-            idx = (image.shape[0] // 4, slice(None))
-        imrow, maskrow, fitrow = image.data[idx], mask[idx], fitvals[idx]
-        ax.plot(points1, imrow, 'b.')
-        ax.plot(points1[maskrow], imrow[maskrow], 'r.')
-        ax.plot(points1, fitrow, 'k')
+            ax_before = image.ndim
+            stack_shape = (-1, npix)
+        image = np.rollaxis(image, axis, ax_before)
+        tmpshape = image.shape
+        image = image.reshape(stack_shape)
+        if weights is not None:
+            weights = np.rollaxis(weights, axis, ax_before).reshape(stack_shape)
 
-        # Find starting index of each selected or omitted range in user_reg
-        # (considering regions outside the array False and starting from the
-        # first True), then subtract 1 from every second index, pair them to
-        # get (start, end) ranges and convert back to 1-indexing for display.
-        # Don't use slice objects directly because their limits can be None,
-        # adjacent or overlapping, whereas here we want a minimal set of
-        # unique ranges as applied to the data:
-        reg_changes = np.where(np.concatenate((user_reg[:1],
-                                               user_reg[1:] != user_reg[:-1],
-                                               user_reg[-1:])))[0]
-        mixed_coords = ax.get_xaxis_transform()  # x in data units, y in frac.
-        # The extra +0.001 here avoids having identical start & end values for
-        # single-pixel ranges, otherwise they don't get drawn:
-        for start, end in zip(reg_changes[::2]+1, reg_changes[1::2]+0.001):
-            ax.annotate('', xy=(start, 0), xytext=(end, 0),
-                        xycoords=mixed_coords, textcoords=mixed_coords,
-                        arrowprops=dict(arrowstyle='|-|,widthA=0.5,widthB=0.5',
-                                        shrinkA=0., shrinkB=0., color='green'))
+        # Create an empty, full-sized mask within which the fitter will
+        # populate only the user-specified region(s):
+        mask = np.zeros(image.shape, dtype=bool)
 
-        plt.show()
+        # Initialize an array with same dtype as input to accumulate fit values
+        # into when looping & because modelling always seems to return float64:
+        fitvals = np.zeros_like(image.data)
 
-    # Restore the ordering & shape of the input array:
-    fitvals = fitvals.reshape(tmpshape)
-    if astropy_model:
-        fitvals = np.rollaxis(fitvals, 0, (axis + 1) or fitvals.ndim)
-    else:
-        fitvals = np.rollaxis(fitvals, -1, axis)
+        # Branch pending on whether we're using an AstroPy model or some other
+        # supported fitting function (principally splines):
+        if astropy_model:
 
-    return fitvals
+            # Treat single model specially because FittingWithOutlierRemoval
+            # fails for "1-model sets" and it should be more efficient anyway:
+            image_to_fit = image
+            if image.ndim == 1:
+                n_models = 1
+            elif image.mask is np.ma.nomask:
+                n_models = image.shape[1]
+            else:
+                # remove fully masked columns otherwise this will lead to
+                # Runtime warnings from Numpy because of divisions by zero.
+                good_cols = (~image.mask).sum(axis=0) > order
+                n_models = np.sum(good_cols)
+                if n_models < image.shape[1]:
+                    image_to_fit = image[:, good_cols]
+                    weights = weights[:, good_cols]
+
+            model_set = func(degree=(order - 1), n_models=n_models,
+                             model_set_axis=(None if n_models == 1 else 1),
+                             **funcargs)
+
+            # Configure iterative linear fitter with rejection:
+            fitter = fitting.FittingWithOutlierRemoval(
+                fitting.LinearLSQFitter(),
+                sigma_clip,
+                niter=iterations,
+                # additional args are passed to outlier_func, i.e. sigma_clip
+                sigma_lower=lsigma,
+                sigma_upper=hsigma,
+                maxiters=1,
+                cenfunc='mean',
+                stdfunc='std',
+                grow=grow  # requires AstroPy 4.2 (#10613)
+            )
+
+            # Fit the pixel data with rejection of outlying points:
+            fitted_model, fitted_mask = fitter(
+                model_set,
+                points[user_reg], image_to_fit[user_reg],
+                weights=None if weights is None else weights[user_reg]
+            )
+
+            # Determine the evaluated model values we want to return:
+            if image.ndim > 1 and n_models < image.shape[1]:
+                # If we removed bad columns, we now need to fill them properly
+                # in the output array
+                fitvals[:, good_cols] = fitted_model(points,
+                                                     model_set_axis=False)
+                # this is quite ugly, but seems the best way to assign to an
+                # array with a mask on both dimensions. This is equivalent to:
+                #   mask[user_reg, masked_cols] = fitted_mask
+                mask[user_reg[:, None] & good_cols] = fitted_mask.flat
+            else:
+                fitvals[:] = fitted_model(points, model_set_axis=False)
+                mask[user_reg] = fitted_mask
+
+        else:
+
+            # If there are no weights, produce a None for every row:
+            weights = iter(lambda: None, True) if weights is None else weights
+
+            user_masked = ~user_reg
+
+            for n, (imrow, wrow) in enumerate(zip(image, weights)):
+
+                # Deal with weights being None or all undefined in regions
+                # without data (should we allow for NaNs as well?):
+                wrow = (wrow[user_reg] if wrow is not None and np.any(wrow)
+                        else None)
+
+                # Could generalize this a bit further using another dict to map
+                # parameter names in function_map, eg. weights -> w?
+                fitted_model = func(points[user_reg], imrow[user_reg],
+                                    order=order, w=wrow, niter=iterations,
+                                    grow=int(grow), lsigma=lsigma,
+                                    hsigma=hsigma, maxiters=1, **funcargs)
+
+                # Determine model values to be returned. This is somewhat
+                # specific to the spline fitter and using fitted_model.data
+                # only improves performance by ~1% compared with re-evaluating
+                # everything, but it's the only way to get the mask:
+                fitvals[n][user_reg] = fitted_model.data
+                mask[n][user_reg] = fitted_model.mask
+                fitvals[n][user_masked] = fitted_model(points[user_masked])
+                # fitvals[n] = fitted_model(points)
+
+        # TEST: Plot the fit:
+        if plot:
+            import matplotlib.pyplot as plt
+            fig, ax = plt.subplots()
+            points1 = points+1
+            if astropy_model:
+                idx = slice(None) if n_models == 1 else \
+                      (slice(None), image.shape[1] // 4)
+            else:
+                idx = (image.shape[0] // 4, slice(None))
+            imrow, maskrow, fitrow = (image.data[idx], mask[idx], fitvals[idx])
+            ax.plot(points1, imrow, 'b.')
+            ax.plot(points1[maskrow], imrow[maskrow], 'r.')
+            ax.plot(points1, fitrow, 'k')
+
+            # Find starting index of each selected or omitted range in user_reg
+            # (considering regions outside the array False and starting from
+            # the first True), then subtract 1 from every second index, pair
+            # them to get (start, end) ranges and convert back to 1-indexing
+            # for display. Don't use slice objects directly because their
+            # limits can be None, adjacent or overlapping, whereas here we want
+            # a minimal set of unique ranges as applied to the data:
+            reg_changes = np.where(
+                np.concatenate((user_reg[:1],
+                                user_reg[1:] != user_reg[:-1],
+                                user_reg[-1:]))
+            )[0]
+            mixed_coords = ax.get_xaxis_transform()  # x data units, y frac.
+            # The extra +0.001 here avoids having identical start & end values
+            # for single-pixel ranges, otherwise they don't get drawn:
+            for start, end in zip(reg_changes[::2]+1, reg_changes[1::2]+0.001):
+                ax.annotate('', xy=(start, 0), xytext=(end, 0),
+                            xycoords=mixed_coords, textcoords=mixed_coords,
+                            arrowprops=dict(
+                                arrowstyle='|-|,widthA=0.5,widthB=0.5',
+                                shrinkA=0., shrinkB=0., color='green'
+                            ))
+
+            plt.show()
+
+        # Restore the ordering & shape of the input array:
+        fitvals = fitvals.reshape(tmpshape)
+        if astropy_model:
+            fitvals = np.rollaxis(fitvals, 0, (axis + 1) or fitvals.ndim)
+        else:
+            fitvals = np.rollaxis(fitvals, -1, axis)
+
+        return fitvals
