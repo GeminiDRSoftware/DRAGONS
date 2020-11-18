fix Jacobian calculation for flux conservation

gempy/library/transform.py
@@ -790,7 +790,7 @@ class GeoMap:
         transformed = (self._transform(*grids)[::-1] if len(shape) > 1
                        else self._transform(grids))
         #self.coords = [coord.astype(np.float32) for coord in transformed]
-        self.coords = transformed
+        self.coords = transformed  # Y then X
 
     def affinity(self):
         """
@@ -1019,11 +1019,11 @@ class DataGroup:
                     # derivatives so expand the output pixel grid.
                     if conserve:
 
-                        self.log.warning("Flux conservation has not been fully"
+                        self.log.warning("Flux conservation has not been fully "
                                          "tested for non-affine transforms")
 
                         jacobian_shape = tuple(length + 2 for length in trans_output_shape)
-                        transform.append(reduce(Model.__and__, [models.Shift(1)] * ndim))
+                        transform.append(reduce(Model.__and__, [models.Shift(-1)] * ndim))
 
                         # These are the coordinates in the input frame corresponding to each
                         # (subsampled) output pixel in a frame with an additional 1-pixel boundary
@@ -1032,14 +1032,17 @@ class DataGroup:
                         for num_axis in range(ndim):
                             coords = jacobian_mapping.coords[num_axis]
                             for denom_axis in range(ndim):
+                                # We're numerically estimating the partial
+                                # derivatives 2*dx/dx', 2*dx/dy', etc., multiplied
+                                # by 1/subsample
                                 diff_coords = coords - np.roll(coords, 2, axis=denom_axis)
                                 slice_ = [slice(1, -1)] * ndim
                                 slice_[denom_axis] = slice(2, None)
                                 # Account for the fact that we are measuring
                                 # differences in the subsampled plane
-                                det_matrices[num_axis, denom_axis] = 2. / (
-                                    diff_coords[tuple(slice_)].flatten() * subsample)
-                        jfactor = 1. / abs(np.linalg.det(np.moveaxis(det_matrices, -1, 0))).reshape(trans_output_shape)
+                                det_matrices[num_axis, denom_axis] = (0.5 * subsample *
+                                    diff_coords[tuple(slice_)].flatten())
+                        jfactor = abs(np.linalg.det(np.moveaxis(det_matrices, -1, 0))).reshape(trans_output_shape)
                         # Delete the extra Shift(1) and put a better jfactor in the list
                         del transform[-1]
                         self.jfactors[-1] = np.mean(jfactor)