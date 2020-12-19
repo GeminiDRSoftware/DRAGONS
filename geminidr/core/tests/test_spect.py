03963fce1da55becb330481e1778604b76a14452
chris-simpson <csimpson@gemini.edu>
2020-12-18 17:09:33 -1000

    improve robustness of input data to test

--- geminidr/core/tests/test_spect.py
+++ geminidr/core/tests/test_spect.py
@@ -244,7 +244,9 @@ def test_sky_correct_from_slit_with_multiple_sources():
     ad[0].data += source
     ad[0].data += sky(ad[0].data, axis=1)
     ad[0].APERTURE = get_aperture_table(height, width, center=height // 2)
-    ad[0].APERTURE.add_row([2, y1, -3, 3])
+    # Ensure a new row is added correctly, regardless of column order
+    new_row = {'number': 2, 'c0': y1, 'aper_lower': -3, 'aper_upper': 3}
+    ad[0].APERTURE.add_row([new_row[c] for c in ad[0].APERTURE.colnames])
 
     # Running the test ----------------
     p = primitives_spect.Spect([])
@@ -458,6 +460,7 @@ def get_aperture_table(height, width, center=None):
     center = height // 2 if center is None else center
     apmodel = models.Chebyshev1D(degree=0, domain=[0, width-1], c0=center)
     aperture = am.model_to_table(apmodel)
+    aperture['number'] = 1
     aperture['aper_lower'] = -3
     aperture['aper_upper'] = 3
 
