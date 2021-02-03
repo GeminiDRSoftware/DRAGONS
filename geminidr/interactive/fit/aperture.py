e96f044a04d0a0c48d90c757443e1563a6cc2c87
Simon Conseil <contact@saimon.org>
2021-02-03 19:02:47 -0300

    Move classes to the aperture module

--- geminidr/interactive/fit/aperture.py
+++ geminidr/interactive/fit/aperture.py
@@ -1,18 +1,21 @@
-import numpy as np
 from bokeh.layouts import column, row
-from bokeh.models import (Button, CheckboxGroup, Div, Range1d, Select, Slider,
-                          Spacer, Spinner, TextInput)
+from bokeh.models import (BoxAnnotation, Button, CheckboxGroup,
+                          ColumnDataSource, CustomJS, Div, LabelSet, Select,
+                          Slider, Spacer, Span, Spinner, TextInput, Whisker)
 from bokeh.plotting import figure
 
-from geminidr.interactive import interactive, server
+from geminidr.interactive import server
 from geminidr.interactive.controls import Controller
-from geminidr.interactive.interactive import GIApertureModel, GIApertureView
+from geminidr.interactive.interactive import (PrimitiveVisualizer,
+                                              hamburger_helper)
 from gempy.library.tracing import find_apertures
 
 __all__ = ["interactive_find_source_apertures", ]
 
 
 class CustomWidget:
+    """Defines a default handler that set the value on the model."""
+
     def __init__(self, title, model, attr, handler=None):
         self.title = title
         self.attr = attr
@@ -103,7 +106,7 @@ class SelectLine(CustomWidget):
         self.select.value = self.value
 
 
-class FindSourceAperturesModel(GIApertureModel):
+class FindSourceAperturesModel:
     def __init__(self, ext, **aper_params):
         """
         Create an aperture model with the given initial set of inputs.
@@ -113,7 +116,8 @@ class FindSourceAperturesModel(GIApertureModel):
         UI and then recalculate the fit as often as desired.
 
         """
-        super().__init__()
+        self.aperture_id = 1
+        self.listeners = list()
         self.ext = ext
         self._aper_params = aper_params.copy()
         self.reset()
@@ -123,6 +127,10 @@ class FindSourceAperturesModel(GIApertureModel):
     def aper_params(self):
         """Return the actual param dict from the instance attributes."""
         return {name: getattr(self, name) for name in self._aper_params.keys()}
+
+    def add_listener(self, listener):
+        """Add a listener for update to the apertures."""
+        self.listeners.append(listener)
 
     def reset(self):
         """Reset model to its initial values."""
@@ -171,22 +179,7 @@ class FindSourceAperturesModel(GIApertureModel):
         return aperture_id
 
     def adjust_aperture(self, aperture_id, location, start, end):
-        """
-        Adjust an existing aperture by ID to a new range.
-        This will alert all subscribed listeners.
-
-        Parameters
-        ----------
-        aperture_id : int
-            ID of the aperture to adjust
-        location : float
-            X coodinate of the new aperture location
-        start : float
-            X coordinate of the new start of range
-        end : float
-            X coordiante of the new end of range
-
-        """
+        """Adjust an existing aperture by ID to a new range."""
         if location < start or location > end:
             raise ValueError("Location of aperture must be between start and end")
         self.locations[aperture_id-1] = location
@@ -195,14 +188,7 @@ class FindSourceAperturesModel(GIApertureModel):
             listener.handle_aperture(aperture_id, location, start, end)
 
     def delete_aperture(self, aperture_id):
-        """
-        Delete an aperture by ID.
-
-        Parameters
-        ----------
-        aperture_id : int
-            Aperture id to delete
-        """
+        """Delete an aperture by ID."""
         del self.locations[aperture_id-1]
         del self.all_limits[aperture_id-1]
 
@@ -218,11 +204,323 @@ class FindSourceAperturesModel(GIApertureModel):
         self.all_limits = []
 
 
-class FindSourceAperturesVisualizer(interactive.PrimitiveVisualizer):
+class AperturePlotView:
+    """
+    Create a visible glyph-set to show the existance
+    of an aperture on the given figure.  This display
+    will update as needed in response to panning/zooming.
+
+    Parameters
+    ----------
+    fig : :class:`~bokeh.plotting.Figure`
+        Bokeh Figure to attach to
+    aperture_id : int
+        ID of the aperture (for displaying)
+    start : float
+        Start of the x-range for the aperture
+    end : float
+        End of the x-range for the aperture
+    """
+    def __init__(self, fig, aperture_id, location, start, end):
+        self.aperture_id = aperture_id
+
+        self.source = ColumnDataSource({
+            'id': [str(aperture_id)],
+            'end': [end],
+            'location': [location],
+            'start': [start],
+            'whisker_y': [0],
+        })
+
+        if fig.document:
+            fig.document.add_next_tick_callback(
+                lambda: self.build_ui(fig))
+        else:
+            self.build_ui(fig)
+
+    def compute_ymid(self, fig):
+        if fig.y_range.start is not None and fig.y_range.end is not None:
+            ymin = fig.y_range.start
+            ymax = fig.y_range.end
+            return (ymax - ymin)*.8 + ymin
+        else:
+            return 0
+
+    def update_id(self, aperture_id):
+        self.aperture_id = aperture_id
+        self.source.data['id'] = [aperture_id]
+
+    def build_ui(self, fig):
+        """
+        Build the view in the figure.
+
+        This call creates the UI elements for this aperture in the
+        parent figure.  It also wires up event listeners to adjust
+        the displayed glyphs as needed when the view changes.
+
+        Parameters
+        ----------
+        fig : :class:`~bokeh.plotting.Figure`
+            bokeh figure to attach glyphs to
+
+        """
+        ymid = self.compute_ymid(fig)
+
+        self.box = BoxAnnotation(left=self.source.data['start'][0],
+                                 right=self.source.data['end'][0],
+                                 fill_alpha=0.1,
+                                 fill_color='green')
+        fig.add_layout(self.box)
+
+        self.label = LabelSet(source=self.source, x="location", y=ymid,
+                              y_offset=2, text="id")
+        fig.add_layout(self.label)
+
+        self.whisker = Whisker(source=self.source, base="whisker_y",
+                               lower="start", upper="end", dimension='width',
+                               line_color="purple")
+        fig.add_layout(self.whisker)
+
+        self.location = Span(location=self.source.data['location'][0],
+                             dimension='height', line_color='green',
+                             line_dash='dashed', line_width=1)
+        fig.add_layout(self.location)
+
+        self.fig = fig
+
+        fig.y_range.on_change(
+            'start', lambda attr, old, new: self.update_viewport())
+        fig.y_range.on_change(
+            'end', lambda attr, old, new: self.update_viewport())
+
+        # convince the aperture lines to update on zoom
+        fig.y_range.js_on_change(
+            'end', CustomJS(args=dict(plot=fig),
+                            code="plot.properties.renderers.change.emit()"))
+
+    def update_viewport(self):
+        """
+        Update the view in the figure whenever we detect a change in the
+        display area of the view.  By redrawing, we ensure the lines and axis
+        label are in view, at 80% of the way up the visible Y axis.
+        """
+        ymid = self.compute_ymid(self.fig)
+        if ymid:
+            self.label.y = ymid
+            self.source.data['whisker_y'] = [ymid]
+
+    def update(self, location, start, end):
+        """
+        Alter the coordinate range for this aperture. This will adjust the
+        shaded area and the arrows/label for this aperture as displayed on
+        the figure.
+
+        Parameters
+        ----------
+        start : float
+            new starting x coordinate
+        end : float
+            new ending x coordinate
+        """
+        self.box.left = start
+        self.box.right = end
+        self.source.data['start'] = [start]
+        self.source.data['end'] = [end]
+        self.source.data['location'] = [location]
+        self.location.location = location
+        # self.label.x = location+2
+
+    def delete(self):
+        """Delete this aperture from it's view."""
+        # TODO removing causes problems, because bokeh, sigh
+        # TODO could create a list of disabled labels/boxes to reuse instead
+        # of making new ones (if we have one to recycle)
+        self.label.visible = False
+        self.box.visible = False
+        self.location.visible = False
+        self.whisker.visible = False
+
+
+class ApertureLineView:
+    def __init__(self, model, aperture_id, location, start, end):
+        """ Create text inputs for the start, location and end of an aperture.
+
+        Parameters
+        ----------
+        model : :class:`ApertureModel`
+            The model that tracks the apertures and their ranges
+        aperture_id : int
+            The ID of the aperture
+        start, location, end : float
+            The start, location and end of the aperture
+
+        """
+        self.model = model
+        self.aperture_id = aperture_id
+        self.location = location
+        self.start = start
+        self.end = end
+
+        def _del_button_handler():
+            self.model.delete_aperture(self.aperture_id)
+
+        button = Button(label="Del", width=48)
+        button.on_click(_del_button_handler)
+
+        start_input = Spinner(width=96, value=start)
+        location_input = Spinner(width=96, value=location)
+        end_input = Spinner(width=96, value=end)
+
+        def _inputs_handler(attr, old, new):
+            self.location = location_input.value
+            self.start = min(start_input.value, end_input.value)
+            self.end = max(start_input.value, end_input.value)
+            self.model.adjust_aperture(self.aperture_id, self.location,
+                                       self.start, self.end)
+
+        start_input.on_change("value", _inputs_handler)
+        location_input.on_change("value", _inputs_handler)
+        end_input.on_change("value", _inputs_handler)
+
+        self.component = row([Div(align='end'),
+                              start_input,
+                              location_input,
+                              end_input,
+                              button])
+        self.update_title()
+
+    def update_title(self):
+        self.component.children[0].text = f"Aperture {self.aperture_id}"
+
+    def update_viewport(self, start, end):
+        """
+        Respond to a viewport update.
+
+        This checks the visible range of the viewport against the current range
+        of this aperture.  If the aperture is not fully contained within the
+        new visible area, all UI elements are disabled.  If the aperture is in
+        range, the start and stop values for the slider are capped to the
+        visible range.
+
+        Parameters
+        ----------
+        start : float
+            Visible start of x axis
+        end : float
+            Visible end of x axis
+        """
+        disabled = self.start < start or self.end > end
+        for child in self.component.children:
+            child.disabled = disabled
+            # self.slider.children[0].start = start
+            # self.slider.children[0].end = end
+
+
+class ApertureView:
+    """
+    UI elements for displaying the current set of apertures.
+
+    This class manages a set of colored regions on a figure to
+    show where the defined apertures are, along with a numeric
+    ID for each.
+
+    Parameters
+    ----------
+    model : :class:`ApertureModel`
+        Model for tracking the apertures, may be shared across multiple views
+    fig : :class:`~bokeh.plotting.Figure`
+        bokeh plot for displaying the regions
+
+    """
+    def __init__(self, model, fig):
+        # The list of AperturePlotView widget (aperture plots)
+        self.aperture_plots = []
+        # The list of ApertureLineView widget (text inputs)
+        self.aperture_lines = []
+
+        self.fig = fig
+
+        # The hamburger menu, which needs to have access to the aperture line
+        # widgets (inner_controls)
+        self.inner_controls = column()
+        self.inner_controls.height_policy = "auto"
+        self.controls = hamburger_helper("Apertures", self.inner_controls)
+
+        self.model = model
+        model.add_listener(self)
+
+        self.view_start = fig.x_range.start
+        self.view_end = fig.x_range.end
+
+        # listen here because ap sliders can come and go, and we don't have to
+        # convince the figure to release those references since it just ties to
+        # this top-level container
+        fig.x_range.on_change('start', lambda attr, old, new:
+                              self.update_viewport(new, self.view_end))
+        fig.x_range.on_change('end', lambda attr, old, new:
+                              self.update_viewport(self.view_start, new))
+
+    def update_viewport(self, start, end):
+        """Handle a change in the view to enable/disable aperture lines."""
+        # Bokeh docs provide no indication of the datatype or orientation of
+        # the start/end tuples, so I have left the doc blank for now
+        self.view_start = start
+        self.view_end = end
+        for apline in self.aperture_lines:
+            apline.update_viewport(start, end)
+
+    def handle_aperture(self, aperture_id, location, start, end):
+        """Handle an updated or added aperture.
+
+        Parameters
+        ----------
+        aperture_id : int
+            ID of the aperture to update or create in the view
+        location, start, end : float
+            Location, start and end of the aperture in x coordinates
+
+        """
+        if aperture_id <= len(self.aperture_plots):
+            ap = self.aperture_plots[aperture_id-1]
+            ap.update(location, start, end)
+        else:
+            ap = AperturePlotView(self.fig, aperture_id, location, start, end)
+            self.aperture_plots.append(ap)
+
+            ap = ApertureLineView(self.model, aperture_id, location, start,
+                                  end)
+            self.aperture_lines.append(ap)
+            self.inner_controls.children.append(ap.component)
+
+    def delete_aperture(self, aperture_id):
+        """Remove an aperture by ID. If the ID is not recognized, do nothing.
+
+        Parameters
+        ----------
+        aperture_id : int
+            ID of the aperture to remove
+        """
+        idx = aperture_id-1
+        if aperture_id <= len(self.aperture_plots):
+            self.aperture_plots[idx].delete()
+            self.inner_controls.children.remove(
+                self.aperture_lines[idx].component)
+            del self.aperture_plots[idx]
+            del self.aperture_lines[idx]
+
+        for ap in self.aperture_plots[idx:]:
+            ap.update_id(ap.aperture_id - 1)
+
+        for ap in self.aperture_lines[idx:]:
+            ap.aperture_id -= 1
+            ap.update_title()
+
+
+class FindSourceAperturesVisualizer(PrimitiveVisualizer):
     def __init__(self, model):
         """
         Create a view for finding apertures with the given
-        :class:`geminidr.interactive.fit.aperture.FindSourceAperturesModel`
+        :class:`FindSourceAperturesModel`
 
         Parameters
         ----------
@@ -304,7 +602,7 @@ class FindSourceAperturesVisualizer(interactive.PrimitiveVisualizer):
         fig.height_policy = 'fixed'
         fig.width_policy = 'fit'
 
-        aperture_view = GIApertureView(self.model, fig)
+        aperture_view = ApertureView(self.model, fig)
         self.model.add_listener(self)
 
         fig.step(x=range(self.model.profile.shape[0]),
