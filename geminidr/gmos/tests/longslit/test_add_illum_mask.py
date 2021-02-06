9b1a4773eaabb834b868629c3abe9c060e2de19e
chris-simpson <csimpson@gemini.edu>
2021-02-05 16:21:58 -1000

    still trying to write a test

--- geminidr/gmos/tests/longslit/test_add_illum_mask.py
+++ geminidr/gmos/tests/longslit/test_add_illum_mask.py
@@ -13,19 +13,21 @@ datasets_and_locations = [("S20200122S0020.fits", 747),
                           ("S20100108S0052.fits", 1611)]
 
 
+@pytest.fixture(scope='module')
 def ad(request):
     file_on_disk = download_from_archive(request.param)
     return astrodata.open(file_on_disk)
 
 
-def start(request):
+@pytest.fixture(scope='module')
+def start_row(request):
     return request.param
 
 
 @pytest.mark.dragons_remote_data
 @pytest.mark.gmosls
-@pytest.mark.parametrize("ad,start", datasets_and_locations, indirect=True)
-def test_add_illum_mask_position(ad, start):
+@pytest.mark.parametrize("ad,start_row", datasets_and_locations, indirect=True)
+def test_add_illum_mask_position(ad, start_row):
     p = GMOSLongslit([ad])
     p.prepare()
     ad = p.addIllumMaskToDQ().pop()
@@ -33,5 +35,5 @@ def test_add_illum_mask_position(ad, start):
     # Chop off the bottom 200 rows because of the bias issue
     # and the bottom of the longslit mask for pre-Hamamatsu data
     # (larger CCDs) and  choose a middle column in case of edge effects
-    start_row = ad[0].mask[200:,100].argmax() + 200
-    assert abs(start_row - start) <= 2
+    actual_start_row = ad[0].mask[200:,100].argmax() + 200
+    assert abs(start_row - actual_start_row) <= 2
