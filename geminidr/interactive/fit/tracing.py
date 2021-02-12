276f7e61a3c3071ff0939a6a6a6dc9ef0cc634ed
Bruno Quint <b1quint@gmail.com>
2021-02-12 19:38:44 -0300

    Cleaned up tracing.py

--- geminidr/interactive/fit/tracing.py
+++ geminidr/interactive/fit/tracing.py
@@ -12,6 +12,10 @@ from .. import server
 __all__ = ["interactive_trace_apertures", ]
 
 
+class TraceAperturesVisualizer(Fit1DVisualizer):
+    pass
+
+
 def interactive_trace_apertures(ext, _config, _fit1d_params):
     """
     Run traceApertures() interactively.
@@ -28,51 +32,6 @@ def interactive_trace_apertures(ext, _config, _fit1d_params):
     Returns
     -------
     """
-    def _get_tracing_knots(conf, extra):
-        """
-        This function is used by the interactive fitter to
-        generate the x,y,weights to use for each fit.
-
-        Parameters
-        ----------
-        conf : ???
-            ???
-        extra : ???
-            ???
-
-        Returns
-        -------
-        """
-        all_tracing_knots = []
-        dispaxis = 2 - ext.dispersion_axis()  # python sense
-        print(conf)
-
-        for _i, _loc in enumerate(ext.APERTURE['c0'].data):
-            _c0 = int(_loc + 0.5)
-
-            _spectrum = ext.data[_c0] \
-                if dispaxis == 1 else ext.data[:, _c0]
-
-            _start = np.argmax(astrotools.boxcar(_spectrum, size=3))
-
-            _, _in_coords = tracing.trace_lines(
-                ext, axis=dispaxis, cwidth=5,
-                initial=[_loc], initial_tolerance=None,
-                max_missed=extra['max_missed'], max_shift=extra['max_shift'],
-                nsum=extra['nsum'], rwidth=None, start=_start,
-                step=extra['step'])
-
-            _in_coords = np.ma.masked_array(_in_coords)
-            _in_coords.mask = np.zeros_like(_in_coords)  # ToDo: This should not be required
-
-            spectral_tracing_knots = _in_coords[1 - dispaxis]
-            spatial_tracing_knots = _in_coords[dispaxis]
-
-            all_tracing_knots.append(
-                [spectral_tracing_knots, spatial_tracing_knots])
-
-        return all_tracing_knots
-
     ap_table = ext.APERTURE
     fit_par_list = [_fit1d_params] * len(ap_table)
     domain_list = [[ap['domain_start'], ap['domain_end']] for ap in ap_table]
@@ -89,36 +48,93 @@ def interactive_trace_apertures(ext, _config, _fit1d_params):
             "Tracing step: ", int, 10, min=1),
     }
 
-    visualizer = Fit1DVisualizer(_get_tracing_knots,
-                                 config=_config,
-                                 fitting_parameters=fit_par_list,
-                                 tab_name_fmt="Aperture {}",
-                                 xlabel='x',
-                                 ylabel='y',
-                                 reinit_params=[],
-                                 reinit_extras=reinit_extras,
-                                 domains=domain_list,
-                                 title="Trace Apertures")
+    def data_provider(conf, extra):
+        return trace_apertures_data_provider(ext, conf, extra)
+
+    visualizer = TraceAperturesVisualizer(
+        data_provider,
+        config=_config,
+        fitting_parameters=fit_par_list,
+        tab_name_fmt="Aperture {}",
+        xlabel='x',
+        ylabel='y',
+        reinit_extras=reinit_extras,
+        domains=domain_list,
+        title="Trace Apertures")
 
     server.interactive_fitter(visualizer)
+    models = visualizer.results()
 
-    list_of_final_models = visualizer.results()
     all_aperture_tables = []
     dispaxis = 2 - ext.dispersion_axis()  # python sense
 
-    for final_model, ap in zip(list_of_final_models, ext.APERTURE):
+    for final_model, ap in zip(models, ap_table):
         location = ap['c0']
         this_aptable = astromodels.model_to_table(final_model.model)
 
         # Recalculate aperture limits after rectification
         apcoords = final_model.evaluate(np.arange(ext.shape[dispaxis]))
 
-        this_aptable["aper_lower"] = \
-            ap["aper_lower"] + (location - apcoords.min())
-        this_aptable["aper_upper"] = \
-            ap["aper_upper"] - (apcoords.max() - location)
+        this_aptable["aper_lower"] = (
+                ap["aper_lower"] + (location - apcoords.min()))
+
+        this_aptable["aper_upper"] = (
+                ap["aper_upper"] - (apcoords.max() - location))
 
         all_aperture_tables.append(this_aptable)
 
     new_aptable = table.vstack(all_aperture_tables, metadata_conflicts="silent")
     return new_aptable
+
+
+def trace_apertures_data_provider(ext, conf, extra):
+    """
+    Function used by the interactive fitter to generate the a list with
+    pairs of [x, y] data containing the knots used for tracing.
+
+    Parameters
+    ----------
+    ext : AstroData
+        Single extension of data containing an .APERTURE table.
+    conf : dict
+        Dictionary containing default traceApertures() parameters.
+    extra : dict
+        Dictionary containing extra parameters used to re-create the input data
+        for fitting interactively.
+
+    Returns
+    -------
+    list : pairs of (x, y) for each aperture center, where x is the
+    spectral position of the knots, and y is the spacial position of the
+    knots.
+    """
+    all_tracing_knots = []
+    dispaxis = 2 - ext.dispersion_axis()  # python sense
+
+    for _i, _loc in enumerate(ext.APERTURE['c0'].data):
+        _c0 = int(_loc + 0.5)
+
+        _spectrum = (ext.data[_c0] if dispaxis == 1
+                     else ext.data[:, _c0])
+
+        _start = np.argmax(astrotools.boxcar(_spectrum, size=3))
+
+        _, _in_coords = tracing.trace_lines(
+            ext, axis=dispaxis, cwidth=5,
+            initial=[_loc], initial_tolerance=None,
+            max_missed=extra['max_missed'], max_shift=extra['max_shift'],
+            nsum=extra['nsum'], rwidth=None, start=_start,
+            step=extra['step'])
+
+        _in_coords = np.ma.masked_array(_in_coords)
+
+        # ToDo: This should not be required
+        _in_coords.mask = np.zeros_like(_in_coords)
+
+        spectral_tracing_knots = _in_coords[1 - dispaxis]
+        spatial_tracing_knots = _in_coords[dispaxis]
+
+        all_tracing_knots.append(
+            [spectral_tracing_knots, spatial_tracing_knots])
+
+    return all_tracing_knots
