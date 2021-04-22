6f384f7fe30d5f895b3ead94b70988fdf4aff724
chris-simpson <csimpson@gemini.edu>
2021-04-22 09:48:44 -1000

    add some tests for Section()

--- astrodata/tests/test_utils.py
+++ astrodata/tests/test_utils.py
@@ -4,6 +4,7 @@ import astropy.units as u
 from astropy.io import fits
 from astropy.table import Table
 from astrodata.fits import header_for_table, card_filter, update_header
+from astrodata import Section
 
 
 def test_header_for_table():
@@ -48,3 +49,16 @@ def test_update_header():
     # Check that comments have been merged
     assert list(hdr['COMMENT']) == ['A super useful comment', 'Another comment']
     assert list(hdr['HISTORY']) == ['This is historic', 'And not so useful']
+
+
+def test_section():
+    s = Section.from_string("[1:1024,1:512")
+    assert tuple(s) == (0, 1024, 0, 512)
+    assert s.asIRAFsection() == "[1:1024,1:512]"
+    assert s.asslice() == [slice(0, 512), slice(0, 1024)]
+    assert (s.x1, s.x2, s.y1, s.y2) == (0, 1024, 0, 512)
+    assert (s[0], s[1], s[2], s[3]) == (0, 1024, 0, 512)
+
+    s = Section.from_shape((8, 512, 1024))
+    assert tuple(s) == (0, 1024, 0, 512, 0, 8)
+    assert s.z2 == 8
