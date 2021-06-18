fix logic where spatially-collapsed 1D mask (used to find regions of high variance) wasn't being reset

geminidr/core/primitives_spect.py
@@ -1620,7 +1620,7 @@ class Spect(PrimitivesBASE):
                 mean, sigma, _ = gt.measure_bg_from_image(ndd, sampling=1)
 
                 # Mask sky-line regions and find clumps of unmasked pixels
-                mask1d[var1d > mean + 3*sigma] = 1
+                mask1d = (var1d > mean + 3*sigma)
                 slices = np.ma.clump_unmasked(np.ma.masked_array(var1d, mask1d))
                 sky_regions = [slice_ for slice_ in slices
                                if slice_.stop - slice_.start >= min_sky_pix]