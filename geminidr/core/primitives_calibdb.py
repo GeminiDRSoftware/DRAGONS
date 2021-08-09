0be3201e92bfafd534a6156597d8068d6e74026b
Kathleen Labrie <kathleen.labrie.phd@gmail.com>
2021-08-09 12:24:36 -1000

    the suffix solution wasn't quite right, should be more robust now.

--- geminidr/core/primitives_calibdb.py
+++ geminidr/core/primitives_calibdb.py
@@ -379,8 +379,11 @@ class CalibDB(PrimitivesBASE):
             if suffix:
                 ad.update_filename(suffix=suffix, strip=True)
             else:  # None.  Keep the one it has now.  (eg. 'stack' for imaging)
-                if '_' in ad.filename:
-                    suffix = '_'+ad.filename.split('_')[-1].split('.')[0]
+                # Got to do a bit of gymnastic
+                root, filetype = os.path.splitext(ad.orig_filename)
+                m = re.match('(.*){}(.*)'.format(re.escape(root)), ad.filename)
+                if m.groups()[1]:
+                    suffix, filetype = os.path.splitext(m.groups()[1])
                 else:
                     suffix = ''
 
