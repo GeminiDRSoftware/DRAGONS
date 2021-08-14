a90d62c748eb8a2e0a646fe05e8e4b637dea0c3e
Oliver <oly@oberdorf.org>
2021-08-13 16:51:03 -1000

    removed ut_datetime break test

--- geminidr/core/tests/test_nearIR.py
+++ geminidr/core/tests/test_nearIR.py
@@ -14,14 +14,13 @@ from geminidr.core import primitives_nearIR
 
 def test_remove_first_frame():
     # ad input list maker functions
-    def make_ad(timestamp, add_timestamp=True):
+    def make_ad(timestamp):
         ad = create_zero_filled_fake_astrodata(100, 200)
-        if add_timestamp:
-            ad.ut_datetime = timestamp
+        ad.ut_datetime = timestamp
         return ad
 
-    def make_ads(timestamps, skip_idx=None):
-        return [make_ad(ts, skip_idx != idx) for idx, ts in enumerate(timestamps)]
+    def make_ads(timestamps):
+        return [make_ad(ts) for idx, ts in enumerate(timestamps)]
 
     jan = datetime(year=2021, month=1, day=1)
     feb = datetime(year=2021, month=2, day=1)
@@ -50,15 +49,6 @@ def test_remove_first_frame():
     assert len(ad_out) == 2
     assert ad_out[0] == ad_in[0]
     assert ad_out[1] == ad_in[2]
-
-    # missing ut_datetime
-    _p = primitives_nearIR.NearIR([])
-    ad_in = make_ads([jan, feb, mar], skip_idx=1)
-    ad_out = _p.removeFirstFrame(ad_in)
-    assert len(ad_out) == 3
-    assert ad_out[0] == ad_in[0]
-    assert ad_out[1] == ad_in[1]
-    assert ad_out[2] == ad_in[2]
 
     # duplicate ut_datetime
     _p = primitives_nearIR.NearIR([])
