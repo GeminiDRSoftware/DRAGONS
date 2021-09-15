0c499b9521a06ad9cc38176f3f161cbeef61cb41
Kathleen Labrie <kathleen.labrie.phd@gmail.com>
2021-09-15 13:42:38 -1000

    more darn suffix manipulation.  this fixes the case where the orig_filename is the same as the ad.filename.

--- geminidr/core/primitives_calibdb.py
+++ geminidr/core/primitives_calibdb.py
@@ -270,6 +270,7 @@ class CalibDB(PrimitivesBASE):
                                 (not is_science and 'calibs' in self.upload)):
                 try:
                     upload_calibration(fname, is_science=is_science)
+                    pass
                 except:
                     log.warning("Unable to upload file to {} system"
                                 .format("science" if is_science else "calibration"))
@@ -379,13 +380,21 @@ class CalibDB(PrimitivesBASE):
             if suffix:
                 ad.update_filename(suffix=suffix, strip=True)
             else:  # None.  Keep the one it has now.  (eg. 'stack' for imaging)
-                # Got to do a bit of gymnastic
+                # Got to do a bit of gymnastic to figure what the current
+                # suffix is.  If orig.filename and filename are equal and have
+                # `_`, I have to assume that the last `_` is a suffix.  (KL)
                 root, filetype = os.path.splitext(ad.orig_filename)
-                m = re.match('(.*){}(.*)'.format(re.escape(root)), ad.filename)
-                if m.groups()[1]:
-                    suffix, filetype = os.path.splitext(m.groups()[1])
+                if ad.orig_filename == ad.filename:
+                    pre, post = ad.orig_filename.rsplit('_', 1)
+                    suffix, filetype = os.path.splitext(post)
+                    suffix = '_' + suffix
                 else:
-                    suffix = ''
+                    m = re.match('(.*){}(.*)'.format(re.escape(root)), ad.filename)
+                    if m.groups()[1] and m.groups()[1] != filetype:
+
+                        suffix, filetype = os.path.splitext(m.groups()[1])
+                    else:
+                        suffix = ''
 
             ad.phu.set('PROCMODE', self.mode)
 
