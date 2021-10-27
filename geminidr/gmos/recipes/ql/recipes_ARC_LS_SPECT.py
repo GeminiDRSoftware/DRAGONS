c855d082298af87f5837fdd1d74462d69bcf03f1
chris-simpson <csimpson@gemini.edu>
2021-10-27 11:14:28 -1000

    recipes_ARC_LS_SPECT: SQ now imports QL, instead of QL importing SQ

--- geminidr/gmos/recipes/ql/recipes_ARC_LS_SPECT.py
+++ geminidr/gmos/recipes/ql/recipes_ARC_LS_SPECT.py
@@ -5,21 +5,6 @@ Default is "reduce".
 """
 recipe_tags = {'GMOS', 'SPECT', 'LS', 'ARC'}
 
-from geminidr.gmos.recipes.ql.recipes_common import makeIRAFCompatible
-
-def makeProcessedArc(p):
-    p.prepare()
-    p.addDQ()
-    p.addVAR(read_noise=True)
-    p.overscanCorrect()
-    p.ADUToElectrons()
-    p.addVAR(poisson_noise=True)
-    p.mosaicDetectors()
-    p.makeIRAFCompatible()
-    p.determineWavelengthSolution()
-    p.determineDistortion()
-    p.storeProcessedArc()
-    p.writeOutputs()
-
+from ..sq.recipes_ARC_LS_SPECT import makeProcessedArc
 
 _default = makeProcessedArc
