2be4e9791db62f6b477bb17579390258b671e38b
Oliver <oly@oberdorf.org>
2021-12-17 12:39:22 -1000

    removing AstroData import, was not using

--- recipe_system/reduction/coreReduce.py
+++ recipe_system/reduction/coreReduce.py
@@ -23,7 +23,7 @@ import gemini_instruments
 
 from gempy.utils import logutils
 
-from astrodata import AstroDataError, AstroData
+from astrodata import AstroDataError
 
 from recipe_system import __version__
 
