250d664121132db22b7026918b902c000fe36c5e
Oliver <oly@oberdorf.org>
2022-02-08 16:33:23 -1000

    tests for ra/dec string handling for various instruments

--- gemini_instruments/gmos/tests/test_gmos.py
+++ gemini_instruments/gmos/tests/test_gmos.py
@@ -69,6 +69,16 @@ def test_tag_as_standard_real():
     assert 'STANDARD' in ad.tags
 
 
+def test_ra_dec_from_text(astrofaker):
+    ad = astrofaker.create('GMOS-S', ['SPECT'],
+                           extra_keywords={'RA': '03:48:30.113',
+                                           'DEC': '+24:20:43.00',
+                                           'DATE-OBS': '2021-01-01T12:00:00.000'}
+                           )
+    assert ad.ra() == 57.12547083333333
+    assert ad.dec() == 24.345277777777778
+
+
 if __name__ == '__main__':
 
     pytest.main()
---
+++ gemini_instruments/oscir/tests/test_oscir.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+def test_ra_dec_from_text(astrofaker):
+    ad = astrofaker.create('OSCIR', ['IMAGE'],
+                           extra_keywords={'RA': '03:48:30.113',
+                                           'DEC': '+24:20:43.00',
+                                           'DATE-OBS': '2021-01-01T12:00:00.000'}
+                           )
+    assert ad.ra() == 57.12547083333333
+    assert ad.dec() == 24.345277777777778
+
+
+if __name__ == "__main__":
+    pytest.main()
