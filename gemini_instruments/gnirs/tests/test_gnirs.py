e21718311c2dbb50721c25e4d4fc1c1158fe5b09
Oliver <oly@oberdorf.org>
2022-02-09 14:44:47 -1000

    GNIRS is very tied to the WCS ra/dec so nothing ot test for string handling of the RA/DEC keywords

--- gemini_instruments/gnirs/tests/test_gnirs.py
+++ gemini_instruments/gnirs/tests/test_gnirs.py
@@ -3,6 +3,7 @@ import pytest
 import astrodata
 import astrodata.testing
 import gemini_instruments
+from gemini_instruments.gnirs import AstroDataGnirs
 
 test_files = [
     "N20190206S0279.fits",
@@ -155,16 +156,6 @@ def test_ra_and_dec_wcs_fallback(ad, monkeypatch):
     monkeypatch.setattr(ad, 'wcs_dec', _fake_wcs_call)
     assert(ad.ra() == ad.phu.get('RA', None))
     assert(ad.dec() == ad.phu.get('DEC', None))
-
-
-def test_ra_dec_from_text(astrofaker):
-    ad = astrofaker.create('GNIRS', ['IMAGE'],
-                           extra_keywords={'RA': '03:48:30.113',
-                                           'DEC': '+24:20:43.00',
-                                           'DATE-OBS': '2021-01-01T12:00:00.000'}
-                           )
-    assert ad.ra() == 57.12547083333333
-    assert ad.dec() == 24.345277777777778
 
 
 if __name__ == "__main__":
