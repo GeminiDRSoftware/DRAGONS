cbc5f92efb6f6f1177533d04e57ff88dd5b8a696
Oliver <oly@oberdorf.org>
2022-02-11 14:08:20 -1000

    removing paths that try to use WCS that is not setup for ra/dec string test

--- gemini_instruments/gnirs/tests/test_gnirs.py
+++ gemini_instruments/gnirs/tests/test_gnirs.py
@@ -162,8 +162,8 @@ def test_ra_dec_from_text():
     ad = AstroDataGnirs()
     ad.phu['RA'] = '03:48:30.113'
     ad.phu['DEC'] = '+24:20:43.00'
-    assert pytest.approx(ad.ra(), 57.12547083333333)
-    assert pytest.approx(ad.dec(), 24.345277777777778)
+    assert pytest.approx(ad.target_ra(), 57.12547083333333)
+    assert pytest.approx(ad.target_dec(), 24.345277777777778)
 
 
 if __name__ == "__main__":
