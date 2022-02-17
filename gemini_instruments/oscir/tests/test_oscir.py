72e168dd3005b17be0149173a68206b89613eb97
Oliver Oberdorf <9758989+olyoberdorf@users.noreply.github.com>
2022-02-17 08:13:15 -1000

    support target ra dec string values (#326)
    * updated various ra/dec descriptors to use base class parsing with instrument-defined RA/DEC keywords and detection of textual representations to convert to float
    
    * fix for Cirpass dec keyword name
    
    * using astropy Angle for ra/dec conversions
    
    * handling bad ra/dec strings by logging a warning and returning None

---
+++ gemini_instruments/oscir/tests/test_oscir.py
@@ -0,0 +1,17 @@
+import pytest
+
+from gemini_instruments.oscir import AstroDataOscir
+
+
+def test_ra_dec_from_text():
+    ad = AstroDataOscir()
+    ad.phu['RA'] = '03:48:30.113'
+    ad.phu['DEC'] = '+24:20:43.00'
+    assert pytest.approx(ad.ra(), 57.12547083333333)
+    assert pytest.approx(ad.dec(), 24.345277777777778)
+    assert pytest.approx(ad.target_ra(), 57.12547083333333)
+    assert pytest.approx(ad.target_dec(), 24.345277777777778)
+
+
+if __name__ == "__main__":
+    pytest.main()
