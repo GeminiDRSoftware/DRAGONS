d1dc08188c51a78291678c50bcebbd561a3ec1d9
Oliver <oly@oberdorf.org>
2022-02-17 14:10:06 -1000

    adding max_separation to set of reinit params for findApertures

--- geminidr/core/primitives_spect.py
+++ geminidr/core/primitives_spect.py
@@ -1692,7 +1692,7 @@ class Spect(Resample):
                     config = self.params[self.myself()]
                     config.update(**params)
                     reinit_params = ["percentile", "min_sky_region", "use_snr", "min_snr", "section", "max_apertures",
-                                     "threshold", "sizing_method"]
+                                     "threshold", "sizing_method", "max_separation"]
                     title_overrides = {
                         "percentile": "Percentile (use mean if no value)",
                         "min_sky_region": "Min sky region",
@@ -1700,7 +1700,8 @@ class Spect(Resample):
                         "min_snr": "SNR threshold for peak detection",
                         "max_apertures": "Max Apertures (empty means no limit)",
                         "threshold": "Threshold",
-                        "sizing_method": "Sizing method"
+                        "sizing_method": "Sizing method",
+                        "max_separation": "Max separation from target (arcsecs)"
                     }
                     ui_params = UIParameters(config, reinit_params=reinit_params, extras={},
                                              title_overrides=title_overrides,
