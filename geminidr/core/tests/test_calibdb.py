a8fda9222bbfc6348cf9df5dc808338d683f5cca
Oliver <oly@oberdorf.org>
2022-03-28 17:41:54 -1000

    running without a mode

--- geminidr/core/tests/test_calibdb.py
+++ geminidr/core/tests/test_calibdb.py
@@ -12,9 +12,9 @@ def test_update_datalab(ad):
     kw_datalab = ad._keyword_for('data_label')
     orig_datalab = 'GN-2001A-Q-9-52-001'
     ad.phu[kw_datalab] = orig_datalab
-    _update_datalab(ad, '_flat', 'ql', kw_lut)
+    _update_datalab(ad, '_flat', '', kw_lut)
     assert ad.phu[kw_datalab] == orig_datalab + '-FLAT'
-    _update_datalab(ad, '_flat', 'ql', kw_lut)
+    _update_datalab(ad, '_flat', '', kw_lut)
     assert ad.phu[kw_datalab] == orig_datalab + '-FLAT'
-    _update_datalab(ad, '_bias', 'ql', kw_lut)
+    _update_datalab(ad, '_bias', '', kw_lut)
     assert ad.phu[kw_datalab] == orig_datalab + '-BIAS'
