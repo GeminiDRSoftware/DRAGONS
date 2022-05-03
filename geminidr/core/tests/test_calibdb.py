3db76c1559dba914a27ff26d40d22e7faa1b5e21
Kathleen Labrie <kathleen.labrie.phd@gmail.com>
2022-05-03 08:42:38 -1000

    add obsid to fake data as update_datalab needs it now.  Also add OBSID to the keyword-descriptor association table. No idea why it was missing.

--- geminidr/core/tests/test_calibdb.py
+++ geminidr/core/tests/test_calibdb.py
@@ -10,8 +10,11 @@ def ad(astrofaker):
 def test_update_datalab(ad):
     kw_lut = {'DATALAB': 'comment'}
     kw_datalab = ad._keyword_for('data_label')
+    kw_obsid = ad._keyword_for('observation_id')
     orig_datalab = 'GN-2001A-Q-9-52-001'
+    orig_obsid = 'GN-2001A-Q-9-52'
     ad.phu[kw_datalab] = orig_datalab
+    ad.phu[kw_obsid] = orig_obsid
     _update_datalab(ad, '_flat', 'sq', kw_lut)
     assert ad.phu[kw_datalab] == orig_datalab + '-SQ-FLAT'
     _update_datalab(ad, '_flat', 'sq', kw_lut)
