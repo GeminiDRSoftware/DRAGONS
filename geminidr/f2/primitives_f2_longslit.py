0619c47879ae8fda01d90bdca812e29b42c66000
chris-simpson <csimpson@gemini.edu>
2022-06-08 17:24:52 -1000

    add max_perpendicular_offset to F2Longslit._fields_overlap() that was missing

--- geminidr/f2/primitives_f2_longslit.py
+++ geminidr/f2/primitives_f2_longslit.py
@@ -23,8 +23,9 @@ class F2Longslit(F2Spect):
         super()._initialize(adinputs, **kwargs)
         self._param_update(parameters_f2_longslit)
 
-    def _fields_overlap(self, ad1, ad2, frac_FOV=1.0):
+    def _fields_overlap(self, ad1, ad2, frac_FOV=1.0, max_perpendicular_offset=None):
         slit_length = (1300 if ad1.is_ao() else 1460) * ad1.pixel_scale()
         slit_width = int(ad1.focal_plane_mask()[0]) * ad1.pixel_scale()
-        return super()._fields_overlap(ad1, ad2, frac_FOV=frac_FOV,
-                                       slit_length=slit_length, slit_width=slit_width)
+        return super()._fields_overlap(
+            ad1, ad2, frac_FOV=frac_FOV, slit_length=slit_length,
+            slit_width=slit_width, max_perpendicular_offset=max_perpendicular_offset)
