3b82ba35a00bcf0de02aa7b1539d796570b83761
soraisam <soraisam.antares@gmail.com>
2022-06-16 12:42:16 -1000

    adding first draft of gnirs science recipe

---
+++ geminidr/gnirs/recipes/sq/recipes_LS_SPECT.py
@@ -0,0 +1,45 @@
++"""
+Recipes available to data with tags ['GNIRS', 'SPECT', LS'].
+Default is "reduceScience".
+"""
+recipe_tags = {'GNIRS', 'SPECT', 'LS'}
+
+def reduceScience(p):
+    """
+    To be updated as development continues: This recipe processes GNIRS longslit 
+    spectroscopic data, currently up to wavelength calibration.
+
+    Parameters
+    ----------
+    p : :class:`geminidr.gnirs.primitives_gnirs_longslit.GNIRSLongslit`
+
+    """
+    p.prepare()
+    p.addDQ()
+    # p.nonlinearityCorrect() # non-linearity correction tbd 
+    p.ADUToElectrons()
+    p.addVAR(poisson_noise=True, read_noise=True)
+    p.darkCorrect()
+    # p.writeOutputs() # delete me after dark correction verification
+    p.flatCorrect()
+    # p.writeOutputs() # delete me after flat correction verification
+    p.attachWavelengthSolution()
+    # p.writeOutputs() # delete me after wavecal verification
+    # TBD below 
+    p.distortionCorrect()
+    # p.writeOutputs()
+    # p.separateSky()
+    # p.associateSky()
+    # p.skyCorrect()
+    p.adjustWCSToReference()
+    p.resampleToCommonFrame()
+    p.scaleCountsToReference()
+    p.stackFrames()
+    p.findApertures()
+    p.traceApertures()
+    p.storeProcessedScience(suffix="_2D")
+    p.extractSpectra()
+    p.storeProcessedScience(suffix="_1D")
+    
+    
+_default = reduceScience
