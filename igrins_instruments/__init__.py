2558a97efae8869c254947d4af7965fb8d9abe20
Jae-Joon Lee <lee.j.joon@gmail.com>
2022-06-28 14:55:04 +0900

    move igrinsdr and other under src

--- igrins_instruments/__init__.py
+++
@@ -1,6 +0,0 @@
-# Import the modules under this package to trigger any class
-# registering that may be needed.
-
-import astrodata
-
-from . import igrins
--- igrinsdr/doc/usermanuals/IGRINSDR_UserManual/README
+++
@@ -1,1 +0,0 @@
-Location of the Sphinx documents.
