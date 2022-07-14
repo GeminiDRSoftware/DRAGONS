adea95cce379d9adf63d04a06863b330781a1c51
Daniel Berke <berke.daniel@gmail.com>
2022-07-13 16:59:10 -1000

    Add some tests, refactor data files used into fixtures

--- geminidr/gnirs/tests/longslit/test_sky_stacking.py
+++ geminidr/gnirs/tests/longslit/test_sky_stacking.py
@@ -2,23 +2,30 @@
 Tests for sky stacking and subtraction for GNIRS longslit spectroscopy.
 """
 
+import pytest
+
 import astrodata
 from astrodata.testing import download_from_archive
 import gemini_instruments
 from geminidr.gnirs.primitives_gnirs_longslit import GNIRSLongslit
 import pytest
 
+# ---- Fixtures ---------------------------------------------------------------
+@pytest.fixture
+def gnirs_objects():
+    files = ('N20141119S0331.fits', 'N20141119S0334.fits')
+    return [astrodata.open(download_from_archive(f)) for f in files]
+
+@pytest.fixture
+def gnirs_skies():
+    files = ('N20141119S0332.fits', 'N20141119S0333.fits')
+    return [astrodata.open(download_from_archive(f)) for f in files]
+
 
 # ---- Tests ------------------------------------------------------------------
-@pytest.mark.gnirs
-def test_associate_sky_abba():
+def test_associate_sky_abba(gnirs_objects, gnirs_skies):
 
-    files = ['N20141119S0331.fits', 'N20141119S0332.fits',
-             'N20141119S0333.fits', 'N20141119S0334.fits']
-
-    data = [astrodata.open(download_from_archive(f)) for f in files]
-
-    p = GNIRSLongslit(data)
+    p = GNIRSLongslit(gnirs_objects + gnirs_skies)
     p.prepare()
     p.separateSky()
     p.associateSky()
@@ -32,3 +39,40 @@ def test_associate_sky_abba():
     # check that the A frames get the B frames as skies
     for ad in (a1, a2):
         assert set(ad.SKYTABLE['SKYNAME']) == b_frames
+
+def test_associate_sky_pass_skies(gnirs_objects, gnirs_skies):
+
+    in_sky_names = set([ad.filename for ad in gnirs_skies])
+
+    p = GNIRSLongslit(gnirs_objects)
+    # Don't run separate sky to simulate resuming work with known skies.
+    p.associateSky(sky=gnirs_skies)
+
+    out_sky_names = set([ad.phu['ORIGNAME'] for ad in p.streams['sky']])
+
+    assert in_sky_names == out_sky_names
+
+def test_associate_sky_exclude_all(gnirs_objects, gnirs_skies):
+
+    p = GNIRSLongslit(gnirs_objects + gnirs_skies)
+    p.prepare()
+    p.separateSky()
+    p.associateSky(distance=10)  # Offset is only 5"
+
+    for ad in p.showList():
+        with pytest.raises(AttributeError):
+            ad.SKYTABLE
+
+def test_associate_sky_use_all(gnirs_objects, gnirs_skies):
+
+    # Grab a sky frame from an unrelated but relatively nearby observation.
+    extra_frame = [astrodata.open(download_from_archive('N20141107S0360.fits'))]
+
+    p = GNIRSLongslit(gnirs_objects + gnirs_skies + extra_frame)
+    p.prepare()
+    p.separateSky()
+    p.associateSky(distance=10, use_all=True)
+
+    # Check that all the "object" and "sky" frames have the extra frame as sky.
+    for ad in p.streams['main']:
+        assert (ad.SKYTABLE['SKYNAME'][0] == 'N20141107S0360_skyAssociated.fits')
