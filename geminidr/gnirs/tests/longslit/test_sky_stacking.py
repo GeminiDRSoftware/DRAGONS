f74492563d12f8d4d3388c312175bb2351d2f81c
Daniel Berke <berke.daniel@gmail.com>
2022-07-22 14:18:05 -1000

    Implement/fix NIR LS tests, correct for "use_all" change in associateSky
    This commit adds some tests and tweaks others to be consistent
    between the three NIR instruments with longslit observing modes:
    GNIRS, F2, and NIRI. It also fixes some tests in test_preprocess.py
    which no longer passed after Chris Simpson's change to the "use_all"
    parameter.

--- geminidr/gnirs/tests/longslit/test_sky_stacking.py
+++ geminidr/gnirs/tests/longslit/test_sky_stacking.py
@@ -12,62 +12,69 @@ import pytest
 
 # ---- Fixtures ---------------------------------------------------------------
 @pytest.fixture
-def gnirs_objects():
-    files = ('N20141119S0331.fits', 'N20141119S0334.fits')
-    return [astrodata.open(download_from_archive(f)) for f in files]
-
-@pytest.fixture
-def gnirs_skies():
-    files = ('N20141119S0332.fits', 'N20141119S0333.fits')
-    return [astrodata.open(download_from_archive(f)) for f in files]
-
+def gnirs_abba():
+    return [astrodata.open(download_from_archive(f)) for f in
+            ('N20141119S0331.fits', 'N20141119S0332.fits',
+             'N20141119S0333.fits', 'N20141119S0334.fits')]
 
 # ---- Tests ------------------------------------------------------------------
-def test_associate_sky_abba(gnirs_objects, gnirs_skies):
+def test_associate_sky_abba(gnirs_abba):
 
-    p = GNIRSLongslit(gnirs_objects + gnirs_skies)
-    p.prepare()
+    p = GNIRSLongslit(gnirs_abba)
+    # The perpendicular offsets are reported as 0.14" for some frames but the
+    # slit width is only 0.1", so the WCS needs to be reset.
+    p.prepare(bad_wcs="new")
     p.separateSky()
     p.associateSky()
 
-    a1, a2 = p.streams['main']
-    b1, b2 = p.streams['sky']
+    a1, b1, b2, a2 = p.showList()
 
+    a_frames = {'N20141119S0331_skyAssociated.fits',
+                'N20141119S0334_skyAssociated.fits'}
     b_frames = {'N20141119S0332_skyAssociated.fits',
                 'N20141119S0333_skyAssociated.fits'}
 
-    # check that the A frames get the B frames as skies
+    # check that the A frames get the B frames as skies, and vice versa
     for ad in (a1, a2):
         assert set(ad.SKYTABLE['SKYNAME']) == b_frames
+    for ad in (b1, b2):
+        assert set(ad.SKYTABLE['SKYNAME']) == a_frames
 
-def test_associate_sky_pass_skies(gnirs_objects, gnirs_skies):
+def test_associate_sky_pass_skies(gnirs_abba):
 
-    in_sky_names = set([ad.filename for ad in gnirs_skies])
+    in_sky_names = set([ad.filename for ad in gnirs_abba[1:3]])
 
-    p = GNIRSLongslit(gnirs_objects)
+    p = GNIRSLongslit(gnirs_abba)
     # Don't run separate sky to simulate resuming work with known skies.
-    p.associateSky(sky=gnirs_skies)
+    p.associateSky(sky=gnirs_abba[1:3])
 
     out_sky_names = set([ad.phu['ORIGNAME'] for ad in p.streams['sky']])
 
     assert in_sky_names == out_sky_names
 
-@pytest.mark.parametrize('use_all', [True, False])
-def test_associate_sky_use_all(use_all, gnirs_objects, gnirs_skies):
+def test_associate_sky_use_all(gnirs_abba):
 
-    in_sky_names = set([ad.filename for ad in gnirs_skies])
+    in_sky_names = set([ad.filename for ad in gnirs_abba])
 
-    p = GNIRSLongslit(gnirs_objects + gnirs_skies)
+    p = GNIRSLongslit(gnirs_abba)
     p.prepare()
     p.separateSky()
-    # Offset is only 5" so this will exclude skies if 'use_all' is False.
-    p.associateSky(distance=10, use_all=use_all)
+    p.associateSky(distance=0, use_all=True)
 
-    if use_all:
-        out_sky_names = set([ad.phu['ORIGNAME'] for ad in p.streams['sky']])
+    for ad in p.showList():
+        skies = set([s.replace('_skyAssociated', '')
+                     for s in ad.SKYTABLE['SKYNAME']])
 
-        assert in_sky_names == out_sky_names
-    else:
-        for ad in p.showList():
-            with pytest.raises(AttributeError):
-                ad.SKYTABLE
+        # Check that each AD has all the other frames as skies, but not itself.
+        assert skies == in_sky_names - set([ad.phu['ORIGNAME']])
+
+def test_associate_sky_exclude_all(gnirs_abba):
+    p = GNIRSLongslit(gnirs_abba)
+    p.prepare()
+    p.separateSky()
+    # Offset is 5" so this will exclude skies if 'use_all' is False.
+    p.associateSky(distance=10)
+
+    for ad in p.showList():
+        with pytest.raises(AttributeError):
+            ad.SKYTABLE
--- geminidr/niri/tests/longslit/test_sky_stacking.py
+++ geminidr/niri/tests/longslit/test_sky_stacking.py
@@ -12,18 +12,88 @@ from geminidr.niri.primitives_niri_longslit import NIRILongslit
 
 # ---- Fixtures ---------------------------------------------------------------
 @pytest.fixture
-def abcde_files():
-    return np.array(['N20070204S0098.fits',
-                     'N20070204S0099.fits',
-                     'N20070204S0100.fits',
-                     'N20070204S0101.fits',
-                     'N20070204S0102.fits',
-                     'N20070204S0103.fits',
-                     'N20070204S0104.fits'])
+def niri_abba():
+    return [astrodata.open(download_from_archive(f)) for f in
+            ('N20100602S0437.fits', 'N20100602S0438.fits',
+             'N20100602S0439.fits', 'N20100602S0440.fits')]
+
+@pytest.fixture
+def niri_abcde():
+    return [astrodata.open(download_from_archive(f)) for f in
+            ('N20070204S0098.fits',
+             'N20070204S0099.fits',
+             'N20070204S0100.fits',
+             'N20070204S0101.fits',
+             'N20070204S0102.fits',
+             'N20070204S0103.fits',
+             'N20070204S0104.fits')]
 
 
 # ---- Tests ------------------------------------------------------------------
-def test_associate_sky_abcde(abcde_files):
+def test_associate_sky_abba(niri_abba):
+
+    p = NIRILongslit(niri_abba)
+    # The perpendicular offsets are reported as 0.14" for some frames but the
+    # slit width is only 0.1", so the WCS needs to be reset.
+    p.prepare()
+    p.separateSky()
+    p.associateSky()
+
+    a1, b1, b2, a2 = p.showList()
+
+    a_frames = {'N20100602S0437_skyAssociated.fits',
+                'N20100602S0440_skyAssociated.fits'}
+    b_frames = {'N20100602S0438_skyAssociated.fits',
+                'N20100602S0439_skyAssociated.fits'}
+
+    # check that the A frames get the B frames as skies, and vice versa
+    for ad in (a1, a2):
+        assert set(ad.SKYTABLE['SKYNAME']) == b_frames
+    for ad in (b1, b2):
+        assert set(ad.SKYTABLE['SKYNAME']) == a_frames
+
+def test_associate_sky_pass_skies(niri_abba):
+
+    in_sky_names = set([ad.filename for ad in niri_abba[1:3]])
+
+    p = NIRILongslit(niri_abba)
+    # Don't run separate sky to simulate resuming work with known skies.
+    p.associateSky(sky=niri_abba[1:3])
+
+    out_sky_names = set([ad.phu['ORIGNAME'] for ad in p.streams['sky']])
+
+    assert in_sky_names == out_sky_names
+
+def test_associate_sky_use_all(niri_abba):
+
+    in_sky_names = set([ad.filename for ad in niri_abba])
+
+    p = NIRILongslit(niri_abba)
+    p.prepare()
+    p.separateSky()
+    p.associateSky(distance=0, use_all=True)
+
+    for ad in p.showList():
+        skies = set([s.replace('_skyAssociated', '')
+                     for s in ad.SKYTABLE['SKYNAME']])
+
+        # Check that each AD has all the other frames as skies, but not itself.
+        assert skies == in_sky_names - set([ad.phu['ORIGNAME']])
+
+def test_associate_sky_exclude_all(niri_abba):
+    p = NIRILongslit(niri_abba)
+    p.prepare()
+    p.separateSky()
+    # Offset is ~10" so this will exclude skies if 'use_all' is False.
+    p.associateSky(distance=15)
+
+    for ad in p.showList():
+        with pytest.raises(AttributeError):
+            ad.SKYTABLE
+
+
+
+def test_associate_sky_abcde(niri_abcde):
 
     results = {'N20070204S0098.fits': [1, 2, 3],
                'N20070204S0099.fits': [0, 2, 3, 4],
@@ -33,30 +103,30 @@ def test_associate_sky_abcde(abcde_files):
                'N20070204S0103.fits': [2, 3, 4, 6],
                'N20070204S0104.fits': [3, 4, 5]}
 
-    data = [astrodata.open(download_from_archive(f)) for f in abcde_files]
+    # data = [astrodata.open(download_from_archive(f)) for f in niri_abcde]
 
-    p = NIRILongslit(data)
+    p = NIRILongslit(niri_abcde)
+    # Some frames have bad WCS information, so use 'fix' to take care of it.
     p.prepare(bad_wcs='fix')
     p.separateSky()
     p.associateSky()
 
     for ad in p.showList():
-        skies = set([s.replace('_skyAssociated', '')
-                     for s in ad.SKYTABLE['SKYNAME']])
-        assert skies == set(abcde_files[results[ad.phu['ORIGNAME']]])
+        skies = set([s for s in ad.SKYTABLE['SKYNAME']])
 
-def test_associate_sky_abcde_distance(abcde_files):
-    data = [astrodata.open(download_from_archive(f)) for f in abcde_files]
+        assert skies == set([niri_abcde[i].filename
+                             for i in results[ad.phu['ORIGNAME']]])
 
-    p = NIRILongslit(data)
+def test_associate_sky_abcde_exclude_some(niri_abcde):
+    # data = [astrodata.open(download_from_archive(f)) for f in niri_abcde]
+
+    p = NIRILongslit(niri_abcde)
     p.prepare(bad_wcs='fix')
     p.separateSky()
     # This should eliminate all but two of the frames from being matched with
     # sky (each other, in this case)
     p.associateSky(distance=21.)
 
-    for ad in p.showList():
-        print(ad.filename)
     assert p.showList()[0].SKYTABLE['SKYNAME'][0] == 'N20070204S0104_skyAssociated.fits'
     assert p.showList()[1].SKYTABLE['SKYNAME'][0] == 'N20070204S0099_skyAssociated.fits'
     assert len(p.streams['no_skies']) == 5
