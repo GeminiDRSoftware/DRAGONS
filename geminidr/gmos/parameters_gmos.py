889883de7803ded37bb7e6304d3d97aab3db7bd9
chris-simpson <csimpson@gemini.edu>
2022-08-08 12:41:16 -1000

    rework parameter initialization to take place once after all modules have been imported

--- geminidr/gmos/parameters_gmos.py
+++ geminidr/gmos/parameters_gmos.py
@@ -1,7 +1,7 @@
 # This parameter file contains the parameters related to the primitives located
 # in the primitives_gmos.py file, in alphabetical order.
 from gempy.library import config
-from geminidr.core import parameters_visualize, parameters_ccd
+from geminidr.core import parameters_visualize, parameters_ccd, parameters_standardize
 from geminidr.gemini import parameters_qa
 
 def badamps_check(value):
@@ -16,14 +16,18 @@ def badamps_check(value):
     else:
         return len(badamps) >= 1 and min(badamps) > 0
 
+
 class displayConfig(parameters_visualize.displayConfig):
     remove_bias = config.Field("Remove estimated bias level before displaying?", bool, True)
+
 
 class measureBGConfig(parameters_qa.measureBGConfig):
     remove_bias = config.Field("Remove estimated bias level?", bool, True)
 
+
 class measureIQConfig(parameters_qa.measureIQConfig):
     remove_bias = config.Field("Remove estimated bias level before displaying?", bool, True)
+
 
 class maskFaultyAmpConfig(config.Config):
     suffix = config.Field("Filename suffix", str, "_badAmpMasked", optional=True)
@@ -32,6 +36,11 @@ class maskFaultyAmpConfig(config.Config):
                                 optional=True, check=badamps_check)
     valid_from = config.Field("Mask data taken after this date (YYYYMMDD)", str, None, optional=True)
     valid_to = config.Field("Mask data taken before this date (YYYYMMDD)", str, None, optional=True)
+
+
+class standardizeWCSConfig(parameters_standardize.standardizeWCSConfig):
+    pass
+
 
 class subtractOverscanConfig(parameters_ccd.subtractOverscanConfig):
     nbiascontam = config.RangeField("Number of columns to exclude from averaging",
@@ -43,6 +52,7 @@ class subtractOverscanConfig(parameters_ccd.subtractOverscanConfig):
         config.Config.validate(self)
         if self.function == "spline" and self.order == 0:
             raise ValueError("Must specify a positive spline order, or None")
+
 
 # We need to redefine this to ensure it inherits this version of
 # subtractOverscanConfig.validate()
