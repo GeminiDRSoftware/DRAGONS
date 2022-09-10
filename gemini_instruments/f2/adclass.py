c41d520daee53f05185b1e34a6e1d25ee8383418
Kathleen Labrie <kathleen.labrie.phd@gmail.com>
2022-09-09 14:01:13 -1000

    switch to using WAVELENG for central_wavelength for F2.  that is what GIRAF uses and master has been changed some months ago to use that instead for F2 spectro. Also, it turns out that WAVELENG is set by the seqexec and is easier to adjust then GRWLEN which is set by the instrument and is more difficult to adjust.

--- gemini_instruments/f2/adclass.py
+++ gemini_instruments/f2/adclass.py
@@ -11,7 +11,7 @@ from .. import gmu
 
 
 class AstroDataF2(AstroDataGemini):
-    __keyword_dict = dict(central_wavelength='GRWLEN',
+    __keyword_dict = dict(central_wavelength='WAVELENG',
                           disperser='GRISM',
                           dispersion='DISPERSI',
                           focal_plane_mask='MOSPOS',
