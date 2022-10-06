286b5667c4ca0ecd57c00fa8e7133a280e805b8a
Oliver <oly@oberdorf.org>
2022-10-05 16:31:31 -1000

    reverting to nonheadless to work on dbus issues.  I cant get headless to run correctly

--- geminidr/interactive/server.py
+++ geminidr/interactive/server.py
@@ -70,8 +70,8 @@ class ChromeFix(Chrome):
             opt = self.raise_opts[autoraise]
             if opt: raise_opt = [opt]
 
-        cmdline = [self.name] + raise_opt + args
         # cmdline = ['google-chrome'] + raise_opt + ["--headless"] + args
+        cmdline = ['google-chrome'] + raise_opt + args
 
         if remote or self.background:
             inout = subprocess.DEVNULL
