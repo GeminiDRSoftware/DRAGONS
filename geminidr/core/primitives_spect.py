aaeff317dd8395697ba7daa45591ea7df8e630a0
chris-simpson <csimpson@gemini.edu>
2023-03-01 08:56:16 -1000

    flagCosmicRays(): ensure additional extensions output when debugging are simple ndarrays, not MaskedArrays

--- geminidr/core/primitives_spect.py
+++ geminidr/core/primitives_spect.py
@@ -1925,8 +1925,6 @@ class Spect(Resample):
 
                 # Set up the background and models to be blank initially:
                 background = np.zeros(ext.shape)
-                objfit = np.zeros(ext.shape)
-                skyfit = np.zeros(ext.shape)
 
                 # Fit the object spectrum:
                 if bkgmodel in ('both', 'object'):
@@ -1937,7 +1935,7 @@ class Spect(Resample):
                                     weights=weights,
                                     **fit_1D_params).evaluate()
                 else:
-                    objfit = np.zeros_like(data)
+                    objfit = np.zeros(ext.shape)
                 if debug:
                     ext.OBJFIT = objfit.copy()
 
@@ -1956,7 +1954,7 @@ class Spect(Resample):
                                     weights=weights,
                                     **fit_1D_params).evaluate()
                 else:
-                    skyfit = np.zeros_like(data)
+                    skyfit = np.zeros(ext.shape)
                 if debug:
                     ext.SKYFIT = skyfit
 
