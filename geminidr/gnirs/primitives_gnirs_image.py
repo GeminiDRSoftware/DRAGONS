f0abb93e2a16cac4caba902bf692848be5d0ef34
Kathleen Labrie <kathleen.labrie.phd@gmail.com>
2023-03-30 07:15:04 -1000

    another fix to the merge.

--- geminidr/gnirs/primitives_gnirs_image.py
+++ geminidr/gnirs/primitives_gnirs_image.py
@@ -5,6 +5,9 @@
 # ------------------------------------------------------------------------------
 import os
 
+import numpy as np
+from scipy import ndimage
+
 import astrodata, gemini_instruments
 from geminidr.gemini.lookups import DQ_definitions as DQ
 from gempy.gemini import gemini_tools as gt
@@ -12,6 +15,7 @@ from gempy.gemini import gemini_tools as gt
 from .primitives_gnirs import GNIRS
 from ..core import Image, Photometry
 from . import parameters_gnirs_image
+from .lookups import maskdb
 
 from recipe_system.utils.decorators import parameter_override, capture_provenance
 
@@ -70,8 +74,8 @@ class GNIRSImage(GNIRS, Image, Photometry):
 
         # Fetching a corrected illumination mask with a keyhole that aligns
         # with the science data
-        illum_mask = FOV._create_illum_mask(reference, log, xshift=params["xshift"],
-                                            yshift=params["yshift"])
+        illum_mask = _create_illum_mask(reference, log, xshift=params["xshift"],
+                                        yshift=params["yshift"])
         if illum_mask is None:
             log.warning(f"No illumination mask found for {reference.filename},"
                         " no mask can be added to the DQ planes of the inputs")
