5e59d8db547708eb2ec2cd3abbf37ae51e02f5ce
James E.H. Turner <jturner@gemini.edu>
2023-04-04 14:23:03 -0400

    Remove index cache to try to resolve a conda traceback in Jenkins.

--- .jenkins/scripts/download_and_install_anaconda.sh
+++ .jenkins/scripts/download_and_install_anaconda.sh
@@ -32,6 +32,7 @@ fi
 
 echo ${PATH}
 which conda
+conda clean -i
 conda update --quiet conda
 # These 2 channels need removing if testing old branches has reinstated them:
 conda config --remove channels http://ssb.stsci.edu/astroconda || :
