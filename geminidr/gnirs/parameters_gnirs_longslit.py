642fcdbe855f9dd22d8ec1731628a864da53a8a4
Daniel Berke <berke.daniel@gmail.com>
2023-07-10 11:40:43 -1000

    Move GNIRS-specific determineDistortion() from "spect" module to "longslit"
    Move GNIRS-specific determineDistortionConfig to longslit
    
    Fix import of parameters_spect
    
    More import fixing, should be correct this time

--- geminidr/gnirs/parameters_gnirs_longslit.py
+++ geminidr/gnirs/parameters_gnirs_longslit.py
@@ -1,8 +1,19 @@
 # This parameter file contains the parameters related to the primitives located
 # in the primitives_gnirs_longslit.py file, in alphabetical order.
+from gempy.library import config
 from geminidr.core import parameters_standardize
+from geminidr.core import parameters_spect
 from geminidr.core.parameters_standardize import addIllumMaskToDQConfig
 
 class addDQConfig(parameters_standardize.addDQConfig, addIllumMaskToDQConfig):
     def setDefaults(self):
         self.add_illum_mask = True
+
+class determineDistortionConfig(parameters_spect.determineDistortionConfig):
+    spectral_order = config.RangeField("Fitting order in spectral direction", int, None, min=1, optional=True)
+    min_line_length = config.RangeField("Exclude line traces shorter than this fraction of spatial dimension",
+                                        float, None, min=0., max=1., optional=True)
+    def setDefaults(self):
+        self.min_snr = 10
+        self.max_missed = 2 # helps to filter out tracing on horizontal DC noise pattern
+        self.debug_reject_bad = False
--- geminidr/gnirs/parameters_gnirs_spect.py
+++ geminidr/gnirs/parameters_gnirs_spect.py
@@ -10,15 +10,6 @@ def list_of_ints_check(value):
     [int(x) for x in str(value).split(',')]
     return True
 
-class determineDistortionConfig(parameters_spect.determineDistortionConfig):
-    spectral_order = config.RangeField("Fitting order in spectral direction", int, None, min=1, optional=True)
-    min_line_length = config.RangeField("Exclude line traces shorter than this fraction of spatial dimension",
-                                        float, None, min=0., max=1., optional=True)
-    def setDefaults(self):
-        self.min_snr = 10
-        self.max_missed = 2 # helps to filter out tracing on horizontal DC noise pattern
-        self.debug_reject_bad = False
-
 class determineWavelengthSolutionConfig(parameters_spect.determineWavelengthSolutionConfig):
     order = config.RangeField("Order of fitting function", int, None, min=0,
                               optional=True)
@@ -34,4 +25,3 @@ class skyCorrectConfig(parameters_preprocess.skyCorrectConfig):
         self.offset_sky = False
         self.mask_objects = False
         self.dilation = 0.
-
