ac24185c0230fbedda81aa212daf98e55f41a11d
Daniel Berke <berke.daniel@gmail.com>
2023-07-31 15:58:27 -1000

    Initial commit of flat and arc reduction recipes

---
+++ geminidr/gnirs/recipes/sq/recipes_ARC_XD_SPECT.py
@@ -0,0 +1,30 @@
+"""
+Recipes available to data with tags ['GNIRS', 'SPECT', 'XD'],
+excluding data with tags ['FLAT', 'DARK', 'BIAS'].
+These are GNIRS cross-dispersed arc-lamp or sky-line calibrations.
+Default is "makeProcessedArc".
+"""
+recipe_tags = {'GNIRS', 'SPECT', 'XD', 'ARC'}
+
+def makeProcessedArc(p):
+    """
+    Process GNIRS cross-dispersed arc and calculate wavelength and distortion
+    solutions.  Arcs get stacked if more than one is given.
+
+    Inputs are:
+      * raw arc - no other calibrations required.
+    """
+    p.prepare()
+    p.addDQ()
+    p.addVAR(read_noise=True)
+    p.ADUToElectrons()
+    p.addVAR(poisson_noise=True)
+    p.stackFrames()
+    # p.makeIRAFCompatible()  # no need for XD
+    p.flatCorrect()
+    p.determineWavelengthSolution()
+    p.determineDistortion(debug=True)
+    p.storeProcessedArc()
+
+
+_default = makeProcessedArc
---
+++ geminidr/gnirs/recipes/sq/recipes_FLAT_XD_SPECT.py
@@ -0,0 +1,50 @@
+"""
+Recipes available to data with tags ['GNIRS', 'SPECT', 'XD', 'FLAT'].
+These are GNIRS cross-dispersed (XD) observations.
+Default is "makeProcessedFlat".
+"""
+recipe_tags = {'GNIRS', 'SPECT', 'XD', 'FLAT'}
+
+def makeProcessedFlat(p):
+    """
+    Create a processed flat for GNIRS cross-dispersed data.
+    Inputs are:
+      * raw LAMPON flats (for [spectral] order 3)
+      * raw LAMPOFF flats (confusingly, made with QH lamp on) (for orders 4-8)
+    No darks are needed due to the short exposures.  It was found that using
+    darks was just adding to the noise.
+
+    GNIRS XD flats are taken with two different lamps (the IR lamp and the QH
+    lamp). The IR lamp provides illumination to the first order (#3), while the
+    remaining orders are handled by the QH lamp. Due to a deep absorption
+    feature in the wavelength range of the first order the QH lamp isn't used
+    for flatfielding it, but it provides enough illumination that it can be
+    used for determining the edges of the slits (actually all the same slit,
+    but split out across the detector). Thus, the edges are traced using the
+    QH lamp stack of flats, then the individual slits are cut out in both stacks,
+    and orders 4-8 merged into the file with order 3.
+    """
+    p.prepare()
+    p.addDQ()
+    p.ADUToElectrons()
+    p.addVAR(poisson_noise=True, read_noise=True)
+    p.selectFromInputs(tags='GCAL_IR_OFF,LAMPOFF', outstream='QHLamp')
+    p.removeFromInputs(tags='GCAL_IR_OFF,LAMPOFF')
+    p.stackFlats(stream='main')
+    p.stackFlats(stream='QHLamp')
+    # Illumination of all orders from QH lamp is sufficient to find edges.
+    p.determineSlitEdges(stream='QHLamp')
+    # Transfer SLITEDGE table and slit rectification model (in WCS) back over.
+    p.transferAttribute(stream='main', source='QHLamp', attribute='SLITEDGE')
+    p.transferAttribute(stream='main', source='QHLamp', attribute='wcs')
+    p.cutSlits(stream='main')
+    p.cutSlits(stream='QHLamp')
+    # Bring slts 2-6 from QHLamp stream to main (1-indexed).
+    p.combineSlices(from_stream='QHLamp', ids='2,3,4,5,6')
+    p.clearStream(stream='QHLamp')
+    p.maskBeyondSlit()
+    p.normalizeFlat()
+    p.thresholdFlatfield()
+    p.storeProcessedFlat()
+
+_default = makeProcessedFlat
