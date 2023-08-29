a32d2ad955a53c342cdede998540b82717fd0c93
Daniel Berke <berke.daniel@gmail.com>
2023-08-29 09:59:10 -1000

    Remove min_snr requirment after variance bug-fix, increase minimum lines for XD arc reduction

--- geminidr/gnirs/parameters_gnirs_crossdispersed.py
+++ geminidr/gnirs/parameters_gnirs_crossdispersed.py
@@ -5,6 +5,10 @@ from geminidr.core import parameters_standardize
 from geminidr.core.parameters_standardize import addIllumMaskToDQConfig
 from gempy.library import config
 
+def list_of_ints_check(value):
+    [int(x) for x in str(value).split(',')]
+    return True
+
 
 class addDQConfig(parameters_standardize.addDQConfig, addIllumMaskToDQConfig):
     def setDefaults(self):
@@ -13,8 +17,9 @@ class addDQConfig(parameters_standardize.addDQConfig, addIllumMaskToDQConfig):
 class determineWavelengthSolutionConfig(parameters_spect.determineWavelengthSolutionConfig):
     order = config.RangeField("Order of fitting function", int, 3, min=0,
                               optional=True)
-    min_snr = config.RangeField("Minimum SNR for peak detection", float, 2,
-                                min=1., optional=True)
+    debug_min_lines = config.Field("Minimum number of lines to fit each segment",
+                                   (str, int), '50,20',
+                                   check=list_of_ints_check)
 
     def setDefaults(self):
         self.in_vacuo = True
