fe04e689bd271bb50a9941ced804274356bf46a9
Daniel Berke <berke.daniel@gmail.com>
2023-11-28 11:22:34 -1000

    Simpler method of removing `force_linear` parameter from resampleToCommonFrame()
    Add an explanation, actually remove rather than comment out old code

--- geminidr/core/parameters_crossdispersed.py
+++ geminidr/core/parameters_crossdispersed.py
@@ -5,28 +5,23 @@ from geminidr.core import parameters_generic
 from gempy.library import config
 from astrodata import AstroData
 
+from geminidr.core import parameters_spect
+
 
 class flatCorrectConfig(parameters_generic.calRequirementConfig):
     suffix = config.Field("Filename suffix", str, "_flatCorrected", optional=True)
     flat = config.ListField("Flatfield frame", (str, AstroData), None, optional=True, single=True)
 
 
-class resampleToCommonFrameConfig(config.Config):
-    suffix = config.Field("Filename suffix", str, "_align", optional=True)
-    w1 = config.RangeField("Starting wavelength (nm)", float, None, min=0., optional=True)
-    w2 = config.RangeField("Ending wavelength (nm)", float, None, min=0., optional=True)
-    dw = config.RangeField("Dispersion (nm/pixel)", float, None, min=0.01, optional=True)
-    npix = config.RangeField("Number of pixels in spectrum", int, None, min=2, optional=True)
-    conserve = config.Field("Conserve flux?", bool, None, optional=True)
-    order = config.RangeField("Order of interpolation", int, 1, min=0, max=5, inclusiveMax=True)
-    trim_spatial = config.Field("Trim spatial range to fully-covered region?", bool, True)
-    trim_spectral = config.Field("Trim wavelength range to fully-covered region?", bool, False)
-    dq_threshold = config.RangeField("Fraction from DQ-flagged pixel to count as 'bad'",
-                                      float, 0.001, min=0.)
-
-    def validate(self):
-        config.Config.validate(self)
-        if [self.w1, self.w2, self.dw, self.npix].count(None) == 0:
-            raise ValueError("Maximum 3 of w1, w2, dw, npix must be specified")
-        if self.w1 is not None and self.w2 is not None and self.w2 <= self.w1:
-            raise ValueError("Ending wavelength must be greater than starting wavelength")
+class resampleToCommonFrameConfig(parameters_spect.resampleToCommonFrameConfig):
+    """
+    For cross-dispersed spectra, the `force_linear` parameter is problematic if
+    set to `False`. A linear scale for the entire wavelength range of the
+    observation can be created from any individual order, but a non-linear scale
+    created from an arbitrary order may or may not be able to cover the entire
+    wavelength range. (And a good inverse may not be able to be created, even if
+    it does.) For these reasons, for cross-dispersed use we remove the
+    `force_linear` parameter entirely (it's set to `True` if not found).
+    """
+    def setDefaults(self):
+        del self.force_linear
