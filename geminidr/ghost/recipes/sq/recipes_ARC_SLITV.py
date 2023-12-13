834657eb71e392c5526f9e092926436f21e7871e
chris-simpson <csimpson@gemini.edu>
2023-12-12 15:38:35 -1000

    remove plotSlitFlux() from ARC SLIT recipe

--- geminidr/ghost/recipes/sq/recipes_ARC_SLITV.py
+++ geminidr/ghost/recipes/sq/recipes_ARC_SLITV.py
@@ -5,6 +5,27 @@ Default is ``makeProcessedSlitArc``, which is an alias to
 """
 recipe_tags = set(['GHOST', 'CAL', 'SLITV', 'ARC'])
 
-from .recipes_SLITV import makeProcessedSlit as makeProcessedSlitArc
+def makeProcessedSlitArc(p):
+    """
+    This recipe processes GHOST science data.
+
+    Parameters
+    ----------
+    p : Primitives object
+        A primitive set matching the recipe_tags.
+    """
+
+    p.prepare()
+    p.addDQ()
+    p.addVAR(read_noise=True)
+    p.biasCorrect()
+    p.ADUToElectrons()
+    p.addVAR(poisson_noise=True)
+    p.darkCorrect()
+    p.CRCorrect()
+    p.processSlits()
+    p.stackFrames()
+    p.storeProcessedSlit()
+    return
 
 _default = makeProcessedSlitArc
