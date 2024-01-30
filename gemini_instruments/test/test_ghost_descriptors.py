08480589bcd04afe27b9613d5b3fd0ac8a21ad39
chris-simpson <csimpson@gemini.edu>
2024-01-30 09:32:08 -1000

    add pytest markers to setup.cfg

--- gemini_instruments/test/test_ghost_descriptors.py
+++ gemini_instruments/test/test_ghost_descriptors.py
@@ -19,7 +19,6 @@ from .ghost_lut_descriptors import fixture_data as descriptors_fixture_data
 
 
 # Can only run first test (on a bundle) since other tests require non-archived inputs
-@pytest.mark.ghost
 @pytest.mark.parametrize("data, descriptors", descriptors_fixture_data[:1])
 def test_descriptor(data, descriptors):
     """
