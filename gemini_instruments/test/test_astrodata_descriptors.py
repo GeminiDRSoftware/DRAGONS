f4882fe5853bfcccc6923623b3abbb335da5a2f4
chris-simpson <csimpson@gemini.edu>
2024-01-29 17:28:38 -1000

    fix imports in tag/descriptor testing

--- gemini_instruments/test/test_astrodata_descriptors.py
+++ gemini_instruments/test/test_astrodata_descriptors.py
@@ -6,7 +6,7 @@ import os
 
 THIS_DIR = os.path.dirname(__file__)
 
-from lut_descriptors import fixture_data as descriptors_fixture_data
+from .lut_descriptors import fixture_data as descriptors_fixture_data
 
 class FixtureIterator:
     def __init__(self, data_dict):
