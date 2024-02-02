ddc04c49400d444b50ef97f8f169c3e497785413
chris-simpson <csimpson@gemini.edu>
2024-02-02 09:59:47 -1000

    remove FixtureIterator from tag/desciptor tests to avoid persistent AD objects

--- gemini_instruments/test/test_astrodata_tags.py
+++ gemini_instruments/test/test_astrodata_tags.py
@@ -6,35 +6,27 @@ import os
 
 from .lut_tags import fixture_data as tags_fixture_data
 
-class FixtureIterator:
-    def __init__(self, data_dict):
-        self._data = data_dict
-
-        # The `path_to_inputs` fixture returns $DRAGONS_TEST/test_astrodata_tags
-        # here, which feels weird, so manually create the path to input data as
-        # $DRAGONS_TEST/gemini_instruments/test_astrodata_tags/inputs
-        # DB 20230907
-        path_to_test_data = os.getenv("DRAGONS_TEST")
-        if path_to_test_data is None:
-            pytest.skip('Environment variable not set: $DRAGONS_TEST')
-        path_to_test_data = os.path.expanduser(path_to_test_data).strip()
-        path_to_files = ['gemini_instruments', 'test_astrodata_tags',
-                              'inputs']
-        self.path_to_test_data = os.path.join(path_to_test_data,
-                                              *path_to_files)
-
-    def __iter__(self):
-        for key in sorted(self._data.keys()):
-            (instr, filename) = key
-            if '_' in filename:
-                filepath = os.path.join(self.path_to_test_data, filename)
-                ad = astrodata.open(filepath)
-            else:
-                ad = astrodata.open(
-                    astrodata.testing.download_from_archive(filename))
-            yield filename, ad, set(self._data[key])
 
 @pytest.mark.dragons_remote_data
-@pytest.mark.parametrize("fn,ad,tag_set", FixtureIterator(tags_fixture_data))
-def test_descriptor(fn, ad, tag_set):
-    assert ad.tags == tag_set
+@pytest.mark.parametrize("instr,filename,tag_set",
+                         ([*k]+[v] for k, v in tags_fixture_data.items()))
+def test_descriptor(instr, filename, tag_set):
+    path_to_test_data = os.getenv("DRAGONS_TEST")
+    if path_to_test_data is None:
+        pytest.skip('Environment variable not set: $DRAGONS_TEST')
+
+    path_to_test_data = os.path.expanduser(path_to_test_data).strip()
+    path_to_files = ['gemini_instruments', 'test_astrodata_tags',
+                     'inputs']
+    path_to_test_data = os.path.join(path_to_test_data, *path_to_files)
+
+    if '_' in filename:
+        filepath = os.path.join(path_to_test_data, filename)
+        try:
+            ad = astrodata.open(filepath)
+        except FileNotFoundError:
+            pytest.skip(f"{filename} not found")
+    else:
+        ad = astrodata.open(astrodata.testing.download_from_archive(filename))
+
+    assert ad.tags == set(tag_set)
