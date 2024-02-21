3ffe8d5ddffd90f64aee0d8dc88e3965fba62a1f
chris-simpson <csimpson@gemini.edu>
2024-02-21 09:29:32 -1000

    typo fix and add test for use of synthetic slit profile

---
+++ geminidr/ghost/tests/spect/test_extract_spectra.py
@@ -0,0 +1,24 @@
+import os
+import pytest
+
+import astrodata, gemini_instruments
+from geminidr.ghost.primitives_ghost_spect import GHOSTSpect
+
+
+def test_synthetic_slit_profile(path_to_inputs):
+    """Simple test to confirm that spectra can be extracted
+    with a synthetic slit profile. The output is not checked"""
+    sci_filename = "S20230513S0229_red001_arraysTiled.fits"
+    raw_flat_filename = "S20230511S0035.fits"
+    ad = astrodata.open(os.path.join(path_to_inputs, sci_filename))
+    arm = ad.arm()
+    processed_flat = os.path.join(
+        path_to_inputs,
+        raw_flat_filename.replace(".fits", f"_{arm}001_flat.fits"))
+    processed_slitflat = os.path.join(
+        path_to_inputs,
+        raw_flat_filename.replace(".fits", f"_slit_slitflat.fits"))
+    ucals = {"processed_flat": processed_flat,
+             "processed_slitflat": processed_slitflat}
+    p = GHOSTSpect([ad], ucals=ucals)
+    p.extractSpectra(seeing=0.7)
