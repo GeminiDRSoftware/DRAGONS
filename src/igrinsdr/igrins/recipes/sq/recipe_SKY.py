cea74863c57bf1b295b52c360c8d5795a3e72ade
Jae-Joon Lee <lee.j.joon@gmail.com>
2024-04-27 12:31:05 +0900

    add SKY recipe

---
+++ src/igrinsdr/igrins/recipes/sq/recipe_SKY.py
@@ -0,0 +1,50 @@
+"""
+"""
+from igrinsdr.igrins.primitives_igrins import Igrins
+
+recipe_tags = {'IGRINS'}
+
+def makeSky(p: Igrins):
+    """
+    This recipe performs the standardization and corrections needed to convert
+    the raw input dark images into a single stacked dark image. This output
+    processed dark is stored on disk and its information added to the calibration
+    database using storeProcessedDark and has a name
+    equal to the name of the first input bias image with "_bias.fits" appended.
+
+    Parameters
+    ----------
+    p : PrimitivesCORE object
+        A primitive set matching the recipe_tags.
+    """
+
+    p.prepare(require_wcs=False)
+    p.addDQ()
+    p.addVAR(read_noise=True)
+    # ADUToElectrons requires saturation_level and nonlinearity_level in the
+    # header. Since IGRINS does not have these values defined, we add them
+    # here.
+    p.fixIgrinsHeader()
+    p.referencePixelsCorrect()
+    p.ADUToElectrons()
+    #p.nonlinearityCorrect()
+    p.addVAR(poisson_noise=True)
+
+    p.stackFrames()
+    p.extractSimpleSpec()
+
+    p.identifyOrders()
+    p.identifyLinesAndGetWvlsol()
+
+    # we are skipping save_orderflat step.
+
+    p.extractSpectraMulti()
+    p.identifyMultiline()
+    p.volumeFit()
+
+    p.makeSpectralMaps()
+
+    return
+
+_default = makeSky
+
