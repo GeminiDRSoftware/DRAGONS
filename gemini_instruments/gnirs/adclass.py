9a3f265f9f669da9d722d00736c2af5ba9628612
Kathleen Labrie <kathleen.labrie.phd@gmail.com>
2024-06-18 09:48:13 -1000

    GNIRS. switching to use more fine-grained pixel scales.  Now distinguish between red and blue camera, not just short and long.

--- gemini_instruments/gnirs/adclass.py
+++ gemini_instruments/gnirs/adclass.py
@@ -7,7 +7,7 @@ from ..gemini import AstroDataGemini, use_keyword_if_prepared
 from ..common import build_group_id
 
 from .lookup import detector_properties, nominal_zeropoints, read_modes
-from .lookup import pixel_scale_shrt, pixel_scale_long
+from .lookup import pixel_scale
 
 # NOTE: Temporary functions for test. gempy imports astrodata and
 #       won't work with this implementation
@@ -408,11 +408,7 @@ class AstroDataGnirs(AstroDataGemini):
 
         GNIRS instrument page,
 
-            https://www.gemini.edu/sciops/instruments/gnirs/spectroscopy
-
-        Short camera (0.15"/pix) -- lookup.pixel_scale_shrt
-        Long  camera (0.05"/pix) -- lookup.pixel_scale_long
-
+            https://www.gemini.edu/instrumentation/gnirs/components
 
         Returns
         -------
@@ -426,16 +422,14 @@ class AstroDataGnirs(AstroDataGemini):
 
         """
         try:
-            camera = self.camera().lower()
+            camera = self.camera(pretty=True)
         except AttributeError:
             return None
 
-        if 'short' in camera:
-            return pixel_scale_shrt
-        elif 'long' in camera:
-            return pixel_scale_long
+        if camera in pixel_scale:
+            return pixel_scale[self.camera(pretty=True)]
         else:
-            raise ValueError("Unrecognized GNIRS camera, {}".format(camera))
+            raise ValueError("Unrecognized GNIRS camera, {}".format(self.camera(pretty=True)))
 
 
     @astro_data_descriptor
