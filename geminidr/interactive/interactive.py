GeminiDRSoftware/DRAGONS

commit 41145fcebe610af246b305454bf5c4fccaf9c0a9

Making inline if statements more concise.
This preserves the expected behavior. min_value and max_value are
either None or a number, and the instance in which
max_value would act unexpectedly (max_value == 0) should raise a
warning (added here)

geminidr/interactive/interactive.py
@@ -747,13 +747,23 @@ def build_text_slider(title, value, step, min_value, max_value, obj=None,
         # If the field has no max, set to None.
         max_value = getattr(field, 'max', None)
 
+    # Check that max_value is None or greater than 0.
+    if max_value is not None and max_value <= 0:
+        max_value = None
+        _log.warn(
+            msg="max_value must be greater than 0. Setting to None."
+        )
+
     if value is None:
-        start = min_value if min_value is not None else 0
-        end = max_value if max_value is not None else 10
+        # If the value is None or Falsey, set to a default value
+        start = min_value or 0
+        end = max_value or 10
         slider_kwargs = {"value": start, "show_value": False}
+
     else:
-        start = min(value, min_value) if min_value is not None else min(value, 0)
-        end = max(value, max_value) if max_value is not None else max(10, value*2)
+        #if min/max value is None or Falsey, use a default.
+        start = min(value, min_value or 0)
+        end = max(value, max_value or 2 * value, 10)
         slider_kwargs = {"value": value, "show_value": True}
 
     # trying to convince int-based sliders to behave

commit c882bb434c92fb5af978c5c98b5499caa352c02c

Partially undo commit #41145fc which mistakenly changed the endpoint of text sliders

geminidr/interactive/interactive.py
@@ -1088,11 +1088,10 @@ def build_text_slider(
         start = min_value or 0
         end = max_value or 10
         slider_kwargs = {"value": start, "show_value": False}
-
     else:
         # if min/max value is None/Falsey, use a default.
         start = min(value, min_value or 0)
-        end = max(value, max_value or 2 * value, 10)
+        end = max(value, max_value) if max_value is not None else max(10, value * 2)
         slider_kwargs = {"value": value, "show_value": True}
 
     # Fix the start/end values to the min/max values if requested