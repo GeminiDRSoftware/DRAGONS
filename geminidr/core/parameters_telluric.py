f4a1c39dcbd3398e65f474c9fc7fcda7cf141c8b
chris-simpson <csimpson@gemini.edu>
2024-07-19 11:44:21 -1000

    initial copy-and-paste of telluric code to confirm primitive inheritance and LSF imports

---
+++ geminidr/core/parameters_telluric.py
@@ -0,0 +1,50 @@
+from gempy.library import config
+from . import parameters_generic, parameters_spect
+from astrodata import AstroData
+from telluric.utils.physics import parse_magnitude
+
+
+def validate_magstr(value):
+    return None not in parse_magnitude(value)
+
+
+class fitTelluricConfig(config.core_1Dfitting_config):
+    suffix = config.Field("Filename suffix", str, "_telluricFitted", optional=True)
+    bbtemp = config.RangeField("Stellar blackbody temperature", float, 9650,
+                               min=3000, max=30000)
+    magnitude = config.Field("Magnitude normalization", str, "K=10",
+                             check=validate_magstr)
+    abmag = config.Field("Magnitude is AB (rather than Vega)?", bool, False)
+    lsf_scaling = config.RangeField("LSF scaling factor", float, None,
+                                    min=0.5, max=2.0, optional=True)
+    regions = config.Field("Wavelength sample regions (nm)", str, None, optional=True,
+                           check=parameters_spect.validate_regions_float)
+    interactive = config.Field("Display interactive fitter?", bool, False)
+    weighting = config.ChoiceField("Weighting scheme", str,
+                                   allowed={"variance": "Inverse variance",
+                                            "uniform": "Uniform weights"},
+                                   default="variance", optional=False)
+    shift_tolerance = config.RangeField(
+        "Tolerance (pixels) for ignoring cross-correlation shift",
+        float, None, min=0, max=5, inclusiveMax=True, optional=True)
+    apply_shift = config.Field("Permanently apply pixel shift?", bool, True)  # debug?
+
+    def setDefaults(self):
+        self.niter = 1
+        self._fields['order'].max = 20
+
+
+class telluricCorrectConfig(parameters_generic.calRequirementConfig):
+    suffix = config.Field("Filename suffix", str, "_telluricCorrected", optional=True)
+    telluric = config.ListField("File with telluric model", (AstroData, str), None,
+                                optional=True, single=True)
+    apply_model = config.Field("Apply the absorption model rather than the data?",
+                               bool, True, optional=False)
+    manual_shift = config.RangeField("Manual shift in pixels", float, None, min=-5, max=5,
+                                     inclusiveMin=True, inclusiveMax=True, optional=True)
+    delta_airmass = config.RangeField("Airmass difference", float, None, min=-1, max=1,
+                                      inclusiveMin=True, inclusiveMax=True, optional=True)
+    shift_tolerance = config.RangeField(
+        "Tolerance (pixels) for ignoring cross-correlation shift",
+        float, None, min=0, max=5, inclusiveMax=True, optional=True)
+    apply_shift = config.Field("Permanently apply pixel shift?", bool, True)  # debug?
---
+++ gempy/library/convolution.py
@@ -0,0 +1,181 @@
+from functools import partial
+
+import numpy as np
+from datetime import datetime
+
+"""
+Various convolution functions should go here. They can have as many parameters
+as you like, but the "line_spread_function" that is passed must be compatible
+with the definition in the convolve() function, i.e., take 2 parameters: a
+wavelength and an array of wavelength offsets
+"""
+
+
+def gaussian_constant_r(w0, dw, r):
+    """A Gaussian with FWHM = w0 / r"""
+    sigma = 0.42466 * w0 / r
+    z = np.exp(-0.5 * (dw / sigma) ** 2)
+    return z / z.sum()
+
+
+def boxcar(w0, dw, width):
+    """A boxcar of full width width nm"""
+    z = abs(dw) <= 0.5 * width
+    return z / z.sum()
+
+
+def convolve(w, y, func, dw, sampling=1):
+    """
+    Convolve a spectrum with a line spread function that is itself a function
+    of wavelength. There are various assumptions here, e.g. the wavelength
+    scale is approximately linear across the size of the kernel, that may need
+    to be revisited later.
+
+    Parameters
+    ----------
+    w: array
+        wavelengths
+    y: array
+        data
+    func: callable
+        function that defines the convolution kernel representing the line
+        spread function. This must take 2 arguments: the central wavelength
+        and an array of wavelength offsets. The returned kernel must be
+        normalized (sum=1).
+    dw: float
+        half-extent of kernel (in nm)
+    sampling: int
+        to speed up the calculation, the kernel can be calculated at discrete
+        intervals and then interpolated to get its form at other locations
+
+    Returns
+    -------
+    array: the convolved array, of the same size as x
+    """
+    #start = datetime.now()
+    # Calculate size of kernel in pixels
+    diffs = np.diff(w)
+    if any(diffs <= 0):
+        raise ValueError("Wavelength array must be monotonically increasing")
+    i = int(dw / diffs.min() + 1)
+
+    kernel_size = i + i + 1
+    yout = np.zeros_like(y)
+    nw = y.shape[-1]
+    if sampling == 1:  # this is a bit quicker
+        for j in range(0, nw):
+            start = max(j-i, 0)
+            yout[..., start:j+i+1] = (yout[..., start:j+i+1] +
+                                      np.outer(y.T[j], func(w[j], w[start:j+i+1] - w[j])))
+    else:
+        ww = w[:nw:sampling]
+        yconv = np.zeros((ww.size, kernel_size))
+        for jj, j in enumerate(range(0, nw, sampling)):
+            start = max(j-i, 0)
+            yconv[jj, start-j+i:] = func(w[j], w[start:j+i+1]-w[j])
+        for j in range(kernel_size):
+            z = y * np.interp(w, ww, yconv[:, j])
+            yout[..., j:j+nw-i] += z[..., i:nw+i-j]
+
+    #print(datetime.now() - start)
+    return yout
+
+
+def downsample(wout, w, data):
+    """
+    Resample a high-resolution "spectrum" to a lower resolution. The sampling
+    must be to a significantly lower resolution because there is no splitting
+    of input pixels. The input pixels which overlap with a given output pixel
+    are simply averaged. This is no longer used, as it has been replaced by
+    "resample()".
+
+    Parameters
+    ----------
+    wout: array
+        output wavelength array
+    w: array
+        input wavelength array (*must* be in increasing order)
+    data: array
+        data to be resampled
+
+    Returns
+    -------
+    resampled array of same size as wout
+    """
+    raise RuntimeError("Running downsample()")
+    diffs = np.diff(w)
+    if any(diffs <= 0):
+        raise ValueError("Wavelength array must be monotonically increasing")
+
+    if len(data.shape) > 1:
+        output = np.empty((data.shape[0], wout.size), dtype=data.dtype)
+    else:
+        output = np.empty_like(wout, dtype=data.dtype)
+    indices = np.arange(w.size)
+    edges = np.r_[[1.5 * wout[0] - 0.5 * wout[1]],
+                  np.array([wout[:-1], wout[1:]]).mean(axis=0),
+                  [1.5 * wout[-1] - 0.5 * wout[-2]]]
+
+    # have to do some flipping if wavelengths are in descending order
+    if wout[1] > wout[0]:
+        xedges = np.interp(edges, w, indices)
+    else:
+        xedges = np.interp(edges[::-1], w, indices)
+    for i in range(wout.size):
+        x1, x2 = xedges[i:i+2]
+        output[..., i] = data[..., int(x1+1):int(x2+1)].mean(axis=-1)
+    return output if wout[1] > wout[0] else output[..., ::-1]
+
+
+def resample(wout, w, data):
+    """
+    Resample a spectrum to a different output wavelength array. This follows
+    the method of nddops.sum1d() in DRAGONS by taking fractional pixels. There
+    will be uncertain (possibly fatal) edge effects if the input array does
+    not fully cover the wavelength regime of the output array.
+
+    Parameters
+    ----------
+    wout: array
+        output wavelength array
+    w: array
+        input wavelength array (*must* be in increasing order)
+    data: array
+        data to be resampled
+
+    Returns
+    -------
+    resampled array of same size as wout
+    """
+    dw_in = np.diff(w)
+    if any(dw_in < 0):
+        raise ValueError("Wavelength array must be monotonically increasing")
+
+    if len(data.shape) > 1:
+        output = np.empty((data.shape[0], wout.size), dtype=data.dtype)
+    else:
+        output = np.empty_like(wout, dtype=data.dtype)
+    indices = np.arange(w.size)
+    edges = np.r_[[1.5 * wout[0] - 0.5 * wout[1]],
+                  np.array([wout[:-1], wout[1:]]).mean(axis=0),
+                  [1.5 * wout[-1] - 0.5 * wout[-2]]]
+    dw_out = abs(np.diff(edges))
+
+    # have to do some flipping if wavelengths are in descending order
+    if wout[1] > wout[0]:
+        xedges = np.interp(edges, w, indices)
+    else:
+        xedges = np.interp(edges[::-1], w, indices)
+    for i in range(wout.size):
+        x1, x2 = xedges[i:i+2]
+        ix1 = int(np.floor(x1 + 0.5))
+        ix2 = int(np.ceil(x2 - 0.5))
+        fx1 = ix1 - x1 + 0.5
+        fx2 = x2 - ix2 + 0.5
+        #output[..., i] = (fx1 * data[..., ix1] + data[..., ix1+1:ix2].sum(axis=-1) +
+        #                  fx2 * data[..., ix2]) / (x2 - x1)
+        output[..., i] = (fx1 * data[..., ix1] * dw_in[ix1] +
+                          (data[..., ix1 + 1:ix2] * dw_in[ix1 + 1:ix2]).sum(axis=-1) +
+                          fx2 * data[..., ix2] * dw_in[ix2]) / dw_out[i]
+    return output if wout[1] > wout[0] else output[..., ::-1]
+
