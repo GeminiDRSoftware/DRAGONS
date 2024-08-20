87a8af449e8680d2811c6a8b19b61ce1902b7c57
Daniel Berke <berke.daniel@gmail.com>
2024-08-20 12:03:10 -1000

    Increase atol for test due to platform differences on Jenkins

--- geminidr/f2/tests/longslit/test_determine_distortion.py
+++ geminidr/f2/tests/longslit/test_determine_distortion.py
@@ -223,7 +223,9 @@ def test_regression_for_determine_distortion_using_wcs(
 
     X, Y = np.mgrid[:ad[0].shape[0], :ad[0].shape[1]]
 
-    np.testing.assert_allclose(model(X, Y), ref_model(X, Y), atol=0.05)
+    # Increasing atol to 0.07 due to S20180114S0104_flatCorrected.fits producing
+    # slightly different results on Jenkins vs. on MacOS. DB 20240820
+    np.testing.assert_allclose(model(X, Y), ref_model(X, Y), atol=0.07)
 
 
 @pytest.mark.gnirs
