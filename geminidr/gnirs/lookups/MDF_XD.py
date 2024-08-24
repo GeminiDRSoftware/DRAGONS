0f07aa6995162bd2bb4f39e25ae2ff7b1bacbca9
Daniel Berke <berke.daniel@gmail.com>
2024-08-23 17:06:37 -1000

    Better organize look-up tables
    Attempts to better classify the many configurations of GNIRS which
    produce meaningfully different results. Entries are now classified
    in a system by:
    Site (north/south
    Camera (short/long)
    Grating (10/32/111 l/mm)
    Prism (SXD/LXD)
    
    Both files use the same order, which should hopefully help with
    future maintenance.

--- geminidr/gnirs/lookups/MDF_XD.py
+++ geminidr/gnirs/lookups/MDF_XD.py
@@ -11,15 +11,11 @@
 # definitions based on a key generated from the 'telescope', '_prism', 'decker',
 # '_grating', and 'camera' attributes of a file.
 slit_info = {
-# Gemini South, Short camera, 32/mm grating
-'Gemini-South_SXD_G5509_SC_XD_32/mm_G5506_ShortBlue_G5521': (
-    (1, 2, 3, 4, 5, 6),                 # slit ID
-    (325, 435, 512, 584, 659, 742.5),   # x_ccd
-    (512, 512, 512, 512, 512, 512),     # y_ccd
-    (3, 4, 5, 6, 7, 8),                 # specorder
-    (7.0, 7.0, 7.0, 7.0, 7.0, 7.0),     # width_arcsec
-    (43.8, 42.8, 42.8, 43, 43, 43)      # width_pixels
-    ),
+# ------------------------------- Gemini North --------------------------------
+# -------------------------------  Short camera
+# North, Short, 10 l/mm, SXD
+# North, Short, 10 l/mm, LXD
+# North, Short, 32 l/mm, SXD
 'Gemini-North_SXD_G5536_SCXD_G5531_32/mm_G5533_ShortBlue_G5540': (
     (1, 2, 3, 4, 5, 6),                 # slit ID
     (290, 400, 477, 550, 624, 707),     # x_ccd
@@ -28,14 +24,8 @@ slit_info = {
     (7.0, 7.0, 7.0, 7.0, 7.0, 7.0),     # width_arcsec
     (46, 47, 47, 47, 47, 47)            # width_pixels
     ),
-'Gemini-North_LXD_G5535_LCXD_G5531_10/mm_G5532_LongBlue_G5542': (
-    (1, 2, 3, 4, 5, 6),                 # slit ID
-    (145, 381, 531, 657, 778, 916),     # x_ccd
-    (175, 175, 175, 175, 175, 175),     # y_ccd
-    (3, 4, 5, 6, 7, 8),                 # specorder
-    (5.0, 5.0, 5.0, 5.0, 5.0, 5.0),     # width_arcsec
-    (104, 106, 104, 105, 105, 105)      # width_pixels
-    ),
+# North, Short, 32 l/mm, LXD
+# North, Short, 111 l/mm, SXD
 'Gemini-North_SXD_G5536_SCXD_G5531_111/mm_G5534_ShortBlue_G5540': (
     (1, 2, 3, 4, 5, 6),                 # slit ID
     (275, 389, 466, 534, 607, 685),     # x_ccd
@@ -44,6 +34,28 @@ slit_info = {
     (5.0, 5.0, 5.0, 5.0, 5.0, 5.0),     # width_arcsec
     (46, 47, 47, 47, 47, 47)            # width_pixels
     ),
+# North, Short, 111 l/mm, LXD
+# --------------------------------- Long camera -------------------------------
+# North, Long, 10 l/mm, SXD
+# North, Long, 10 l/mm, LXD
+'Gemini-North_LXD_G5535_LCXD_G5531_10/mm_G5532_LongBlue_G5542': (
+    (1, 2, 3, 4, 5, 6),                 # slit ID
+    (145, 381, 531, 657, 778, 916),     # x_ccd
+    (175, 175, 175, 175, 175, 175),     # y_ccd
+    (3, 4, 5, 6, 7, 8),                 # specorder
+    (5.0, 5.0, 5.0, 5.0, 5.0, 5.0),     # width_arcsec
+    (104, 106, 104, 105, 105, 105)      # width_pixels
+    ),
+# North, Long, 32 l/mm, SXD
+'Gemini-North_SXD_G5536_SCXD_G5531_32/mm_G5533_LongBlue_G5542': (
+    (1, 2, 3, 4),                       # slit ID
+    (208, 563, 792, 995),               # x_ccd
+    (50, 50, 50, 50),                   # y_ccd
+    (3, 4, 5, 6),                       # specorder
+    (7.0, 7.0, 7.0, 7.0),               # width_arcsec
+    (140, 140, 140, 140)                # width_pixels
+    ),
+# North, Long, 32 l/mm, LXD
 'Gemini-North_LXD_G5535_LCXD_G5531_32/mm_G5533_LongBlue_G5542': (
     (1, 2, 3, 4),                       # slit ID
     (265, 468, 621, 764),               # x_ccd
@@ -52,6 +64,8 @@ slit_info = {
     (5.0, 5.0, 5.0, 5.0),               # width_arcsec
     (100, 100, 100, 100)                # width_pixels
     ),
+# North, Long, 111 l/mm, SXD
+# North, Long, 111 l/mm, LXD
 'Gemini-North_LXD_G5535_LCXD_G5531_111/mm_G5534_LongBlue_G5542': (
     (1, 2, 3, 4),                       # slit ID
     (267, 462, 620, 775),               # x_ccd
@@ -60,4 +74,38 @@ slit_info = {
     (5.0, 5.0, 5.0, 5.0),               # width_arcsec
     (100, 100, 100, 100)                # width_pixels
     ),
+
+# ------------------------------- Gemini South --------------------------------
+# -------------------------------  Short camera
+# South, Short, 10 l/mm, SXD
+# South, Short, 10 l/mm, LXD
+# South, Short, 32 l/mm, SXD
+'Gemini-South_SXD_G5509_SC_XD_32/mm_G5506_ShortBlue_G5521': (
+    (1, 2, 3, 4, 5, 6),                 # slit ID
+    (325, 435, 512, 584, 659, 742.5),   # x_ccd
+    (512, 512, 512, 512, 512, 512),     # y_ccd
+    (3, 4, 5, 6, 7, 8),                 # specorder
+    (7.0, 7.0, 7.0, 7.0, 7.0, 7.0),     # width_arcsec
+    (43.8, 42.8, 42.8, 43, 43, 43)      # width_pixels
+    ),
+# South, Short, 32 l/mm, LXD
+# South, Short, 111 l/mm, SXD
+'Gemini-South_SXD_G5509_SC_XD_111/mm_G5505_ShortBlue_G5513': (
+    (1, 2, 3, 4, 5),                    # slit ID
+    (326, 436, 512, 586, 667),          # x_ccd
+    (512, 512, 512, 512, 512),          # y_ccd
+    (3, 4, 5, 6, 7),                    # specorder
+    (7.8, 7.7, 7.5, 7.0, 7.0),          # width_arcsec
+    (52, 51, 49, 46, 46)                # width_pixels
+    ),
+# South, Short, 111 l/mm, LXD
+# --------------------------------- Long camera -------------------------------
+# South, Long, 10 l/mm, SXD
+# South, Long, 10 l/mm, LXD
+# South, Long, 32 l/mm, SXD
+# South, Long, 32 l/mm, LXD
+# South, Long, 111 l/mm, SXD
+# South, Long, 111 l/mm, LXD
+
+
 }
--- geminidr/gnirs/lookups/orders_XD.py
+++ geminidr/gnirs/lookups/orders_XD.py
@@ -9,33 +9,59 @@
 # '_grating', and 'camera' attributes of a file.
 
 order_info= {
-    # Gemini South, Short camera, 32 l/mm grating
-    'Gemini-South_SXD_G5509_SC_XD_32/mm_G5506_ShortBlue_G5521': (
-        (2210, 1660, 1330, 1110, 950, 830),                # central wavelength, nm
-        (-0.645, -0.479, -0.381, -0.322, -0.273, -0.244),  # dispersion, nm/pixel
+# ------------------------------- Gemini North --------------------------------
+# -------------------------------  Short camera
+# North, Short, 10 l/mm, SXD
+# North, Short, 10 l/mm, LXD
+# North, Short, 32 l/mm, SXD
+# North, Short, 32 l/mm, LXD
+# North, Short, 111 l/mm, SXD
+'Gemini-North_SXD_G5536_SCXD_G5531_111/mm_G5534_ShortBlue_G5540': (
+    (2210, 1660, 1330, 1110, 950, 830),
+    (-0.1853, -0.13882, -0.11117, -0.09242, -0.08, -0.0694),
     ),
+# North, Short, 111 l/mm, LXD
+# --------------------------------- Long camera -------------------------------
+# North, Long, 10 l/mm, SXD
+# North, Long, 10 l/mm, LXD
+'Gemini-North_LXD_G5535_LCXD_G5531_10/mm_G5532_LongBlue_G5542': (
+    (2210, 1660, 1330, 1110, 950, 830),
+    (-0.64504, -0.4847, -0.3895, -0.322, -0.273, -0.244),
+    ),
+# North, Long, 32 l/mm, SXD
+'Gemini-North_SXD_G5536_SCXD_G5531_32/mm_G5533_ShortBlue_G5540': (
+    (2210, 1660, 1330, 1110, 950, 830),
+    (-0.645, -0.479, -0.381, -0.322, -0.273, -0.244),
+    ),
+# North, Long, 32 l/mm, LXD
+'Gemini-North_LXD_G5535_LCXD_G5531_32/mm_G5533_LongBlue_G5542': (
+    (2210, 1660, 1330, 1110),
+    (-0.1853, -0.13882, -0.11117, -0.09242),
+    ),
+# North, Long, 111 l/mm, SXD
+# North, Long, 111 l/mm, LXD
 
-    # Gemini North, Long camera, 32 l/mm grating
-    'Gemini-North_SXD_G5536_SCXD_G5531_32/mm_G5533_ShortBlue_G5540': (
-        (2210, 1660, 1330, 1110, 950, 830),
-        (-0.645, -0.479, -0.381, -0.322, -0.273, -0.244),
+# ------------------------------- Gemini South --------------------------------
+# -------------------------------  Short camera
+# South, Short, 10 l/mm, SXD
+# South, Short, 10 l/mm, LXD
+# South, Short, 32 l/mm, SXD
+'Gemini-South_SXD_G5509_SC_XD_32/mm_G5506_ShortBlue_G5521': (
+    (2210, 1660, 1330, 1110, 950, 830),                # central wavelength, nm
+    (-0.645, -0.479, -0.381, -0.322, -0.273, -0.244),  # dispersion, nm/pixel
     ),
-
-    # Gemini North, Long camera, 10 l/mm grating
-    'Gemini-North_LXD_G5535_LCXD_G5531_10/mm_G5532_LongBlue_G5542': (
-        (2210, 1660, 1330, 1110, 950, 830),
-        (-0.64504, -0.4847, -0.3895, -0.322, -0.273, -0.244),
+# South, Short, 32 l/mm, LXD
+# South, Short, 111 l/mm, SXD
+'Gemini-South_SXD_G5509_SC_XD_111/mm_G5505_ShortBlue_G5513': (
+    (2210, 1660, 1330, 1110, 950),                     # central wavelength, nm
+    (-0.645, -0.479, -0.381, -0.322, -0.273),          # dispersion, nm/pixel
     ),
-
-    # Gemini North, Short camera, 111 l/mm grating
-    'Gemini-North_SXD_G5536_SCXD_G5531_111/mm_G5534_ShortBlue_G5540': (
-        (2210, 1660, 1330, 1110, 950, 830),
-        (-0.1853, -0.13882, -0.11117, -0.09242, -0.08, -0.0694),
-    ),
-
-    # Gemini North, Long camera, 32 l/mm grating
-    'Gemini-North_LXD_G5535_LCXD_G5531_32/mm_G5533_LongBlue_G5542': (
-        (2210, 1660, 1330, 1110),
-        (-0.1853, -0.13882, -0.11117, -0.09242),
-    ),
+# South, Short, 111 l/mm, LXD
+# --------------------------------- Long camera -------------------------------
+# South, Long, 10 l/mm, SXD
+# South, Long, 10 l/mm, LXD
+# South, Long, 32 l/mm, SXD
+# South, Long, 32 l/mm, LXD
+# South, Long, 111 l/mm, SXD
+# South, Long, 111 l/mm, LXD
 }
