0607bf4af7a426e722d7f49845e06fd8c7e0b1e0
chris-simpson <csimpson@gemini.edu>
2024-09-19 15:42:20 -1000

    remove all GHOST QA recipes

--- geminidr/ghost/recipes/qa/recipes_ARC.py
+++
@@ -1,9 +0,0 @@
-"""
-Recipes available to data with tags ['GHOST', 'CAL', 'ARC'].
-Default is "makeProcessedArc".
-"""
-recipe_tags = set(['GHOST', 'CAL', 'ARC'])
-
-from ..sq.recipes_ARC import makeProcessedArc
-
-_default = makeProcessedArc
