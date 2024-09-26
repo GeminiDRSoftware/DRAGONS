b3f8020f4ac0a9280f929a4ef71f8a500a114f6d
Daniel Berke <berke.daniel@gmail.com>
2024-09-25 14:28:30 -1000

    Remove some debugging code

--- geminidr/gnirs/tests/crossdispersed/test_determine_distortion.py
+++ geminidr/gnirs/tests/crossdispersed/test_determine_distortion.py
@@ -61,15 +61,10 @@ def test_regression_for_determine_distortion_using_wcs(
         p.determineDistortion(**fixed_parameters_for_determine_distortion)
         distortion_determined_ad = p.writeOutputs().pop()
 
-    distortion_determined_ad.write("/Users/daniel.berke/tmp/"
-                                   f"{distortion_determined_ad.filename}",
-                                   overwrite=True)
-
     ref_ad = ref_ad_factory(distortion_determined_ad.filename)
     model = distortion_determined_ad[0].wcs.get_transform(
         "pixels", "distortion_corrected")[2]
     ref_model = ref_ad[0].wcs.get_transform("pixels", "distortion_corrected")[2]
-    # breakpoint()
 
     # Otherwise we're doing something wrong!
     assert model.__class__.__name__ == ref_model.__class__.__name__ == "Chebyshev2D"
