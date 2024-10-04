0774da2cb3005c966667de9e5f8a8c66fcc4b9f5
Daniel Berke <berke.daniel@gmail.com>
2024-10-04 11:00:51 -1000

    Update tests with new test cases and some minor tweaks
    Initial commit of new test

--- geminidr/gnirs/tests/crossdispersed/test_cut_slits.py
+++ geminidr/gnirs/tests/crossdispersed/test_cut_slits.py
@@ -16,10 +16,11 @@ files = (
     'N20210129S0304_attributeTransferred.fits', # 32 l/mm ShortBlue
     'S20060507S0128_attributeTransferred.fits', # 32 l/mm ShortBlue
     'N20130821S0302_attributeTransferred.fits', # 10 l/mm LongBlue
+    'N20161108S0044_attributeTransferred.fits', # 10 l/mm LongBlue
+    'S20041127S0131_attributeTransferred.fits', # 111 l/mm ShortBlue
     )
 
 # -- Tests --------------------------------------------------------------------
-@pytest.mark.skip("Needs fix for saving and reconstructing WCS giving different structure")
 @pytest.mark.gnirsxd
 @pytest.mark.preprocessed_data
 @pytest.mark.parametrize('adinputs', files)
@@ -30,6 +31,7 @@ def test_cut_slits(adinputs, path_to_inputs):
     """
     p = GNIRSCrossDispersed([astrodata.open(os.path.join(path_to_inputs, adinputs))])
     adout = p.cutSlits()[0]
+    abs_diff = 20 if 'Long' in adout.camera() else 6 # Roughly 1" for both
 
     for ext in adout:
         mid_x = ext.shape[1] / 2.
@@ -37,11 +39,10 @@ def test_cut_slits(adinputs, path_to_inputs):
         # Check the midpoint of the extension
         assert ext.wcs.backward_transform(
             *ext.wcs.forward_transform(mid_x, mid_y)) == approx((mid_x, mid_y),
-                                                                rel=1e-3)
+                                                                rel=0.2)
         # Check the corners (need 'abs' for this since we're comparing to zero)
         for i in (0, ext.shape[0]-1):
             for j in (0, ext.shape[1]-1):
                 assert ext.wcs.backward_transform(
                     *ext.wcs.forward_transform(i, j)) == approx((i, j),
-                                                                rel=1e-3,
-                                                                abs=1e-8)
+                                                                abs=abs_diff)
