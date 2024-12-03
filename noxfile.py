2b5f959f222748d4f3168de211cc44c9774b1cba
teald <teal.dillon@gmail.com>
2024-12-02 16:53:52 -0800

    ci(nox): Add arguments to venv, install package

--- noxfile.py
+++ noxfile.py
@@ -15,8 +15,7 @@ If you find a good use case for automation, great! Before adding it to this
 already large file, please consider the following questions about the scope of
 this file:
 
-1. Is your automation meant to perform setup or teardown for a test? If so,
-   try to do it in pytest.
+1. Is your automation meant to perform setup or teardown for a test? If so, try to do it in pytest.
 
    + If you are working with something like ``devpi``, where it's important that
      some packages be isolated, then this is the appropriate place for a change.
@@ -44,14 +43,18 @@ from pathlib import Path
 import nox
 
 
-def create_venv(session: nox.Session):
+def create_venv(session: nox.Session) -> Path:
     """Create a new virtual environment using a running session."""
-    default_venv_path = "venv/"
+    default_venv_path = Path("venv/")
     default_venv_prompt = "dragons_venv"
 
-    session.run(
-        "python", "-m", "venv", default_venv_path, "--prompt", default_venv_prompt
-    )
+    venv_args = ["--prompt", default_venv_prompt, "--clear", "--upgrade-deps"]
+
+    session.run("python", "-m", "venv", default_venv_path, *venv_args)
+
+    assert default_venv_path.exists()
+
+    return default_venv_path
 
 
 def install_dependencies(
@@ -60,10 +63,15 @@ def install_dependencies(
     target_python: Path | None = None,
 ):
     """Install dependencies using a running session."""
+    if target_python is None:
+        target_python = Path(session.virtualenv.bin) / "python"
+
+    session.run(str(target_python), "-m", "pip", "install", "-e", ".", external=True)
 
 
 @nox.session(venv_backend="venv")
 def devenv(session: nox.Session):
     """Generate a new development environment."""
-    create_venv(session)
-    install_dependencies(session)
+    venv_path = create_venv(session)
+    venv_python = venv_path / "bin" / "python"
+    install_dependencies(session, target_python=venv_python)
