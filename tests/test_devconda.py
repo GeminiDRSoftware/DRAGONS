afe332458e5476e55eafe1587ed292a81f990b97
teald <teal.dillon@gmail.com>
2024-12-04 10:15:16 -0800

    test(nox): Testing for devconda environment

--- tests/test_devconda.py
+++ tests/test_devconda.py
@@ -1,5 +1,7 @@
 import subprocess
 
+import pytest
+
 
 def test_create_developer_conda(
     fresh_dragons_dir,
@@ -9,19 +11,39 @@ def test_create_developer_conda(
     """Test generating a develoment environment with venv."""
     command = ["nox", "-s", "devconda"]
 
-    devenv_result = subprocess.run(command, capture_output=True)
+    devconda_result = subprocess.run(command, capture_output=True)
 
-    helpers.validate_result(devenv_result)
+    helpers.validate_result(devconda_result)
 
     # Specified in create_venv() in noxfile.py
     expected_conda_env_name = "dragons_dev"
 
-    conda_info_result = subprocess.run(["conda", "info", "-e"], capture_output=True)
+    assert helpers.check_for_conda_environment(
+        expected_conda_env_name
+    ), f"Conda env {expected_conda_env_name} not found."
 
-    helpers.validate_result(conda_info_result)
 
-    stdout = conda_info_result.stdout.decode("utf-8")
+@pytest.mark.parametrize("name_flag", ["-n", "--name"])
+def test_create_developer_conda_with_custom_name(
+    name_flag,
+    fresh_dragons_dir,
+    helpers,
+):
+    """Test creating conda dev env with custom name."""
+    env_name = "custom_name"
 
-    assert any(
-        expected_conda_env_name in line for line in stdout.splitlines()
-    ), "Env not found!"
+    command = ["nox", "-s", "devconda", "--", name_flag, env_name]
+
+    devconda_result = subprocess.run(command, capture_output=True)
+
+    helpers.validate_result(devconda_result)
+
+    # Specified in create_venv() in noxfile.py
+    expected_conda_env_name = env_name
+
+    assert helpers.check_for_conda_environment(
+        expected_conda_env_name
+    ), f"Conda env {expected_conda_env_name} not found."
+
+    # Cleanup!
+    helpers.clear_conda_environment(env_name)
