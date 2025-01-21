a6de50e1b0679cf26f29b9288c191f35f09b90bb
Jae-Joon Lee <lee.j.joon@gmail.com>
2025-01-21 16:22:53 +0900

    remove dependency on simplejson

--- src/igrinsdr/igrins/json_helper.py
+++ src/igrinsdr/igrins/json_helper.py
@@ -1,8 +1,25 @@
-import json
-import simplejson
+from json import JSONEncoder, dumps, loads
 
 import numpy as np
 from astropy.table import Table
+
+import math
+
+# NanConvert from from https://stackoverflow.com/questions/28639953/python-json-encoder-convert-nans-to-null-instead
+
+def nan2None(obj):
+    if isinstance(obj, dict):
+        return {k:nan2None(v) for k,v in obj.items()}
+    elif isinstance(obj, list):
+        return [nan2None(v) for v in obj]
+    elif isinstance(obj, float) and math.isnan(obj):
+        return None
+    return obj
+
+class NanConverter(JSONEncoder):
+    def encode(self, obj, *args, **kwargs):
+        return super().encode(nan2None(obj), *args, **kwargs)
+
 
 def encode_array(obj):
     if hasattr(obj, "to_json"):
@@ -22,8 +39,8 @@ def json_dumps(obj, *kl, **kw):
     if "default" not in kw:
         kw["default"] = encode_array
 
-    kw["ignore_nan"] = True
-    return simplejson.dumps(obj, *kl, **kw)
+    kw["cls"] = NanConverter
+    return dumps(obj, *kl, **kw)
 
 def dict_to_table(j):
 
@@ -46,6 +63,6 @@ def table_to_dict(tbl):
     j = {}
     for row in tbl:
         if row["encoder_name"] == "json":
-            j[row["key"]] = json.loads(row["encoded"])
+            j[row["key"]] = loads(row["encoded"])
 
     return j
