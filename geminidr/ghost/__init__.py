22f4d9ff9f4095e5e79f97a54c9247f60b6d97e2
Paul Hirst <phirstgemini@users.noreply.github.com>
2025-03-07 09:11:48 -1000

    merge enh/fitsstorage - Update DRAGONS to use Fits Storage 3.4.x for local calibration manager (#478)
    * Deprecate GeminiObsDB and GeminiCalMgr in favor of FitsStorage 3.4.x. Update localmanager to call updated FitsStorage code.
    
    * add FitsStorage v3.4.0b1 dependecy to tox.ini
    
    * remove obsolete calibration_ghost.py file. The Calibration_GHOST class is now in FitsStorage.
    
    * replace deprecated log.warn call with log.warning
    
    * suggestions from Teal to raise the underlying exception as well when raising LocalManager exceptions.

--- geminidr/ghost/__init__.py
+++ geminidr/ghost/__init__.py
@@ -1,9 +1,1 @@
 
-# For development, let's patch in our calibration_ghost implementation to make life easier.
-# Once it is finalized, we can migrate it back into the Gemini CalMgr code directly, or
-# implement some more formal override option
-#print("OVERRIDING GHOST CALIBRATIONS")
-
-#from gemini_calmgr.cal import inst_class
-#from .calibration_ghost import CalibrationGHOST
-#inst_class["GHOST"] = CalibrationGHOST
