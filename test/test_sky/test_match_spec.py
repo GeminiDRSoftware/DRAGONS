55fe42df9f6e25b295f297ce1f4a97ff5f12d834
Jae-Joon Lee <lee.j.joon@gmail.com>
2025-04-08 07:36:53 +0900

    convert some python test files to notebook friendly

--- test/test_sky/test_match_spec.py
+++ test/test_sky/test_match_spec.py
@@ -1,8 +1,12 @@
+# %%
+
 # import astropy.io.fits as pyfits
 import numpy as np
 import astrodata
 import igrins_instruments
+import matplotlib.pyplot as plt
 
+# %%
 
 # fn = "N20240429S0204_K.fits"
 band = "H"
@@ -22,77 +26,74 @@ from igrinsdr.igrins.procedures.match_orders import match_specs_w_shift, match_s
 
 orders_ref, s_list_ref = get_ref_spectra(band)
 
+# %%
+
+ext = adout[0]
+spec1d = ext.SPEC1D
+
+s_list_ = spec1d["specs"]
+s_list = [np.array(s, dtype=np.float64) for s in s_list_]
 
 
-if True:
-    ext = adout[0]
-    spec1d = ext.SPEC1D
+# to plot the order match result for visual inspection.
+delta_indx, displacements = match_specs_w_shift(s_list_ref, s_list, frac_thresh=0.3)
 
-    s_list_ = spec1d["specs"]
-    s_list = [np.array(s, dtype=np.float64) for s in s_list_]
+norm = np.nanmax(s_list) / 1.e1
+norm_ref = np.nanmax(s_list_ref) / 1.e1
 
+x = np.arange(2048)
+norders = len(displacements)
 
-    # to plot the order match result for visual inspection.
-    delta_indx, displacements = match_specs_w_shift(s_list_ref, s_list, frac_thresh=0.3)
+fig, axs = plt.subplots(norders, 1, num=1, clear=True)
 
-    norm = np.nanmax(s_list) / 1.e1
-    norm_ref = np.nanmax(s_list_ref) / 1.e1
+for ax, (o, shift) in zip(axs, displacements):
+    s_ref = s_list_ref[o + delta_indx]
+    s = s_list[o]
+    ax.plot(x, s_ref/norm_ref)
+    ax.plot(x + shift, s/norm + 0.5)
+    ax.set_ylim(-0.2, 1.5)
+    ax.annotate(str(orders_ref[o+delta_indx]), (0, 1), xytext=(5, -5),
+                xycoords="axes fraction", textcoords="offset points",
+                ha="left", va="top", )
 
-    x = np.arange(2048)
-    norders = len(displacements)
+# %%
 
-    import matplotlib.pyplot as plt
-    fig, axs = plt.subplots(norders, 1, num=1, clear=True)
+# This is to check if the shift_from_ref column in the spec1d is reasonable.
+ext = adout[0]
+spec1d = ext.SPEC1D
 
-    for ax, (o, shift) in zip(axs, displacements):
-        s_ref = s_list_ref[o + delta_indx]
-        s = s_list[o]
-        ax.plot(x, s_ref/norm_ref)
-        ax.plot(x + shift, s/norm + 0.5)
-        ax.set_ylim(-0.2, 1.5)
-        ax.annotate(str(orders_ref[o+delta_indx]), (0, 1), xytext=(5, -5),
-                    xycoords="axes fraction", textcoords="offset points",
-                    ha="left", va="top", )
+spec1d_orders = spec1d["orders"]
+center_indx = len(spec1d_orders) // 2
+selected_orders = spec1d_orders[max(center_indx - 5, 0):min(center_indx + 5, len(spec1d_orders))]
 
+s_list_ = spec1d["specs"]
+s_list = [np.array(s, dtype=np.float64) for s in s_list_]
 
+s_dict = dict(zip(spec1d_orders, zip(s_list, spec1d["shift_from_ref"])))
+s_dict_ref = dict(zip(orders_ref, s_list_ref))
 
-if True:
-    # This is to check if the shift_from_ref column in the spec1d is reasonable.
-    ext = adout[0]
-    spec1d = ext.SPEC1D
+norm = np.nanmax(s_list) / 1.e1
+norm_ref = np.nanmax(s_list_ref) / 1.e1
 
-    spec1d_orders = spec1d["orders"]
-    center_indx = len(spec1d_orders) // 2
-    selected_orders = spec1d_orders[max(center_indx - 5, 0):min(center_indx + 5, len(spec1d_orders))]
+x = np.arange(2048)
 
-    s_list_ = spec1d["specs"]
-    s_list = [np.array(s, dtype=np.float64) for s in s_list_]
+import matplotlib.pyplot as plt
+fig, axs = plt.subplots(len(selected_orders), 1, num=1, clear=True)
 
-    s_dict = dict(zip(spec1d_orders, zip(s_list, spec1d["shift_from_ref"])))
-    s_dict_ref = dict(zip(orders_ref, s_list_ref))
+for ax, o in zip(axs, selected_orders):
+    s_ref = s_dict_ref[o]
+    s, shift = s_dict[o]
+    ax.plot(x + shift, s_ref/norm_ref)
+    ax.plot(x, s/norm + 0.5)
+    ax.set_ylim(-0.2, 1.5)
+    ax.annotate(str(o), (0, 1), xytext=(5, -5),
+                xycoords="axes fraction", textcoords="offset points",
+                ha="left", va="top", )
 
-    norm = np.nanmax(s_list) / 1.e1
-    norm_ref = np.nanmax(s_list_ref) / 1.e1
-
-    x = np.arange(2048)
-
-    import matplotlib.pyplot as plt
-    fig, axs = plt.subplots(len(selected_orders), 1, num=1, clear=True)
-
-    for ax, o in zip(axs, selected_orders):
-        s_ref = s_dict_ref[o]
-        s, shift = s_dict[o]
-        ax.plot(x + shift, s_ref/norm_ref)
-        ax.plot(x, s/norm + 0.5)
-        ax.set_ylim(-0.2, 1.5)
-        ax.annotate(str(o), (0, 1), xytext=(5, -5),
-                    xycoords="axes fraction", textcoords="offset points",
-                    ha="left", va="top", )
+# %%
 
 
-
-
-if True:
+if False:
     # simple test
 
     delta_indx = match_specs(s_list_ref, s_list, frac_thresh=0.3)
