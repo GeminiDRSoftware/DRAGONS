avoid NaNs in telluric-corrected data if a shift has been applied to the data-derived absorption by extrapolating that absorption and masking pixels

geminidr/core/primitives_telluric.py
@@ -411,7 +411,7 @@ class Telluric(Spect):
                             make_interp_spline(tell_waves[~np.isnan(tellabs)],
                                                tellabs[~np.isnan(tellabs)], k=1)
 
-                            # TODO: If we're interactive we can override apply_model if necessary
+            # TODO: If we're interactive we can override apply_model if necessary
             if not interactive:
                 if apply_model and not has_model:
                     log.warning(f"{ad.filename} has no TELLFIT model but "
@@ -482,7 +482,7 @@ class Telluric(Spect):
                     if apply_model:
                         trans = convolution.resample(tspek.waves, wconv, tconv)
                     else:
-                        trans = tellabs_dict[ext.hdr.get('SPECORDR')]
+                        trans = tellabs_dict[ext.hdr.get('SPECORDR')](tspek.waves)
 
                     # Old cross-correlation method
                     #pixel_shift = peak_finding.cross_correlate_subpixels(
@@ -598,12 +598,14 @@ class Telluric(Spect):
             else:
                 tcal.perform_all_fits()
 
+            abs_spectra = tcal.absorption_spectra()
             i_model = 0
             for ext in ad:
                 if len(ext.shape) > 1:
                     continue
 
-                ext.divide(tcal.abs_final[i_model])
+                ext.divide(next(abs_spectra))
+                #ext.divide(tcal.abs_final[i_model])
                 i_model += 1
 
                 if apply_shift:
@@ -698,7 +700,6 @@ class Telluric(Spect):
             log.stdinfo(f"Using generic linelist {atran_linelist}")
         except FileNotFoundError:
             try:  # prevent using previously-created linelist (for now)
-                linelist = LineList("X")
                 linelist = LineList(atran_linelist)
                 log.stdinfo("Using previously-created linelist in current "
                             f"directory {atran_linelist}")

gempy/library/calibrator.py
@@ -5,10 +5,13 @@ from astropy.modeling import models, fitting
 from astropy.stats import sigma_clip
 from astropy import units as u
 
+from geminidr.gemini.lookups import DQ_definitions as DQ
 from gempy.library import astrotools as at
 from gempy.library.fitting import fit_1D
 
-from .telluric import A0Spectrum, TelluricModels, TelluricSpectrum
+from astrodata.nddata import NDAstroData
+
+from .telluric import A0Spectrum, TelluricSpectrum
 from .telluric_models import MultipleTelluricModels, Planck
 
 from datetime import datetime
@@ -407,6 +410,29 @@ class TelluricCorrector(Calibrator):
         corrected_data = models.Tabular1D(
             points=self.x[index], lookup_table=at.divide0(self.spectra[index].data, trans))
         return corrected_data
+
+    def absorption_spectra(self):
+        """
+        When there is a pixel shift, the absorption spectrum derived from the
+        telluric spectrum can't be extrapolated and so will have NaNs at one
+        end, which will be propagated into the telluric-corrected science
+        spectrum. We want these pixels to have values but to be masked, and
+        this method performs that change.
+        """
+        for abs_spek in self.abs_final:
+            mask = np.where(np.isnan(abs_spek), DQ.no_data, DQ.good)
+            if mask.sum() == 0:
+                yield abs_spek
+
+            data = abs_spek.copy()
+            first_unmasked = mask.argmin()
+            if first_unmasked == 0:  # masked pixels are at the end
+                last_unmasked = mask[::-1].argmin() + 1
+                data[-last_unmasked:] = data[-(last_unmasked+1)]
+            else:  # masked pixels are at the start
+                data[:first_unmasked] = data[first_unmasked]
+            result = NDAstroData(data=data, mask=mask)
+            yield result
 
     # def perform_all_fits(self):
     #     # This needs to return a model that describes the "fit" for each