7e51ae781d219b98cfe66a092981d2227af72557
phirst <phirst@gemini.edu>
2025-05-28 13:31:55 -1000

    add pre param

--- geminidr/core/parameters_stats.py
+++ geminidr/core/parameters_stats.py
@@ -2,5 +2,5 @@ from gempy.library import config
 
 
 class statsConfig(config.Config):
-    pre = config.Field('4-character header prefix', str, None, optional=True)
+    prefix = config.Field('4-character header prefix', str, None, optional=True)
 
