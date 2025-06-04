9b3b4e0f8fc36ecc91c0c66f369daa50eede4a69
phirst <phirst@gemini.edu>
2025-06-04 11:53:08 -1000

    add initial flat check recipe

--- geminidr/gmos/recipes/sq/recipes_FLAT_IMAGE.py
+++ geminidr/gmos/recipes/sq/recipes_FLAT_IMAGE.py
@@ -33,5 +33,24 @@ def makeProcessedFlat(p):
     p.storeProcessedFlat()
     return
 
+def checkFlat1(p):
+    """
+    For checking count levels in flat field.
+
+    Parameters
+    ----------
+    p : PrimitivesBASE object
+        A primitive set matching the recipe_tags.
+    """
+    p.prepare()
+    p.addDQ()
+    p.addVAR(read_noise=True)
+    p.overscanCorrect()
+    p.biasCorrect()
+    p.ADUToElectrons()
+    p.addVAR(poisson_noise=True)
+    p.stats(prefix='FLAT')
+    p.writeOutputs(strip=True, suffix='_checkFlat1')
+    return
 
 _default = makeProcessedFlat
