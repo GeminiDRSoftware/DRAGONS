fix calculation of variance in sum1d()

gempy/library/nddops.py
@@ -650,10 +650,10 @@ def sum1d(ndd, x1, x2, proportional_variance=True):
                ndd.variance[ix1:ix2].sum() +
                (fx2 if proportional_variance else fx2*fx2)*ndd.variance[ix2])
     else:
-        # calculate_variance returns the uncertainty on the mean
+        # calculate_variance returns the standard error on the mean
         # mask=None because we ignore the mask is the above calculation
         var = NDStacker.calculate_variance(
             ndd.data[ix1+1:ix2, np.newaxis],
-            mask=None, out_data=data/(x2-x1)) * (ix2 - ix1 - 1) ** 2
+            mask=None, out_data=data/(x2-x1)) * (ix2 - ix1 - 1)
 
     return NDD(data, mask, var)