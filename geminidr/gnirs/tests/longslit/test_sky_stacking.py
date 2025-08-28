c6a38598f0f39915bf45c28141f819146a027974
chris-simpson <chris.simpson@noirlab.edu>
2025-08-28 13:53:34 -1000

    docstring for test and set bad_wcs="ignore"

--- geminidr/gnirs/tests/longslit/test_sky_stacking.py
+++ geminidr/gnirs/tests/longslit/test_sky_stacking.py
@@ -10,6 +10,7 @@ import gemini_instruments
 from geminidr.gnirs.primitives_gnirs_longslit import GNIRSLongslit
 import pytest
 
+
 # ---- Parameters -------------------------------------------------------------
 associate_sky_params = {'time': 600., 'min_skies': 3, 'distance': 1,
                         'max_skies': None, 'use_all': False, 'sky': None}
@@ -22,6 +23,7 @@ def gnirs_abba():
     return [astrodata.open(download_from_archive(f)) for f in
             ('N20141119S0331.fits', 'N20141119S0332.fits',
              'N20141119S0333.fits', 'N20141119S0334.fits')]
+
 
 # ---- Tests ------------------------------------------------------------------
 @pytest.mark.dragons_remote_data
@@ -48,6 +50,7 @@ def test_associate_sky_abba(gnirs_abba):
     for ad in (b1, b2):
         assert set(ad.SKYTABLE['SKYNAME']) == a_frames
 
+
 @pytest.mark.dragons_remote_data
 @pytest.mark.gnirsls
 def test_associate_sky_pass_skies(gnirs_abba):
@@ -62,14 +65,22 @@ def test_associate_sky_pass_skies(gnirs_abba):
 
     assert in_sky_names == out_sky_names
 
+
 @pytest.mark.dragons_remote_data
 @pytest.mark.gnirsls
 def test_associate_sky_use_all(gnirs_abba):
-
+    """
+    This test checks whether the 'use_all' option in associateSky works by
+    allowing science frames to be used as skies. It uses an ABBA sequence and
+    relies on the second A position being slightly offset from the first so
+    that it can be used as a sky with a very small distance threshold. For
+    This reason, we must set bad_wcs="ignore" to prevent prepare() from making
+    the two A positions identical.
+    """
     in_sky_names = set([ad.filename for ad in gnirs_abba])
 
     p = GNIRSLongslit(gnirs_abba)
-    p.prepare(bad_wcs="new")
+    p.prepare(bad_wcs="ignore")
     p.separateSky()
     p.associateSky(distance=0, use_all=True)
 
@@ -79,6 +90,7 @@ def test_associate_sky_use_all(gnirs_abba):
 
         # Check that each AD has all the other frames as skies, but not itself.
         assert skies == in_sky_names - set([ad.phu['ORIGNAME']])
+
 
 @pytest.mark.dragons_remote_data
 @pytest.mark.gnirsls
@@ -92,6 +104,7 @@ def test_associate_sky_exclude_all(gnirs_abba):
     for ad in p.showList():
         with pytest.raises(AttributeError):
             ad.SKYTABLE
+
 
 @pytest.mark.dragons_remote_data
 @pytest.mark.gnirsls
