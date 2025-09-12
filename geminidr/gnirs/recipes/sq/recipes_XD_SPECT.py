11c22f0875b9269870b89f021376ebb6a3bf93d8
chris-simpson <chris.simpson@noirlab.edu>
2025-09-12 09:14:39 -1000

    remove reduceScienceWithAdjustmentFromSkyLines recipe

--- geminidr/gnirs/recipes/sq/recipes_XD_SPECT.py
+++ geminidr/gnirs/recipes/sq/recipes_XD_SPECT.py
@@ -134,41 +134,4 @@ def  makeWavecalFromSkyAbsorption(p):
     p.storeProcessedArc(force=True)
 
 
-def reduceScienceWithAdjustmentFromSkylines(p):
-    """
-    To be updated as development continues: This recipe processes GNIRS cross-dispersed
-    spectroscopic data, currently up to basic spectral extraction without telluric correction.
-
-    Parameters
-    ----------
-    p : :class:`geminidr.gnirs.primitives_gnirs_crossdispersed.GNIRSCrossDispersed`
-
-    """
-    p.prepare()
-    p.addDQ()
-    # p.nonlinearityCorrect() # non-linearity correction tbd
-    p.ADUToElectrons()
-    p.addVAR(poisson_noise=True, read_noise=True)
-    # p.darkCorrect() # no dark correction for GNIRS data
-    p.selectFromInputs(outstream="temp")
-    p.flatCorrect(stream="temp")
-    p.attachWavelengthSolution(stream="temp")
-    p.adjustWavelengthZeroPoint(stream="temp", shift=None)
-    p.separateSky()
-    p.associateSky()
-    p.skyCorrect()
-    # p.cleanReadout()  # algorithm doesn't work on cut data
-    p.flatCorrect()
-    p.transferAttribute(stream="main", source="temp", attribute="wcs")
-    p.distortionCorrect()
-    p.adjustWCSToReference()
-    p.resampleToCommonFrame()
-    p.scaleCountsToReference()
-    p.stackFrames()
-    p.findApertures()
-    p.traceApertures()
-    p.storeProcessedScience(suffix="_2D")
-    p.extractSpectra()
-    p.storeProcessedScience(suffix="_1D")
-
 _default = reduceScience
