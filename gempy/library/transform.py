41c00a3691181e469694a036c170278e9115875f
James E.H. Turner <jturner@gemini.edu>
2025-09-25 12:33:51 -0300

    Don't try to read/update array_section if the keyword has already been deleted at an earlier pass (in mosaicDetectors or distortionCorrect).
    This was causing a ValueError when running resampleToCommonFrame more than once (or just once with a separate mosaicDetectors step), since the first invocation was adding back CCDSEC with a value of '[]', which the second one was then unable to parse.This was causing a ValueError when invoking resampleToCommonFrame more than once (or once after separate mosaicDetectors & distortionCorrect), since a bad CCDSEC value was getting added back in the first invocation, which wasn't parseable in the second one.

--- gempy/library/transform.py
+++ gempy/library/transform.py
@@ -1747,13 +1747,14 @@ def resample_from_wcs(ad, frame_name, attributes=None, interpolant="linear",
 
         # array_section only has meaning now if the inputs were from a
         # single physical array
-        if len(blocks) == 1:
-            all_arrsec = np.array([ext.array_section() for ext in ad]).T
-            ad_out.hdr[keywords['array']] = \
-                '[' + ','.join('{}:{}'.format(min(c1) + 1, max(c2))
-                               for c1, c2 in zip(all_arrsec[::2], all_arrsec[1::2])) + ']'
-        else:
-            del ad_out.hdr[keywords['array']]
+        if keywords['array'] in ad_out.hdr:
+            if len(blocks) == 1:
+                all_arrsec = np.array([ext.array_section() for ext in ad]).T
+                ad_out.hdr[keywords['array']] = \
+                    '[' + ','.join('{}:{}'.format(min(c1) + 1, max(c2))
+                                   for c1, c2 in zip(all_arrsec[::2], all_arrsec[1::2])) + ']'
+            else:
+                del ad_out.hdr[keywords['array']]
 
     # Try to assign an array name for this based on commonality
     if not is_single:
