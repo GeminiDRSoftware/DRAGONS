7866ba8c0e28606d6ce4297423788d4c9aea7ca3
chris-simpson <chris.simpson@noirlab.edu>
2026-04-24 14:14:45 -1000

    have gemini_instruments import igrins2 and alphabetize instruments

--- gemini_instruments/__init__.py
+++ gemini_instruments/__init__.py
@@ -4,29 +4,29 @@
 from astrodata import version
 __version__ = version()
 
-import astrodata
-
 from . import gemini
 
 from . import bhros
+from . import cirpass
 from . import f2
+from . import flamingos
 from . import ghost
 from . import gmos
 from . import gnirs
 from . import gpi
 from . import graces
 from . import gsaoi
+from . import hokupaa_quirc
+from . import hrwfs
+from . import igrins
+from . import igrins2
 from . import michelle
 from . import nici
 from . import nifs
 from . import niri
-from . import phoenix
 from . import oscir
-from . import trecs
-from . import igrins
-from . import flamingos
-from . import hokupaa_quirc
-from . import hrwfs
-from . import cirpass
+from . import phoenix
 from . import texes
-from . import skycam
+from . import texes
+from . import trecs
+from . import texes
