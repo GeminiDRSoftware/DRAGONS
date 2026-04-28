e8fe1d61c138ef72c31329997636f75a87ca5b7d
Paul Hirst <phirst@gemini.edu>
2026-04-27 17:18:02 -1000

    updates to QA checkArc recipe - effectively run in SQ mode so that determineWavelengthSolution raises if it does not find a fit

--- geminidr/gmos/recipes/qa/recipes_ARC_LS_SPECT.py
+++ geminidr/gmos/recipes/qa/recipes_ARC_LS_SPECT.py
@@ -36,6 +36,12 @@ def checkArc(p):
     :param p:
     :return:
     """
+    # In QA mode, if determineWavelengthSolution cannot find a good arc fit it
+    # uses a linear model based on the headers, whereas in SQ mode it raises an
+    # exception. For the purposes of checking arcs, we want everything to
+    # behave as if it is in SQ mode, so we just set that here
+    p.mode = 'sq'
+
     p.prepare(require_wcs=False)
     p.addDQ()
     p.addVAR(read_noise=True)
@@ -43,8 +49,11 @@ def checkArc(p):
     p.ADUToElectrons()
     p.addVAR(poisson_noise=True)
     p.mosaicDetectors()
-    p.determineWavelengthSolution()
-    p.determineDistortion()
+    # We force center=None to ensure it always gets the central row.
+    # This is the default anyway, but this prevents it being overridden.
+    p.determineWavelengthSolution(center=None)
+    # We don't need determineDistortion to check Arcs, and it's slow.
+    # p.determineDistortion()
     p.monitorWavelengthSolution()
     p.writeOutputs()
 
